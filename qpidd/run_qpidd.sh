43aa07e33ec6640a66d01a06861137ffb48b2743
Gordon Sim <gsim@redhat.com>
2016-03-16 17:47:57 +0000

    a few fixes

--- qpidd/run_qpidd.sh
+++ qpidd/run_qpidd.sh
@@ -3,4 +3,4 @@
 VOLUME="/var/run/qpidd/"
 BASE=$(dirname $0)
 DATA_DIR=$($BASE/get_data_dir.py $VOLUME)
-exec /sbin/qpidd --auth no --queue-pattern '/queue/' --topic-pattern '/topic/' --data-dir "$VOLUME$DATA_DIR" --federation_tag $DATA_DIR
+exec /sbin/qpidd --auth no --queue-pattern '/queue/' --topic-pattern '/topic/' --data-dir "$VOLUME$DATA_DIR" --federation-tag $DATA_DIR
