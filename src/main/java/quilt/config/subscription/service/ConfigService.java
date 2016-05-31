537d9bba6953a04d20cba356b4d93171fab1e7de
Ulf Lilleengen <lulf@redhat.com>
2016-05-31 11:44:57 +0200

    Get token and namespace from secrets

--- src/main/java/quilt/config/subscription/service/ConfigService.java
+++ src/main/java/quilt/config/subscription/service/ConfigService.java
@@ -14,6 +14,10 @@ import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 /**
  * Main entrypoint for configuration service with arg parsing.
  *
@@ -25,9 +29,6 @@ public class ConfigService {
 
         Options options = new Options();
         options.addOption(new Option("s", "openshiftUri", true, "Openshift URI"));
-        options.addOption(requiredOption("n", "namespace", true, "Openshift namespace"));
-        options.addOption(requiredOption("u", "user", true, "Openshift user"));
-        options.addOption(requiredOption("t", "token", true, "Openshift auth token"));
         options.addOption(new Option("l", "listenAddress", true, "AMQP listen address"));
         options.addOption(new Option("p", "port", true, "AMQP listen port"));
         options.addOption(new Option("h", "help", true, "Usage"));
@@ -37,14 +38,13 @@ public class ConfigService {
             CommandLine cmd = parser.parse(options, args);
 
             String openshiftUri = cmd.getOptionValue("s", "https://localhost:8443");
-            String openshiftToken = cmd.getOptionValue("t");
-            String openshiftUser = cmd.getOptionValue("u");
-            String openshiftNamespace = cmd.getOptionValue("n");
             String listenAddress = cmd.getOptionValue("l", "0.0.0.0");
             int listenPort = Integer.parseInt(cmd.getOptionValue("p", "5672"));
 
+            String openshiftNamespace = getOpenshiftNamespace();
+
             IClient client = new ClientFactory().create(openshiftUri, new NoopSSLCertificateCallback());
-            client.setAuthorizationStrategy(new TokenAuthorizationStrategy(openshiftToken, openshiftUser));
+            client.setAuthorizationStrategy(new TokenAuthorizationStrategy(getAuthenticationToken()));
 
             OpenshiftConfigMapDatabase database = new OpenshiftConfigMapDatabase(client, openshiftNamespace);
             database.start();
@@ -54,13 +54,24 @@ public class ConfigService {
             server.run();
         } catch (ParseException e) {
             HelpFormatter formatter = new HelpFormatter();
-            formatter.printHelp("config-service", options);
+            formatter.printHelp("config-subscription-service", options);
+        } catch (IOException e) {
+            System.out.println("Error running config subscription service");
+            System.exit(1);
         }
     }
 
-    private static Option requiredOption(String shortName, String longName, boolean takesArguments, String description) {
-        Option o = new Option(shortName, longName, takesArguments, description);
-        o.setRequired(true);
-        return o;
+    private static final String SERVICEACCOUNT_PATH = "/var/run/secrets/kubernetes.io/serviceaccount";
+
+    private static String getOpenshiftNamespace() throws IOException {
+        return readFile(new File(SERVICEACCOUNT_PATH, "namespace"));
+    }
+
+    private static String getAuthenticationToken() throws IOException {
+        return readFile(new File(SERVICEACCOUNT_PATH, "token"));
+    }
+
+    private static String readFile(File file) throws IOException {
+        return new String(Files.readAllBytes(file.toPath()));
     }
 }
