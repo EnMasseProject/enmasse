987ea7c7a7226ab1234a563e6e4a82703a8a5668
Gordon Sim <gsim@redhat.com>
2016-06-02 19:36:33 +0100

    updated router and broker images to support colocated use and preconfiguration

--- artemis/run_artemis.sh
+++ artemis/run_artemis.sh
@@ -5,6 +5,19 @@ BASE=$(dirname $0)
 INSTANCE=$($BASE/get_free_instance.py $VOLUME)
 if [ ! -d "$INSTANCE" ]; then
     $ARTEMIS_HOME/bin/artemis create $INSTANCE --user admin --password admin --role admin --allow-anonymous
+    if [ -n "$QUEUE_NAME" ]; then
+        cat > /tmp/snippet <<EOF
+      <queues>
+          <queue name="$QUEUE_NAME">
+             <address>$QUEUE_NAME</address>
+          </queue>
+      </queues>
+EOF
+        sed -i -e '/<\/address-settings>/r /tmp/snippet' $INSTANCE/etc/broker.xml
+        sed -i -e 's/0.0.0.0:5672/0.0.0.0:5673/' $INSTANCE/etc/broker.xml
+    elif [ -n "$TOPIC_NAME" ]; then
+        echo "ERROR: topics not yet supported"
+    fi
 fi
 
 exec $INSTANCE/bin/artemis run
--- qdrouterd/run_qdr.sh
+++ qdrouterd/run_qdr.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
 export MY_IP_ADDR=$(hostname -I)
+if [ -z "$RAGENT_SERVICE_HOST" ]; then
+    echo "ERROR: router agent service not configured";
+    export RAGENT_SERVICE_HOST=localhost;
+    export RAGENT_SERVICE_PORT=55672;
+fi
 envsubst < /etc/qpid-dispatch/qdrouterd.conf.template > /tmp/qdrouterd.conf
+if [ -n "$QUEUE_NAME" ]; then
+    export ADDRESS_NAME=$QUEUE_NAME;
+    envsubst < /etc/qpid-dispatch/colocated.snippet >> /tmp/qdrouterd.conf
+elif [ -n "$TOPIC_NAME" ]; then
+    export ADDRESS_NAME=$TOPIC_NAME;
+    envsubst < /etc/qpid-dispatch/colocated.snippet >> /tmp/qdrouterd.conf
+fi
 exec /sbin/qdrouterd --conf /tmp/qdrouterd.conf
