340e606424ed5156642ee18069e560fd7b63656e
Ulf Lilleengen <lulf@redhat.com>
2016-06-22 15:08:13 +0200

    Trigger smoketest after push

---
+++ .trigger-travis.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+body='{
+"request": {
+  "branch":"master"
+}}'
+
+curl -s -X POST \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/json" \
+  -H "Travis-API-Version: 3" \
+  -H "Authorization: token $TRAVIS_TOKEN" \
+  -d "$body" \
+  https://api.travis-ci.org/repo/EnMasseProject%2Fsmoketest/requests
