a59150453eb1b3c5115e4517aa31d96ccc1f50ab
Gordon Sim <gsim@redhat.com>
2016-06-22 21:28:32 +0100

    Only update rc if something pertinent has changed

--- generator-lib/src/main/java/enmasse/rc/admin/BrokerManager.java
+++ generator-lib/src/main/java/enmasse/rc/admin/BrokerManager.java
@@ -1,5 +1,7 @@
 package enmasse.rc.admin;
 
+import com.openshift.restclient.model.IContainer;
+import com.openshift.restclient.model.IPort;
 import com.openshift.restclient.model.IReplicationController;
 import enmasse.rc.generator.ConfigGenerator;
 import enmasse.rc.model.Destination;
@@ -7,8 +9,10 @@ import enmasse.rc.model.LabelKeys;
 import enmasse.rc.openshift.OpenshiftClient;
 
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
@@ -67,13 +71,58 @@ public class BrokerManager {
 
     private void brokerModified(IReplicationController controller) {
         IReplicationController oldController = openshiftClient.getBroker(controller.getName());
-        oldController.setContainers(controller.getContainers());
-        oldController.setReplicas(controller.getReplicas());
-        oldController.setReplicaSelector(controller.getReplicaSelector());
+        if (!equivalent(controller, oldController)) {
+            log.log(Level.INFO, "Modifying replication controller " + controller.getName());
+            oldController.setContainers(controller.getContainers());
+            oldController.setReplicas(controller.getReplicas());
+            oldController.setReplicaSelector(controller.getReplicaSelector());
 
-        for (Map.Entry<String, String> label : controller.getLabels().entrySet()) {
-            oldController.addLabel(label.getKey(), label.getValue());
+            for (Map.Entry<String, String> label : controller.getLabels().entrySet()) {
+                oldController.addLabel(label.getKey(), label.getValue());
+            }
+            openshiftClient.updateBroker(oldController);
         }
-        openshiftClient.updateBroker(oldController);
+    }
+
+    private static boolean equivalent(IReplicationController a, IReplicationController b) {
+        return equivalent(a.getContainers(), b.getContainers())
+            && a.getLabels().equals(b.getLabels())
+            && a.getReplicaSelector().equals(b.getReplicaSelector());
+    }
+
+    private static boolean equivalent(Collection<IContainer> a, Collection<IContainer> b) {
+        return a.size() == b.size() && equivalent(index(a), index(b));
+    }
+
+    private static boolean equivalent(Map<String, IContainer> a, Map<String, IContainer> b) {
+        if (a.size() != b.size()) return false;
+        for (String name : a.keySet()) {
+            if (!equivalent(a.get(name), b.get(name))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean equivalent(IContainer a, IContainer b) {
+        if (a == null) return b == null;
+        else if (b != null) return false;
+        else return a.getImage().equals(b.getImage())
+                 && a.getEnvVars().equals(b.getEnvVars())
+                 && equivalent(a.getPorts(), b.getPorts())
+                 && a.getVolumeMounts().equals(b.getVolumeMounts());
+    }
+
+    private static boolean equivalent(Set<IPort> a, Set<IPort> b) {
+        if (a.size() != b.size()) return false;
+        else return index(a).equals(index(b));
+    }
+
+    private static Map<String, IContainer> index(Collection<IContainer> in) {
+        return in.stream().collect(Collectors.toMap(c -> c.getName(), c -> c));
+    }
+
+    private static Map<Integer, String> index(Set<IPort> in) {
+        return in.stream().collect(Collectors.toMap(p -> p.getContainerPort(), p -> p.getName()));
     }
 }
