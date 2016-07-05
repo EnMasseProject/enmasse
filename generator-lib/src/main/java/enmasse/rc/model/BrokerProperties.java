luksa/storage-controller

Add ability to generate rc with secret volume defined through which router gets ssl cert and key

generator-lib/src/main/java/enmasse/rc/model/BrokerProperties.java
@@ -15,14 +15,23 @@ public class BrokerProperties {
 
     private final DockerImageURI routerImage;
     private final int routerPort;
+    private final String routerSecretName;
+    private final String routerSecretPath;
 
 
     public BrokerProperties(DockerImageURI brokerImage, int brokerPort, List<String> brokerMounts, DockerImageURI routerImage, int routerPort) {
+        this(brokerImage, brokerPort, brokerMounts, routerImage, routerPort, null, null);
+    }
+
+    public BrokerProperties(DockerImageURI brokerImage, int brokerPort, List<String> brokerMounts, DockerImageURI routerImage, int routerPort,
+                            String routerSecretName, String routerSecretPath) {
         this.brokerImage = brokerImage;
         this.brokerPort = brokerPort;
         this.brokerMounts = brokerMounts;
         this.routerImage = routerImage;
         this.routerPort = routerPort;
+        this.routerSecretName = routerSecretName;
+        this.routerSecretPath = routerSecretPath;
     }
 
     public int brokerPort() {
@@ -45,13 +54,23 @@ public class BrokerProperties {
         return this.routerImage;
     }
 
+    public String routerSecretName() {
+        return this.routerSecretName;
+    }
+
+    public String routerSecretPath() {
+        return this.routerSecretPath;
+    }
+
     public static class Builder {
         private DockerImageURI brokerImage = new DockerImageURI("gordons/artemis:latest");
         private int brokerPort = 5673;
         private List<String> brokerMounts = Collections.singletonList("/var/run/artemis");
 
-        private DockerImageURI routerImage = new DockerImageURI("gordons/qdrouterd:v7");
+        private DockerImageURI routerImage = new DockerImageURI("gordons/qdrouterd:v8");
         private int routerPort = 5672;
+        private String routerSecretName = "qdrouterd-certs";
+        private String routerSecretPath = "/etc/qpid-dispatch/ssl/";
 
         public Builder brokerImage(DockerImageURI brokerImage) {
             this.brokerImage = brokerImage;
@@ -78,8 +97,18 @@ public class BrokerProperties {
             return this;
         }
 
+        public Builder routerSecretName(String name) {
+            this.routerSecretName = name;
+            return this;
+        }
+
+        public Builder routerSecretPath(String path) {
+            this.routerSecretPath = path;
+            return this;
+        }
+
         public BrokerProperties build() {
-            return new BrokerProperties(brokerImage, brokerPort, brokerMounts, routerImage, routerPort);
+            return new BrokerProperties(brokerImage, brokerPort, brokerMounts, routerImage, routerPort, routerSecretName, routerSecretPath);
         }
     }
 }

EnMasseProject/enmasse

generator-lib/src/main/java/enmasse/rc/model/BrokerProperties.java
@@ -12,14 +12,23 @@ public class BrokerProperties {
 
     private final DockerImageURI routerImage;
     private final int routerPort;
+    private final String routerSecretName;
+    private final String routerSecretPath;
 
 
     public BrokerProperties(DockerImageURI brokerImage, int brokerPort, String brokerMountPath, DockerImageURI routerImage, int routerPort) {
+        this(brokerImage, brokerPort, brokerMountPath, routerImage, routerPort, null, null);
+    }
+
+    public BrokerProperties(DockerImageURI brokerImage, int brokerPort, String brokerMountPath, DockerImageURI routerImage, int routerPort,
+                            String routerSecretName, String routerSecretPath) {
         this.brokerImage = brokerImage;
         this.brokerPort = brokerPort;
         this.brokerMountPath = brokerMountPath;
         this.routerImage = routerImage;
         this.routerPort = routerPort;
+        this.routerSecretName = routerSecretName;
+        this.routerSecretPath = routerSecretPath;
     }
 
     public int brokerPort() {
@@ -42,13 +51,23 @@ public class BrokerProperties {
         return this.routerImage;
     }
 
+    public String routerSecretName() {
+        return this.routerSecretName;
+    }
+
+    public String routerSecretPath() {
+        return this.routerSecretPath;
+    }
+
     public static class Builder {
         private DockerImageURI brokerImage = new DockerImageURI("enmasseproject/artemis:latest");
         private int brokerPort = 5673;
         private String brokerMountPath = "/var/run/artemis";
 
-        private DockerImageURI routerImage = new DockerImageURI("gordons/qdrouterd:v7");
+        private DockerImageURI routerImage = new DockerImageURI("gordons/qdrouterd:v8");
         private int routerPort = 5672;
+        private String routerSecretName = "qdrouterd-certs";
+        private String routerSecretPath = "/etc/qpid-dispatch/ssl/";
 
         public Builder brokerImage(DockerImageURI brokerImage) {
             this.brokerImage = brokerImage;
@@ -75,8 +94,18 @@ public class BrokerProperties {
             return this;
         }
 
+        public Builder routerSecretName(String name) {
+            this.routerSecretName = name;
+            return this;
+        }
+
+        public Builder routerSecretPath(String path) {
+            this.routerSecretPath = path;
+            return this;
+        }
+
         public BrokerProperties build() {
-            return new BrokerProperties(brokerImage, brokerPort, brokerMountPath, routerImage, routerPort);
+            return new BrokerProperties(brokerImage, brokerPort, brokerMountPath, routerImage, routerPort, routerSecretName, routerSecretPath);
         }
     }
 }