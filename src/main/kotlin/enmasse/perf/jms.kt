d29c63354e6271da8bb7c122079100803105be1a
Ulf Lilleengen <lulf@redhat.com>
2016-08-25 22:31:28 +0200

    Add connect timeout

--- src/main/kotlin/enmasse/perf/jms.kt
+++ src/main/kotlin/enmasse/perf/jms.kt
@@ -11,5 +11,6 @@ fun createQueueContext(endpoint: Endpoint, address: String): Context {
     env.put(Context.INITIAL_CONTEXT_FACTORY, "org.apache.qpid.jms.jndi.JmsInitialContextFactory")
     env.put("connectionfactory.enmasse", "amqp://${endpoint.host}:${endpoint.port}")
     env.put("queue.${address}", address)
+    env.put("jms.connectTimeout", 60)
     return javax.naming.InitialContext(env);
 }
