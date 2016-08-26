1cf440c1f4f19d6082ff2d6eb35301a17bee3f03
Ulf Lilleengen <lulf@redhat.com>
2016-08-26 11:25:45 +0200

    Scale replication controllers

---
+++ scripts/scale_controller.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+ADDRESS=$1
+REPLICAS=$2
+
+for i in `seq 1 300`
+do
+    echo "Checking for replication controller, attempt $i"
+    ./oc get replicationcontroller controller-$ADDRESS
+    if [ $? -eq 0 ]; then
+        echo "Found replication controller, scaling up"
+        ./oc scale replicationcontroller controller-$ADDRESS --replicas=$REPLICAS
+        exit 0
+    fi
+    sleep 1
+done
+exit 1
