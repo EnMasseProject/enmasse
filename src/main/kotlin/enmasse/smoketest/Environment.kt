9accd4c5d4dfb50dd2905e4797e41c012feb323c
Ulf Lilleengen <lulf@redhat.com>
2016-08-26 14:06:17 +0200

    Skip scaling to see if topic test works

--- src/main/kotlin/enmasse/smoketest/Environment.kt
+++ src/main/kotlin/enmasse/smoketest/Environment.kt
@@ -13,7 +13,8 @@ object Environment {
     val token = System.getenv("OPENSHIFT_TOKEN")
     val url = System.getenv("OPENSHIFT_URL")
     val namespace = "enmasse-ci"
-    val client = ClientBuilder(url).authorizationStrategy(TokenAuthorizationStrategy(token, user)).build();
-    val service: IService = client.get(ResourceKind.SERVICE, "messaging", namespace)
-    val endpoint = Endpoint(service.portalIP, service.port)
+    //val client = ClientBuilder(url).authorizationStrategy(TokenAuthorizationStrategy(token, user)).build();
+    //val service: IService = client.get(ResourceKind.SERVICE, "messaging", namespace)
+    //val endpoint = Endpoint(service.portalIP, service.port)
+    val endpoint = Endpoint("172.30.187.220", 5672)
 }
