1e9d5821742a7c2cf07f7876b37db4cac28fc68a
Ulf Lilleengen <lulf@redhat.com>
2016-08-29 14:49:34 +0200

    Revert "Drop scaling for now"
    This reverts commit 6909fbf2da2cb2f9d56e7e7004375c98f19ffcbf.

--- scripts/scale_controller.sh
+++ scripts/scale_controller.sh
@@ -7,8 +7,8 @@ do
     echo "Checking for replication controller, attempt $i"
     ./oc get replicationcontroller controller-$ADDRESS
     if [ $? -eq 0 ]; then
-        # echo "Found replication controller, scaling up"
-        # ./oc scale replicationcontroller controller-$ADDRESS --replicas=$REPLICAS
+        echo "Found replication controller, scaling up"
+        ./oc scale replicationcontroller controller-$ADDRESS --replicas=$REPLICAS
         exit 0
     fi
     sleep 5
