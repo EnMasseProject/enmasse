3bfe794b16fd9f80eecda9b5206468182322bed2
Ulf Lilleengen <lulf@redhat.com>
2016-08-29 10:33:55 +0200

    Add script to wait until all pods are running

---
+++ scripts/wait_until_up.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+EXPECTED_PODS=$1
+while true
+do
+    num_running=`./oc get pods | grep -c Running`
+    if [ "$num_running" -eq "$EXPECTED_PODS" ]; then
+        echo "ALL UP!"
+        exit 0
+    else
+        echo "$num_running/$EXPECTED_PODS up"
+    fi
+    sleep 5
+done
