138f23902b2fa10a10d2f92f575d9ed26e34fc41
Ulf Lilleengen <lulf@redhat.com>
2016-09-02 10:26:12 +0200

    Add timeout to wait script

--- scripts/wait_until_up.sh
+++ scripts/wait_until_up.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 EXPECTED_PODS=$1
+TIMEOUT=300
+NOW=$(date +%s)
+END=$(($NOW + $TIMEOUT))
+echo "Waiting until $END"
 while true
 do
+    NOW=$(date +%s)
+    if [ $NOW -gt $END ]; then
+        echo "Timed out waiting for nodes to come up!"
+        exit 1
+    fi
     num_running=`oc get pods | grep -c Running`
     if [ "$num_running" -eq "$EXPECTED_PODS" ]; then
         echo "ALL UP!"
