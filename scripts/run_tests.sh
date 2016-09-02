a01d56c05e7c64117b9b1dc511614de80e7d1ad7
Ulf Lilleengen <lulf@redhat.com>
2016-09-02 09:39:07 +0200

    Run test with 4 brokers

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -10,8 +10,8 @@ oc policy add-role-to-user view system:serviceaccount:$(oc project -q):default
 oc policy add-role-to-user edit system:serviceaccount:$(oc project -q):deployer
 curl -s https://raw.githubusercontent.com/enmasseproject/openshift-configuration/master/enmasse-template.yaml | oc process -f - | oc create -f -
 $DIR/wait_until_up.sh 6
-$DIR/scale_controller.sh mytopic 2
-$DIR/wait_until_up.sh 7
+$DIR/scale_controller.sh mytopic 4
+$DIR/wait_until_up.sh 9
 
 sleep 120
 
