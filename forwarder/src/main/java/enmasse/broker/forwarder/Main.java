cdf8131486dbd48353153b46994c6cb86927dae5
Ulf Lilleengen <lulf@redhat.com>
2016-09-12 11:26:19 +0200

    Set license header

--- forwarder/src/main/java/enmasse/broker/forwarder/Main.java
+++ forwarder/src/main/java/enmasse/broker/forwarder/Main.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 Red Hat Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package enmasse.broker.forwarder;
 
 import com.openshift.restclient.ClientBuilder;
@@ -15,8 +31,6 @@ import java.nio.file.Files;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * @author Ulf Lilleengen
@@ -25,7 +39,6 @@ public class Main {
 
     public static void main(String [] args) throws IOException, InterruptedException {
         System.setProperty(FileResolver.CACHE_DIR_BASE_PROP_NAME, "/tmp/vert.x");
-        Logger.getGlobal().setLevel(Level.INFO);
 
         Map<String, String> env = System.getenv();
         IClient client = createClient(env);
