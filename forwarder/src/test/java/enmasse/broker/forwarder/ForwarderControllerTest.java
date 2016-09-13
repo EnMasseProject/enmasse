71f532c846a17e9ca5b1ae939fc74417d71a8519
Ulf Lilleengen <lulf@redhat.com>
2016-09-13 09:24:42 +0200

    Refactor test

--- forwarder/src/test/java/enmasse/broker/forwarder/ForwarderControllerTest.java
+++ forwarder/src/test/java/enmasse/broker/forwarder/ForwarderControllerTest.java
@@ -17,31 +17,20 @@
 package enmasse.broker.forwarder;
 
 import enmasse.discovery.Host;
-import io.vertx.core.AsyncResult;
-import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
 import io.vertx.proton.ProtonClient;
-import io.vertx.proton.ProtonConnection;
-import io.vertx.proton.ProtonSender;
-import org.apache.qpid.proton.amqp.Symbol;
-import org.apache.qpid.proton.amqp.messaging.AmqpValue;
-import org.apache.qpid.proton.amqp.messaging.Source;
-import org.apache.qpid.proton.amqp.messaging.Target;
-import org.apache.qpid.proton.message.Message;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
@@ -56,9 +45,10 @@ public class ForwarderControllerTest {
     private Vertx vertx = Vertx.vertx(new VertxOptions().setWorkerPoolSize(10));
     private String localHost = "127.0.0.1";
     private final String address = "mytopic";
-    private TestBroker serverA = new TestBroker(localHost, 5672, address);
-    private TestBroker serverB = new TestBroker(localHost, 5673, address);
-    private TestBroker serverC = new TestBroker(localHost, 5674, address);
+    ProtonClient client = ProtonClient.create(vertx);
+    private TestBroker serverA = new TestBroker(client, localHost, 5672, address);
+    private TestBroker serverB = new TestBroker(client, localHost, 5673, address);
+    private TestBroker serverC = new TestBroker(client, localHost, 5674, address);
 
     @Before
     public void setup() throws Exception {
@@ -92,19 +82,14 @@ public class ForwarderControllerTest {
         waitForConnections(serverB, 1, timeout);
         waitForConnections(serverB, 1, timeout);
 
-        CompletableFuture<List<String>> resultA = new CompletableFuture<>();
-        CompletableFuture<List<String>> resultB = new CompletableFuture<>();
+        CompletableFuture<List<String>> resultB = serverB.recvMessages(2, 60, TimeUnit.SECONDS);
+        CompletableFuture<List<String>> resultC = serverC.recvMessages(2, 60, TimeUnit.SECONDS);
 
-        CountDownLatch latch = new CountDownLatch(2);
-        ProtonClient.create(vertx).connect(localHost, 5673, new TestHandler(latch, resultA, 2));
-        ProtonClient.create(vertx).connect(localHost, 5674, new TestHandler(latch, resultB, 2));
-        latch.await(120, TimeUnit.SECONDS);
+        serverA.sendMessage("Hello 1", 60, TimeUnit.SECONDS);
+        serverA.sendMessage("Hello 2", 60, TimeUnit.SECONDS);
 
-        sendMessageTo(5672, "Hello 1");
-        sendMessageTo(5672, "Hello 2");
-
-        assertMessages(resultA.get(120, TimeUnit.SECONDS), "Hello 1", "Hello 2");
         assertMessages(resultB.get(120, TimeUnit.SECONDS), "Hello 1", "Hello 2");
+        assertMessages(resultC.get(120, TimeUnit.SECONDS), "Hello 1", "Hello 2");
         vertx.close();
     }
 
@@ -113,23 +98,6 @@ public class ForwarderControllerTest {
         for (String message : messages) {
             assertThat(result, hasItem(message));
         }
-    }
-
-    private void sendMessageTo(int port, String body) {
-        ProtonClient client = ProtonClient.create(vertx);
-        client.connect(localHost, port, event -> {
-            ProtonConnection connection = event.result().open();
-            Target target = new Target();
-            target.setAddress(address);
-            target.setCapabilities(Symbol.getSymbol("topic"));
-            ProtonSender sender = connection.createSender(address);
-            sender.setTarget(target);
-            sender.open();
-            Message message = Message.Factory.create();
-            message.setBody(new AmqpValue(body));
-            message.setAddress(address);
-            sender.send(message);
-        });
     }
 
     private static void waitForConnections(TestBroker server, int num, long timeout) throws InterruptedException {
@@ -142,36 +110,5 @@ public class ForwarderControllerTest {
             Thread.sleep(1000);
         }
         assertTrue(server.numConnected() >= num);
-    }
-
-    private class TestHandler implements Handler<AsyncResult<ProtonConnection>> {
-        private final CompletableFuture<List<String>> result;
-        private final List<String> data = new ArrayList<>();
-        private final int numExpected;
-        private final CountDownLatch latch;
-
-        public TestHandler(CountDownLatch latch, CompletableFuture<List<String>> result, int numExpected) {
-            this.latch = latch;
-            this.result = result;
-            this.numExpected = numExpected;
-        }
-
-        @Override
-        public void handle(AsyncResult<ProtonConnection> event) {
-            ProtonConnection connection = event.result().open();
-            Source source = new Source();
-            source.setAddress(address);
-            source.setCapabilities(Symbol.getSymbol("topic"));
-            connection.createReceiver(address)
-                    .openHandler(handler -> latch.countDown())
-                    .setSource(source)
-                    .handler((delivery, message) -> {
-                        data.add((String) ((AmqpValue) message.getBody()).getValue());
-                        if (data.size() == numExpected) {
-                            result.complete(data);
-                        }
-                    })
-                    .open();
-        }
     }
 }
--- forwarder/src/test/java/enmasse/broker/forwarder/TestBroker.java
+++ forwarder/src/test/java/enmasse/broker/forwarder/TestBroker.java
@@ -16,15 +16,28 @@
 
 package enmasse.broker.forwarder;
 
+import io.vertx.proton.ProtonClient;
+import io.vertx.proton.ProtonConnection;
+import io.vertx.proton.ProtonSender;
 import org.apache.activemq.artemis.api.core.TransportConfiguration;
 import org.apache.activemq.artemis.core.config.Configuration;
 import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
 import org.apache.activemq.artemis.core.remoting.impl.netty.NettyAcceptorFactory;
 import org.apache.activemq.artemis.core.server.embedded.EmbeddedActiveMQ;
+import org.apache.qpid.proton.amqp.Symbol;
+import org.apache.qpid.proton.amqp.messaging.AmqpValue;
+import org.apache.qpid.proton.amqp.messaging.Source;
+import org.apache.qpid.proton.amqp.messaging.Target;
+import org.apache.qpid.proton.message.Message;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 
 public class TestBroker {
@@ -32,8 +45,10 @@ public class TestBroker {
     private final int port;
     private final String address;
     private final EmbeddedActiveMQ server = new EmbeddedActiveMQ();
+    private final ProtonClient protonClient;
 
-    public TestBroker(String host, int port, String address) {
+    public TestBroker(ProtonClient client, String host, int port, String address) {
+        this.protonClient = client;
         this.host = host;
         this.port = port;
         this.address = address;
@@ -55,6 +70,48 @@ public class TestBroker {
         server.setConfiguration(config);
 
         server.start();
+    }
+
+    public void sendMessage(String messageBody, long timeout, TimeUnit timeUnit) throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(1);
+        protonClient.connect(host, port, event -> {
+            ProtonConnection connection = event.result().open();
+            Target target = new Target();
+            target.setAddress(address);
+            target.setCapabilities(Symbol.getSymbol("topic"));
+            ProtonSender sender = connection.createSender(address);
+            sender.setTarget(target);
+            sender.open();
+            Message message = Message.Factory.create();
+            message.setBody(new AmqpValue(messageBody));
+            message.setAddress(address);
+            sender.send(message, delivery -> latch.countDown());
+        });
+        latch.await(timeout, timeUnit);
+    }
+
+    public CompletableFuture<List<String>> recvMessages(long numMessages, long attachTimeout, TimeUnit timeUnit) throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(1);
+        CompletableFuture<List<String>> future = new CompletableFuture<>();
+        List<String> messages = new ArrayList<>();
+        protonClient.connect(host, port, event -> {
+            ProtonConnection connection = event.result().open();
+            Source source = new Source();
+            source.setAddress(address);
+            source.setCapabilities(Symbol.getSymbol("topic"));
+            connection.createReceiver(address)
+                    .openHandler(opened -> latch.countDown())
+                    .setSource(source)
+                    .handler((delivery, message) -> {
+                        messages.add((String) ((AmqpValue) message.getBody()).getValue());
+                        if (messages.size() == numMessages) {
+                            future.complete(new ArrayList<>(messages));
+                        }
+                    })
+                    .open();
+        });
+        latch.await(attachTimeout, timeUnit);
+        return future;
     }
 
     public int numConnected() {
