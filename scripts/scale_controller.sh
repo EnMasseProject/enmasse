0a0a1c42b6b727e3302b3764c2f18a7afe866ace
Ulf Lilleengen <lulf@redhat.com>
2016-09-16 14:34:04 +0200

    Use deployment configs

--- scripts/scale_controller.sh
+++ scripts/scale_controller.sh
@@ -4,11 +4,11 @@ REPLICAS=$2
 
 for i in `seq 1 60`
 do
-    echo "Checking for replication controller, attempt $i"
-    oc get replicationcontroller $CONTROLLER
+    echo "Checking for controller, attempt $i"
+    oc get dc $CONTROLLER
     if [ $? -eq 0 ]; then
-        echo "Found replication controller, scaling up"
-        oc scale replicationcontroller $CONTROLLER --replicas=$REPLICAS
+        echo "Found controller, scaling up"
+        oc scale dc $CONTROLLER --replicas=$REPLICAS
         exit 0
     fi
     sleep 5
