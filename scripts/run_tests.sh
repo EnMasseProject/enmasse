38e3686447a4257f8d74876f0e73d83d520137a3
Ulf Lilleengen <lulf@redhat.com>
2016-09-16 15:09:11 +0200

    Add extra wait after deployment is done

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -10,6 +10,7 @@ oc policy add-role-to-user view system:serviceaccount:$(oc project -q):default
 oc policy add-role-to-user edit system:serviceaccount:$(oc project -q):deployer
 curl -s https://raw.githubusercontent.com/enmasseproject/openshift-configuration/master/enmasse-template.yaml | oc process -f - | oc create -f -
 $DIR/wait_until_up.sh 7 || exit 1
+sleep 20 # TODO: check deployment config instead
 $DIR/scale_controller.sh topic-mytopic 4 || exit 1
 $DIR/wait_until_up.sh 10 || exit 1
 
