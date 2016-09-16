5087efb8430f96c7a2a126fff28940235b062667
Ulf Lilleengen <lulf@redhat.com>
2016-09-16 16:06:34 +0200

    Retry scaling until it works

--- scripts/scale_controller.sh
+++ scripts/scale_controller.sh
@@ -6,10 +6,20 @@ for i in `seq 1 60`
 do
     echo "Checking for controller, attempt $i"
     oc get dc $CONTROLLER
-    if [ $? -eq 0 ]; then
+    if [ $? -eq 0 ]
+    then
         echo "Found controller, scaling up"
-        oc scale dc $CONTROLLER --replicas=$REPLICAS
-        exit 0
+        while true
+        do
+            oc scale dc $CONTROLLER --replicas=$REPLICAS
+            if [ $? -eq 0 ]
+            then
+                exit 0
+            else
+                echo "Error scaling, retrying in 5 seconds"
+                sleep 5
+            fi
+        done
     fi
     sleep 5
 done
