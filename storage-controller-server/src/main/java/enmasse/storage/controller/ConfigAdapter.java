8026a81b1daec82b8e786368751a6bb3cc320e75
Ulf Lilleengen <lulf@redhat.com>
2016-09-19 15:15:15 +0200

    Use openshift restclient directly
    * This makes the storage controller independent of the configmap bridge

--- storage-controller-server/src/main/java/enmasse/storage/controller/ConfigAdapter.java
+++ storage-controller-server/src/main/java/enmasse/storage/controller/ConfigAdapter.java
@@ -18,36 +18,101 @@ package enmasse.storage.controller;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.openshift.internal.restclient.model.ConfigMap;
+import com.openshift.internal.restclient.model.KubernetesResource;
+import com.openshift.restclient.IOpenShiftWatchListener;
+import com.openshift.restclient.IWatcher;
+import com.openshift.restclient.ResourceKind;
+import com.openshift.restclient.model.IConfigMap;
+import com.openshift.restclient.model.IResource;
 import enmasse.storage.controller.admin.ConfigSubscriber;
-import io.vertx.proton.ProtonDelivery;
-import io.vertx.proton.ProtonMessageHandler;
-import org.apache.qpid.proton.amqp.messaging.AmqpValue;
-import org.apache.qpid.proton.message.Message;
+import enmasse.storage.controller.openshift.OpenshiftClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
+import java.util.List;
 
-public class ConfigAdapter implements ProtonMessageHandler {
+public class ConfigAdapter implements IOpenShiftWatchListener {
     private static final ObjectMapper mapper = new ObjectMapper();
     private static final Logger log = LoggerFactory.getLogger(ConfigAdapter.class.getName());
 
+    private IWatcher watcher;
+    private final OpenshiftClient openshiftClient;
+    private final String configName;
     private final ConfigSubscriber configSubscriber;
 
-    public ConfigAdapter(ConfigSubscriber configSubscriber) {
+    public ConfigAdapter(OpenshiftClient openshiftClient, String configName, ConfigSubscriber configSubscriber) {
+        this.openshiftClient = openshiftClient;
+        this.configName = configName;
         this.configSubscriber = configSubscriber;
     }
 
-    @Override
-    public void handle(ProtonDelivery delivery, Message message) {
+    private void configUpdated(IConfigMap configMap) {
+        if (!configName.equals(configMap.getName())) {
+            return;
+        }
         try {
-            if (message.getBody() instanceof AmqpValue) {
-                JsonNode root = mapper.readTree((String) ((AmqpValue) message.getBody()).getValue());
-                configSubscriber.configUpdated(root.get("json"));
+            if (configMap.getData().containsKey("json")) {
+                log.info("Got new config for " + configName + " with data: " + configMap.getData().get("json"));
+                JsonNode root = mapper.readTree(configMap.getData().get("json"));
+                configSubscriber.configUpdated(root);
+            } else {
+                log.info("Got empty config for " + configName);
+                configSubscriber.configUpdated(mapper.createObjectNode());
             }
-        } catch (IOException e) {
+        } catch (Exception e) {
             log.warn("Error handling address config update", e);
         }
 
+    }
+
+    public void start() {
+        watcher = openshiftClient.watch(this, ResourceKind.CONFIG_MAP);
+    }
+
+    public void stop() {
+        if (watcher != null) {
+            watcher.stop();
+        }
+    }
+
+    @Override
+    public void connected(List<IResource> resources) {
+        IConfigMap map = null;
+        for (IResource resource : resources) {
+            if (resource.getName().equals(configName)) {
+                map = createMap(resource);
+            }
+        }
+
+        if (map != null) {
+            configUpdated(map);
+        }
+    }
+
+    // TODO: This is a workaroaund for https://github.com/openshift/openshift-restclient-java/issues/208
+    private IConfigMap createMap(IResource resource) {
+        KubernetesResource r = (KubernetesResource) resource;
+        return new ConfigMap(r.getNode(), r.getClient(), r.getPropertyKeys());
+    }
+
+    @Override
+    public void disconnected() {
+        log.info("Disconnected, restarting watch");
+        try {
+            this.watcher = openshiftClient.watch(this, ResourceKind.CONFIG_MAP);
+        } catch (Exception e) {
+            log.error("Error re-watching on disconnect", e);
+        }
+    }
+
+    @Override
+    public void received(IResource resource, ChangeType change) {
+        configUpdated(createMap(resource));
+    }
+
+    @Override
+    public void error(Throwable err) {
+        log.error("Got error from watcher", err);
     }
 }
