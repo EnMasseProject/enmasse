2dcdc477cbd806ac2203a48a3e7d2c70b2f678f4
Ulf Lilleengen <lulf@redhat.com>
2016-09-28 13:51:52 +0200

    Use new path to template

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -8,7 +8,9 @@ oc delete project enmasse-ci
 oc new-project enmasse-ci
 oc policy add-role-to-user view system:serviceaccount:$(oc project -q):default
 oc policy add-role-to-user edit system:serviceaccount:$(oc project -q):deployer
-oc process -f https://raw.githubusercontent.com/enmasseproject/openshift-configuration/master/enmasse-template.yaml | oc create -f -
+
+oc process -f https://raw.githubusercontent.com/enmasseproject/openshift-configuration/master/generated/enmasse-template.yaml | oc create -f -
+
 $DIR/wait_until_up.sh 7 || exit 1
 sleep 20 # TODO: check deployment config instead
 $DIR/scale_controller.sh topic-mytopic 4 || exit 1
