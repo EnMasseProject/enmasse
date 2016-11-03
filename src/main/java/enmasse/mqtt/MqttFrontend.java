c7ef8ed0307ddda63e6956171fe8f722c8e61a80
ppatierno <ppatierno@live.com>
2016-11-03 10:43:48 +0100

    Added starting and stopping MQTT server in the MQTT frontend verticle

--- src/main/java/enmasse/mqtt/MqttFrontend.java
+++ src/main/java/enmasse/mqtt/MqttFrontend.java
@@ -18,8 +18,12 @@ package enmasse.mqtt;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.mqtt.MqttEndpoint;
+import io.vertx.mqtt.MqttServer;
+import io.vertx.mqtt.MqttServerOptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 
@@ -31,17 +35,71 @@ public class MqttFrontend extends AbstractVerticle {
 
     private static final Logger LOG = LoggerFactory.getLogger(MqttFrontend.class);
 
+    @Value(value = "${enmasse.mqtt.bindaddress:0.0.0.0}")
+    private String bindAddress;
+    @Value(value = "${enmasse.mqtt.listenport:1883}")
+    private int listenPort;
+
+    private MqttServer server;
+
+    /**
+     * Start the MQTT server component
+     *
+     * @param startFuture
+     */
+    private void bindMqttServer(Future<Void> startFuture) {
+
+        MqttServerOptions options = new MqttServerOptions();
+        options.setHost(this.bindAddress).setPort(this.listenPort);
+
+        this.server = MqttServer.create(this.vertx, options);
+
+        this.server
+                .endpointHandler(this::handleMqttEndpointConnection)
+                .listen(done -> {
+
+                    if (done.succeeded()) {
+                        LOG.info("MQTT frontend running on {}:{}", this.bindAddress, this.server.actualPort());
+                        startFuture.complete();
+                    } else {
+                        LOG.error("Error while starting up MQTT frontend", done.cause());
+                        startFuture.fail(done.cause());
+                    }
+
+                });
+    }
+
+    private void handleMqttEndpointConnection(MqttEndpoint endpoint) {
+        // TODO
+    }
+
     @Override
     public void start(Future<Void> startFuture) throws Exception {
 
         LOG.info("Starting MQTT frontend verticle...");
-        startFuture.complete();
+        this.bindMqttServer(startFuture);
     }
 
     @Override
     public void stop(Future<Void> stopFuture) throws Exception {
 
         LOG.info("Stopping MQTT frontend verticle ...");
-        stopFuture.complete();
+
+        Future<Void> shutdownTracker = Future.future();
+        shutdownTracker.setHandler(done -> {
+           if (done.succeeded()) {
+               LOG.info("MQTT frontend has been shut down successfully");
+               stopFuture.complete();
+           } else {
+               LOG.info("Error while shutting down MQTT frontend", done.cause());
+               stopFuture.fail(done.cause());
+           }
+        });
+
+        if (this.server != null) {
+            this.server.close(shutdownTracker.completer());
+        } else {
+            shutdownTracker.complete();
+        }
     }
 }
