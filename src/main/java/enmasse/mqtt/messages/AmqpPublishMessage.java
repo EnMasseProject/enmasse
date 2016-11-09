28017875fc750d909130e06e60d4fb66236f17cb
ppatierno <ppatierno@live.com>
2016-11-09 11:36:46 +0100

    Added first step for mapping AMQP - MQTT publish message

--- src/main/java/enmasse/mqtt/messages/AmqpPublishMessage.java
+++ src/main/java/enmasse/mqtt/messages/AmqpPublishMessage.java
@@ -16,7 +16,17 @@
 
 package enmasse.mqtt.messages;
 
+import io.vertx.core.buffer.Buffer;
+import io.vertx.proton.ProtonHelper;
+import org.apache.qpid.proton.amqp.Binary;
+import org.apache.qpid.proton.amqp.Symbol;
+import org.apache.qpid.proton.amqp.messaging.Data;
+import org.apache.qpid.proton.amqp.messaging.MessageAnnotations;
+import org.apache.qpid.proton.amqp.messaging.Section;
 import org.apache.qpid.proton.message.Message;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Represents an AMQP_PUBLISH message
@@ -28,6 +38,30 @@ public class AmqpPublishMessage {
     public static final String AMQP_RETAIN_ANNOTATION = "x-retain";
     public static final String AMQP_DESIDERED_SND_SETTLE_MODE_ANNOTATION = "x-desidered-snd-settle-mode";
 
+    private int qos;
+    private boolean isDup;
+    private boolean isRetain;
+    private String topic;
+    private Buffer payload;
+
+    /**
+     * Constructor
+     *
+     * @param qos   QoS level
+     * @param isDup if the message is a duplicate
+     * @param isRetain  if the message needs to be retained
+     * @param topic topic on which the message is published
+     * @param payload   message payload
+     */
+    public AmqpPublishMessage(int qos, boolean isDup, boolean isRetain, String topic, Buffer payload) {
+
+        this.qos = qos;
+        this.isDup = isDup;
+        this.isRetain = isRetain;
+        this.topic = topic;
+        this.payload = payload;
+    }
+
     /**
      * Return an AMQP_PUBLISH message from the raw AMQP one
      *
@@ -36,8 +70,39 @@ public class AmqpPublishMessage {
      */
     public static AmqpPublishMessage from(Message message) {
 
-        // TODO:
-        return new AmqpPublishMessage();
+        if (!message.getSubject().equals(SUBJECT)) {
+            throw new IllegalArgumentException("AMQP message subject is no 'publish'");
+        }
+
+        MessageAnnotations messageAnnotations = message.getMessageAnnotations();
+        if (messageAnnotations == null) {
+            throw new IllegalArgumentException("AMQP message has no annotations");
+        } else {
+
+            boolean isRetain = false;
+            if (messageAnnotations.getValue().containsKey(Symbol.valueOf(AMQP_RETAIN_ANNOTATION))) {
+                isRetain = (boolean) messageAnnotations.getValue().get(Symbol.valueOf(AMQP_RETAIN_ANNOTATION));
+            }
+
+            int qos = 0;
+            if (messageAnnotations.getValue().containsKey(Symbol.valueOf(AMQP_DESIDERED_SND_SETTLE_MODE_ANNOTATION))) {
+                // TODO : qos is not unique from settle mode
+            }
+
+            boolean isDup = (message.getDeliveryCount() > 0);
+
+            String topic = message.getAddress();
+
+            Section section = message.getBody();
+            if ((section != null) && (section instanceof Data)) {
+
+                Buffer payload = Buffer.buffer(((Data) section).getValue().getArray());
+                return new AmqpPublishMessage(qos, isDup, isRetain, topic, payload);
+
+            } else {
+                throw new IllegalArgumentException("AMQP message wrong body type");
+            }
+        }
     }
 
     /**
@@ -47,7 +112,64 @@ public class AmqpPublishMessage {
      */
     public Message toAmqp() {
 
-        // TODO:
-        return null;
+        Message message = ProtonHelper.message();
+
+        message.setSubject(SUBJECT);
+
+        Map<Symbol, Object> map = new HashMap<>();
+        map.put(Symbol.valueOf(AMQP_RETAIN_ANNOTATION), this.isRetain);
+        map.put(Symbol.valueOf(AMQP_DESIDERED_SND_SETTLE_MODE_ANNOTATION), AmqpHelper.toSenderSettleMode(this.qos));
+        MessageAnnotations messageAnnotations = new MessageAnnotations(map);
+        message.setMessageAnnotations(messageAnnotations);
+
+        message.setAddress(this.topic);
+
+        message.setDeliveryCount(this.isDup ? 1 : 0);
+
+        // the payload could be null (or empty)
+        if (this.payload != null)
+            message.setBody(new Data(new Binary(this.payload.getBytes())));
+
+        return message;
+    }
+
+    /**
+     * QoS level
+     * @return
+     */
+    public int qos() {
+        return this.qos;
+    }
+
+    /**
+     * If the message is a duplicate
+     * @return
+     */
+    public boolean isDup() {
+        return this.isDup;
+    }
+
+    /**
+     * If the message needs to be retained
+     * @return
+     */
+    public boolean isRetain() {
+        return this.isRetain;
+    }
+
+    /**
+     * Topic on which the message is published
+     * @return
+     */
+    public String topic() {
+        return this.topic;
+    }
+
+    /**
+     * Message payload
+     * @return
+     */
+    public Buffer payload() {
+        return this.payload;
     }
 }
