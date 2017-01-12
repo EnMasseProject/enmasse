dc8329fde84a295b4b6549f8a089fd9967775cf0
Ulf Lilleengen <lulf@redhat.com>
2017-01-12 09:16:43 +0100

    Upgrade to openshift 1.3.2

--- scripts/install_openshift.sh
+++ scripts/install_openshift.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 DEST=$1
 mkdir -p $DEST
-wget https://github.com/openshift/origin/releases/download/v1.3.0-rc1/openshift-origin-server-v1.3.0-rc1-ac0bb1bf6a629e0c262f04636b8cf2916b16098c-linux-64bit.tar.gz -O openshift.tar.gz
+wget https://github.com/openshift/origin/releases/download/v1.3.2/openshift-origin-server-v1.3.2-ac1d579-linux-64bit.tar.gz -O openshift.tar.gz
 tar xzf openshift.tar.gz -C $DEST --strip-components 1
