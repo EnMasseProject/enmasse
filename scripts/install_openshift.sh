9a9ac051c9a02485cadfb2418148f3ec8b374fe1
Ulf Lilleengen <lulf@redhat.com>
2017-03-29 19:29:31 +0200

    Print docker log

--- scripts/install_openshift.sh
+++ scripts/install_openshift.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 DEST=$1
 mkdir -p $DEST
 wget https://github.com/openshift/origin/releases/download/v1.4.1/openshift-origin-client-tools-v1.4.1-3f9807a-linux-64bit.tar.gz -O openshift.tar.gz
@@ -7,3 +8,4 @@ tar xzf openshift.tar.gz -C $DEST --strip-components 1
 sudo sh -c 'echo DOCKER_OPTS="-H tcp://127.0.0.1:2375 -H unix:///var/run/docker.sock --insecure-registry 172.30.0.0/16" > /etc/default/docker'
 sudo service docker restart
 sudo cat /etc/default/docker
+sudo cat /var/log/docker.log
