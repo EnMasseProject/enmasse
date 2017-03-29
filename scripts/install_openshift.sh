c5377e64870d6c40a5ce6b3f4eb8d44f6b74ef9f
Ulf Lilleengen <lulf@redhat.com>
2017-03-29 19:16:01 +0200

    Print docker config

--- scripts/install_openshift.sh
+++ scripts/install_openshift.sh
@@ -3,3 +3,6 @@ DEST=$1
 mkdir -p $DEST
 wget https://github.com/openshift/origin/releases/download/v1.4.1/openshift-origin-client-tools-v1.4.1-3f9807a-linux-64bit.tar.gz -O openshift.tar.gz
 tar xzf openshift.tar.gz -C $DEST --strip-components 1
+
+sudo ls /etc/default
+sudo cat /etc/default/docker
