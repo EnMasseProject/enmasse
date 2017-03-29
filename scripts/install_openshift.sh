d82097e42bb1e9b4ffc8617ea0e64f14d74aed34
Ulf Lilleengen <lulf@redhat.com>
2017-03-29 16:57:35 +0200

    Use oc cluster up to start openshift

--- scripts/install_openshift.sh
+++ scripts/install_openshift.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 DEST=$1
 mkdir -p $DEST
-wget https://github.com/openshift/origin/releases/download/v1.3.2/openshift-origin-server-v1.3.2-ac1d579-linux-64bit.tar.gz -O openshift.tar.gz
+wget https://github.com/openshift/origin/releases/download/v1.4.1/openshift-origin-client-tools-v1.4.1-3f9807a-linux-64bit.tar.gz -O openshift.tar.gz
 tar xzf openshift.tar.gz -C $DEST --strip-components 1
--- scripts/start_openshift.sh
+++ scripts/start_openshift.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DIR=$1
 mkdir -p logs
-sudo $DIR/openshift start 2> logs/os.err > logs/os.log &
+sudo $DIR/oc cluster up
