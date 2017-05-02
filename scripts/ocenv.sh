4f9214a2ed2ff531f3771ec57ebca9ec08724121
Ulf Lilleengen <lulf@redhat.com>
2017-05-02 19:54:24 +0200

    Use simpler way of getting token

--- scripts/ocenv.sh
+++ scripts/ocenv.sh
@@ -4,6 +4,6 @@ NAMESPACE=${2-myproject}
 USER=${3-developer}
 
 export OPENSHIFT_USER=$USER
-export OPENSHIFT_TOKEN=`oc config view -o jsonpath="{.users[?(@.name == \"${USER}/${HOST}:8443\")].user.token}"`
+export OPENSHIFT_TOKEN=`oc whoami -t`
 export OPENSHIFT_MASTER_URL=https://${HOST}:8443
 export OPENSHIFT_NAMESPACE=${NAMESPACE}
