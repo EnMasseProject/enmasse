3c898b1da3c42af392beaac6eb0477d248e508d4
Ulf Lilleengen <lulf@redhat.com>
2017-05-04 10:13:28 +0200

    Fix directory references

--- scripts/start_openshift.sh
+++ scripts/start_openshift.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 set -x
 DIR=$1
+SDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CDIR=$SDIR/config
+
 mkdir -p logs
-sudo $DIR/openshift start --write-config=$DIR 2> logs/os.err > logs/os.log &
-ls $DIR
+mkdir -p $CDIR
+sudo $DIR/openshift start --write-config=$CDIR
+ls $CDIR
 
 export MYIP=`ip route get 8.8.8.8 | head -1 | cut -d' ' -f8`
 echo "MYIP: $MYIP"
-sed -i -e "s/router.default.svc.cluster.local/${MYIP}.nip.io/g" $DIR/master-config.yaml
+sed -i -e "s/router.default.svc.cluster.local/${MYIP}.nip.io/g" $CDIR/master-config.yaml
 echo "NEW CONFIG: "
-cat $DIR/master-config.yaml
-sudo $DIR/openshift start --master-config=master-config.yaml 2> logs/os.err > logs/os.log &
+cat $CDIR/master-config.yaml
+sudo $DIR/openshift start --master-config=$CDIR/master-config.yaml 2> logs/os.err > logs/os.log &
 sleep 30
 sudo chown -R $USER openshift.local.config
 $DIR/oadm --config openshift.local.config/master/admin.kubeconfig policy add-scc-to-user hostnetwork system:serviceaccount:default:router
 $DIR/oadm --config openshift.local.config/master/admin.kubeconfig policy add-cluster-role-to-user cluster-reader system:serviceaccount:default:router
 $DIR/oadm --config openshift.local.config/master/admin.kubeconfig router
-k
