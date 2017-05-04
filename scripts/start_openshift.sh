54cd831eb0883b2fe296da8ccdd2b5231a80e723
Ulf Lilleengen <lulf@redhat.com>
2017-05-04 08:12:45 +0200

    Set router subdomain

--- scripts/start_openshift.sh
+++ scripts/start_openshift.sh
@@ -5,11 +5,8 @@ mkdir -p logs
 sudo $DIR/openshift start 2> logs/os.err > logs/os.log &
 sleep 30
 sudo chown -R $USER openshift.local.config
+MYIP=`ip route get 8.8.8.8 | head -1 | cut -d' ' -f8`
+echo "MYIP: $MYIP"
 $DIR/oadm --config openshift.local.config/master/admin.kubeconfig policy add-scc-to-user hostnetwork system:serviceaccount:default:router
 $DIR/oadm --config openshift.local.config/master/admin.kubeconfig policy add-cluster-role-to-user cluster-reader system:serviceaccount:default:router
-$DIR/oadm --config openshift.local.config/master/admin.kubeconfig router
-
-IP=`hostname -I`
-echo "START : $IP"
-OTHERIP=`ip route get 8.8.8.8 | head -1 | cut -d' ' -f7`
-echo "OTHERIP: $OTHERIP"
+$DIR/oadm --config openshift.local.config/master/admin.kubeconfig router --subdomain="${MYIP}.nip.io"
