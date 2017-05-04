6d5a0896f076645de71002b7859fefb44042f4a0
Ulf Lilleengen <lulf@redhat.com>
2017-05-04 10:28:54 +0200

    Fix path to master config

--- scripts/start_openshift.sh
+++ scripts/start_openshift.sh
@@ -11,11 +11,14 @@ ls $CDIR
 
 export MYIP=`ip route get 8.8.8.8 | head -1 | cut -d' ' -f8`
 echo "MYIP: $MYIP"
-sed -i -e "s/router.default.svc.cluster.local/${MYIP}.nip.io/g" $CDIR/master-config.yaml
+sed -i -e "s/router.default.svc.cluster.local/${MYIP}.nip.io/g" $CDIR/master/master-config.yaml
 echo "NEW CONFIG: "
-cat $CDIR/master-config.yaml
-sudo $DIR/openshift start --master-config=$CDIR/master-config.yaml 2> logs/os.err > logs/os.log &
+cat $CDIR/master/master-config.yaml
+sudo $DIR/openshift start --master-config=$CDIR/master/master-config.yaml 2> logs/os.err > logs/os.log &
 sleep 30
+cat logs/os.err
+cat logs/os.log
+
 sudo chown -R $USER openshift.local.config
 $DIR/oadm --config openshift.local.config/master/admin.kubeconfig policy add-scc-to-user hostnetwork system:serviceaccount:default:router
 $DIR/oadm --config openshift.local.config/master/admin.kubeconfig policy add-cluster-role-to-user cluster-reader system:serviceaccount:default:router
