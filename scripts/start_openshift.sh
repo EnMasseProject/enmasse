d5a80ad05b851b5c752fe7568393ac064e9236f2
Ulf Lilleengen <lulf@redhat.com>
2017-05-04 08:07:32 +0200

    Print ip

--- scripts/start_openshift.sh
+++ scripts/start_openshift.sh
@@ -8,3 +8,6 @@ sudo chown -R $USER openshift.local.config
 $DIR/oadm --config openshift.local.config/master/admin.kubeconfig policy add-scc-to-user hostnetwork system:serviceaccount:default:router
 $DIR/oadm --config openshift.local.config/master/admin.kubeconfig policy add-cluster-role-to-user cluster-reader system:serviceaccount:default:router
 $DIR/oadm --config openshift.local.config/master/admin.kubeconfig router
+
+IP=`hostname -I`
+echo "START : $IP"
