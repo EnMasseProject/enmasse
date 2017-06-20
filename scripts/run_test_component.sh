af8ec65b9b71a22f6806380e695a23700be8896f
Ulf Lilleengen <lulf@redhat.com>
2017-06-20 12:42:16 +0200

    Pass all arguments to run_test

--- scripts/run_test_component.sh
+++ scripts/run_test_component.sh
@@ -12,7 +12,7 @@ MULTITENANT=${MULTITENANT:-false}
 oc login -u ${OPENSHIFT_USER} -p ${OPENSHIFT_PASSWD} --insecure-skip-tls-verify=true ${OPENSHIFT_URL}
 
 setup_test $OPENSHIFT_PROJECT $ENMASSE_DIR $MULTITENANT $OPENSHIFT_URL $OPENSHIFT_USER
-run_test $OPENSHIFT_PROJECT true || failure=$(($failure + 1))
+run_test $OPENSHIFT_PROJECT true $MULTITENANT $OPENSHIFT_URL $OPENSHIFT_USER || failure=$(($failure + 1))
 teardown_test $OPENSHIFT_PROJECT
 
 if [ $failure -gt 0 ]
