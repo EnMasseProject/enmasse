b05194d4edd3d0f4f1099fd338f479898fedf2b2
Ulf Lilleengen <lulf@redhat.com>
2017-06-23 16:37:03 +0200

    Don't tear down tests as logs wont be printed

--- scripts/run_test_component.sh
+++ scripts/run_test_component.sh
@@ -13,7 +13,7 @@ oc login -u ${OPENSHIFT_USER} -p ${OPENSHIFT_PASSWD} --insecure-skip-tls-verify=
 
 setup_test $OPENSHIFT_PROJECT $ENMASSE_DIR $MULTITENANT $OPENSHIFT_URL $OPENSHIFT_USER
 run_test $OPENSHIFT_PROJECT true $MULTITENANT $OPENSHIFT_URL $OPENSHIFT_USER || failure=$(($failure + 1))
-teardown_test $OPENSHIFT_PROJECT
+#teardown_test $OPENSHIFT_PROJECT
 
 if [ $failure -gt 0 ]
 then
