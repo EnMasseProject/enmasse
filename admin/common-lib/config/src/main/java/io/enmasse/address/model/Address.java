689bfebdd52ffdef68bde62809e1d309092d5aea
Ulf Lilleengen <lulf@redhat.com>
2017-07-04 09:56:22 +0200

    Add descriptions to interfaces

--- admin/common-lib/config/src/main/java/io/enmasse/address/model/Address.java
+++ admin/common-lib/config/src/main/java/io/enmasse/address/model/Address.java
@@ -1,12 +1,56 @@
+/*
+ * Copyright 2017 Red Hat Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.enmasse.address.model;
 
 /**
- * Represents an Address in the EnMasse address model
+ * Represents an Address in the address model
  */
 public interface Address {
+    /**
+     * Return the name of this address. This should not be assumed to be the same as the address.
+     *
+     * @return The name
+     */
     String getName();
+
+    /**
+     * Return the address string.
+     *
+     * @return The address
+     */
     String getAddress();
-    String getAddressSpace();
+
+    /**
+     * The address space of this address.
+     *
+     * @return The address space
+     */
+    AddressSpace getAddressSpace();
+
+    /**
+     * The address type of this address
+     *
+     * @return The type
+     */
     AddressType getType();
+
+    /**
+     * The plan used for this address.
+     *
+     * @return The plan
+     */
     Plan getPlan();
 }
--- admin/common-lib/config/src/main/java/io/enmasse/address/model/Plan.java
+++ admin/common-lib/config/src/main/java/io/enmasse/address/model/Plan.java
@@ -1,9 +1,35 @@
+/*
+ * Copyright 2017 Red Hat Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.enmasse.address.model;
 
 /**
  * Represents an EnMasse Plan
  */
 public interface Plan {
+    /**
+     * Get the name of the plan.
+     *
+     * @return The name
+     */
     String getName();
+
+    /**
+     * Get a longer description of the plan.
+     *
+     * @return A textual description
+     */
     String getDescription();
 }
