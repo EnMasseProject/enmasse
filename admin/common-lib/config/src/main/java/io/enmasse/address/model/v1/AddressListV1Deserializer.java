d16a5ea02c5a81522fa757036317139921c998bc
Ulf Lilleengen <lulf@redhat.com>
2017-07-07 19:33:18 +0200

    Parameterize decoders

--- admin/common-lib/config/src/main/java/io/enmasse/address/model/v1/AddressListV1Deserializer.java
+++ admin/common-lib/config/src/main/java/io/enmasse/address/model/v1/AddressListV1Deserializer.java
@@ -34,6 +34,12 @@ class AddressListV1Deserializer extends JsonDeserializer<AddressList> {
 
     private static final ObjectMapper mapper = new ObjectMapper();
 
+    private final AddressV1Deserializer addressDeserializer;
+
+    public AddressListV1Deserializer(AddressV1Deserializer addressDeserializer) {
+        this.addressDeserializer = addressDeserializer;
+    }
+
     @Override
     public AddressList deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JsonProcessingException {
         ObjectNode root = mapper.readValue(jsonParser, ObjectNode.class);
@@ -41,7 +47,7 @@ class AddressListV1Deserializer extends JsonDeserializer<AddressList> {
         if (root.hasNonNull(Fields.ITEMS)) {
             ArrayNode items = (ArrayNode) root.get(Fields.ITEMS);
             for (int i = 0; i < items.size(); i++) {
-                retval.add(AddressV1Deserializer.deserialize((ObjectNode) items.get(i)));
+                retval.add(addressDeserializer.deserialize((ObjectNode) items.get(i)));
             }
         }
         return retval;
