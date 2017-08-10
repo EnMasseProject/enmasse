890624d190d447f2ce9395b1a7890e23c5f9bcdc
Ulf Lilleengen <lulf@redhat.com>
2017-08-10 13:27:42 +0200

    Attempt at stabilizing discovery test
    Related to #109

--- admin/common-lib/discovery/src/test/java/enmasse/discovery/DiscoveryTest.java
+++ admin/common-lib/discovery/src/test/java/enmasse/discovery/DiscoveryTest.java
@@ -3,20 +3,25 @@ package enmasse.discovery;
 import enmasse.config.service.amqp.AMQPServer;
 import enmasse.config.service.model.Subscriber;
 import io.vertx.core.Vertx;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
 import org.apache.qpid.proton.amqp.messaging.AmqpValue;
 import org.apache.qpid.proton.message.Message;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import java.io.IOException;
 import java.util.*;
 import java.util.concurrent.*;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 
+@RunWith(VertxUnitRunner.class)
 public class DiscoveryTest {
 
     private Vertx vertx;
@@ -32,7 +37,7 @@ public class DiscoveryTest {
     }
 
     @Test
-    public void testDiscovery() throws InterruptedException, IOException, TimeoutException, ExecutionException {
+    public void testDiscovery(TestContext context) throws InterruptedException, IOException, ExecutionException {
         Map<String, String> expectedLabelFilter = Collections.singletonMap("my", "key");
         Map<String, String> expectedAnnotationFilter = Collections.singletonMap("my", "annotation");
         CompletableFuture<Set<Host>> changedHosts = new CompletableFuture<>();
@@ -44,31 +49,36 @@ public class DiscoveryTest {
             subscriptionQueue.put(subscriber);
         }));
 
-        vertx.deployVerticle(testServer);
+        vertx.deployVerticle(testServer, context.asyncAssertSuccess());
         DiscoveryClient client = new DiscoveryClient(new Endpoint("127.0.0.1", waitForPort(testServer)), "podsense", expectedLabelFilter, expectedAnnotationFilter, Optional.empty());
         client.addListener(changedHosts::complete);
-        vertx.deployVerticle(client);
+        vertx.deployVerticle(client, context.asyncAssertSuccess());
 
         Subscriber subscriber = subscriptionQueue.poll(1, TimeUnit.MINUTES);
         subscriber.resourcesUpdated(createResponse("False", "Pending"));
         try {
             changedHosts.get(10, TimeUnit.SECONDS);
             fail("Pending hosts should not update host set");
-        } catch (TimeoutException e) {
+        } catch (TimeoutException ignored) {
         }
 
         subscriber.resourcesUpdated(createResponse("False", "Running"));
         try {
             changedHosts.get(10, TimeUnit.SECONDS);
             fail("Ready must be true before returning host");
-        } catch (TimeoutException e) {
+        } catch (TimeoutException ignored) {
         }
 
         subscriber.resourcesUpdated(createResponse("True", "Running"));
-        Set<Host> actual = changedHosts.get(1, TimeUnit.MINUTES);
-        assertThat(actual.size(), is(1));
-        Host actualHost = actual.iterator().next();
-        assertThat(actualHost.getHostname(), is("10.0.0.1"));
+        try {
+            Set<Host> actual = changedHosts.get(2, TimeUnit.MINUTES);
+            assertThat(actual.size(), is(1));
+            Host actualHost = actual.iterator().next();
+            assertThat(actualHost.getHostname(), is("10.0.0.1"));
+        } catch (Exception e) {
+            fail("Unexpected exception" + e.getMessage());
+            e.printStackTrace();
+        }
     }
 
     public Message createResponse(String ready, String phase) {
