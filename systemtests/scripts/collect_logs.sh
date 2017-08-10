2012b8b8c88d0bd2ca3bb76dc3e37288257b1814
Ulf Lilleengen <lulf@redhat.com>
2017-08-10 17:05:23 +0200

    Use new syntax for logs within a container

--- systemtests/scripts/collect_logs.sh
+++ systemtests/scripts/collect_logs.sh
@@ -18,7 +18,7 @@ for pod in `oc get pods -o jsonpath='{.items[*].metadata.name}'`
 do
     for container in `oc get pod $pod -o jsonpath='{.spec.containers[*].name}'`
     do
-        runcmd "oc logs -c $container $pod > ${ARTIFACTS_DIR}/logs/${pod}_${container}.log"
+        runcmd "oc logs $pod $container > ${ARTIFACTS_DIR}/logs/${pod}_${container}.log"
         if [ "$container" == "router" ]; then
             runcmd "oc rsh -c $container $pod qdmanage query --type=address > ${ARTIFACTS_DIR}/logs/${pod}_${container}_router_address.txt"
             runcmd "oc rsh -c $container $pod qdmanage query --type=connection > ${ARTIFACTS_DIR}/logs/${pod}_${container}_router_connection.txt"
