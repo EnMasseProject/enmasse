b37edde68fff52b2ebb6f17210198b286a7a1664
Ulf Lilleengen <lulf@redhat.com>
2017-08-10 09:36:04 +0200

    Ensure that unsubscribe is run on vertx context
    This should fix #107

--- artemis/artemis-shutdown-hook/src/test/java/enmasse/broker/prestop/TestSubscriber.java
+++ artemis/artemis-shutdown-hook/src/test/java/enmasse/broker/prestop/TestSubscriber.java
@@ -89,7 +89,7 @@ public class TestSubscriber implements AutoCloseable {
     }
 
     public void unsubscribe() {
-        connection.close();
+        vertx.runOnContext(v -> connection.close());
     }
 
     @Override
