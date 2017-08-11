6a5aef0f39ad58624509328d83584eca6013b44a
Ulf Lilleengen <lulf@redhat.com>
2017-08-11 08:19:09 +0200

    Revert "Use new syntax for logs within a container"
    This reverts commit 2012b8b8c88d0bd2ca3bb76dc3e37288257b1814.

--- systemtests/scripts/print_logs.sh
+++ systemtests/scripts/print_logs.sh
@@ -15,7 +15,7 @@ for pod in `oc get pods -o jsonpath='{.items[*].metadata.name}'`
 do
     for container in `oc get pod $pod -o jsonpath='{.spec.containers[*].name}'`
     do
-        runcmd "oc logs $pod $container"
+        runcmd "oc logs -c $container $pod"
         if [ "$container" == "router" ]; then
             runcmd "oc rsh -c $container $pod qdmanage query --type=address"
             runcmd "oc rsh -c $container $pod qdmanage query --type=connection"
