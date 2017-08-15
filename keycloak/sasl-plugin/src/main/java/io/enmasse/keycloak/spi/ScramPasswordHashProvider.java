8943625153dcc7cc8d8b5a0b9eba436bb6215da6
rgodfrey <rgodfrey@apache.org>
2017-08-15 11:54:16 +0200

    Update Dockerfile and config to start keycloak with sasl plugin

---
+++ keycloak/sasl-plugin/src/main/java/io/enmasse/keycloak/spi/ScramPasswordHashProvider.java
@@ -0,0 +1,176 @@
+/*
+ * Copyright 2016 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.enmasse.keycloak.spi;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Arrays;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.keycloak.common.util.Base64;
+import org.keycloak.credential.CredentialModel;
+import org.keycloak.credential.hash.PasswordHashProvider;
+import org.keycloak.models.PasswordPolicy;
+import org.keycloak.models.UserCredentialModel;
+
+public class ScramPasswordHashProvider implements PasswordHashProvider
+{
+
+    private final String providerId;
+
+    private final String algorithm;
+    private final String hmacName;
+    private final String digestName;
+    private final int defaultIterations;
+
+    private static final byte[] INT_1 = new byte[]{0, 0, 0, 1};
+
+    public ScramPasswordHashProvider(String providerId,
+                                     String algorithm,
+                                     int defaultIterations,
+                                     final String hmacName, final String digestName) {
+        this.providerId = providerId;
+        this.algorithm = algorithm;
+        this.defaultIterations = defaultIterations;
+        this.hmacName = hmacName;
+        this.digestName = digestName;
+    }
+
+    @Override
+    public boolean policyCheck(PasswordPolicy policy, CredentialModel credential) {
+        return credential.getHashIterations() == policy.getHashIterations() && providerId.equals(credential.getAlgorithm());
+    }
+
+    @Override
+    public void encode(String rawPassword, int iterations, CredentialModel credential) {
+        if (iterations == -1) {
+            iterations = defaultIterations;
+        }
+
+        byte[] salt = getSalt();
+        String encodedPassword = encode(rawPassword, iterations, salt);
+
+        credential.setAlgorithm(providerId);
+        credential.setType(UserCredentialModel.PASSWORD);
+        credential.setSalt(salt);
+        credential.setHashIterations(iterations);
+        credential.setValue(encodedPassword);
+    }
+
+    @Override
+    public boolean verify(String rawPassword, CredentialModel credential) {
+
+        String storedCredential = credential.getValue();
+        String[] storedAndServerKeys = storedCredential.split("|", 2);
+
+        try
+        {
+            byte[] saltedPassword = createSaltedPassword(credential.getSalt(), rawPassword, credential.getHashIterations());
+            byte[] clientKey = computeHmac(saltedPassword, "Client Key");
+
+            byte[] storedKey = MessageDigest.getInstance(getDigestName()).digest(clientKey);
+
+            byte[] serverKey = computeHmac(saltedPassword, "Server Key");
+
+            return (Arrays.equals(Base64.decode(storedAndServerKeys[0]), storedKey)
+               && Arrays.equals(Base64.decode(storedAndServerKeys[1]), serverKey));
+        }
+        catch (IllegalArgumentException | IOException | NoSuchAlgorithmException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void close() {
+    }
+
+    private byte[] createSaltedPassword(byte[] salt, String password, int iterationCount) {
+        Mac mac = createShaHmac(password.getBytes(StandardCharsets.US_ASCII));
+
+        mac.update(salt);
+        mac.update(INT_1);
+        byte[] result = mac.doFinal();
+
+        byte[] previous = null;
+        for(int i = 1; i < iterationCount; i++) {
+            mac.update(previous != null? previous: result);
+            previous = mac.doFinal();
+            for(int x = 0; x < result.length; x++) {
+                result[x] ^= previous[x];
+            }
+        }
+
+        return result;
+
+    }
+
+    private byte[] computeHmac(final byte[] key, final String string) {
+        Mac mac = createShaHmac(key);
+        mac.update(string.getBytes(StandardCharsets.US_ASCII));
+        return mac.doFinal();
+    }
+
+
+    private Mac createShaHmac(final byte[] keyBytes) {
+        try {
+            SecretKeySpec key = new SecretKeySpec(keyBytes, getHmacName());
+            Mac mac = Mac.getInstance(getHmacName());
+            mac.init(key);
+            return mac;
+        } catch (NoSuchAlgorithmException | InvalidKeyException e) {
+            throw new IllegalArgumentException(e.getMessage(), e);
+        }
+    }
+
+    private String getHmacName() {
+        return hmacName;
+    }
+
+    private String getDigestName() {
+        return digestName;
+    }
+
+    private String encode(String rawPassword, int iterations, byte[] salt) {
+        try {
+            byte[] saltedPassword = createSaltedPassword(salt, rawPassword, iterations);
+            byte[] clientKey = computeHmac(saltedPassword, "Client Key");
+
+            byte[] storedKey = MessageDigest.getInstance(getDigestName()).digest(clientKey);
+            byte[] serverKey = computeHmac(saltedPassword, "Server Key");
+
+            return Base64.encodeBytes(storedKey) + "|" + Base64.encodeBytes(serverKey);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private byte[] getSalt() {
+        byte[] buffer = new byte[16];
+        SecureRandom secureRandom = new SecureRandom();
+        secureRandom.nextBytes(buffer);
+        return buffer;
+    }
+
+}
---
+++ keycloak/src/main/xsl/addSaslPlugin.xsl
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+
+<xsl:stylesheet version="2.0"
+                xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+                xmlns:keycloak="urn:jboss:domain:keycloak-server:1.1">
+
+    <xsl:output method="xml" indent="yes"/>
+
+    <xsl:template match="//keycloak:subsystem">
+        <xsl:copy>
+            <xsl:apply-templates select="node()|@*"/>
+            <spi name="amqp">
+                <provider name="amqp-server" enabled="true">
+                    <properties>
+                        <property name="enableTls" value="false"/>
+                        <property name="host" value="0.0.0.0"/>
+                        <property name="port" value="5677"/>
+                        <property name="defaultDomain" value="enmasse"/>
+                    </properties>
+                </provider>
+            </spi>
+        </xsl:copy>
+    </xsl:template>
+
+    <xsl:template match="@*|node()">
+        <xsl:copy>
+            <xsl:apply-templates select="@*|node()"/>
+        </xsl:copy>
+    </xsl:template>
+
+</xsl:stylesheet>
