e5e8d382d85aa1652918aaf966e254caac840a3d
Ulf Lilleengen <lulf@redhat.com>
2017-08-18 09:38:05 +0200

    Cleanup script

--- keycloak/src/main/sh/start-keycloak.sh
+++ keycloak/src/main/sh/start-keycloak.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 KEYCLOAK_DIR=/opt/jboss/keycloak
-KEYCLOAK_CONFIG=${KEYCLOAK_DIR}/standalone/configuration/standalone.xml
-PASSWORD_SECRET=/var/run/keycloak/admin.key
 
-if [ -f "${PASSWORD_SECRET}" ]; then
-    ${KEYCLOAK_DIR}/bin/add-user-keycloak.sh -u admin -p `cat ${PASSWORD_SECRET}`
+if [ -n ${ADMIN_USER} ] && [ -n ${ADMIN_PASSWORD} ]; then
+    ${KEYCLOAK_DIR}/bin/add-user-keycloak.sh -u ${ADMIN_USER} -p ${ADMIN_PASSWORD}
 fi
 
 export JAVA_OPTS="-Dvertx.cacheDirBase=/tmp -Djboss.bind.address=0.0.0.0"
