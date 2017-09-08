f7123f0d1a310555f9335ce055a8d9cde262213f
Ulf Lilleengen <lulf@redhat.com>
2017-09-08 14:37:28 +0200

    Use TLS by default locally

--- systemtests/scripts/ocenv.sh
+++ systemtests/scripts/ocenv.sh
@@ -4,6 +4,7 @@ NAMESPACE=${2-myproject}
 USER=${3-developer}
 
 export OPENSHIFT_USER=$USER
+export OPENSHIFT_USE_TLS=true
 export OPENSHIFT_TOKEN=`oc whoami -t`
 export OPENSHIFT_MASTER_URL=https://${HOST}:8443
 export OPENSHIFT_NAMESPACE=${NAMESPACE}
