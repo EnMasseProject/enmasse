3b51117d5576d5297a1d1b51bf6090166c693a26
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2017-09-11 20:06:01 +0200

    Use built-in docker registry of openshift instance for PR builds

--- systemtests/scripts/install_openshift.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -x
-DEST=$1
-mkdir -p $DEST
-wget https://github.com/openshift/origin/releases/download/v1.4.1/openshift-origin-server-v1.4.1-3f9807a-linux-64bit.tar.gz -O openshift.tar.gz
-tar xzf openshift.tar.gz -C $DEST --strip-components 1
-export PATH="$PATH:$DEST"
