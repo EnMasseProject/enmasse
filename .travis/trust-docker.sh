84dab13b2f4a8b34571b68d5f32be838ab199b09
Ulf Lilleengen <lulf@redhat.com>
2017-09-11 15:59:52 +0200

    Trust local openshift registry

--- .travis/trust-docker.sh
+++ .travis/trust-docker.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
 sudo cat /etc/default/docker
+sudo service docker stop
+sudo sh -c 'echo INSECURE_REGISTRY=\"--insecure-registry 172.30.0.0/16\" >> /etc/default/docker'
+sudo cat /etc/default/docker
+sudo service docker start
