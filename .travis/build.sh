5aef14beb18202a54e25eebd417ce1e6a7ce449b
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2017-09-11 20:06:01 +0200

    Use built-in docker registry of openshift instance for PR builds

--- .travis/build.sh
+++ .travis/build.sh
@@ -6,16 +6,31 @@ if [ "$VERSION" != "latest" ]; then
     export TAG=$VERSION
 fi
 
+if [ "$TRAVIS_BRANCH" != "master" ] || [ "$TRAVIS_PULL_REQUEST" != "false" ]
+then
+    export DOCKER_REGISTRY="172.30.1.1:5000"
+    export DOCKER_ORG=enmasseci
+fi
+
 echo "Building EnMasse"
 make
 
 echo "Tagging Docker Images"
 make docker_tag
 
-echo "Logging in to Docker Hub"
-docker login -u $DOCKER_USER -p $DOCKER_PASS
+if [ "$TRAVIS_BRANCH" != "master" ] || [ "$TRAVIS_PULL_REQUEST" != "false" ]
+then
+    echo "Logging into to local docker registry"
+    oc login -u test -p test --insecure-skip-tls-verify=true https://localhost:8443
+    oc new-project enmasseci
 
-echo "Pushing images to Docker Hub"
+    docker login -u enmasseci -p `oc whoami -t` 172.30.1.1:5000
+else
+    echo "Logging in to Docker Hub"
+    docker login -u $DOCKER_USER -p $DOCKER_PASS
+fi
+
+echo "Pushing images to Docker Registry"
 make docker_push
 
 echo "Running systemtests"
--- .travis/trust-docker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-sudo cat /etc/default/docker
-sudo service docker stop
-sudo sh -c 'echo INSECURE_REGISTRY=\"--insecure-registry 172.30.0.0/16\" >> /etc/default/docker'
-sudo cat /etc/default/docker
-sudo service docker start
