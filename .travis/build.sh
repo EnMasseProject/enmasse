f511cb2aa5f6d2d7f8f24ebf2e6b92367eecffdf
Ulf Lilleengen <lulf@redhat.com>
2017-09-18 12:16:47 +0200

    Skip building and run systemtests directly

--- .travis/build.sh
+++ .travis/build.sh
@@ -13,25 +13,27 @@ then
 fi
 
 echo "Building EnMasse"
-MOCHA_ARGS="--reporter=mocha-junit-reporter" make
-
-echo "Tagging Docker Images"
-make docker_tag
+#MOCHA_ARGS="--reporter=mocha-junit-reporter" make
 #
-if [ "$TRAVIS_BRANCH" != "master" ] || [ "$TRAVIS_PULL_REQUEST" != "false" ]
-then
-    echo "Logging into to local docker registry"
-    oc login -u test -p test
-    oc new-project enmasseci
-    docker login -u enmasseci -p `oc whoami -t` 172.30.1.1:5000
-else
-    make UPLOAD_TAG=latest docker_tag
-    echo "Logging in to Docker Hub"
-    docker login -u $DOCKER_USER -p $DOCKER_PASS
-fi
+#echo "Tagging Docker Images"
+#make docker_tag
+##
+#if [ "$TRAVIS_BRANCH" != "master" ] || [ "$TRAVIS_PULL_REQUEST" != "false" ]
+#then
+#    echo "Logging into to local docker registry"
+#    oc login -u test -p test
+#    oc new-project enmasseci
+#    docker login -u enmasseci -p `oc whoami -t` 172.30.1.1:5000
+#else
+#    make UPLOAD_TAG=latest docker_tag
+#    echo "Logging in to Docker Hub"
+#    docker login -u $DOCKER_USER -p $DOCKER_PASS
+#fi
+#
+#echo "Pushing images to Docker Registry"
+#make docker_push
 
-echo "Pushing images to Docker Registry"
-make docker_push
+export TAG=latest
 
 echo "Running systemtests"
 ./systemtests/scripts/run_test_component.sh templates/install systemtests
