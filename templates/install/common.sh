b73fa72701d557a2f9b1f21b2617789f5f7adace
Ulf Lilleengen <lulf@redhat.com>
2017-09-22 09:56:45 +0200

    Set LC_CTYPE when generating password

--- templates/install/common.sh
+++ templates/install/common.sh
@@ -27,7 +27,7 @@ function tempdir() {
 }
 
 function random_string() {
-    head /dev/urandom | tr -dc A-Za-z0-9 | head -c 32
+    LC_CTYPE=C head /dev/urandom | tr -dc A-Za-z0-9 | head -c 32
 }
 
 function create_csr() {
