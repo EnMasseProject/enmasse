b4275857172436a725ba1b57a5b50399320d8746
Ulf Lilleengen <lulf@redhat.com>
2017-09-23 04:58:08 +0200

    Support custom proton client options for client

--- admin/common-lib/amqp/src/main/java/io/enmasse/amqp/SyncRequestClient.java
+++ admin/common-lib/amqp/src/main/java/io/enmasse/amqp/SyncRequestClient.java
@@ -17,13 +17,13 @@
 package io.enmasse.amqp;
 
 import io.vertx.core.Vertx;
-import io.vertx.proton.ProtonClient;
-import io.vertx.proton.ProtonConnection;
-import io.vertx.proton.ProtonReceiver;
-import io.vertx.proton.ProtonSender;
+import io.vertx.core.net.PemKeyCertOptions;
+import io.vertx.core.net.PemTrustOptions;
+import io.vertx.proton.*;
 import org.apache.qpid.proton.amqp.messaging.Source;
 import org.apache.qpid.proton.message.Message;
 
+import java.io.File;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
@@ -36,15 +36,21 @@ public class SyncRequestClient implements AutoCloseable {
     private final String host;
     private final int port;
     private final Vertx vertx;
+    private final ProtonClientOptions clientOptions;
 
     public SyncRequestClient(String host, int port) {
         this(host, port, Vertx.vertx());
     }
 
     public SyncRequestClient(String host, int port, Vertx vertx) {
+        this(host, port, vertx, new ProtonClientOptions());
+    }
+
+    public SyncRequestClient(String host, int port, Vertx vertx, ProtonClientOptions clientOptions) {
         this.host = host;
         this.port = port;
         this.vertx = vertx;
+        this.clientOptions = clientOptions;
     }
 
     public Message request(Message message, long timeout, TimeUnit timeUnit) throws InterruptedException, TimeoutException, ExecutionException {
@@ -52,7 +58,7 @@ public class SyncRequestClient implements AutoCloseable {
         CompletableFuture<Message> response = new CompletableFuture<>();
 
         ProtonClient client = ProtonClient.create(vertx);
-        client.connect(host, port, connectEvent -> {
+        client.connect(clientOptions, host, port, connectEvent -> {
             if (connectEvent.succeeded()) {
                 ProtonConnection connection = connectEvent.result();
                 connection.open();
