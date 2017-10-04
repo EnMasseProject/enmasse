1be59e4bd723113430d7dfeeee27680f79199388
Ulf Lilleengen <lulf@redhat.com>
2017-10-04 20:56:47 +0200

    Use environment variables to configure openshift client

--- keycloak-controller/src/main/java/io/enmasse/keycloak/controller/Main.java
+++ keycloak-controller/src/main/java/io/enmasse/keycloak/controller/Main.java
@@ -17,16 +17,59 @@ package io.enmasse.keycloak.controller;
 
 import io.enmasse.k8s.api.AddressSpaceApi;
 import io.enmasse.k8s.api.ConfigMapAddressSpaceApi;
+import io.fabric8.kubernetes.client.Config;
+import io.fabric8.kubernetes.client.ConfigBuilder;
 import io.fabric8.openshift.client.DefaultOpenShiftClient;
 import io.fabric8.openshift.client.OpenShiftClient;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Map;
+
 public class Main {
     public static void main(String [] args) throws Exception {
+        Map<String, String> env = System.getenv();
 
-        OpenShiftClient openShiftClient = new DefaultOpenShiftClient();
+        String openshiftUri = String.format("https://%s:%s", getEnvOrThrow(env, "KUBERNETES_SERVICE_HOST"), getEnvOrThrow(env, "KUBERNETES_SERVICE_PORT"));
+
+        Config config = new ConfigBuilder().withMasterUrl(openshiftUri).withOauthToken(getAuthenticationToken()).withNamespace(getNamespace()).build();
+        OpenShiftClient openShiftClient = new DefaultOpenShiftClient(config);
         AddressSpaceApi addressSpaceApi = new ConfigMapAddressSpaceApi(openShiftClient);
         KeycloakManager keycloakManager = new KeycloakManager(new Keycloak(KeycloakParams.fromEnv(System.getenv())));
 
         addressSpaceApi.watchAddressSpaces(keycloakManager);
+    }
+
+    private static String getEnvOrThrow(Map<String, String> env, String envVar) {
+        String var = env.get(envVar);
+        if (var == null) {
+            throw new IllegalArgumentException(String.format("Unable to find value for required environment var '%s'", envVar));
+        }
+        return var;
+    }
+
+    private static final String SERVICEACCOUNT_PATH = "/var/run/secrets/kubernetes.io/serviceaccount";
+
+    private static String getNamespace() throws IOException {
+        String namespace = System.getenv("KUBERNETES_NAMESPACE");
+        if (namespace == null) {
+            return readFile(new File(SERVICEACCOUNT_PATH, "namespace"));
+        } else {
+            return namespace;
+        }
+    }
+
+    private static String getAuthenticationToken() throws IOException {
+        String token = System.getenv("KUBERNETES_TOKEN");
+        if (token == null) {
+            return readFile(new File(SERVICEACCOUNT_PATH, "token"));
+        } else {
+            return token;
+        }
+    }
+
+    private static String readFile(File file) throws IOException {
+        return new String(Files.readAllBytes(file.toPath()));
     }
 }
