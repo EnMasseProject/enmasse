8c5f2238bd039d1d8220cd4de58ba0e7f132eb3a
Tomas Kratky <tkratky@redhat.com>
2017-10-11 10:43:32 +0200

    UPD changed path to persistent-volume.json from 'build' to 'include' because it does not exist in 'build' folder in 'setup openshift' stage

--- systemtests/scripts/provision-storage.sh
+++ systemtests/scripts/provision-storage.sh
@@ -4,4 +4,4 @@ PV_NAME=$2
 
 mkdir -p ${MY_DIR}
 chmod -R 777 ${MY_DIR}
-oc process -f templates/build/persistent-volume.json HOST_DIR=${MY_DIR} NAME=${PV_NAME} | oc create -f -
+oc process -f templates/include/persistent-volume.json HOST_DIR=${MY_DIR} NAME=${PV_NAME} | oc create -f -
