98aa234d83df6c481701d817d137387891fd32e5
Ulf Lilleengen <lulf@redhat.com>
2017-10-30 12:34:44 +0100

    Capture stdout and stderr to separate files

--- .travis/upload-artifacts.sh
+++ .travis/upload-artifacts.sh
@@ -53,7 +53,7 @@ else
         cp $i artifacts/test-reports
     done
 
-    docker logs origin 2>&1 > artifacts/openshift.log
+    docker logs origin > artifacts/openshift.stdout.log 2> artifacts/openshift.stderr.log
 
     cp templates/build/enmasse-${VERSION}.tgz artifacts/
 
