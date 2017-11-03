f9e807f2f36f942248e091953b651d01beeccb77
Ulf Lilleengen <lulf@redhat.com>
2017-11-03 09:10:55 +0100

    Provision storage in setup phase

--- systemtests/scripts/setup-openshift.sh
+++ systemtests/scripts/setup-openshift.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+SCRIPTDIR=`dirname $0`
+
 oc cluster up $OC_CLUSTER_ARGS
 oc login -u system:admin
 oc cluster status
@@ -8,4 +10,13 @@ do
     oc cluster status
 done
 
+
 sleep 30
+
+# Setup persistent storage
+for i in `seq 1 15`
+do
+    echo "Setup persistent storage ${i}"
+    $SCRIPTDIR/provision-storage.sh "/tmp/mydir${i}" "pv-${i}"
+done
+oc get pv
