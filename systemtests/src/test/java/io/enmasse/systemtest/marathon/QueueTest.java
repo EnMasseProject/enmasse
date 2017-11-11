023ed588ce571c22a1d8424b30df49f9f0158fa4
David Kornel <kornys@outlook.com>
2017-11-11 19:03:50 +0100

    added new test case testQueueSendReceiveLong

---
+++ systemtests/src/test/java/io/enmasse/systemtest/marathon/QueueTest.java
@@ -0,0 +1,69 @@
+package io.enmasse.systemtest.marathon;
+
+import io.enmasse.systemtest.*;
+import io.enmasse.systemtest.amqp.AmqpClient;
+import io.enmasse.systemtest.amqp.AmqpClientFactory;
+import org.apache.qpid.proton.message.Message;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+import static org.hamcrest.CoreMatchers.is;
+
+public class QueueTest extends MarathonTestBase{
+
+    @Test
+    public void testQueueSendReceiveLong() throws Exception {
+        AddressSpace addressSpace = new AddressSpace("test-queue-sendreceive-brokered",
+                "test-queue-sendreceive-brokered",
+                AddressSpaceType.BROKERED);
+        createAddressSpace(addressSpace, "none");
+
+        int msgCount = 1000;
+        int queueCount = 10;
+        int senderCount = 10;
+        int recvCount = 20;
+
+        List<Destination> queueList = new ArrayList<>();
+
+        //create queues
+        for(int i = 0; i < queueCount; i++){
+            queueList.add(Destination.queue(String.format("test-queue-sendreceive-%d", i)));
+        }
+        setAddresses(addressSpace, queueList.toArray(new Destination[0]));
+
+        AmqpClientFactory amqpFactory = createAmqpClientFactory(addressSpace);
+
+        List<String> msgBatch = TestUtils.generateMessages(msgCount);
+
+        runTestInLoop(30, () -> {
+            //create client
+            AmqpClient client = amqpFactory.createQueueClient(addressSpace);
+            client.getConnectOptions().setUsername("test").setPassword("test");
+
+            //attach receivers
+            List<Future<List<Message>>> recvResults = new ArrayList<>();
+            for (int i = 0; i < recvCount / 2; i++) {
+                recvResults.add(client.recvMessages(queueList.get(i).getAddress(), msgCount / 2));
+                recvResults.add(client.recvMessages(queueList.get(i).getAddress(), msgCount / 2));
+            }
+
+            //attach senders
+            for(int i = 0; i < senderCount; i++ ) {
+                collector.checkThat(client.sendMessages(queueList.get(i).getAddress(), msgBatch,
+                        10, TimeUnit.MINUTES).get(1, TimeUnit.MINUTES), is(msgBatch.size()));
+            }
+
+            //check received messages
+            for (int i = 0; i < recvCount; i++) {
+                collector.checkThat(recvResults.get(i).get().size(), is(msgCount / 2));
+            }
+
+            client.close();
+            Thread.sleep(2000);
+        });
+    }
+}
