c289f038421875d581a6420e783e5a20aa1d6d65
Tomas Kratky <tkratky@redhat.com>
2017-11-13 13:58:12 +0100

    UPD setup_tck script - fail if address space is not created

--- systemtests/scripts/setup_tck.sh
+++ systemtests/scripts/setup_tck.sh
@@ -9,6 +9,6 @@ DEFAULT_ADDRESS_SPACE=$2
 sudo yum -y install patch
 
 #setup environment
-create_addres_space ${DEFAULT_ADDRESS_SPACE} './systemtests/templates/tckAddressSpace.json'
+create_addres_space ${DEFAULT_ADDRESS_SPACE} './systemtests/templates/tckAddressSpace.json' || return 1
 create_addresses ${DEFAULT_ADDRESS_SPACE} './systemtests/templates/tckAddresses.json'
 create_user ${CLI_ID} './systemtests/templates/tckUser.json' ${DEFAULT_ADDRESS_SPACE}
