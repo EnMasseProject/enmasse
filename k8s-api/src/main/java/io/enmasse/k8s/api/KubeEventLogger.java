4fb0389034fa506b014df39f7ce11a25d9e8553c
Ulf Lilleengen <lulf@redhat.com>
2017-11-23 14:37:13 +0100

    Add java interface for reporting events and a k8s implementation
    * Add use of events in ConfigMapAddressSpaceApi as an example
    
    Issue #465

---
+++ k8s-api/src/main/java/io/enmasse/k8s/api/KubeEventLogger.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2017 Red Hat Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.enmasse.k8s.api;
+
+import io.fabric8.kubernetes.api.model.Event;
+import io.fabric8.kubernetes.api.model.EventBuilder;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClientException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Clock;
+import java.time.Instant;
+
+public class KubeEventLogger implements EventLogger {
+    private static final Logger log = LoggerFactory.getLogger(KubeEventLogger.class);
+    private final KubernetesClient kubeClient;
+    private final String namespace;
+    private final Clock clock;
+    private final String componentName;
+
+    public KubeEventLogger(KubernetesClient kubeClient, String namespace, Clock clock, String componentName) {
+        this.kubeClient = kubeClient;
+        this.namespace = namespace;
+        this.clock = clock;
+        this.componentName = componentName;
+    }
+
+    @Override
+    public void log(String reason, String message, String type) {
+        String eventName = componentName + "." + (reason + message + type).hashCode();
+        Event existing = kubeClient.events().inNamespace(namespace).withName(eventName).get();
+        String timestamp = Instant.now(clock).toString();
+        try {
+            if (existing != null && existing.getType().equals(type) && existing.getReason().equals(reason)) {
+                existing.setCount(existing.getCount() + 1);
+                existing.setLastTimestamp(timestamp);
+                kubeClient.events().inNamespace(namespace).withName(eventName).replace(existing);
+            } else {
+                Event newEvent = new EventBuilder()
+                        .withNewMetadata()
+                        .withName(eventName)
+                        .endMetadata()
+                        .withCount(1)
+                        .withReason(reason)
+                        .withMessage(message)
+                        .withType(type)
+                        .withFirstTimestamp(timestamp)
+                        .withLastTimestamp(timestamp)
+                        .withNewSource()
+                        .withComponent(componentName)
+                        .endSource()
+                        .build();
+                kubeClient.events().inNamespace(namespace).withName(eventName).create(newEvent);
+            }
+        } catch (KubernetesClientException e) {
+            log.warn("Error reporting event", e);
+        }
+    }
+}
---
+++ k8s-api/src/test/java/io/enmasse/k8s/api/KubeEventLoggerTest.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2017 Red Hat Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.enmasse.k8s.api;
+
+import io.fabric8.kubernetes.api.model.DoneableEvent;
+import io.fabric8.kubernetes.api.model.Event;
+import io.fabric8.kubernetes.api.model.EventList;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.dsl.MixedOperation;
+import io.fabric8.kubernetes.client.dsl.Resource;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.startsWith;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class KubeEventLoggerTest {
+    @Test
+    public void testLogger() {
+        String ns = "myspace";
+        String component = "me";
+        Clock clock = Clock.fixed(Instant.ofEpochSecond(10), ZoneId.of("UTC"));
+
+        String message = "it crashed";
+
+        KubernetesClient mockClient = mock(KubernetesClient.class);
+        MixedOperation<Event, EventList, DoneableEvent, Resource<Event, DoneableEvent>> eventOperation = mock(MixedOperation.class);
+        Resource<Event, DoneableEvent> eventResource = mock(Resource.class);
+
+        when(mockClient.events()).thenReturn(eventOperation);
+        when(eventOperation.inNamespace(any())).thenReturn(eventOperation);
+        when(eventOperation.withName(startsWith("me."))).thenReturn(eventResource);
+        when(eventResource.get()).thenReturn(null);
+
+        EventLogger logger = new KubeEventLogger(mockClient, ns, clock, component);
+        logger.log("Deleted", "it crashed", "Error");
+
+        ArgumentCaptor<Event> eventArgumentCaptor = ArgumentCaptor.forClass(Event.class);
+        verify(eventResource).create(eventArgumentCaptor.capture());
+        Event newEvent = eventArgumentCaptor.getValue();
+        assertNotNull(newEvent);
+        assertThat(newEvent.getMessage(), is(message));
+        assertThat(newEvent.getReason(), is("Deleted"));
+        assertThat(newEvent.getType(), is("Error"));
+        assertThat(newEvent.getFirstTimestamp(), is(clock.instant().toString()));
+        assertThat(newEvent.getLastTimestamp(), is(clock.instant().toString()));
+        assertThat(newEvent.getCount(), is(1));
+
+        newEvent.setFirstTimestamp(Instant.ofEpochSecond(5).toString());
+        when(eventResource.get()).thenReturn(newEvent);
+        logger.log("Deleted", "it crashed", "Error");
+
+        eventArgumentCaptor = ArgumentCaptor.forClass(Event.class);
+        verify(eventResource).create(eventArgumentCaptor.capture());
+        newEvent = eventArgumentCaptor.getValue();
+        assertNotNull(newEvent);
+        assertThat(newEvent.getMessage(), is(message));
+        assertThat(newEvent.getReason(), is("Deleted"));
+        assertThat(newEvent.getType(), is("Error"));
+        assertThat(newEvent.getFirstTimestamp(), is(Instant.ofEpochSecond(5).toString()));
+        assertThat(newEvent.getLastTimestamp(), is(clock.instant().toString()));
+        assertThat(newEvent.getCount(), is(2));
+    }
+}
