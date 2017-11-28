fe302075ffc845bfef0afa49bcb15b408e7f2172
Ulf Lilleengen <lulf@redhat.com>
2017-11-28 16:48:41 +0100

    Build versioned jars from release version
    * Specify release.version in a file
    * Update file when creating releases
    * Use version in docker images as well as jar artifacts

--- release-scripts/update-snapshot-version.sh
+++ release-scripts/update-snapshot-version.sh
@@ -4,9 +4,11 @@ VERSION=$1
 git checkout master
 
 mvn versions:set -DnewVersion=${VERSION}
-
+echo $VERSION > release.version
+git status
+echo "Press ENTER to commit ${VERSION}"
+read
 git commit -a -m "Update pom.xml snapshot versions to ${VERSION}"
-
 echo "Press ENTER to push pom changes to GitHub"
 read
 
