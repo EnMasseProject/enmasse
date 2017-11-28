3811b5b5296a98894b09e5e01be55d1a885a72ae
Ulf Lilleengen <lulf@redhat.com>
2017-11-28 10:20:27 +0100

    Set default thread exception handler before starting tests

--- systemtests/src/test/java/io/enmasse/systemtest/marathon/MarathonTestBase.java
+++ systemtests/src/test/java/io/enmasse/systemtest/marathon/MarathonTestBase.java
@@ -2,13 +2,46 @@ package io.enmasse.systemtest.marathon;
 
 import io.enmasse.systemtest.Logging;
 import io.enmasse.systemtest.TestBase;
+import org.apache.http.util.ExceptionUtils;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.rules.ErrorCollector;
 
 import java.util.Date;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class MarathonTestBase extends TestBase {
+
+    @BeforeClass
+    public void initThreadDump() {
+        Logging.log.info("Setting default exception handler for threads");
+        Thread.setDefaultUncaughtExceptionHandler((thread, e) -> {
+            if (e instanceof OutOfMemoryError) {
+                Logging.log.error("Got OOM, dumping thread info");
+                printThreadDump();
+            } else {
+                Logging.log.error("Caught exception {}", e);
+            }
+        });
+    }
+
+    public static void printThreadDump() {
+        Map<Thread, StackTraceElement[]> allThreads = Thread.getAllStackTraces();
+        Iterator<Thread> iterator = allThreads.keySet().iterator();
+        while (iterator.hasNext()) {
+            StringBuilder sb = new StringBuilder();
+            Thread key = iterator.next();
+            StackTraceElement[] trace = allThreads.get(key);
+            sb.append(key + "\r\n");
+            for (int i = 0; i < trace.length; i++) {
+                sb.append(" " + trace[i] + "\r\n");
+            }
+            Logging.log.error(sb.toString());
+        }
+    }
+
 
     @Rule
     public ErrorCollector collector = new ErrorCollector();
