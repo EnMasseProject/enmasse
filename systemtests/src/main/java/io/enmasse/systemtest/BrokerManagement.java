06bd0f5d928ecf4845052d743a4e578b85cc1f33
Tomas Kratky <tkratky@redhat.com>
2017-11-28 13:30:00 +0100

    UPD BrokerManagement - refactored

--- systemtests/src/main/java/io/enmasse/systemtest/BrokerManagement.java
+++ systemtests/src/main/java/io/enmasse/systemtest/BrokerManagement.java
@@ -2,16 +2,15 @@ package io.enmasse.systemtest;
 
 import io.enmasse.systemtest.amqp.AmqpClient;
 
-import java.io.IOException;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeoutException;
 
 public abstract class BrokerManagement {
 
     protected String managementAddress;
+    protected String resourceProperty;
+    protected String operationProperty;
 
-    public abstract List<String> getQueueNames(AmqpClient queueClient, Destination replyQueue, Destination dest) throws Exception;
+    public abstract List<String> getQueueNames(AmqpClient queueClient, Destination replyQueue, String topic) throws Exception;
 
     public abstract int getSubscriberCount(AmqpClient queueClient, Destination replyQueue, String queue) throws Exception;
 
