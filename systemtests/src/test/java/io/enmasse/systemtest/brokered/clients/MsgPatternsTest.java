b710080238eec6de1e0794b513036a5bb1b3105c
Tomas Kratky <tkratky@redhat.com>
2017-11-28 15:19:38 +0100

    UPD MsgPatternsTest - timeout for subscriber increased from 1 to 30s

--- systemtests/src/test/java/io/enmasse/systemtest/brokered/clients/MsgPatternsTest.java
+++ systemtests/src/test/java/io/enmasse/systemtest/brokered/clients/MsgPatternsTest.java
@@ -221,7 +221,7 @@ public class MsgPatternsTest extends ClientTestBase {
 
         //set up subscriber3
         arguments.put(Argument.SELECTOR, "a AND b");
-        arguments.put(Argument.TIMEOUT, "1");
+        arguments.put(Argument.TIMEOUT, "30"); //because this client should receive no messages
         subscriber3.setArguments(arguments);
 
         Future<Boolean> result1 = subscriber.runAsync();
