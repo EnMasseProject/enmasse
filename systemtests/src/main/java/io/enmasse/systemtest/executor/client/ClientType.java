741d27b85fbf7c30a84a0f371cea4a070ecb4ad8
David Kornel <kornys@outlook.com>
2017-11-28 12:00:12 +0100

    FIX wrong openwire client bind

--- systemtests/src/main/java/io/enmasse/systemtest/executor/client/ClientType.java
+++ systemtests/src/main/java/io/enmasse/systemtest/executor/client/ClientType.java
@@ -36,9 +36,9 @@ public enum ClientType {
             case CLI_JAVA_ARTEMIS_JMS_RECEIVER:
                 return "./client_executable/cli-artemis-jms.jar";
             case CLI_JAVA_OPENWIRE_JMS_SENDER:
-                return "./client_executable/cli-artemis-jms.jar";
+                return "./client_executable/cli-activemq.jar";
             case CLI_JAVA_OPENWIRE_JMS_RECEIVER:
-                return "./client_executable/cli-artemis-jms.jar";
+                return "./client_executable/cli-activemq.jar";
             default:
                 return "";
         }
