1452c4c1d08558e9db1e2bd04096eeeb0d9cf04e
Tomas Kratky <tkratky@redhat.com>
2017-11-29 13:27:14 +0100

    FIX Test bases - catch of test failure moved into SystemTestRunListener

--- systemtests/src/test/java/io/enmasse/systemtest/marathon/MarathonTestBase.java
+++ systemtests/src/test/java/io/enmasse/systemtest/marathon/MarathonTestBase.java
@@ -2,46 +2,13 @@ package io.enmasse.systemtest.marathon;
 
 import io.enmasse.systemtest.Logging;
 import io.enmasse.systemtest.TestBase;
-import org.apache.http.util.ExceptionUtils;
-import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.rules.ErrorCollector;
 
 import java.util.Date;
-import java.util.Iterator;
-import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class MarathonTestBase extends TestBase {
-
-    @BeforeClass
-    public static void initThreadDump() {
-        Logging.log.info("Setting default exception handler for threads");
-        Thread.setDefaultUncaughtExceptionHandler((thread, e) -> {
-            if (e instanceof OutOfMemoryError) {
-                Logging.log.error("Got OOM, dumping thread info");
-                printThreadDump();
-            } else {
-                Logging.log.error("Caught exception {}", e);
-            }
-        });
-    }
-
-    public static void printThreadDump() {
-        Map<Thread, StackTraceElement[]> allThreads = Thread.getAllStackTraces();
-        Iterator<Thread> iterator = allThreads.keySet().iterator();
-        while (iterator.hasNext()) {
-            StringBuilder sb = new StringBuilder();
-            Thread key = iterator.next();
-            StackTraceElement[] trace = allThreads.get(key);
-            sb.append(key + "\r\n");
-            for (int i = 0; i < trace.length; i++) {
-                sb.append(" " + trace[i] + "\r\n");
-            }
-            Logging.log.error(sb.toString());
-        }
-    }
-
 
     @Rule
     public ErrorCollector collector = new ErrorCollector();
@@ -49,16 +16,16 @@ public class MarathonTestBase extends TestBase {
     protected void runTestInLoop(int durationMinutes, TestLoop test) {
         Logging.log.info(String.format("Starting test running for %d minutes at %s",
                 durationMinutes, new Date().toString()));
-        for(long stop = System.nanoTime() + TimeUnit.MINUTES.toNanos(durationMinutes); stop > System.nanoTime();) {
+        for (long stop = System.nanoTime() + TimeUnit.MINUTES.toNanos(durationMinutes); stop > System.nanoTime(); ) {
             try {
                 test.run();
-            }catch (Exception ex){
+            } catch (Exception ex) {
                 collector.addError(ex);
             }
         }
     }
 
-    protected interface TestLoop{
+    protected interface TestLoop {
         void run() throws Exception;
     }
 }
