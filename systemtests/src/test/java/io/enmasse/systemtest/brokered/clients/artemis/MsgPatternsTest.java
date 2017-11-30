f59b56225003e8e257ba676a60baeebf59efff7b
David Kornel <kornys@outlook.com>
2017-11-30 13:39:43 +0100

    Disable core and openwire tests

--- systemtests/src/test/java/io/enmasse/systemtest/brokered/clients/artemis/MsgPatternsTest.java
+++ systemtests/src/test/java/io/enmasse/systemtest/brokered/clients/artemis/MsgPatternsTest.java
@@ -6,37 +6,38 @@ import org.junit.Test;
 
 public class MsgPatternsTest extends io.enmasse.systemtest.brokered.clients.MsgPatternsTest {
 
-    @Test
+    //Disabled until we create mechanism for fetch clients certificates
+    //@Test
     public void testBasicMessage() throws Exception {
         doBasicMessageTest(new ArtemisJMSClientSender(), new ArtemisJMSClientReceiver());
     }
 
-    @Test
+    //@Test
     public void testRoundRobinReceiver() throws Exception {
         doRoundRobinReceiverTest(new ArtemisJMSClientSender(), new ArtemisJMSClientReceiver(), new ArtemisJMSClientReceiver());
     }
 
-    @Test
+    //@Test
     public void testTopicSubscribe() throws Exception {
         doTopicSubscribeTest(new ArtemisJMSClientSender(), new ArtemisJMSClientReceiver(), new ArtemisJMSClientReceiver(), true);
     }
 
-    @Test
+    //@Test
     public void testMessageBrowse() throws Exception {
         doMessageBrowseTest(new ArtemisJMSClientSender(), new ArtemisJMSClientReceiver(), new ArtemisJMSClientReceiver());
     }
 
-    @Test
+    //@Test
     public void testDrainQueue() throws Exception {
         doDrainQueueTest(new ArtemisJMSClientSender(), new ArtemisJMSClientReceiver());
     }
 
-    @Test
+    //@Test
     public void testMessageSelectorQueue() throws Exception{
         doMessageSelectorQueueTest(new ArtemisJMSClientSender(), new ArtemisJMSClientReceiver());
     }
 
-    @Test
+    //@Test
     public void testMessageSelectorTopic() throws Exception{
         doMessageSelectorTopicTest(new ArtemisJMSClientSender(), new ArtemisJMSClientReceiver(),
                 new ArtemisJMSClientReceiver(), new ArtemisJMSClientReceiver(), true);
