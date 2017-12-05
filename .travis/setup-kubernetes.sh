1128e3252372a1599ca534212153bf9f5590c98a
Ulf Lilleengen <lulf@redhat.com>
2017-12-05 17:31:16 +0100

    Copy kubectl to /usr/bin

--- .travis/setup-kubernetes.sh
+++ .travis/setup-kubernetes.sh
@@ -2,6 +2,8 @@
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube
 curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x kubectl
 
+sudo cp kubectl /usr/bin
+
 export MINIKUBE_WANTUPDATENOTIFICATION=false
 export MINIKUBE_WANTREPORTERRORPROMPT=false
 export MINIKUBE_HOME=$HOME
@@ -9,17 +11,8 @@ export CHANGE_MINIKUBE_NONE_USER=true
 mkdir $HOME/.kube || true
 touch $HOME/.kube/config
 
-docker run -d -p 5000:5000 registry
+#docker run -d -p 5000:5000 registry
 
 export KUBECONFIG=$HOME/.kube/config
 sudo -E ./minikube start --vm-driver=none --insecure-registry localhost:5000
-
-# this for loop waits until kubectl can access the api server that Minikube has created
-for i in {1..150}; do # timeout for 5 minutes
-   ./kubectl get po &> /dev/null
-   if [ $? -ne 1 ]; then
-      break
-  fi
-  sleep 2
-done
-
+sudo -E ./minikube addons enable default-storageclass
