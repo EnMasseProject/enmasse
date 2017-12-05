924372acc7f47d4efec62d678af6f86a174668e4
Ulf Lilleengen <lulf@redhat.com>
2017-12-05 17:14:53 +0100

    Try to run tests using minikube

--- .travis/build.sh
+++ .travis/build.sh
@@ -6,7 +6,6 @@ COMMIT=${COMMIT:-latest}
 VERSION=`cat release.version`
 TAG=${TAG:-latest}
 DOCKER_ORG=${DOCKER_ORG:-$USER}
-OPENSHIFT_KUBECONFIG=${OPENSHIFT_KUBECONFIG:-/tmp/openshift/config/master/admin.kubeconfig}
 SYSTEMTEST_ARGS=${SYSTEMTEST_ARGS:-SmokeTest}
 
 if [ "$TAG" != "latest" ]; then
@@ -15,23 +14,20 @@ fi
 
 if [ "$BRANCH" != "master" ] && [ "$BRANCH" != "$VERSION" ] || [ "$PULL_REQUEST" != "false" ]
 then
-    export DOCKER_REGISTRY="172.30.1.1:5000"
-    export DOCKER_ORG=enmasseci
+    export DOCKER_REGISTRY="localhost:5000"
 fi
 
+export MOCHA_ARGS="--reporter=mocha-junit-reporter"
+
 echo "Building EnMasse with tag $TAG, commit $COMMIT, version $VERSION from $BRANCH. PR: $PULL_REQUEST"
-MOCHA_ARGS="--reporter=mocha-junit-reporter" make
+make
 
 echo "Tagging Docker Images"
 make TAG=$COMMIT docker_tag
 #
 if [ "$BRANCH" != "master" ] && [ "$BRANCH" != "$TAG" ] || [ "$PULL_REQUEST" != "false" ]
 then
-    echo "Logging into to local docker registry"
-    oc login -u test -p test --insecure-skip-tls-verify=true https://localhost:8443
-    oc new-project enmasseci
-
-    docker login -u enmasseci -p `oc whoami -t` 172.30.1.1:5000
+    echo "Using local registry"
 else
     make docker_tag
     echo "Logging in to Docker Hub"
@@ -42,4 +38,4 @@ echo "Pushing images to Docker Registry"
 make TAG=$COMMIT docker_push
 
 echo "Running systemtests"
-./systemtests/scripts/run_test_component.sh templates/install ${OPENSHIFT_KUBECONFIG} systemtests ${SYSTEMTEST_ARGS}
+./systemtests/scripts/run_test_kubernetes.sh templates/install systemtests ${SYSTEMTEST_ARGS}
---
+++ systemtests/scripts/run_test_kubernetes.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+CURDIR=`readlink -f \`dirname $0\``
+source ${CURDIR}/test_func.sh
+
+ENMASSE_DIR=$1
+SYSTEMTESTS=$2
+TESTCASE=$3
+failure=0
+
+SANITIZED_PROJECT=$OPENSHIFT_PROJECT
+SANITIZED_PROJECT=${SANITIZED_PROJECT//_/-}
+SANITIZED_PROJECT=${SANITIZED_PROJECT//\//-}
+export OPENSHIFT_PROJECT=$SANITIZED_PROJECT
+
+kubectl create namespace $OPENSHIFT_PROJECT
+kubectl config set-context $(kubectl config current-context) --namespace=$OPENSHIFT_PROJECT
+
+${ENMASSE_DIR}/deploy-kubernetes.sh -n $OPENSHIFT_PROJECT -o multitenant -a "none standard"
+
+#environment info
+LOG_DIR="${ARTIFACTS_DIR}/kubernetes-info/"
+mkdir -p ${LOG_DIR}
+get_kubernetes_info ${LOG_DIR} services default "-before"
+get_kubernetes_info ${LOG_DIR} pods default "-before"
+
+run_test ${TESTCASE} || failure=$(($failure + 1))
+
+#environment info
+get_kubernetes_info ${LOG_DIR} pv ${OPENSHIFT_PROJECT}
+get_kubernetes_info ${LOG_DIR} pods ${OPENSHIFT_PROJECT} 
+get_kubernetes_info ${LOG_DIR} services default "-after"
+get_kubernetes_info ${LOG_DIR} pods default "-after"
+get_kubernetes_info ${LOG_DIR} events ${OPENSHIFT_PROJECT}
+
+#store artifacts
+$CURDIR/collect_logs.sh ${ARTIFACTS_DIR}
+
+if [ $failure -gt 0 ]
+then
+    echo "Systemtests failed"
+    exit 1
+else
+    teardown_test $OPENSHIFT_PROJECT
+fi
