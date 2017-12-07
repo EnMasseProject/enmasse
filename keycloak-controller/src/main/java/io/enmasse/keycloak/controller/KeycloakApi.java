5b79c9d3680a048fc6695da8c1e35ee7b3f2057c
Ulf Lilleengen <lulf@redhat.com>
2017-12-07 15:39:53 +0100

    Create realm admin user with random credentials

--- keycloak-controller/src/main/java/io/enmasse/keycloak/controller/KeycloakApi.java
+++ keycloak-controller/src/main/java/io/enmasse/keycloak/controller/KeycloakApi.java
@@ -19,6 +19,6 @@ import java.util.Set;
 
 public interface KeycloakApi {
     Set<String> getRealmNames();
-    void createRealm(String realmName);
+    void createRealm(String realmName, String realmAdminUser, String realmAdminPassword);
     void deleteRealm(String realmName);
 }
