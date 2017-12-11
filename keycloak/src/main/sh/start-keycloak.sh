5674bf38c79bc55b3050ea23f5d0ab2af21efd7a
Vanessa Busch <vbusch@redhat.com>
2017-12-11 18:26:00 -0500

    Common cert deploy directory.
    To keep mount points for the upstream and downstream templates consistent, created a cert dif in /opt/enmasse.

--- keycloak/src/main/sh/start-keycloak.sh
+++ keycloak/src/main/sh/start-keycloak.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 KEYSTORE_PATH=${JBOSS_HOME}/standalone/configuration/certificates.keystore
-CERT_PATH=${JBOSS_HOME}/standalone/cert
+CERT_PATH=/opt/enmasse/cert
 
 openssl pkcs12 -export -passout pass:enmasse -in ${CERT_PATH}/tls.crt -inkey ${CERT_PATH}/tls.key -name "server" -out /tmp/certificates-keystore.p12
 keytool -importkeystore -srcstorepass enmasse -deststorepass enmasse -destkeystore ${KEYSTORE_PATH} -srckeystore /tmp/certificates-keystore.p12 -srcstoretype PKCS12
