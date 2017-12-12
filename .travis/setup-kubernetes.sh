792aaa5f3571159bafb94db442f9de42854af944
Ulf Lilleengen <lulf@redhat.com>
2017-12-12 12:28:58 +0100

    Add minikube to path

--- .travis/setup-kubernetes.sh
+++ .travis/setup-kubernetes.sh
@@ -2,7 +2,7 @@
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube
 curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x kubectl
 
-sudo cp kubectl /usr/bin
+sudo cp minikube kubectl /usr/bin
 
 export MINIKUBE_WANTUPDATENOTIFICATION=false
 export MINIKUBE_WANTREPORTERRORPROMPT=false
@@ -14,5 +14,5 @@ touch $HOME/.kube/config
 docker run -d -p 5000:5000 registry
 
 export KUBECONFIG=$HOME/.kube/config
-sudo -E ./minikube start --vm-driver=none --insecure-registry localhost:5000
-sudo -E ./minikube addons enable default-storageclass
+sudo -E minikube start --vm-driver=none --insecure-registry localhost:5000
+sudo -E minikube addons enable default-storageclass
