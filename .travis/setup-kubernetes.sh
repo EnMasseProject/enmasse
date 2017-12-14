04fc947aa68fa08b2da6f64f6d368e2c906df06c
Ulf Lilleengen <lulf@redhat.com>
2017-12-14 20:58:27 +0100

    Add oc tool for some useful debug info

--- .travis/setup-kubernetes.sh
+++ .travis/setup-kubernetes.sh
@@ -2,6 +2,11 @@
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube
 curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x kubectl
 
+mkdir -p /tmp/openshift
+wget https://github.com/openshift/origin/releases/download/v3.7.0/openshift-origin-client-tools-v3.7.0-7ed6862-linux-64bit.tar.gz -O openshift.tar.gz
+tar xzf openshift.tar.gz -C /tmp/openshift --strip-components 1  
+sudo cp /tmp/openshift/oc /usr/bin
+
 sudo cp minikube kubectl /usr/bin
 
 export MINIKUBE_WANTUPDATENOTIFICATION=false
