ee9a05c435d6467c630e51697ca419c9943b2755
Tomas Kratky <tkratky@redhat.com>
2017-12-19 10:49:21 +0100

    NEW webconsole tests for standard address space

---
+++ systemtests/src/test/java/io/enmasse/systemtest/standard/web/ChromeWebConsoleTest.java
@@ -0,0 +1,82 @@
+package io.enmasse.systemtest.standard.web;
+
+import io.enmasse.systemtest.Destination;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class ChromeWebConsoleTest extends StandardWebConsoleTest {
+
+    //@Test
+    public void testCreateDeleteQueue() throws Exception {
+        doTestCreateDeleteAddress(Destination.queue("test-queue"));
+    }
+
+    //@Test
+    public void testCreateDeleteTopic() throws Exception {
+        doTestCreateDeleteAddress(Destination.topic("test-topic"));
+    }
+
+    //@Test
+    public void testCreateDeleteAnycast() throws Exception {
+        doTestCreateDeleteAddress(Destination.anycast("test-anycast-firefox"));
+    }
+
+    //@Test
+    public void testCreateDeleteMulticast() throws Exception {
+        doTestCreateDeleteAddress(Destination.multicast("test-multicast-firefox"));
+    }
+
+    //@Test
+    public void testFilterAddressesByType() throws Exception {
+        doTestFilterAddressesByType();
+    }
+
+    //@Test
+    public void testFilterAddressesByName() throws Exception {
+        doTestFilterAddressesByName();
+    }
+
+    //@Test
+    public void testSortAddressesByName() throws Exception {
+        doTestSortAddressesByName();
+    }
+
+    //@Test
+    public void testSortConnectionsBySenders() throws Exception {
+        doTestSortConnectionsBySenders();
+    }
+
+    //@Test
+    public void testSortConnectionsByReceivers() throws Exception {
+        doTestSortConnectionsByReceivers();
+    }
+
+    //@Test
+    public void testFilterConnectionsByEncrypted() throws Exception {
+        doTestFilterConnectionsByEncrypted();
+    }
+
+    //@Test
+    public void testFilterConnectionsByUser() throws Exception {
+        doTestFilterConnectionsByUser();
+    }
+
+    //@Test
+    public void testFilterConnectionsByHostname() throws Exception {
+        doTestFilterConnectionsByHostname();
+    }
+
+    //@Test
+    public void testSortConnectionsByHostname() throws Exception {
+        doTestSortConnectionsByHostname();
+    }
+
+    @Override
+    public WebDriver buildDriver() {
+        ChromeOptions opts = new ChromeOptions();
+        opts.setHeadless(true);
+        opts.addArguments("--no-sandbox");
+        return new ChromeDriver(opts);
+    }
+}
---
+++ systemtests/src/test/java/io/enmasse/systemtest/standard/web/FirefoxWebConsoleTest.java
@@ -0,0 +1,88 @@
+package io.enmasse.systemtest.standard.web;
+
+import io.enmasse.systemtest.Destination;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+public class FirefoxWebConsoleTest extends StandardWebConsoleTest {
+
+
+    @Test
+    public void testCreateDeleteQueue() throws Exception {
+        doTestCreateDeleteAddress(Destination.queue("test-queue"));
+    }
+
+    @Test
+    public void testCreateDeleteTopic() throws Exception {
+        doTestCreateDeleteAddress(Destination.topic("test-topic"));
+    }
+
+    @Test
+    public void testCreateDeleteAnycast() throws Exception {
+        doTestCreateDeleteAddress(Destination.anycast("test-anycast-firefox"));
+    }
+
+    @Test
+    public void testCreateDeleteMulticast() throws Exception {
+        doTestCreateDeleteAddress(Destination.multicast("test-multicast-firefox"));
+    }
+
+    @Test
+    public void testFilterAddressesByType() throws Exception {
+        doTestFilterAddressesByType();
+    }
+
+    @Test
+    public void testFilterAddressesByName() throws Exception {
+        doTestFilterAddressesByName();
+    }
+
+    @Test
+    public void testSortAddressesByName() throws Exception {
+        doTestSortAddressesByName();
+    }
+
+    @Test
+    public void testSortAddressesByClients() throws Exception {
+        doTestSortAddressesByClients();
+    }
+
+    @Test
+    public void testSortConnectionsBySenders() throws Exception {
+        doTestSortConnectionsBySenders();
+    }
+
+    @Test
+    public void testSortConnectionsByReceivers() throws Exception {
+        doTestSortConnectionsByReceivers();
+    }
+
+    @Test
+    public void testFilterConnectionsByEncrypted() throws Exception {
+        doTestFilterConnectionsByEncrypted();
+    }
+
+    @Test
+    public void testFilterConnectionsByUser() throws Exception {
+        doTestFilterConnectionsByUser();
+    }
+
+    @Test
+    public void testFilterConnectionsByHostname() throws Exception {
+        doTestFilterConnectionsByHostname();
+    }
+
+    @Test
+    public void testSortConnectionsByHostname() throws Exception {
+        doTestSortConnectionsByHostname();
+    }
+
+    @Override
+    public WebDriver buildDriver() {
+        FirefoxOptions opts = new FirefoxOptions();
+        opts.setHeadless(true);
+        return new FirefoxDriver(opts);
+    }
+}
