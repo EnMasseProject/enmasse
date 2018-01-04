673af7df5e4ef401807245da62079287ecff5a16
Gordon Sim <gsim@redhat.com>
2018-01-04 08:59:49 +0000

    further null-check with some debug output

--- address-controller/src/main/java/io/enmasse/controller/auth/AuthController.java
+++ address-controller/src/main/java/io/enmasse/controller/auth/AuthController.java
@@ -46,10 +46,14 @@ public class AuthController {
     }
 
     public void issueExternalCertificates(AddressSpace addressSpace) throws Exception {
-        for (Endpoint endpoint : addressSpace.getEndpoints()) {
-            if (endpoint != null && endpoint.getCertProvider().isPresent()) {
-                String secretName = endpoint.getCertProvider().get().getSecretName();
-                certManager.issueRouteCert(secretName, addressSpace.getNamespace());
+        if (addressSpace == null) {
+            log.info("AuthController.issueExternalCertificates() invoked with null AddressSpace");
+        } else {
+            for (Endpoint endpoint : addressSpace.getEndpoints()) {
+                if (endpoint != null && endpoint.getCertProvider().isPresent()) {
+                    String secretName = endpoint.getCertProvider().get().getSecretName();
+                    certManager.issueRouteCert(secretName, addressSpace.getNamespace());
+                }
             }
         }
     }
