a3c7555b97f8e860a4bd5dbc0cb1c84f1b88a864
Tomas Kratky <tkratky@redhat.com>
2018-01-12 13:17:49 +0100

    UPD store_kubernetes_info - store logs of terminated pods from "default"

--- systemtests/scripts/store_kubernetes_info.sh
+++ systemtests/scripts/store_kubernetes_info.sh
@@ -7,10 +7,13 @@ OPENSHIFT_PROJECT=${2}
 
 #environment info
 get_previous_logs ${LOG_DIR} ${OPENSHIFT_PROJECT}
+get_previous_logs ${LOG_DIR} "default"
+
 get_kubernetes_info ${LOG_DIR} pv ${OPENSHIFT_PROJECT}
 get_kubernetes_info ${LOG_DIR} pods ${OPENSHIFT_PROJECT}
 get_kubernetes_info ${LOG_DIR} services default "-after"
 get_kubernetes_info ${LOG_DIR} pods default "-after"
+
 get_docker_info ${LOG_DIR} origin
 
 
