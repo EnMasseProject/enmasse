589a2a74f7f8a3275fc3865800d001867834028d
Ulf Lilleengen <lulf@redhat.com>
2018-01-16 12:18:34 +0100

    User initializer pod to configure keycloak
    * This separates concerns of configuring keycloak for EnMasse and using
    a supported image for keycloak
    * Rename image from keycloak to keycloak-initializer
    * Store configuration in repository instead of using XSL to transform
    
    This fixes #730

--- keycloak-plugin/src/main/sh/init-keycloak.sh
+++ keycloak-plugin/src/main/sh/init-keycloak.sh
@@ -1,8 +1,11 @@
-#!/bin/bash
-KEYSTORE_PATH=${JBOSS_HOME}/standalone/configuration/certificates.keystore
+#!/bin/sh
+KEYCLOAK_CONFIG=${KEYCLOAK_DIR}/standalone/configuration/
+
+cp ${KEYCLOAK_PLUGIN_DIR}/configuration/* ${KEYCLOAK_CONFIG}/
+cp ${KEYCLOAK_PLUGIN_DIR}/providers/* ${KEYCLOAK_DIR}/providers/
+
+KEYSTORE_PATH=${KEYCLOAK_DIR}/standalone/configuration/certificates.keystore
 CERT_PATH=/opt/enmasse/cert
 
 openssl pkcs12 -export -passout pass:enmasse -in ${CERT_PATH}/tls.crt -inkey ${CERT_PATH}/tls.key -name "server" -out /tmp/certificates-keystore.p12
 keytool -importkeystore -srcstorepass enmasse -deststorepass enmasse -destkeystore ${KEYSTORE_PATH} -srckeystore /tmp/certificates-keystore.p12 -srcstoretype PKCS12
-
-/opt/jboss/docker-entrypoint.sh $@
