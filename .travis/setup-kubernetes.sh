20d87d0f7159e3d03b928d7d6cb30b60ea9dced4
Ulf Lilleengen <lulf@redhat.com>
2018-01-27 00:53:29 +0100

    Hardcode to older kube version

--- .travis/setup-kubernetes.sh
+++ .travis/setup-kubernetes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube
-curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x kubectl
+curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.8.7/bin/linux/amd64/kubectl && chmod +x kubectl
 
 mkdir -p /tmp/openshift
 wget https://github.com/openshift/origin/releases/download/v3.7.0/openshift-origin-client-tools-v3.7.0-7ed6862-linux-64bit.tar.gz -O openshift.tar.gz
