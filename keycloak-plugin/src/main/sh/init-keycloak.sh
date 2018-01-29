2b1eb9db41ef2c74deed3f94fff65c078e3df9af
Ulf Lilleengen <lulf@redhat.com>
2018-01-29 09:35:17 +0100

    Remove keystore if already exists (i.e. reusing previously created volume)
    This fixes #815

--- keycloak-plugin/src/main/sh/init-keycloak.sh
+++ keycloak-plugin/src/main/sh/init-keycloak.sh
@@ -7,5 +7,6 @@ cp ${KEYCLOAK_PLUGIN_DIR}/providers/* ${KEYCLOAK_DIR}/providers/
 KEYSTORE_PATH=${KEYCLOAK_DIR}/standalone/configuration/certificates.keystore
 CERT_PATH=/opt/enmasse/cert
 
+rm -f ${KEYSTORE_PATH}
 openssl pkcs12 -export -passout pass:enmasse -in ${CERT_PATH}/tls.crt -inkey ${CERT_PATH}/tls.key -name "server" -out /tmp/certificates-keystore.p12
 keytool -importkeystore -srcstorepass enmasse -deststorepass enmasse -destkeystore ${KEYSTORE_PATH} -srckeystore /tmp/certificates-keystore.p12 -srcstoretype PKCS12
