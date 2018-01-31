0a998b9dc831e282e89d1193aea0cfc4a2bdcab5
Ulf Lilleengen <lulf@redhat.com>
2018-01-31 12:21:56 +0100

    Don't fail if no tests are executed

--- systemtests/scripts/test_func.sh
+++ systemtests/scripts/test_func.sh
@@ -48,8 +48,8 @@ function run_test() {
     if [ -n "${TESTCASE}" ]; then
         EXTRA_TEST_ARGS="-Dtest=${TESTCASE}"
     fi
-    mvn test -pl systemtests -Psystemtests-shared -Djava.net.preferIPv4Stack=true ${EXTRA_TEST_ARGS}
-    mvn test -pl systemtests -Psystemtests-isolated -Djava.net.preferIPv4Stack=true ${EXTRA_TEST_ARGS}
+    mvn test -pl systemtests -Psystemtests-shared -Djava.net.preferIPv4Stack=true -DfailIfNoTests=false ${EXTRA_TEST_ARGS}
+    mvn test -pl systemtests -Psystemtests-isolated -Djava.net.preferIPv4Stack=true -DfailIfNoTests=false ${EXTRA_TEST_ARGS}
 }
 
 function teardown_test() {
