b97b7d32e0d781cf02ef19487bbff44845ec4adc
Ulf Lilleengen <lulf@redhat.com>
2018-02-01 11:52:21 +0100

    Fix bad merge

--- systemtests/src/test/java/io/enmasse/systemtest/brokered/QueueTest.java
+++ systemtests/src/test/java/io/enmasse/systemtest/brokered/QueueTest.java
@@ -23,13 +23,8 @@ public class QueueTest extends BrokeredTestBase {
      */
     @Test
     public void messageGroupTest() throws Exception {
-<<<<<<< HEAD
         Destination dest = Destination.queue("messageGroupQueue", getDefaultPlan(AddressType.QUEUE));
-        setAddresses(defaultAddressSpace, dest);
-=======
-        Destination dest = Destination.queue("messageGroupQueue");
         setAddresses(sharedAddressSpace, dest);
->>>>>>> UPD TestBaseWithDefault - renamed to TestBaseWithShared
 
         AmqpClient client = amqpClientFactory.createQueueClient(sharedAddressSpace);
 
