90f3ae9843f29fd0f10c083f286d3271e9b00ae4
Tomas Kratky <tkratky@redhat.com>
2018-02-02 14:44:04 +0100

    UPD more debug

--- systemtests/scripts/enable_core_dumps.sh
+++ systemtests/scripts/enable_core_dumps.sh
@@ -6,6 +6,7 @@ mkdir -p ${CORE_DUMPS_LOCATION}
 chmod a+rwx ${CORE_DUMPS_LOCATION}
 echo "${CORE_DUMPS_LOCATION}/core.%e.%p.%h.%t" > /proc/sys/kernel/core_pattern
 
+echo "foo bar baz" > "${CORE_DUMPS_LOCATION}/core.test"
 firefox --display=${DISPLAY} &
 pid_fire=$!
 kill -s 3 ${pid_fire}
