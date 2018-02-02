b919fd3d20e057f3ed409cd9c3d5d0b6c3c00ab6
Tomas Kratky <tkratky@redhat.com>
2018-02-02 13:38:04 +0100

    Debug coredumps

--- systemtests/scripts/enable_core_dumps.sh
+++ systemtests/scripts/enable_core_dumps.sh
@@ -5,3 +5,8 @@ ulimit -c unlimited
 mkdir -p ${CORE_DUMPS_LOCATION}
 chmod a+rwx ${CORE_DUMPS_LOCATION}
 echo "${CORE_DUMPS_LOCATION}/core.%e.%p.%h.%t" > /proc/sys/kernel/core_pattern
+
+firefox --display=${DISPLAY}&
+pid_fire=$!
+kill -s 3 ${pid_fire}
+sleep 5
