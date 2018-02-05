c14f863431e1d35bb9e5aac03d4621c03d73a26d
Tomas Kratky <tkratky@redhat.com>
2018-02-05 23:12:59 +0100

    fixup - back to normal

--- systemtests/scripts/enable_core_dumps.sh
+++ systemtests/scripts/enable_core_dumps.sh
@@ -5,9 +5,3 @@ ulimit -c unlimited
 mkdir -p ${CORE_DUMPS_LOCATION}
 chmod a+rwx ${CORE_DUMPS_LOCATION}
 echo "${CORE_DUMPS_LOCATION}/core.%e.%p.%h.%t" > /proc/sys/kernel/core_pattern
-
-echo "foo bar baz" > "${CORE_DUMPS_LOCATION}/core.test"
-firefox --display=${DISPLAY} &
-pid_fire=$!
-kill -s 11 ${pid_fire}
-sleep 5
