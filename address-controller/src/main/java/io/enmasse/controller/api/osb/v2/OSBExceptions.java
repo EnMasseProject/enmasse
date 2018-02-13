97474b4533560b8daf29a4e080601f89a9d60639
Ulf Lilleengen <lulf@redhat.com>
2018-02-13 15:00:59 +0100

    Update copyright

--- address-controller/src/main/java/io/enmasse/controller/api/osb/v2/OSBExceptions.java
+++ address-controller/src/main/java/io/enmasse/controller/api/osb/v2/OSBExceptions.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright 2018, EnMasse authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
 package io.enmasse.controller.api.osb.v2;
 
 import io.enmasse.controller.common.exceptionmapping.ErrorResponse;
