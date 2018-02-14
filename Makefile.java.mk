33b0dcb111044da81199ef8917f7a1b0339ac35a
Ulf Lilleengen <lulf@redhat.com>
2018-02-14 11:14:49 +0100

    Add SKIP_TESTS variable to skip tests
    * Reduce rebuilds for java components
    * Depend on global mvn compile for builds

--- Makefile.java.mk
+++ Makefile.java.mk
@@ -1,20 +1,28 @@
 TOPDIR=$(dir $(lastword $(MAKEFILE_LIST)))
 include $(TOPDIR)/Makefile.common
+ifeq ($(SKIP_TESTS),true)
+MAVEN_ARGS="-DskipTests"
+endif
 
 ifneq ($(FULL_BUILD),true)
 build:
-	mvn compile
+	cd $(TOPDIR); mvn compile $(MAVEN_ARGS)
 
 test:
-	mvn test
+ifeq ($(SKIP_TESTS),true)
+	$(warning "java tests will be skipped")
+else
+	mvn test $(MAVEN_ARGS)
+endif
 
 package_java:
-	mvn package -DskipTests
+	mvn package -DskipTests $(MAVEN_ARGS)
 
 package: package_java
 endif
 
 clean_java: 
+	mvn clean $(MAVEN_ARGS)
 	rm -rf build target
 
 clean: clean_java
