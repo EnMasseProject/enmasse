24c94b469cb22810e5dc2faaa362addf18f5ebd5
Ulf Lilleengen <lulf@redhat.com>
2018-02-14 11:28:00 +0100

    Use range for copyright year

--- amqp-utils/src/main/java/io/enmasse/amqp/DispatchRouterJ.java
+++ amqp-utils/src/main/java/io/enmasse/amqp/DispatchRouterJ.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016, EnMasse authors.
+ * Copyright 2016-2018, EnMasse authors.
  * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
  */
 package io.enmasse.amqp;
