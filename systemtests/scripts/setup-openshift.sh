934adf130052f6e15fd5364c1b16d0e1460f8b2f
Tomas Kratky <tkratky@redhat.com>
2018-02-21 10:49:11 +0100

    FIX setup-openshift - condition fixed

--- systemtests/scripts/setup-openshift.sh
+++ systemtests/scripts/setup-openshift.sh
@@ -4,7 +4,7 @@ SCRIPTDIR=`dirname $0`
 oc cluster down #for the case that cluster is already running
 
 oc get status
-if [ $? -ne 0 ]; then
+if [ ! $? -ne 0 ]; then
     echo "ERROR: shutting down of openshift cluster failed, tests won't be executed"
     exit 1
 fi
