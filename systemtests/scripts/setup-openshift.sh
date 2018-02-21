6e18749dcf50dfbb9611dea46b59bc0eae8c9726
Tomas Kratky <tkratky@redhat.com>
2018-02-21 09:32:10 +0100

    UPD setup-openshift - added check if cluster was stoped

--- systemtests/scripts/setup-openshift.sh
+++ systemtests/scripts/setup-openshift.sh
@@ -3,6 +3,12 @@ SCRIPTDIR=`dirname $0`
 
 oc cluster down #for the case that cluster is already running
 
+oc get status
+if [ $? -ne 0 ]; then
+    echo "ERROR: shutting down of openshift cluster failed, tests won't be executed"
+    exit 1
+fi
+
 sudo rm -rf /var/lib/origin/openshift.local.pv
 sudo rm -rf /var/log/containers/*
 sudo rm -rf /var/log/pods/*
