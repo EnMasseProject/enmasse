30634a7694da3712cf748f7ebcb41798e620d10d
Ulf Lilleengen <lulf@redhat.com>
2018-02-22 19:42:13 +0100

    Copy phase in Status copy constructor
    This fixes #927

--- address-model-lib/src/main/java/io/enmasse/address/model/Status.java
+++ address-model-lib/src/main/java/io/enmasse/address/model/Status.java
@@ -21,6 +21,7 @@ public class Status {
 
     public Status(io.enmasse.address.model.Status other) {
         this.isReady = other.isReady();
+        this.phase = other.getPhase();
         this.messages.addAll(other.getMessages());
     }
 
