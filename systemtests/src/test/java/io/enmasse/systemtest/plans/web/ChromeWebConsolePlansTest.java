a14d98705ed370877e1be4daa5bd5413e3db5be3
Tomas Kratky <tkratky@redhat.com>
2018-02-23 16:05:33 +0100

    New test - WebConsolePlanTest testCreateAddressPlan

---
+++ systemtests/src/test/java/io/enmasse/systemtest/plans/web/ChromeWebConsolePlansTest.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2018, EnMasse authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+package io.enmasse.systemtest.plans.web;
+
+import io.enmasse.systemtest.AddressType;
+import io.enmasse.systemtest.bases.web.WebConsolePlansTest;
+import org.openqa.selenium.WebDriver;
+
+public class ChromeWebConsolePlansTest extends WebConsolePlansTest {
+
+    @Override
+    protected String getDefaultPlan(AddressType addressType) {
+        return null;
+    }
+
+    @Override
+    public WebDriver buildDriver() {
+        return getChromeDriver();
+    }
+
+    //@Test
+    public void testCreateAddressPlan() throws Exception {
+        doTestCreateAddressPlan();
+    }
+}
---
+++ systemtests/src/test/java/io/enmasse/systemtest/plans/web/FirefoxWebConsolePlansTest.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2018, EnMasse authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+package io.enmasse.systemtest.plans.web;
+
+import io.enmasse.systemtest.AddressType;
+import io.enmasse.systemtest.bases.web.WebConsolePlansTest;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+
+public class FirefoxWebConsolePlansTest extends WebConsolePlansTest {
+
+    @Override
+    protected String getDefaultPlan(AddressType addressType) {
+        return null;
+    }
+
+    @Override
+    public WebDriver buildDriver() {
+        return getFirefoxDriver();
+    }
+
+    @Test
+    public void testCreateAddressPlan() throws Exception {
+        doTestCreateAddressPlan();
+    }
+}
