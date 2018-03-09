ac3933a60f9b93bda60b3c0f0c7d9deb49dca32c
David Kornel <kornys@outlook.com>
2018-03-09 12:36:05 +0100

    REF create package for common tests

--- systemtests/src/test/java/io/enmasse/systemtest/common/plans/web/ChromeWebConsolePlansTest.java
+++ systemtests/src/test/java/io/enmasse/systemtest/common/plans/web/ChromeWebConsolePlansTest.java
@@ -2,14 +2,13 @@
  * Copyright 2018, EnMasse authors.
  * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
  */
-package io.enmasse.systemtest.plans.web;
+package io.enmasse.systemtest.common.plans.web;
 
 import io.enmasse.systemtest.AddressType;
 import io.enmasse.systemtest.bases.web.WebConsolePlansTest;
-import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 
-public class FirefoxWebConsolePlansTest extends WebConsolePlansTest {
+public class ChromeWebConsolePlansTest extends WebConsolePlansTest {
 
     @Override
     protected String getDefaultPlan(AddressType addressType) {
@@ -18,10 +17,10 @@ public class FirefoxWebConsolePlansTest extends WebConsolePlansTest {
 
     @Override
     public WebDriver buildDriver() {
-        return getFirefoxDriver();
+        return getChromeDriver();
     }
 
-    @Test
+    //@Test
     public void testCreateAddressPlan() throws Exception {
         doTestCreateAddressPlan();
     }
--- systemtests/src/test/java/io/enmasse/systemtest/common/plans/web/FirefoxWebConsolePlansTest.java
+++ systemtests/src/test/java/io/enmasse/systemtest/common/plans/web/FirefoxWebConsolePlansTest.java
@@ -2,13 +2,14 @@
  * Copyright 2018, EnMasse authors.
  * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
  */
-package io.enmasse.systemtest.plans.web;
+package io.enmasse.systemtest.common.plans.web;
 
 import io.enmasse.systemtest.AddressType;
 import io.enmasse.systemtest.bases.web.WebConsolePlansTest;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 
-public class ChromeWebConsolePlansTest extends WebConsolePlansTest {
+public class FirefoxWebConsolePlansTest extends WebConsolePlansTest {
 
     @Override
     protected String getDefaultPlan(AddressType addressType) {
@@ -17,10 +18,10 @@ public class ChromeWebConsolePlansTest extends WebConsolePlansTest {
 
     @Override
     public WebDriver buildDriver() {
-        return getChromeDriver();
+        return getFirefoxDriver();
     }
 
-    //@Test
+    @Test
     public void testCreateAddressPlan() throws Exception {
         doTestCreateAddressPlan();
     }
