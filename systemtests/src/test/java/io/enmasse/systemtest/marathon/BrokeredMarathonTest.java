0be4a436a35eac75ba57aa9a39aa17d97a257bf0
David Kornel <kornys@outlook.com>
2018-03-27 18:51:45 +0200

    NEW added standard tests to marathon tests suite

---
+++ systemtests/src/test/java/io/enmasse/systemtest/marathon/BrokeredMarathonTest.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2018, EnMasse authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+package io.enmasse.systemtest.marathon;
+
+import io.enmasse.systemtest.AddressSpace;
+import io.enmasse.systemtest.AddressSpaceType;
+import io.enmasse.systemtest.AddressType;
+import io.enmasse.systemtest.AuthService;
+import org.junit.Test;
+
+public class BrokeredMarathonTest extends MarathonTestBase {
+
+    @Test
+    public void testCreateDeleteAddressSpaceLong() throws Exception {
+        doTestCreateDeleteAddressSpaceLong(
+                new AddressSpace("test-create-delete-brokered-space", AddressSpaceType.BROKERED));
+    }
+
+    @Test
+    public void testCreateDeleteAddressesLong() throws Exception {
+        doTestCreateDeleteAddressesLong(
+                new AddressSpace("test-create-delete-addresses-brokered", AddressSpaceType.BROKERED));
+    }
+
+    @Test
+    public void testCreateDeleteAddressesWithAuthLong() throws Exception {
+        doTestCreateDeleteAddressesWithAuthLong(
+                new AddressSpace("test-create-delete-addresses-auth-brokered",
+                        AddressSpaceType.BROKERED, AuthService.STANDARD));
+    }
+
+    @Test
+    public void testQueueSendReceiveLong() throws Exception {
+        doTestQueueSendReceiveLong(
+                new AddressSpace("test-queue-sendreceive-brokered", AddressSpaceType.BROKERED));
+    }
+
+    @Test
+    public void testCreateDeleteUsersLong() throws Exception {
+        doTestCreateDeleteUsersLong(
+                new AddressSpace("test-create-delete-users-brokered",
+                        AddressSpaceType.BROKERED, AuthService.STANDARD));
+    }
+
+    @Test
+    public void testAuthSendReceiveLong() throws Exception {
+        doTestAuthSendReceiveLong(
+                new AddressSpace("test-auth-send-receive-brokered",
+                        AddressSpaceType.BROKERED, AuthService.STANDARD));
+    }
+
+    @Test
+    public void testCreateDeleteUsersRestartKeyCloakLong() throws Exception {
+        doTestCreateDeleteUsersRestartKeyCloakLong(
+                new AddressSpace("test-create-delete-users-restart-brokered",
+                        AddressSpaceType.BROKERED, AuthService.STANDARD));
+    }
+
+    @Test
+    public void testTopicPubSubLong() throws Exception {
+        doTestTopicPubSubLong(
+                new AddressSpace("test-topic-pubsub-brokered", AddressSpaceType.BROKERED));
+    }
+
+    @Test
+    public void testCreateDeleteAddressesViaAgentLong() throws Exception {
+        doTestCreateDeleteAddressesViaAgentLong(
+                new AddressSpace("brokered-marathon-web-console",
+                        AddressSpaceType.BROKERED, AuthService.STANDARD));
+    }
+
+    @Override
+    protected String getDefaultPlan(AddressType addressType) {
+        switch (addressType) {
+            case QUEUE:
+                return "brokered-queue";
+            case TOPIC:
+                return "brokered-topic";
+        }
+        return null;
+    }
+}
---
+++ systemtests/src/test/java/io/enmasse/systemtest/marathon/StandardMarathonTest.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2018, EnMasse authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+package io.enmasse.systemtest.marathon;
+
+import io.enmasse.systemtest.AddressSpace;
+import io.enmasse.systemtest.AddressSpaceType;
+import io.enmasse.systemtest.AddressType;
+import io.enmasse.systemtest.AuthService;
+import org.junit.Test;
+
+public class StandardMarathonTest extends MarathonTestBase {
+
+    @Test
+    public void testCreateDeleteAddressSpaceLong() throws Exception {
+        doTestCreateDeleteAddressSpaceLong(
+                new AddressSpace("test-create-delete-standard-space", AddressSpaceType.STANDARD));
+    }
+
+    @Test
+    public void testCreateDeleteAddressesLongStandard() throws Exception {
+        doTestCreateDeleteAddressesLong(
+                new AddressSpace("test-create-delete-addresses-standard", AddressSpaceType.STANDARD));
+    }
+
+    @Test
+    public void testCreateDeleteAddressesWithAuthLong() throws Exception {
+        doTestCreateDeleteAddressesWithAuthLong(
+                new AddressSpace("test-create-delete-addresses-auth-standard",
+                        AddressSpaceType.STANDARD, AuthService.STANDARD));
+    }
+
+    @Test
+    public void testQueueSendReceiveLong() throws Exception {
+        doTestQueueSendReceiveLong(
+                new AddressSpace("test-queue-sendreceive-standard", AddressSpaceType.STANDARD));
+    }
+
+    @Test
+    public void testCreateDeleteUsersLong() throws Exception {
+        doTestCreateDeleteUsersLong(
+                new AddressSpace("test-create-delete-users-standard",
+                        AddressSpaceType.STANDARD, AuthService.STANDARD));
+    }
+
+    @Test
+    public void testAuthSendReceiveLong() throws Exception {
+        doTestAuthSendReceiveLong(
+                new AddressSpace("test-auth-send-receive-standard",
+                        AddressSpaceType.STANDARD, AuthService.STANDARD));
+    }
+
+    @Test
+    public void testCreateDeleteUsersRestartKeyCloakLong() throws Exception {
+        doTestCreateDeleteUsersRestartKeyCloakLong(
+                new AddressSpace("test-create-delete-users-restart-standard",
+                        AddressSpaceType.STANDARD, AuthService.STANDARD));
+    }
+
+    @Test
+    public void testTopicPubSubLong() throws Exception {
+        doTestTopicPubSubLong(
+                new AddressSpace("test-topic-pubsub-standard", AddressSpaceType.STANDARD));
+    }
+
+    @Test
+    public void testCreateDeleteAddressesViaAgentLong() throws Exception {
+        doTestCreateDeleteAddressesViaAgentLong(
+                new AddressSpace("standard-marathon-web-console",
+                        AddressSpaceType.STANDARD, AuthService.STANDARD));
+    }
+
+    @Override
+    protected String getDefaultPlan(AddressType addressType) {
+        switch (addressType) {
+            case QUEUE:
+                return "pooled-queue";
+            case TOPIC:
+                return "pooled-topic";
+        }
+        return null;
+    }
+}
