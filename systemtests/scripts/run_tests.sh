6f661a2b80d4a999a75ce5c993bf3637c0d81a50
Tomas Kratky <tkratky@redhat.com>
2018-04-03 14:41:51 +0200

    Execute all tests in run_tests.sh

--- systemtests/scripts/run_tests.sh
+++ systemtests/scripts/run_tests.sh
@@ -5,4 +5,4 @@ if [ -n "$TESTCASE" ]; then
     EXTRA_ARGS="-Dtest=$TESTCASE"
 fi
 
-mvn test -pl systemtests -Psystemtests-isolated -Djava.net.preferIPv4Stack=true $EXTRA_ARGS
+mvn test -pl systemtests -Psystemtests -Djava.net.preferIPv4Stack=true $EXTRA_ARGS
