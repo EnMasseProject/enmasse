ded5a24c2b76afe7f0b80305e6ebe6dd7b3c0a17
David Kornel <kornys@outlook.com>
2018-04-11 14:06:52 +0200

    Websocket tests

---
+++ systemtests/src/test/java/io/enmasse/systemtest/standard/web/ChromeWebSocketBrowserTest.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2018, EnMasse authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+package io.enmasse.systemtest.standard.web;
+
+import io.enmasse.systemtest.AddressType;
+import io.enmasse.systemtest.Destination;
+import io.enmasse.systemtest.bases.ITestBaseBrokered;
+import io.enmasse.systemtest.bases.ITestBaseStandard;
+import io.enmasse.systemtest.bases.web.WebSocketBrowserTest;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+
+@Disabled("chrome driver has issue with headless mode")
+public class ChromeWebSocketBrowserTest extends WebSocketBrowserTest implements ITestBaseStandard {
+
+
+    @Test
+    public void testWebSocketSendReceiveQueue() throws Exception {
+        doWebSocketSendReceive(Destination.queue("websocket-queue", getDefaultPlan(AddressType.QUEUE)));
+    }
+
+    @Test
+    public void testWebSocketSendReceiveTopic() throws Exception {
+        doWebSocketSendReceive(Destination.topic("websocket-topic", getDefaultPlan(AddressType.TOPIC)));
+    }
+
+    @Override
+    public WebDriver buildDriver() {
+        return getChromeDriver();
+    }
+}
