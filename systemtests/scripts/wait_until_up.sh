bb3c96a4446195ddea48ccbda641048d40867293
David Kornel <kornys@outlook.com>
2018-04-30 19:16:48 +0200

    Make wait_until_up.sh work if only oc available

--- systemtests/scripts/wait_until_up.sh
+++ systemtests/scripts/wait_until_up.sh
@@ -2,12 +2,22 @@
 EXPECTED_PODS=$1
 ADDRESS_SPACE=$2
 
+
+if which oc &> /dev/null; then
+    CMD=oc
+elif which kubectl &> /dev/null; then
+    CMD=kubectl
+else
+    echo "Cannot find oc or kubectl command, please check path to ensure it is installed"
+    exit 1
+fi
+
 function waitingContainersReady {
     ADDR_SPACE=$1
-    pods_id=$(kubectl get pods -n ${ADDR_SPACE} | awk 'NR >1 {print $1}')
+    pods_id=$($CMD get pods -n ${ADDR_SPACE} | awk 'NR >1 {print $1}')
     for pod_id in ${pods_id}
     do
-        ready=$(kubectl get -o json pod -n ${ADDR_SPACE}  $pod_id -o jsonpath={.status.containerStatuses[0].ready})
+        ready=$($CMD get -o json pod -n ${ADDR_SPACE}  $pod_id -o jsonpath={.status.containerStatuses[0].ready})
         if [ ${ready} == "false" ]
         then
             return 1
@@ -26,11 +36,11 @@ do
     NOW=$(date +%s)
     if [ $NOW -gt $END ]; then
         echo "Timed out waiting for nodes to come up!"
-        pods=`kubectl get pods -n ${ADDRESS_SPACE}`
+        pods=`$CMD get pods -n ${ADDRESS_SPACE}`
         echo "PODS: $pods"
         exit 1
     fi
-    num_running=`kubectl get pods -n ${ADDRESS_SPACE}| grep -v deploy | grep -c Running`
+    num_running=`$CMD get pods -n ${ADDRESS_SPACE}| grep -v deploy | grep -c Running`
     if [ "$num_running" -eq "$EXPECTED_PODS" ]; then
         waitingContainersReady ${ADDRESS_SPACE}
         if [ $? -gt 0 ]
