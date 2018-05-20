058e72951367fe11263a44e0f29aec27e7d44b24
Ulf Lilleengen <lulf@redhat.com>
2018-05-20 21:41:30 +0200

    Update release scripts

--- release-scripts/make-release.sh
+++ release-scripts/make-release.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+BASE_VERSION=$1
 VERSION=$1
-git checkout -b release-${VERSION}
+git checkout -b release-${BASE_VERSION}
 mvn versions:set -DnewVersion=${VERSION}
 echo $VERSION > release.version
 git status
@@ -10,5 +11,5 @@ git commit -a -m "Update version to ${VERSION}"
 git tag ${VERSION}
 echo "Press ENTER to push the ${VERSION} branch and tag to GitHub"
 read
-git push -u origin release-${VERSION}
+git push -u origin release-${BASE_VERSION}
 git push -u origin ${VERSION}
