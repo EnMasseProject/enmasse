5152751d97a4cdc3c637cfe7e3e1b0738b38fdbf
Ulf Lilleengen <lulf@redhat.com>
2018-05-23 13:27:40 +0200

    Fetch master branch specifically

--- scripts/rebase-release.sh
+++ scripts/rebase-release.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 RELEASE_BRANCH=$1
 git checkout release-${RELEASE_BRANCH}
-git fetch origin
+git fetch origin master
 git rebase origin/master && git push -f origin release-${RELEASE_BRANCH}
