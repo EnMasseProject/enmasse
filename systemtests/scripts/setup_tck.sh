52b7dc8430190dafd7bb9aef59cfe9b272365dbe
Tomas Kratky <tkratky@redhat.com>
2018-08-28 14:08:52 +0200

    use correct address name

--- systemtests/scripts/setup_tck.sh
+++ systemtests/scripts/setup_tck.sh
@@ -16,21 +16,21 @@ PASSWORD=$(cat admin.password)
 #setup environment
 create_address_space ${NAMESPACE} ${DEFAULT_ADDRESS_SPACE} './systemtests/templates/tckAddressSpace.json' || return 1
 
-create_address ${NAMESPACE} ${DEFAULT_ADDRESS_SPACE} "MY_QUEUE_upper" "MY_QUEUE" "queue" "brokered-queue"
-create_address ${NAMESPACE} ${DEFAULT_ADDRESS_SPACE} "MY_QUEUE2" "MY_QUEUE2" "queue" "brokered-queue"
-create_address ${NAMESPACE} ${DEFAULT_ADDRESS_SPACE} "testQ0" "testQ0" "queue" "brokered-queue"
-create_address ${NAMESPACE} ${DEFAULT_ADDRESS_SPACE} "testQ1" "testQ1" "queue" "brokered-queue"
-create_address ${NAMESPACE} ${DEFAULT_ADDRESS_SPACE} "testQ2" "testQ2" "queue" "brokered-queue"
-create_address ${NAMESPACE} ${DEFAULT_ADDRESS_SPACE} "testQueue2" "testQueue2" "queue" "brokered-queue"
-create_address ${NAMESPACE} ${DEFAULT_ADDRESS_SPACE} "Q2" "Q2" "queue" "brokered-queue"
-create_address ${NAMESPACE} ${DEFAULT_ADDRESS_SPACE} "myQueue" "myQueue" "queue" "brokered-queue"
+create_address ${NAMESPACE} ${DEFAULT_ADDRESS_SPACE} "my-queue" "MY_QUEUE" "queue" "brokered-queue"
+create_address ${NAMESPACE} ${DEFAULT_ADDRESS_SPACE} "my-queue2" "MY_QUEUE2" "queue" "brokered-queue"
+create_address ${NAMESPACE} ${DEFAULT_ADDRESS_SPACE} "testq0" "testQ0" "queue" "brokered-queue"
+create_address ${NAMESPACE} ${DEFAULT_ADDRESS_SPACE} "testq1" "testQ1" "queue" "brokered-queue"
+create_address ${NAMESPACE} ${DEFAULT_ADDRESS_SPACE} "testq2" "testQ2" "queue" "brokered-queue"
+create_address ${NAMESPACE} ${DEFAULT_ADDRESS_SPACE} "testqueue2" "testQueue2" "queue" "brokered-queue"
+create_address ${NAMESPACE} ${DEFAULT_ADDRESS_SPACE} "q2" "Q2" "queue" "brokered-queue"
+create_address ${NAMESPACE} ${DEFAULT_ADDRESS_SPACE} "myqueue" "myQueue" "queue" "brokered-queue"
 
-create_address ${NAMESPACE} ${DEFAULT_ADDRESS_SPACE} "MY_TOPIC_upper" "MY_TOPIC" "topic" "brokered-topic"
-create_address ${NAMESPACE} ${DEFAULT_ADDRESS_SPACE} "MY_TOPIC2" "MY_TOPIC2" "topic" "brokered-topic"
-create_address ${NAMESPACE} ${DEFAULT_ADDRESS_SPACE} "testT0" "testT0" "topic" "brokered-topic"
-create_address ${NAMESPACE} ${DEFAULT_ADDRESS_SPACE} "testT1" "testT1" "topic" "brokered-topic"
-create_address ${NAMESPACE} ${DEFAULT_ADDRESS_SPACE} "testT2" "testT2" "topic" "brokered-topic"
-create_address ${NAMESPACE} ${DEFAULT_ADDRESS_SPACE} "myTopic" "myTopic" "topic" "brokered-topic"
+create_address ${NAMESPACE} ${DEFAULT_ADDRESS_SPACE} "my-topic-upper" "MY_TOPIC" "topic" "brokered-topic"
+create_address ${NAMESPACE} ${DEFAULT_ADDRESS_SPACE} "my-topic2" "MY_TOPIC2" "topic" "brokered-topic"
+create_address ${NAMESPACE} ${DEFAULT_ADDRESS_SPACE} "testt0" "testT0" "topic" "brokered-topic"
+create_address ${NAMESPACE} ${DEFAULT_ADDRESS_SPACE} "testt1" "testT1" "topic" "brokered-topic"
+create_address ${NAMESPACE} ${DEFAULT_ADDRESS_SPACE} "testt2" "testT2" "topic" "brokered-topic"
+create_address ${NAMESPACE} ${DEFAULT_ADDRESS_SPACE} "mytopic" "myTopic" "topic" "brokered-topic"
 
 sleep 60 #waiting for addresses are ready
 
