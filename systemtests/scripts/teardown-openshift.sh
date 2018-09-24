ae32e79955cca0fd8cfc7cafc813ee403f36b659
David Kornel <kornys@outlook.com>
2018-09-24 09:04:42 +0200

    Remove old openshift data for openshift 3.10

--- systemtests/scripts/teardown-openshift.sh
+++ systemtests/scripts/teardown-openshift.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-oc logout
-oc cluster down
-sudo rm -rf /var/lib/origin/openshift.local.pv
+CURDIR="$(dirname $(readlink -f ${0}))"
+source "${CURDIR}/test_func.sh"
+
+stop_and_check_openshift
+clean_oc_location
