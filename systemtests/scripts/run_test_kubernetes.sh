1f23cd4b4c11f0f7ff9529b2b4ed9d58c80d35bf
Ulf Lilleengen <lulf@redhat.com>
2018-09-28 13:09:05 +0200

    Rename paths in tarball
    * Replace myproject in documentation examples

--- systemtests/scripts/run_test_kubernetes.sh
+++ systemtests/scripts/run_test_kubernetes.sh
@@ -40,9 +40,9 @@ mkdir -p standard-authservice-cert/
 openssl req -new -x509 -batch -nodes -days 11000 -subj "/O=io.enmasse/CN=standard-authservice.${OPENSHIFT_PROJECT}.svc.cluster.local" -out standard-authservice-cert/tls.crt -keyout standard-authservice-cert/tls.key
 kubectl create secret tls standard-authservice-cert --cert=standard-authservice-cert/tls.crt --key=standard-authservice-cert/tls.key
 
-cp -r ${ENMASSE_DIR}/examples/install/none-authservice/*.yaml ${ENMASSE_DIR}/examples/bundles/enmasse-with-standard-authservice
-sed -i "s/namespace: .*/namespace: ${OPENSHIFT_PROJECT}/" ${ENMASSE_DIR}/examples/bundles/enmasse-with-standard-authservice/*.yaml
-kubectl create -f ${ENMASSE_DIR}/examples/bundles/enmasse-with-standard-authservice
+cp -r ${ENMASSE_DIR}/install/components/none-authservice/*.yaml ${ENMASSE_DIR}/install/bundles/enmasse-with-standard-authservice
+sed -i "s/myproject/${OPENSHIFT_PROJECT}/" ${ENMASSE_DIR}/install/bundles/enmasse-with-standard-authservice/*.yaml
+kubectl create -f ${ENMASSE_DIR}/install/bundles/enmasse-with-standard-authservice
 
 #environment info
 LOG_DIR="${ARTIFACTS_DIR}/kubernetes-info/"
