EnMasseProject/enmasse

commit c4f33b5bf0bae1d531356c1ddfb7988453a34997

fix and improve messages in/out for addresses (#1708)
* fix and improve messages in/out for addresses

* fix search text for message count related tests

* remove commented out code

* on the addresses tab, stack the message counts and add a legend

systemtests/src/main/java/io/enmasse/systemtest/selenium/resources/AddressWebItem.java
@@ -32,8 +32,8 @@ public class AddressWebItem extends WebItem implements Comparable<AddressWebItem
         this.readAdditionalInfo();
         this.sendersCount = getCountOfAdditionalInfoItem("Senders");
         this.receiversCount = getCountOfAdditionalInfoItem("Receivers");
-        this.messagesIn = getCountOfAdditionalInfoItem("Messages In");
-        this.messagesOut = getCountOfAdditionalInfoItem("Messages Out");
+        this.messagesIn = getCountOfAdditionalInfoItem("Incoming Traffic");
+        this.messagesOut = getCountOfAdditionalInfoItem("Outgoing Traffic");
         this.messagesStored = getCountOfAdditionalInfoItem("Stored");
         this.isReady = AddressStatus.READY == this.status;
     }

agent/www/components/addresses/addresses.html
@@ -46,21 +46,18 @@
         </div>
       </div>
       <div class="list-view-pf-additional-info">
-        <div class="list-view-pf-additional-info-item" ng-if="item.type !== 'subscription'">
-          <span class="fa fa-sign-in"></span>
-          <strong>{{item.messages_in_delta}}</strong> Messages In
-        </div>
-        <div class="list-view-pf-additional-info-item" ng-if="item.type !== 'subscription'">
-          <span class="fa fa-sign-out"></span>
-          <strong>{{item.messages_out_delta}}</strong> Messages Out
-        </div>
-        <div class="list-view-pf-additional-info-item" ng-if="item.type === 'subscription'">
-          <span class="fa fa-sign-in"></span>
-          <strong>{{item.shards[0].enqueued}}</strong> Messages In
-        </div>
-        <div class="list-view-pf-additional-info-item" ng-if="item.type === 'subscription'">
-          <span class="fa fa-sign-out"></span>
-          <strong>{{item.shards[0].acknowledged + item.shards[0].killed}}</strong> Messages Out
+        <div class="list-view-pf-additional-info-item-stacked">
+          <div class="list-view-pf-additional-info-item" title="Messages sent to this address in last 5 minutes">
+            <span class="fa fa-sign-in"></span>
+            <strong>{{item.messages_in_delta}}</strong>  Messages In
+          </div>
+          <div class="list-view-pf-additional-info-item" title="Messages sent from this address in last 5 minutes">
+            <span class="fa fa-sign-out"></span>
+            <strong>{{item.messages_out_delta}}</strong>  Messages Out
+          </div>
+          <div class="legend-pf-text">
+            <em>(values over the last 5 minutes)</em>
+          </div>
         </div>
         <div class="list-view-pf-additional-info-item" ng-if="item.type !== 'subscription'">
           <span class="fa pficon-import"></span> <strong>{{item.senders}}</strong> Senders

agent/www/shared/address_service.js
@@ -25,9 +25,10 @@ TimeSeries.prototype.push_delta = function (value) {
     this.last = value;
 }
 
-function WindowedDelta(window) {
+function WindowedDelta(name, window) {
+    this.name = name;
     this.last = undefined;
-    this.deltas = new Array(window);
+    this.deltas = Array(window).fill(0);
     this.current = 0;
 }
 
@@ -39,22 +40,14 @@ WindowedDelta.prototype.push = function (value) {
 };
 
 WindowedDelta.prototype.update = function (value) {
-    if (this.last === undefined || this.last > value) {
-        this.push(value);
-    } else {
+    if (this.last !== undefined && this.last <= value) {
         this.push(value - this.last);
     }
     this.last = value;
 };
 
 WindowedDelta.prototype.total = function (current) {
-    var t = this.last === undefined ? current : current - this.last;
-    for (var i = 0; i < this.deltas.length; i++) {
-        if (this.deltas[i] != undefined) {
-            t += this.deltas[i];
-        }
-    }
-    return t;
+    return this.deltas.reduce(function(a, b) { return a + b; });
 };
 
 function AddressDefinition(a) {
@@ -72,11 +65,12 @@ function AddressDefinition(a) {
     };
     this.update_depth_series();
     this.periodic_deltas = {
-        'messages_in': new WindowedDelta(10),
-        'messages_out': new WindowedDelta(10)
+        'messages_in': new WindowedDelta(this.address + ' messages_in', 60),
+        'messages_out': new WindowedDelta(this.address + ' messages_out', 60)
     };
     for (var name in this.periodic_deltas) {
         this.define_periodic_delta(name);
+        this.periodic_deltas[name].update(this[name]);
     }
 }
 
@@ -87,6 +81,10 @@ AddressDefinition.prototype.define_periodic_delta = function (name) {
 AddressDefinition.prototype.update = function (a) {
     for (var k in a) {
         this[k] = a[k];
+    }
+    if (a.type === 'subscription' && a.shards) {
+        this.messages_in = a.shards[0].enqueued;
+        this.messages_out = a.shards[0].acknowledged + a.shards[0].killed;
     }
 }
 
@@ -119,7 +117,7 @@ function AddressService($http) {
     this.connection.on('message', this.on_message.bind(this));
     this.sender = this.connection.open_sender();
     this.connection.open_receiver();
-    setInterval(this.update_periodic_deltas.bind(this), 30000);
+    setInterval(this.update_periodic_deltas.bind(this), 5000);
     setInterval(this.update_depth_series.bind(this), 30000);
 
     this.tooltip = {}

commit 919bf6d3433b021b07d949567acdfd4e1b03c966

Fix test regression caused by c4f33b5bf0bae1d531356c1ddfb7988453a34997

systemtests/src/main/java/io/enmasse/systemtest/selenium/resources/AddressWebItem.java
@@ -32,8 +32,8 @@ public class AddressWebItem extends WebItem implements Comparable<AddressWebItem
         this.readAdditionalInfo();
         this.sendersCount = getCountOfAdditionalInfoItem("Senders");
         this.receiversCount = getCountOfAdditionalInfoItem("Receivers");
-        this.messagesIn = getCountOfAdditionalInfoItem("Incoming Traffic");
-        this.messagesOut = getCountOfAdditionalInfoItem("Outgoing Traffic");
+        this.messagesIn = getCountOfAdditionalInfoItem("Messages In");
+        this.messagesOut = getCountOfAdditionalInfoItem("Messages Out");
         this.messagesStored = getCountOfAdditionalInfoItem("Stored");
         this.isReady = AddressStatus.READY == this.status;
     }