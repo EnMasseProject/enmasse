e0e4df5d8046a0cfd4d9b8c95629ec6ef291d86e
Gordon Sim <gsim@redhat.com>
2018-10-12 16:08:35 +0100

    fix incorrect log level

--- agent/lib/qdr.js
+++ agent/lib/qdr.js
@@ -106,7 +106,7 @@ Router.prototype.health_check = function () {
         }
 
         if (messages.length) {
-            log.warning('[%s] %s (%s)', this.get_id(), messages.join(', '), ready_state);
+            log.warn('[%s] %s (%s)', this.get_id(), messages.join(', '), ready_state);
         } else if (current.sent !== last.sent || current.recv != last.recv) {
             log.info('[%s] %s sent %d requests and received %d responses since last status', this.get_id(), ready_state, current.sent - last.sent, current.recv - last.recv);
         } else {
