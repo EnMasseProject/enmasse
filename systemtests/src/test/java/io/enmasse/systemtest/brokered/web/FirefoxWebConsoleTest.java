35a6a3ec2a3da8dd64c9be036d53779d74a20f99
David Kornel <kornys@outlook.com>
2018-10-12 09:41:30 +0200

    re-enabled disabled tests due to #1757

--- systemtests/src/test/java/io/enmasse/systemtest/brokered/web/FirefoxWebConsoleTest.java
+++ systemtests/src/test/java/io/enmasse/systemtest/brokered/web/FirefoxWebConsoleTest.java
@@ -74,13 +74,11 @@ class FirefoxWebConsoleTest extends WebConsoleTest implements ITestBaseBrokered,
     }
 
     @Test
-    @Disabled("disable due to #1757")
     void testFilterConnectionsByUser() throws Exception {
         doTestFilterConnectionsByUser();
     }
 
     @Test
-    @Disabled("disable due to #1757")
     void testFilterConnectionsByHostname() throws Exception {
         doTestFilterConnectionsByHostname();
     }
