de8f972ede81f5764c6717ed787f07ed74650b79
Ulf Lilleengen <lulf@redhat.com>
2018-10-22 08:51:24 +0200

    Cleanup setting of commit and version
    * COMMIT no longer used
    * Tag used for image tag to tag/push
    * Version used for image versions
    * Commit hash added to image as env var

--- .travis/push.sh
+++ .travis/push.sh
@@ -2,14 +2,9 @@
 set -e
 PULL_REQUEST=${PULL_REQUEST:-true}
 BRANCH=${BRANCH:-master}
-COMMIT=${COMMIT:-latest}
 VERSION=`cat release.version`
 TAG=${TAG:-latest}
 DOCKER_ORG=${DOCKER_ORG:-$USER}
-
-if [ "$TAG" != "latest" ]; then
-    COMMIT=$TAG
-fi
 
 if [ "$BRANCH" != "master" ] && [ "$BRANCH" != "$TAG" ] || [ "$PULL_REQUEST" != "false" ]
 then
