c64d0477d975a169e14898288a0d732ab170ba95
Ulf Lilleengen <lulf@redhat.com>
2018-10-23 08:26:14 +0200

    Report exceptions when checking status

--- address-space-controller/src/main/java/io/enmasse/controller/StatusController.java
+++ address-space-controller/src/main/java/io/enmasse/controller/StatusController.java
@@ -11,6 +11,8 @@ import io.enmasse.config.AnnotationKeys;
 import io.enmasse.controller.common.Kubernetes;
 import io.enmasse.controller.common.KubernetesHelper;
 import io.enmasse.user.api.UserApi;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.HashSet;
@@ -19,6 +21,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public class StatusController implements Controller {
+    private static final Logger log = LoggerFactory.getLogger(StatusController.class.getName());
     private final Kubernetes kubernetes;
     private final SchemaProvider schemaProvider;
     private final InfraResourceFactory infraResourceFactory;
@@ -54,32 +57,46 @@ public class StatusController implements Controller {
         return type.getInfraConfigDeserializer().fromJson(addressSpace.getAnnotation(AnnotationKeys.APPLIED_INFRA_CONFIG));
     }
 
-    private void checkDeploymentsReady(AddressSpace addressSpace) throws IOException {
-        Set<String> readyDeployments = kubernetes.getReadyDeployments().stream()
-                .map(deployment -> deployment.getMetadata().getName())
-                .collect(Collectors.toSet());
+    private void checkDeploymentsReady(AddressSpace addressSpace) {
+        try {
+            Set<String> readyDeployments = kubernetes.getReadyDeployments().stream()
+                    .map(deployment -> deployment.getMetadata().getName())
+                    .collect(Collectors.toSet());
 
-        InfraConfig infraConfig = Optional.ofNullable(parseCurrentInfraConfig(addressSpace)).orElseGet(() -> getInfraConfig(addressSpace));
-        Set<String> requiredDeployments = infraResourceFactory.createInfraResources(addressSpace, infraConfig).stream()
-                .filter(KubernetesHelper::isDeployment)
-                .map(item -> item.getMetadata().getName())
-                .collect(Collectors.toSet());
+            InfraConfig infraConfig = Optional.ofNullable(parseCurrentInfraConfig(addressSpace)).orElseGet(() -> getInfraConfig(addressSpace));
+            Set<String> requiredDeployments = infraResourceFactory.createInfraResources(addressSpace, infraConfig).stream()
+                    .filter(KubernetesHelper::isDeployment)
+                    .map(item -> item.getMetadata().getName())
+                    .collect(Collectors.toSet());
 
-        boolean isReady = readyDeployments.containsAll(requiredDeployments);
-        if (!isReady) {
-            Set<String> missing = new HashSet<>(requiredDeployments);
-            missing.removeAll(readyDeployments);
+            boolean isReady = readyDeployments.containsAll(requiredDeployments);
+            if (!isReady) {
+                Set<String> missing = new HashSet<>(requiredDeployments);
+                missing.removeAll(readyDeployments);
+                addressSpace.getStatus().setReady(false);
+                addressSpace.getStatus().appendMessage("Following deployments and statefulsets are not ready: " + missing);
+            }
+        } catch (Exception e) {
+            String msg = String.format("Error checking for ready deployments: %s", e.getMessage());
+            log.warn(msg);
             addressSpace.getStatus().setReady(false);
-            addressSpace.getStatus().appendMessage("Following deployments and statefulsets are not ready: " +  missing);
+            addressSpace.getStatus().appendMessage(msg);
         }
     }
 
     private void checkAuthServiceReady(AddressSpace addressSpace) {
         if (AuthenticationServiceType.STANDARD.equals(addressSpace.getAuthenticationService().getType())) {
-            boolean isReady = userApi.realmExists(addressSpace.getAnnotation(AnnotationKeys.REALM_NAME));
-            if (!isReady) {
+            try {
+                boolean isReady = userApi.realmExists(addressSpace.getAnnotation(AnnotationKeys.REALM_NAME));
+                if (!isReady) {
+                    addressSpace.getStatus().setReady(false);
+                    addressSpace.getStatus().appendMessage("Standard authentication service is not configured with realm " + addressSpace.getAnnotation(AnnotationKeys.REALM_NAME));
+                }
+            } catch (Exception e) {
+                String msg = String.format("Error checking authentication service status: %s", e.getMessage());
+                log.warn(msg);
                 addressSpace.getStatus().setReady(false);
-                addressSpace.getStatus().appendMessage("Standard authentication service is not configured with realm " + addressSpace.getAnnotation(AnnotationKeys.REALM_NAME));
+                addressSpace.getStatus().appendMessage(msg);
             }
         }
     }
