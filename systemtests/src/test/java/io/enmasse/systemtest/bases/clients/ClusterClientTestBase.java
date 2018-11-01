5ed72b9fdb33091c587e0d29d2236e9a90bdeb2d
David Kornel <kornys@outlook.com>
2018-11-01 19:24:39 +0100

    Added assert message

--- systemtests/src/test/java/io/enmasse/systemtest/bases/clients/ClusterClientTestBase.java
+++ systemtests/src/test/java/io/enmasse/systemtest/bases/clients/ClusterClientTestBase.java
@@ -115,14 +115,14 @@ public abstract class ClusterClientTestBase extends TestBaseWithShared {
         String receiverId = cliApiClient.sendAndGetId(receiver);
 
         JsonObject response = cliApiClient.sendAndGetStatus(sender);
-        assertThat(response.getInteger("ecode"), is(0));
+        assertThat("Return code of sender is not 0", response.getInteger("ecode"), is(0));
 
         Thread.sleep(30000);
 
         response = cliApiClient.getClientInfo(receiverId);
         log.info(response.toString());
-        assertThat(response.getInteger("ecode"), is(0));
-        assertFalse(response.getString("stdOut").isEmpty());
+        assertThat("Return code of receiver is not 0", response.getInteger("ecode"), is(0));
+        assertFalse(response.getString("stdOut").isEmpty(), "Receiver does not receive message");
         log.info(response.toString());
     }
 }
