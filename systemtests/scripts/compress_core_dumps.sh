9ae0e3887866c612d8094e1ce988798f50e0ef4f
David Kornel <kornys@outlook.com>
2018-11-22 10:17:16 +0100

    Check opened files in artifact dir (#1999)

--- systemtests/scripts/compress_core_dumps.sh
+++ systemtests/scripts/compress_core_dumps.sh
@@ -2,11 +2,7 @@
 CORE_DUMPS_LOCATION=${1}
 ARTIFACTS_DIR=${2}
 if [[ $(ls -A "${CORE_DUMPS_LOCATION}") ]]; then
-    while [[ -n "$(lsof +D "${CORE_DUMPS_LOCATION}")" ]] ; do
-        echo "Core dumps folder is not ready to compress!"
-        lsof +D "${CORE_DUMPS_LOCATION}"
-        sleep 5
-    done
+    wait_until_file_close ${CORE_DUMPS_LOCATION}
     echo "Core dumps folder is ready to compress!"
     sudo tar -czvf core-dumps.tar.gz ${CORE_DUMPS_LOCATION}
     sudo mv core-dumps.tar.gz ${ARTIFACTS_DIR}
