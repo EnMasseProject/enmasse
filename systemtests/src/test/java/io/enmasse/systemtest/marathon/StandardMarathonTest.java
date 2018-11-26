a9f9cc0cbad1f1e962331191c82cc4d81db57086
Sean Davey <sdavey@redhat.com>
2018-11-26 14:41:01 +0100

    Disabled test thats failing in CI, failures caused by test using all server resources

--- systemtests/src/test/java/io/enmasse/systemtest/marathon/StandardMarathonTest.java
+++ systemtests/src/test/java/io/enmasse/systemtest/marathon/StandardMarathonTest.java
@@ -8,6 +8,7 @@ import io.enmasse.systemtest.AddressSpace;
 import io.enmasse.systemtest.AddressSpaceType;
 import io.enmasse.systemtest.AuthService;
 import io.enmasse.systemtest.ability.ITestBaseStandard;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
 
@@ -27,6 +28,7 @@ class StandardMarathonTest extends MarathonTestBase implements ITestBaseStandard
     }
 
     @Test
+    @Disabled("test failing in ci, using all server resources")
     void testCreateHighAddressCountCheckStatusDeleteLong() throws Exception {
         doTestCreateHighAddressCountCheckStatusDeleteLong(
                 new AddressSpace("test-create-addresses-check-status-delete",
