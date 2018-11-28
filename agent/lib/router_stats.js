6f95fecbc6e909273735ee9c21baa9651a688d0e
Ulf Lilleengen <lulf@pvv.ntnu.no>
2018-11-28 23:50:13 +0100

    Dont show router metrics in connection tab (#2053)

--- agent/lib/router_stats.js
+++ agent/lib/router_stats.js
@@ -224,7 +224,7 @@ function is_role_normal (c) {
     return c.role === 'normal';
 }
 
-var internal_identifiers = ['standard-controller', 'agent', 'ragent', 'qdconfigd', 'subserv', 'lwt-service'];
+var internal_identifiers = ['standard-controller', 'agent', 'ragent', 'qdconfigd', 'subserv', 'lwt-service', 'router-metrics'];
 
 function is_internal_identifier (s) {
     return internal_identifiers.indexOf(s) >= 0;
