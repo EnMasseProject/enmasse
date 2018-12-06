ec2a77e2af1e1da3677d6b899fd655f6bd885965
David Kornel <kornys@outlook.com>
2018-12-06 10:56:23 +0100

    Update ocenv.sh

--- systemtests/scripts/ocenv.sh
+++ systemtests/scripts/ocenv.sh
@@ -2,9 +2,11 @@
 HOST=${1:-localhost}
 NAMESPACE=${2:-myproject}
 USER=${3:-developer}
+REGISTER_API_SERVER=${4:-true}
 
-export OPENSHIFT_USER=$USER
+export OPENSHIFT_USER=${USER}
 export OPENSHIFT_USE_TLS=true
-export OPENSHIFT_TOKEN=`oc whoami -t`
+export OPENSHIFT_TOKEN=$(oc whoami -t)
 export OPENSHIFT_URL=https://${HOST}:8443
 export OPENSHIFT_PROJECT=${NAMESPACE}
+export REGISTER_API_SERVER=${REGISTER_API_SERVER}
