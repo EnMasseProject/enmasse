97017c1032109fbfa1aa9c123289e8c315fb0ba4
Jens Reimann <jreimann@redhat.com>
2018-12-12 13:13:22 +0100

    Merge user and common model into one

--- api-model/src/main/java/io/enmasse/common/api/model/AbstractResource.java
+++ api-model/src/main/java/io/enmasse/common/api/model/AbstractResource.java
@@ -5,6 +5,20 @@
 
 package io.enmasse.common.api.model;
 
+import io.fabric8.kubernetes.api.model.Doneable;
+import io.sundr.builder.annotations.Buildable;
+import io.sundr.builder.annotations.Inline;
+
+@Buildable(
+        editableEnabled = false,
+        generateBuilderPackage = false,
+        builderPackage = "io.fabric8.kubernetes.api.builder",
+        inline = @Inline(
+                type = Doneable.class,
+                prefix = "Doneable",
+                value = "done"
+                )
+        )
 public abstract class AbstractResource<T> {
 
     private final String kind;
