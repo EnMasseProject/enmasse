7aff69ec2c6f000e51ae351615a4d83418748ada
Ulf Lilleengen <lulf@redhat.com>
2018-12-13 14:48:57 +0100

    Add missing file

---
+++ systemtests/src/main/java/io/enmasse/systemtest/amqp/ReceiverStatus.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2017-2018, EnMasse authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+package io.enmasse.systemtest.amqp;
+
+import org.apache.qpid.proton.message.Message;
+
+import java.util.List;
+import java.util.concurrent.Future;
+
+public interface ReceiverStatus {
+    Future<List<Message>> getResult();
+    int getNumReceived();
+}
