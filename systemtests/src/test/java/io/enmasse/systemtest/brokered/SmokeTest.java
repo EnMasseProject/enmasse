c4e88c7de56d7642ebf254b92af0ae4a17a23bee
Sean Davey <sdavey@redhat.com>
2019-01-08 13:42:42 +0100

    added static HTTP_CONFLICT to testCreateAlreadyExistingAddress

--- systemtests/src/test/java/io/enmasse/systemtest/brokered/SmokeTest.java
+++ systemtests/src/test/java/io/enmasse/systemtest/brokered/SmokeTest.java
@@ -19,6 +19,7 @@ import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 import static io.enmasse.systemtest.TestTag.nonPR;
+import static java.net.HttpURLConnection.HTTP_CONFLICT;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -107,7 +108,7 @@ class SmokeTest extends TestBaseWithShared implements ITestBaseBrokered {
         setAddresses(addressSpaceA, queueA);
 
         Destination topicA = Destination.topic(addr_name, getDefaultPlan(AddressType.TOPIC));
-        assertThrows(AddressAlreadyExistsException.class, () -> setAddresses(addressSpaceA, 409, topicA),
+        assertThrows(AddressAlreadyExistsException.class, () -> setAddresses(addressSpaceA, HTTP_CONFLICT, topicA),
                 "setAddresses does not throw right exception");
     }
 }
