69ee2ae2413da46d0a111c7d31f294c387d7d2c5
Ulf Lilleengen <lulf@redhat.com>
2019-01-23 03:27:19 +0100

    Fix logging

--- broker-plugin/sasl-delegation/src/main/java/io/enmasse/artemis/sasl_delegation/SaslGroupBasedSecuritySettingsPlugin.java
+++ broker-plugin/sasl-delegation/src/main/java/io/enmasse/artemis/sasl_delegation/SaslGroupBasedSecuritySettingsPlugin.java
@@ -117,7 +117,6 @@ public class SaslGroupBasedSecuritySettingsPlugin implements SecuritySettingPlug
                         fixedAddress.append(c);
                     }
                     address = fixedAddress.toString();
-                    LOG.infov("AFTER: Singleword({0}), anyWords({1}), delimited({2})", singleWord, anyWords, delimeter);
                     if(knownAddresses.add(address)) {
 
                         String singleWordString = String.valueOf(singleWord);
@@ -125,7 +124,7 @@ public class SaslGroupBasedSecuritySettingsPlugin implements SecuritySettingPlug
                         String anyWordsString = String.valueOf(anyWords);
 
                         Set<Role> roles = new HashSet<>();
-                        LOG.infov("Adding permissions for address {0} due to group {1}", address, group);
+                        LOG.debugv("Adding permissions for address {0} due to group {1}", address, group);
                         roles.add(new Role("send_" + address, true, false, false, false, true, false, false, false, true, false));
                         roles.add(new Role("recv_" + address, false, true, true, true, true, true, false, true, true, false));
                         roles.add(new Role("browse_" + address, false, false, false, false, false, false, false, true, false, false));
