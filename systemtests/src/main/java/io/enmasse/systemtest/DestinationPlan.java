7b010882b8cd2b5c9069df5554a4fed68814e207
David Kornel <kornys@outlook.com>
2019-02-02 18:34:01 +0100

    Test for issue 2270

--- systemtests/src/main/java/io/enmasse/systemtest/DestinationPlan.java
+++ systemtests/src/main/java/io/enmasse/systemtest/DestinationPlan.java
@@ -11,8 +11,12 @@ public enum DestinationPlan {
     STANDARD_SMALL_TOPIC("standard-small-topic"),
     STANDARD_LARGE_QUEUE("standard-large-queue"),
     STANDARD_LARGE_TOPIC("standard-large-topic"),
+    STANDARD_XLARGE_QUEUE("standard-xlarge-queue"),
+    STANDARD_XLARGE_TOPIC("standard-xlarge-topic"),
     STANDARD_SMALL_ANYCAST("standard-small-anycast"),
-    STANDARD_SMALL_MULTICAST("standard-small-multicast");
+    STANDARD_SMALL_MULTICAST("standard-small-multicast"),
+    STANDARD_SMALL_SUBSCRIPTION("standard-small-subscription"),
+    STANDARD_LARGE_SUBSCRIPTION("standard-large-subscription");
 
     private String plan;
 
