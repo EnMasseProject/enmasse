4630edd5b3736aa97086013cf45917329cf969a6
Fabian Martinez <famartin@redhat.com>
2019-02-08 12:30:34 +0100

    cleaned imports

--- systemtests/src/main/java/io/enmasse/systemtest/GlobalLogCollector.java
+++ systemtests/src/main/java/io/enmasse/systemtest/GlobalLogCollector.java
@@ -4,12 +4,9 @@
  */
 package io.enmasse.systemtest;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -21,7 +18,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 import org.slf4j.Logger;
 
