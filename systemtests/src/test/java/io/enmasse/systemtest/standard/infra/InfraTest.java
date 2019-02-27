00f20da349a5df8acd6482689e77e93a9d93199d
Fabian Martinez <famartin@redhat.com>
2019-02-27 12:47:51 +0100

    fix identation problems

--- systemtests/src/test/java/io/enmasse/systemtest/standard/infra/InfraTest.java
+++ systemtests/src/test/java/io/enmasse/systemtest/standard/infra/InfraTest.java
@@ -38,11 +38,11 @@ import io.fabric8.kubernetes.api.model.Pod;
 import io.fabric8.kubernetes.api.model.ResourceRequirements;
 
 @Tag(isolated)
-class InfraTest extends InfraTestBase implements ITestBaseStandard{
+class InfraTest extends InfraTestBase implements ITestBaseStandard {
 
-	@Test
-	void testCreateInfra() throws Exception {
-		testInfra = new InfraConfigDefinition("test-infra-1", AddressSpaceType.STANDARD,  Arrays.asList(
+    @Test
+    void testCreateInfra() throws Exception {
+        testInfra = new InfraConfigDefinition("test-infra-1", AddressSpaceType.STANDARD, Arrays.asList(
                 new BrokerInfraSpec(Arrays.asList(
                         new InfraResource("memory", "512Mi"),
                         new InfraResource("storage", "1Gi"))),
@@ -50,15 +50,17 @@ class InfraTest extends InfraTestBase implements ITestBaseStandard{
                         new InfraResource("memory", "256Mi")), 200, 2),
                 new AdminInfraSpec(Collections.singletonList(
                         new InfraResource("memory", "512Mi")))), environment.enmasseVersion());
-		plansProvider.createInfraConfig(testInfra);
+        plansProvider.createInfraConfig(testInfra);
 
-		exampleAddressPlan = new AddressPlanDefinition("example-queue-plan", AddressType.TOPIC, Arrays.asList(
-        		new AddressResource("broker", 1.0), new AddressResource("router", 1.0)));
+        exampleAddressPlan = new AddressPlanDefinition("example-queue-plan", AddressType.TOPIC, Arrays.asList(
+                new AddressResource("broker", 1.0),
+                new AddressResource("router", 1.0)));
 
         plansProvider.createAddressPlan(exampleAddressPlan);
 
         AddressSpacePlanDefinition exampleSpacePlan = new AddressSpacePlanDefinition("example-space-plan",
-        		testInfra.getName(), AddressSpaceType.STANDARD,
+                testInfra.getName(),
+                AddressSpaceType.STANDARD,
                 Arrays.asList(
                         new AddressSpaceResource("broker", 3.0),
                         new AddressSpaceResource("router", 3.0),
@@ -68,93 +70,98 @@ class InfraTest extends InfraTestBase implements ITestBaseStandard{
         plansProvider.createAddressSpacePlan(exampleSpacePlan);
 
         exampleAddressSpace = new AddressSpace("example-address-space", AddressSpaceType.STANDARD,
-        		exampleSpacePlan.getName(), AuthService.STANDARD);
+                exampleSpacePlan.getName(), AuthService.STANDARD);
         createAddressSpace(exampleAddressSpace);
 
         setAddresses(exampleAddressSpace, Destination.topic("example-queue", exampleAddressPlan.getName()));
 
         assertInfra("512Mi", Optional.of("1Gi"), 2, "256Mi", "512Mi");
 
-	}
+    }
 
-	@Test
-	void testIncrementInfra() throws Exception {
-	    testReplaceInfra("1Gi", "2Gi", 3, "512Mi", "768Mi");
-	}
+    @Test
+    void testIncrementInfra() throws Exception {
+        testReplaceInfra("1Gi", "2Gi", 3, "512Mi", "768Mi");
+    }
 
-	@Test
-	void testDecrementInfra() throws Exception {
-	    testReplaceInfra("512Mi", "512Mi", 1, "128Mi", "256Mi");
-	}
+    @Test
+    void testDecrementInfra() throws Exception {
+        testReplaceInfra("512Mi", "512Mi", 1, "128Mi", "256Mi");
+    }
 
-	void testReplaceInfra(String brokerMemory, String brokerStorage, int routerReplicas, String routerMemory, String adminMemory) throws Exception {
-	    testCreateInfra();
+    void testReplaceInfra(String brokerMemory, String brokerStorage, int routerReplicas, String routerMemory, String adminMemory) throws Exception {
+        testCreateInfra();
 
-	    Boolean updatePersistentVolumeClaim = volumeResizingSupported();
+        Boolean updatePersistentVolumeClaim = volumeResizingSupported();
 
-	    InfraConfigDefinition infra = new InfraConfigDefinition("test-infra-2", AddressSpaceType.STANDARD,  Arrays.asList(
-	            new BrokerInfraSpec(Arrays.asList(
-	                    new InfraResource("memory", brokerMemory),
-	                    new InfraResource("storage", brokerStorage)), updatePersistentVolumeClaim),
-	            new RouterInfraSpec(Collections.singletonList(
-	                    new InfraResource("memory", routerMemory)), 200, routerReplicas),
-	            new AdminInfraSpec(Collections.singletonList(
-	                    new InfraResource("memory", adminMemory)))), environment.enmasseVersion());
-	    plansProvider.createInfraConfig(infra);
+        InfraConfigDefinition infra = new InfraConfigDefinition("test-infra-2", AddressSpaceType.STANDARD, Arrays.asList(
+                new BrokerInfraSpec(Arrays.asList(
+                        new InfraResource("memory", brokerMemory),
+                        new InfraResource("storage", brokerStorage)), updatePersistentVolumeClaim),
+                new RouterInfraSpec(Collections.singletonList(
+                        new InfraResource("memory", routerMemory)), 200, routerReplicas),
+                new AdminInfraSpec(Collections.singletonList(
+                        new InfraResource("memory", adminMemory)))), environment.enmasseVersion());
+        plansProvider.createInfraConfig(infra);
 
-	    AddressSpacePlanDefinition exampleSpacePlan = new AddressSpacePlanDefinition("example-space-plan-2",
-	            infra.getName(), AddressSpaceType.STANDARD,
-	            Arrays.asList(
-	                    new AddressSpaceResource("broker", 3.0),
-	                    new AddressSpaceResource("router", 3.0),
-	                    new AddressSpaceResource("aggregate", 5.0)),
-	            Arrays.asList(exampleAddressPlan));
+        AddressSpacePlanDefinition exampleSpacePlan = new AddressSpacePlanDefinition("example-space-plan-2",
+                infra.getName(), AddressSpaceType.STANDARD,
+                Arrays.asList(
+                        new AddressSpaceResource("broker", 3.0),
+                        new AddressSpaceResource("router", 3.0),
+                        new AddressSpaceResource("aggregate", 5.0)),
+                Arrays.asList(exampleAddressPlan));
+        plansProvider.createAddressSpacePlan(exampleSpacePlan);
 
-	    plansProvider.createAddressSpacePlan(exampleSpacePlan);
+        exampleAddressSpace.setPlan(exampleSpacePlan.getName());
+        replaceAddressSpace(exampleAddressSpace);
 
-	    exampleAddressSpace.setPlan(exampleSpacePlan.getName());
-	    replaceAddressSpace(exampleAddressSpace);
+        waitUntilInfraReady(
+                () -> assertInfra(brokerMemory,
+                        updatePersistentVolumeClaim != null && updatePersistentVolumeClaim ? Optional.of(brokerStorage) : Optional.empty(),
+                        routerReplicas,
+                        routerMemory,
+                        adminMemory),
+                new TimeoutBudget(5, TimeUnit.MINUTES));
 
-	    waitUntilInfraReady(
-	            () -> assertInfra(brokerMemory, updatePersistentVolumeClaim!=null && updatePersistentVolumeClaim ? Optional.of(brokerStorage) : Optional.empty(), routerReplicas, routerMemory, adminMemory),
-	            new TimeoutBudget(5, TimeUnit.MINUTES));
+    }
 
-	}
+    @Test
+    void testReadInfra() throws Exception {
+        testCreateInfra();
+        InfraConfigDefinition actualInfra = plansProvider.getStandardInfraConfig(testInfra.getName());
 
-	@Test
-	void testReadInfra() throws Exception {
-		testCreateInfra();
-		InfraConfigDefinition actualInfra = plansProvider.getStandardInfraConfig(testInfra.getName());
+        assertEquals(testInfra.getName(), actualInfra.getName());
+        assertEquals(testInfra.getType(), actualInfra.getType());
 
-		assertEquals(testInfra.getName(), actualInfra.getName());
-		assertEquals(testInfra.getType(), actualInfra.getType());
+        AdminInfraSpec expectedAdmin = (AdminInfraSpec) getInfraComponent(testInfra, InfraSpecComponent.ADMIN_INFRA_RESOURCE);
+        AdminInfraSpec actualAdmin = (AdminInfraSpec) getInfraComponent(actualInfra, InfraSpecComponent.ADMIN_INFRA_RESOURCE);
+        assertEquals(expectedAdmin.getRequiredValueFromResource("memory"), actualAdmin.getRequiredValueFromResource("memory"));
 
-		AdminInfraSpec expectedAdmin = (AdminInfraSpec) getInfraComponent(testInfra, InfraSpecComponent.ADMIN_INFRA_RESOURCE);
-		AdminInfraSpec actualAdmin = (AdminInfraSpec) getInfraComponent(actualInfra, InfraSpecComponent.ADMIN_INFRA_RESOURCE);
-		assertEquals(expectedAdmin.getRequiredValueFromResource("memory"), actualAdmin.getRequiredValueFromResource("memory"));
+        BrokerInfraSpec expectedBroker = (BrokerInfraSpec) getInfraComponent(testInfra, InfraSpecComponent.BROKER_INFRA_RESOURCE);
+        BrokerInfraSpec actualBroker = (BrokerInfraSpec) getInfraComponent(actualInfra, InfraSpecComponent.BROKER_INFRA_RESOURCE);
+        assertEquals(expectedBroker.getRequiredValueFromResource("memory"), actualBroker.getRequiredValueFromResource("memory"));
+        assertEquals(expectedBroker.getRequiredValueFromResource("storage"), actualBroker.getRequiredValueFromResource("storage"));
+        assertEquals(expectedBroker.getAddressFullPolicy(), actualBroker.getAddressFullPolicy());
+        assertEquals(expectedBroker.getStorageClassName(), actualBroker.getStorageClassName());
 
-		BrokerInfraSpec expectedBroker = (BrokerInfraSpec) getInfraComponent(testInfra, InfraSpecComponent.BROKER_INFRA_RESOURCE);
-		BrokerInfraSpec actualBroker = (BrokerInfraSpec) getInfraComponent(actualInfra, InfraSpecComponent.BROKER_INFRA_RESOURCE);
-		assertEquals(expectedBroker.getRequiredValueFromResource("memory"), actualBroker.getRequiredValueFromResource("memory"));
-		assertEquals(expectedBroker.getRequiredValueFromResource("storage"), actualBroker.getRequiredValueFromResource("storage"));
-		assertEquals(expectedBroker.getAddressFullPolicy(), actualBroker.getAddressFullPolicy());
-		assertEquals(expectedBroker.getStorageClassName(), actualBroker.getStorageClassName());
+        RouterInfraSpec expectedRouter = (RouterInfraSpec) getInfraComponent(testInfra, InfraSpecComponent.ROUTER_INFRA_RESOURCE);
+        RouterInfraSpec actualRouter = (RouterInfraSpec) getInfraComponent(actualInfra, InfraSpecComponent.ROUTER_INFRA_RESOURCE);
+        assertEquals(expectedRouter.getRequiredValueFromResource("memory"), actualRouter.getRequiredValueFromResource("memory"));
+        assertEquals(expectedRouter.getLinkCapacity(), actualRouter.getLinkCapacity());
+        assertEquals(expectedRouter.getMinReplicas(), actualRouter.getMinReplicas());
+    }
 
-		RouterInfraSpec expectedRouter = (RouterInfraSpec) getInfraComponent(testInfra, InfraSpecComponent.ROUTER_INFRA_RESOURCE);
-		RouterInfraSpec actualRouter = (RouterInfraSpec) getInfraComponent(actualInfra, InfraSpecComponent.ROUTER_INFRA_RESOURCE);
-		assertEquals(expectedRouter.getRequiredValueFromResource("memory"), actualRouter.getRequiredValueFromResource("memory"));
-		assertEquals(expectedRouter.getLinkCapacity(), actualRouter.getLinkCapacity());
-		assertEquals(expectedRouter.getMinReplicas(), actualRouter.getMinReplicas());
-	}
-
-	private boolean assertInfra(String brokerMemory, Optional<String> brokerStorage, int routerReplicas, String routermemory, String adminMemory) {
+    private boolean assertInfra(String brokerMemory, Optional<String> brokerStorage, int routerReplicas, String routermemory, String adminMemory) {
         log.info("Checking router infra");
         List<Pod> routerPods = TestUtils.listRouterPods(kubernetes, exampleAddressSpace);
         assertEquals(routerReplicas, routerPods.size(), "incorrect number of routers");
 
         for (Pod router : routerPods) {
             ResourceRequirements resources = router.getSpec().getContainers().stream()
-                    .filter(container -> container.getName().equals("router")).findFirst().map(Container::getResources)
+                    .filter(container -> container.getName().equals("router"))
+                    .findFirst()
+                    .map(Container::getResources)
                     .get();
             assertEquals(routermemory, resources.getLimits().get("memory").getAmount(),
                     "Router memory limit incorrect");
@@ -164,6 +171,6 @@ class InfraTest extends InfraTestBase implements ITestBaseStandard{
         assertAdminConsole(adminMemory);
         assertBroker(brokerMemory, brokerStorage);
         return true;
-   	}
+    }
 
 }
