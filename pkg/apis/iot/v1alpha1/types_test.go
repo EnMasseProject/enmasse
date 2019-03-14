9f554d171f43e817aef99ddcbe5346592e4922f4
Jens Reimann <ctron@dentrassi.de>
2019-03-14 19:25:45 +0100

    Add the generic IoTProject config section to Go (#2448)

---
+++ pkg/apis/iot/v1alpha1/types_test.go
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2019, EnMasse authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+
+package v1alpha1
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+const testExpected1 = `{"downstreamStrategy":{},"configuration":{"enabled":true,"foo":{"bar":"baz","bar2":2}}}`
+
+// Test to decode and re-encode the generic config section
+func TestSerializeConfiguration(t *testing.T) {
+	spec := &IoTProjectSpec{}
+	if err := json.Unmarshal([]byte(`
+{
+	"configuration": {
+		"enabled": true,
+		"foo": {
+			"bar": "baz",
+			"bar2": 2
+        }
+	}
+}
+`), spec); err != nil {
+		t.Fatalf("Failed to decode: %v", err)
+		return
+	}
+
+	data, err := json.Marshal(spec)
+	if err != nil {
+		t.Fatalf("Failed to encode: %v", err)
+		return
+	}
+
+	if string(data) != testExpected1 {
+		t.Error("Expected JSON to be: ", testExpected1, " but was ", string(data))
+	}
+}
