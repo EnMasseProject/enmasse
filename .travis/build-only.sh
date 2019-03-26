dbda26ed89d88c6e734464a00dc2c58263412e74
Ulf Lilleengen <lulf@redhat.com>
2019-03-26 13:41:57 +0100

    Parallelize container build

--- .travis/build-only.sh
+++ .travis/build-only.sh
@@ -10,6 +10,6 @@ export DOCKER_REGISTRY="localhost:5000"
 # We do a slim build and drop all output to shrink the console output
 
 make SKIP_TESTS=true > /dev/null
-make docker_build > /dev/null
+make -j 4 docker_build > /dev/null
 make -j 4 docker_tag docker_push > /dev/null
 make -C templates > /dev/null
--- .travis/build.sh
+++ .travis/build.sh
@@ -23,7 +23,7 @@ make clean
 
 time make
 
-time make docker_build
+time make -j4 docker_build
 
 echo "Tagging Docker Images"
 if use_external_registry
