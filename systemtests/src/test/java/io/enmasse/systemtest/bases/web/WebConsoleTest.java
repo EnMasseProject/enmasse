da0856f81a0bc6fa1918562ae225b81558d0db46
David Kornel <kornys@outlook.com>
2019-04-21 18:23:55 +0200

    Wait for item present

--- systemtests/src/test/java/io/enmasse/systemtest/bases/web/WebConsoleTest.java
+++ systemtests/src/test/java/io/enmasse/systemtest/bases/web/WebConsoleTest.java
@@ -741,7 +741,8 @@ public abstract class WebConsoleTest extends TestBaseWithShared implements ISele
         consoleWebPage.next();
 
         // assert new address is Topic
-        assertEquals(AddressType.TOPIC.toString(), consoleWebPage.getAddressItem(destTopic).getType(),
+        assertEquals(AddressType.TOPIC.toString(),
+                ((AddressWebItem) selenium.waitUntilItemPresent(60, () -> consoleWebPage.getAddressItem(destTopic))).getType(),
                 "Console failed, expected TOPIC type");
 
         waitForDestinationsReady(sharedAddressSpace, destTopic);
