e13b9a77c2b582609a2fa8838d7b5f64b7d8f300
Vanessa <vbusch@redhat.com>
2019-04-23 14:32:11 -0400

    Modified way of detecting if we are kubernetes
    Signed-off-by: Vanessa <vbusch@redhat.com>

--- console/console-init/ui/src/components/MessagingInstances/MessagingInstance/InstanceLoader.js
+++ console/console-init/ui/src/components/MessagingInstances/MessagingInstance/InstanceLoader.js
@@ -11,45 +11,42 @@ class InstanceLoader {
   };
 
   loadNamespaces() {
-    return axios.get('apis/project.openshift.io/v1/').then(() => {
+
+    if (window.env.OPENSHIFT_AVAILABLE) {
       return axios.get('apis/project.openshift.io/v1/projects')
-          .then(response => {
-            return this.translateNamespaces(response.data);
-          })
-          .catch(error => {
-              console.log('FAILED to load namespaces', error);
-              throw(error);
-            }
-          );
-      }
-    ).catch(() => {
+        .then(response => {
+          return this.translateNamespaces(response.data);
+        })
+        .catch(error => {
+            console.log('FAILED to load namespaces', error);
+            throw(error);
+          }
+        );
+    } else {
       console.log('Can not load namespaces in Kubernetes');
-      return new Promise((resolve, reject) =>
-        resolve([]));
-    });
+      return new Promise((resolve, reject) => resolve([]));
+    }
   }
 
   loadInstances() {
-    return axios.get('apis/project.openshift.io/v1/')
-      .then(() => {
-        return this.loadNamespaces().then(namespaces => {
-            let promises = [];
-            namespaces.forEach(namespace => {
-              promises.push(loadMessagingInstance(namespace));
-            });
-            return Promise.all(promises)
-              .then(values => {
-                return [].concat.apply([], values);
-              })
-              .catch(error => {
-                console.log('failed: ', error);
-                return error;
-              });
+    if (window.env.OPENSHIFT_AVAILABLE) {
+      return this.loadNamespaces().then(namespaces => {
+        let promises = [];
+        namespaces.forEach(namespace => {
+          promises.push(loadMessagingInstance(namespace));
+        });
+        return Promise.all(promises)
+          .then(values => {
+            return [].concat.apply([], values);
+          })
+          .catch(error => {
+            console.log('failed: ', error);
+            return error;
           });
-        })
-      .catch(() => {
-        return loadMessagingInstances();
       });
+    } else {
+      return loadMessagingInstances();
+    }
   }
 };
 
