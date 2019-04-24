72c5393911479bfd56c8600d1406bd12e876251c
Ulf Lilleengen <lulf@redhat.com>
2019-04-24 07:03:03 +0200

    Set up microk8s

--- .travis/setup-kubernetes.sh
+++ .travis/setup-kubernetes.sh
@@ -14,20 +14,10 @@ ansible-playbook ${SYSTEMTESTS_DIR}/ansible/playbooks/environment.yml \
     --extra-vars "{\"openshift_client_url\": \"${OPENSHIFT_CLIENT_URL}\", \"minikube_url\": \"${MINIKUBE_RELEASE_URL}\", \"kubectl_url\": \"${KUBECTL_RELEASE_URL}\"}" \
     -t openshift,kubectl,minikube
 
-export MINIKUBE_WANTUPDATENOTIFICATION=false
-export MINIKUBE_WANTREPORTERRORPROMPT=false
-export MINIKUBE_HOME=$HOME
-export CHANGE_MINIKUBE_NONE_USER=true
-mkdir $HOME/.kube || true
-touch $HOME/.kube/config
-
 sudo sh -c 'sed -e 's/journald/json-file/g' -i /etc/docker/daemon.json'
 sudo service docker restart && sleep 20
 
-docker run -d -p 5000:5000 registry
+sudo snap install microk8s --classic
+microk8s.enable dns storage registry
 
 export KUBECONFIG=$HOME/.kube/config
-sudo -E minikube start --vm-driver=none --bootstrapper=localkube --kubernetes-version v1.9.4 --insecure-registry localhost:5000
-minikube config set WantUpdateNotification false
-minikube update-context
-sudo -E minikube addons enable default-storageclass
