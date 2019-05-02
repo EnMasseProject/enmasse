fa815cfb8398c816187a23283b390ae1b4ebc815
Trystram Jean-Baptiste <jbtrystram@redhat.com>
2019-05-02 15:16:47 +0200

    update api-model
    Signed-off-by: Trystram Jean-Baptiste <jbtrystram@redhat.com>

---
+++ api-model/src/main/java/io/enmasse/iot/model/v1/InfinispanDeviceRegistry.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2019, EnMasse authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+
+package io.enmasse.iot.model.v1;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+import io.fabric8.kubernetes.api.model.Doneable;
+import io.sundr.builder.annotations.Buildable;
+import io.sundr.builder.annotations.Inline;
+
+@Buildable(
+        editableEnabled = false,
+        generateBuilderPackage = false,
+        builderPackage = "io.fabric8.kubernetes.api.builder",
+        inline = @Inline(
+                type = Doneable.class,
+                prefix = "Doneable",
+                value = "done"
+                )
+        )
+@JsonInclude(NON_NULL)
+public class InfinispanDeviceRegistry {
+
+    private ContainerConfig container;
+    private String infinispanServerAddress;
+
+    public ContainerConfig getContainer() {
+        return container;
+    }
+    public void setContainer(ContainerConfig container) {
+        this.container = container;
+    }
+
+    public String getInfinispanServerAddress() {
+        return infinispanServerAddress;
+    }
+    public void setInfinispanServerAddress(String infinispanServerAddress) {
+        this.infinispanServerAddress = infinispanServerAddress;
+    }
+}
