e9980b83a91332babe31e5bd1ca6a27fec98ea91
Fabian Martinez <famartin@redhat.com>
2019-05-07 12:53:18 +0200

    test of messaging during restart core components

--- systemtests/src/test/java/io/enmasse/systemtest/common/CommonTest.java
+++ systemtests/src/test/java/io/enmasse/systemtest/common/CommonTest.java
@@ -8,6 +8,7 @@ import io.enmasse.address.model.Address;
 import io.enmasse.address.model.AddressSpace;
 import io.enmasse.address.model.AuthenticationServiceType;
 import io.enmasse.systemtest.*;
+import io.enmasse.systemtest.amqp.AmqpClient;
 import io.enmasse.systemtest.bases.TestBase;
 import io.enmasse.systemtest.cmdclients.KubeCMDClient;
 import io.enmasse.systemtest.utils.AddressSpaceUtils;
@@ -18,12 +19,18 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 import static io.enmasse.systemtest.TestTag.isolated;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -54,6 +61,7 @@ class CommonTest extends TestBase {
         labels.add(new Label("name", "keycloak"));
         labels.add(new Label("name", "keycloak-controller"));
         labels.add(new Label("name", "address-space-controller"));
+        labels.add(new Label("name", "enmasse-operator"));
 
         UserCredentials user = new UserCredentials("frantisek", "dobrota");
         AddressSpace standard = AddressSpaceUtils.createAddressSpaceObject("addr-space-restart-standard", AddressSpaceType.STANDARD, AuthenticationServiceType.STANDARD);
@@ -115,6 +123,114 @@ class CommonTest extends TestBase {
         assertTrue(KubeCMDClient.runQDstat(qdRouterName, "-c", "--sasl-username=jenda", "--sasl-password=cenda").getRetCode());
         assertTrue(KubeCMDClient.runQDstat(qdRouterName, "-a", "--sasl-username=jenda", "--sasl-password=cenda").getRetCode());
         assertTrue(KubeCMDClient.runQDstat(qdRouterName, "-l", "--sasl-username=jenda", "--sasl-password=cenda").getRetCode());
+    }
+
+    @Test
+    void testMessagingDuringRestartComponents() throws Exception {
+        List<Label> labels = new LinkedList<>();
+        labels.add(new Label("component", "api-server"));
+        labels.add(new Label("name", "address-space-controller"));
+        labels.add(new Label("name", "enmasse-operator"));
+
+        UserCredentials user = new UserCredentials("frantisek", "dobrota");
+        AddressSpace standard = AddressSpaceUtils.createAddressSpaceObject("addr-space-restart-standard", AddressSpaceType.STANDARD, AuthenticationServiceType.STANDARD);
+        AddressSpace brokered = AddressSpaceUtils.createAddressSpaceObject("addr-space-restart-brokered", AddressSpaceType.BROKERED, AuthenticationServiceType.STANDARD);
+        createAddressSpaceList(standard, brokered);
+        createUser(brokered, user);
+        createUser(standard, user);
+
+        List<Address> brokeredAddresses = getAllBrokeredAddresses();
+        List<Address> standardAddresses = getAllStandardAddresses();
+
+        setAddresses(brokered, brokeredAddresses.toArray(new Address[0]));
+        setAddresses(standard, standardAddresses.toArray(new Address[0]));
+
+        assertCanConnect(brokered, user, brokeredAddresses);
+        assertCanConnect(standard, user, standardAddresses);
+
+        log.info("------------------------------------------------------------");
+        log.info("------------------- Start with restating -------------------");
+        log.info("------------------------------------------------------------");
+
+        List<Pod> pods = kubernetes.listPods();
+        int runningPodsBefore = pods.size();
+        log.info("Number of running pods before restarting any: {}", runningPodsBefore);
+
+        for(Address addr : brokeredAddresses) {
+            log.info("Starting messaging in address {} and address space {}", addr.getSpec().getAddress(), brokered.getMetadata().getName());
+            for (Label label : labels) {
+                doMessagingDuringRestart(label, runningPodsBefore, user, brokered, addr);
+            }
+        }
+
+        for(Address addr : standardAddresses) {
+            log.info("Starting messaging in address {} and address space {}", addr.getSpec().getAddress(), standard.getMetadata().getName());
+            for (Label label : labels) {
+                doMessagingDuringRestart(label, runningPodsBefore, user, standard, addr);
+            }
+        }
+
+    }
+
+    private void doMessagingDuringRestart(Label label, int runningPodsBefore, UserCredentials user, AddressSpace brokered, Address addr) throws Exception {
+        log.info("Starting messaging");
+        AddressType addressType = AddressType.getEnum(addr.getSpec().getType());
+        AmqpClient client = amqpClientFactory.createAddressClient(brokered, addressType);
+        client.getConnectOptions().setCredentials(user);
+        AtomicInteger counter = new AtomicInteger(0);
+        CompletableFuture<Object> future = doConcurrentMessaging(client, addr.getSpec().getAddress(), counter);
+        log.info("Restarting {}", label.labelValue);
+        KubeCMDClient.deletePodByLabel(label.getLabelName(), label.getLabelValue());
+        TestUtils.waitForExpectedReadyPods(kubernetes, runningPodsBefore, new TimeoutBudget(10, TimeUnit.MINUTES));
+        if(future.isCompletedExceptionally()) {
+            future.get();
+        }
+        future.complete(new Object());
+        assertTrue(counter.get() > 1, "receive messages did not work");
+    }
+
+    private CompletableFuture<Object> doConcurrentMessaging(AmqpClient client, String address, AtomicInteger counter) {
+
+        CompletableFuture<Object> resultPromise = new CompletableFuture<>();
+
+        CompletableFuture.runAsync(()->{
+            client.recvMessages(address, msg -> {
+                log.info("Message received");
+                if(resultPromise.isDone()) {
+                    return true;
+                }
+                try {
+                    Thread.sleep(2000);
+                } catch ( InterruptedException e ) {
+                    e.printStackTrace();
+                    resultPromise.completeExceptionally(e);
+                    return true;
+                }
+                try {
+                    sendMessage(client, address, counter);
+                } catch ( Exception e ) {
+                    e.printStackTrace();
+                    resultPromise.completeExceptionally(e);
+                    return true;
+                }
+                return false;
+            });
+            try {
+                sendMessage(client, address, counter);
+            } catch ( Exception e ) {
+                e.printStackTrace();
+                resultPromise.completeExceptionally(e);
+            }
+        }, runnable -> new Thread(runnable).start());
+
+        return resultPromise;
+    }
+
+    private void sendMessage(AmqpClient client, String address, AtomicInteger counter) throws Exception {
+        counter.incrementAndGet();
+        Future<Integer> sent = client.sendMessages(address, Collections.singletonList(UUID.randomUUID().toString()));
+        log.info("Message sent");
+        assertEquals(1, sent.get(15, TimeUnit.SECONDS));
     }
 
     private void assertSystemWorks(AddressSpace brokered, AddressSpace standard, UserCredentials existingUser,
