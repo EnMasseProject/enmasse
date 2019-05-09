a76f34a94fccef36fcf1bb491b0e61169c64e59d
Keith Wall <kwall@apache.org>
2019-05-09 14:08:02 +0100

    Bump Artemis to 2.8.0

--- Makefile.env.mk
+++ Makefile.env.mk
@@ -41,7 +41,7 @@ GRAFANA_IMAGE ?= grafana/grafana:5.3.1
 APPLICATION_MONITORING_OPERATOR_IMAGE ?= quay.io/integreatly/application-monitoring-operator:0.0.5
 KUBE_STATE_METRICS_IMAGE ?= quay.io/coreos/kube-state-metrics:v1.4.0
 QDROUTERD_BASE_IMAGE ?= enmasseproject/qdrouterd-base:1.6.0-rc1
-BROKER_IMAGE ?= quay.io/enmasse/artemis-base:2.6.4
+BROKER_IMAGE ?= quay.io/enmasse/artemis-base:2.8.0
 
 CONTROLLER_MANAGER_IMAGE   ?= $(DOCKER_REGISTRY_PREFIX)$(DOCKER_ORG)/controller-manager:$(IMAGE_VERSION)
 
