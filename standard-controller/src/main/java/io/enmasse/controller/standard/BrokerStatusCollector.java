e366e239591a05529844e76d9e7580dfb053c3c2
Keith Wall <kwall@apache.org>
2019-05-10 17:01:19 +0100

    Guard against unexpected queue depth values (ARTEMIS-1982) when determining if a Broker is drained.

--- standard-controller/src/main/java/io/enmasse/controller/standard/BrokerStatusCollector.java
+++ standard-controller/src/main/java/io/enmasse/controller/standard/BrokerStatusCollector.java
@@ -8,8 +8,14 @@ import io.enmasse.amqp.Artemis;
 import io.enmasse.amqp.SyncRequestClient;
 import io.fabric8.kubernetes.api.model.Pod;
 import io.fabric8.kubernetes.client.internal.readiness.Readiness;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 class BrokerStatusCollector {
+    private static final Logger log = LoggerFactory.getLogger(BrokerStatusCollector.class);
+
     private final Kubernetes kubernetes;
     private final BrokerClientFactory brokerClientFactory;
 
@@ -20,16 +26,34 @@ class BrokerStatusCollector {
 
     long getQueueMessageCount(String queue, String clusterId) throws Exception {
         long totalMessageCount = 0;
-        for (Pod broker : kubernetes.listBrokers(clusterId)) {
+        List<Pod> pods = kubernetes.listBrokers(clusterId);
+        for (Pod broker : pods) {
             if (Readiness.isPodReady(broker)) {
                 try (
                         SyncRequestClient brokerClient = brokerClientFactory.connectBrokerManagementClient(broker.getStatus().getPodIP(), 5673);
                         Artemis artemis = new Artemis(brokerClient);
                         ) {
-                    totalMessageCount += artemis.getQueueMessageCount(queue);
+                    long queueMessageCount = artemis.getQueueMessageCount(queue);
+                    if (queueMessageCount < 0) {
+                        // ARTEMIS-1982?
+                        throw new IllegalStateException(String.format("Depth for queue '%s' on broker pod '%s' in cluster '%s' reported negative (%d)",
+                                queue,
+                                broker.getMetadata().getName(),
+                                clusterId,
+                                queueMessageCount));
+                    }
+                    totalMessageCount += queueMessageCount;
                 }
+            } else {
+                throw new IllegalStateException(String.format("Broker pod '%s' in cluster '%s' was not ready (%s), cannot get depth for queue '%s' at this time.",
+                        broker.getMetadata().getName(),
+                        clusterId,
+                        broker.getStatus(),
+                        queue
+                ));
             }
         }
+        log.info("Queue '{}' on cluster '{}' ({} shard(s)) has depth: {}", queue,  clusterId, pods.size(), totalMessageCount);
         return totalMessageCount;
     }
 }
