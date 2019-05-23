e51c5c3969adc94f6b6cd52b73849713600f81fa
Fabian Martinez <46371672+famartinrh@users.noreply.github.com>
2019-05-23 12:06:39 +0200

    infinispan and file device registry tests and move it to smoke tests (#2841)
    * device registry test for both versions and move it to smoke tests

---
+++ systemtests/src/test/java/io/enmasse/systemtest/iot/registry/FileDeviceRegistryTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2019, EnMasse authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+package io.enmasse.systemtest.iot.registry;
+
+import java.nio.ByteBuffer;
+
+import io.enmasse.iot.model.v1.IoTConfig;
+import io.enmasse.iot.model.v1.IoTConfigBuilder;
+import io.enmasse.systemtest.CertBundle;
+import io.enmasse.systemtest.utils.CertificateUtils;
+
+public class FileDeviceRegistryTest extends DeviceRegistryTestBase{
+
+    @Override
+    protected IoTConfig provideIoTConfig() throws Exception {
+        CertBundle certBundle = CertificateUtils.createCertBundle();
+        return new IoTConfigBuilder()
+                .withNewMetadata()
+                .withName("default")
+                .endMetadata()
+                .withNewSpec()
+                .withNewServices()
+                .withNewDeviceRegistry()
+                .withNewFile()
+                .endFile()
+                .endDeviceRegistry()
+                .endServices()
+                .withNewAdapters()
+                .withNewMqtt()
+                .withNewEndpoint()
+                .withNewKeyCertificateStrategy()
+                .withCertificate(ByteBuffer.wrap(certBundle.getCert().getBytes()))
+                .withKey(ByteBuffer.wrap(certBundle.getKey().getBytes()))
+                .endKeyCertificateStrategy()
+                .endEndpoint()
+                .endMqtt()
+                .endAdapters()
+                .endSpec()
+                .build();
+    }
+
+}
---
+++ systemtests/src/test/java/io/enmasse/systemtest/iot/registry/InfinispanDeviceRegistryTest.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2019, EnMasse authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+package io.enmasse.systemtest.iot.registry;
+
+import java.nio.ByteBuffer;
+
+import io.enmasse.iot.model.v1.IoTConfig;
+import io.enmasse.iot.model.v1.IoTConfigBuilder;
+import io.enmasse.systemtest.CertBundle;
+import io.enmasse.systemtest.Endpoint;
+import io.enmasse.systemtest.SystemtestsKubernetesApps;
+import io.enmasse.systemtest.utils.CertificateUtils;
+
+public class InfinispanDeviceRegistryTest extends DeviceRegistryTestBase{
+
+    @Override
+    protected IoTConfig provideIoTConfig() throws Exception {
+        Endpoint infinispanEndpoint = SystemtestsKubernetesApps.deployInfinispanServer(kubernetes.getInfraNamespace());
+        CertBundle certBundle = CertificateUtils.createCertBundle();
+        return new IoTConfigBuilder()
+                .withNewMetadata()
+                .withName("default")
+                .endMetadata()
+                .withNewSpec()
+                .withNewServices()
+                .withNewDeviceRegistry()
+                .withNewInfinispan()
+                .withInfinispanServerAddress(infinispanEndpoint.toString())
+                .endInfinispan()
+                .endDeviceRegistry()
+                .endServices()
+                .withNewAdapters()
+                .withNewMqtt()
+                .withNewEndpoint()
+                .withNewKeyCertificateStrategy()
+                .withCertificate(ByteBuffer.wrap(certBundle.getCert().getBytes()))
+                .withKey(ByteBuffer.wrap(certBundle.getKey().getBytes()))
+                .endKeyCertificateStrategy()
+                .endEndpoint()
+                .endMqtt()
+                .endAdapters()
+                .endSpec()
+                .build();
+    }
+
+    @Override
+    protected void removeIoTConfig() throws Exception {
+        super.removeIoTConfig();
+        SystemtestsKubernetesApps.deleteInfinispanServer(kubernetes.getInfraNamespace());
+    }
+
+
+}
