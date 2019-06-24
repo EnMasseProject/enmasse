909c8fa26f427b71b2d999ed9ea29cf6ef5c4c4c
Jens Reimann <jreimann@redhat.com>
2019-06-24 15:30:31 +0200

    Switch to Hono 1.0-M4

--- iot/iot-device-registry-infinispan/src/test/java/io/enmasse/iot/registry/infinispan/CacheRegistrationServiceTest.java
+++ iot/iot-device-registry-infinispan/src/test/java/io/enmasse/iot/registry/infinispan/CacheRegistrationServiceTest.java
@@ -5,42 +5,33 @@
 
 package io.enmasse.iot.registry.infinispan;
 
-import io.vertx.ext.unit.junit.VertxUnitRunner;
 import org.eclipse.hono.service.registration.AbstractCompleteRegistrationServiceTest;
 import org.eclipse.hono.service.registration.CompleteRegistrationService;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Rule;
-import org.junit.rules.Timeout;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import io.vertx.junit5.VertxExtension;
 
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Tests verifying behavior of {@link CacheRegistrationService}.
  *
  */
-@Ignore
-@RunWith(VertxUnitRunner.class)
+@Disabled
+@ExtendWith(VertxExtension.class)
 public class CacheRegistrationServiceTest extends AbstractCompleteRegistrationServiceTest {
 
     private static CacheRegistrationService service;
     private static EmbeddedHotRodServer server;
 
-
-    /**
-     * Global timeout for all test cases.
-     */
-    @Rule
-    public Timeout globalTimeout = new Timeout(5, TimeUnit.SECONDS);
-
     /**
      * Spin up the service using Infinispan EmbeddedCache.
      * @throws IOException if the Protobuf spec file cannot be found.
      */
-    @Before
+    @BeforeEach
     public void setUp() throws IOException {
 
         server = new EmbeddedHotRodServer();
@@ -52,7 +43,7 @@ public class CacheRegistrationServiceTest extends AbstractCompleteRegistrationSe
      *
      * @return
      */
-    @After
+    @AfterEach
     public void cleanUp() {
         server.stop();
     }
