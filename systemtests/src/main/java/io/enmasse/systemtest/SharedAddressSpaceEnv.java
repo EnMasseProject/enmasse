f93f8a789990b9c77ce739797c6e403abd707cdc
obabec <ond.babec@gmail.com>
2019-07-18 08:21:06 +0200

    Copyright header

--- systemtests/src/main/java/io/enmasse/systemtest/SharedAddressSpaceEnv.java
+++ systemtests/src/main/java/io/enmasse/systemtest/SharedAddressSpaceEnv.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright 2017-2018, EnMasse authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
 package io.enmasse.systemtest;
 
 import io.enmasse.admin.model.v1.AddressSpacePlan;
