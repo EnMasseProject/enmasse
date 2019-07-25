4925baf521d43c3f101f38596f1e13873d736d17
Rob Shelly <rshelly@redhat.com>
2019-07-25 23:42:03 +0100

    Removed dashes from metrics names

--- metrics-api/src/test/java/io/enmasse/metrics/api/PrometheusMetricsFormatterTest.java
+++ metrics-api/src/test/java/io/enmasse/metrics/api/PrometheusMetricsFormatterTest.java
@@ -16,25 +16,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PrometheusMetricsFormatterTest {
 
-    private static String appPrefix;
-
-    @BeforeAll
-    public static void setup() {
-        System.out.println("Starting tests");
-
-        try (java.io.InputStream inputStream = PrometheusMetricsFormatter.class.getResourceAsStream("/metrics.properties");){
-            if (inputStream == null) {
-                throw new IllegalStateException();
-            }
-            java.util.Properties properties = new Properties();
-            properties.load(inputStream);
-            appPrefix = properties.getProperty("appPrefix");
-        } catch (IOException e) {
-            System.out.println("Error reading app prefix {}");
-            throw new IllegalStateException();
-        }
-    }
-
     @Test
     public void testEscaping() {
         PrometheusMetricsFormatter formatter = new PrometheusMetricsFormatter();
@@ -43,7 +24,7 @@ public class PrometheusMetricsFormatterTest {
 
         String value = formatter.format(Collections.singletonList(metric));
         System.out.println(value);
-        String expected = "# HELP " + appPrefix + "_m1 mdesc\n# TYPE " + appPrefix + "_m1 gauge\n" + appPrefix + "_m1{key1=\"value1\",key2=\"\\\\this\\\"is\\nescaped\\\\\"} 2 3\n";
+        String expected = "# HELP enmasse_m1 mdesc\n# TYPE enmasse_m1 gauge\nenmasse_m1{key1=\"value1\",key2=\"\\\\this\\\"is\\nescaped\\\\\"} 2 3\n";
         assertEquals(expected, value);
     }
 
