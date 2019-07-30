02d766cc5b21f33eab2112ba47fb283902db7a40
Fabian Martinez <famartin@redhat.com>
2019-07-30 12:20:06 +0200

    split CommonTest.testMessagePersistent into multiple tests, one per address tested

---
+++ systemtests/src/test/java/io/enmasse/systemtest/bases/common/PersistentMessagesTestBase.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2019, EnMasse authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+package io.enmasse.systemtest.bases.common;
+
+import static io.enmasse.systemtest.TestTag.isolated;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.qpid.proton.message.Message;
+import org.junit.jupiter.api.Tag;
+import org.slf4j.Logger;
+
+import io.enmasse.address.model.Address;
+import io.enmasse.address.model.AddressSpace;
+import io.enmasse.systemtest.CustomLogger;
+import io.enmasse.systemtest.TimeoutBudget;
+import io.enmasse.systemtest.UserCredentials;
+import io.enmasse.systemtest.amqp.AmqpClient;
+import io.enmasse.systemtest.bases.TestBaseWithShared;
+import io.enmasse.systemtest.utils.AddressUtils;
+import io.enmasse.systemtest.utils.TestUtils;
+
+@Tag(isolated)
+public class PersistentMessagesTestBase extends TestBaseWithShared {
+
+    private static Logger log = CustomLogger.getLogger();
+
+    protected void doTestQueuePersistentMessages(Address address, int messagesBatch) throws Exception {
+        setAddresses(address);
+
+        int podCount = kubernetes.listPods().size();
+
+        sendDurableMessages(getSharedAddressSpace(), address, defaultCredentials, messagesBatch);
+
+        restartBrokers(podCount);
+
+        // Seems that the service/route can sometimes not be immediately available despite the pod being Ready.
+        assertConnectable(getSharedAddressSpace(), defaultCredentials);
+        receiveDurableMessages(getSharedAddressSpace(), address, defaultCredentials, messagesBatch);
+
+    }
+
+    protected void doTestTopicPersistentMessages(Address topic, Address subscription) throws Exception {
+        setAddresses(topic, subscription);
+
+        int podCount = kubernetes.listPods().size();
+
+        AmqpClient client = amqpClientFactory.createTopicClient(getSharedAddressSpace());
+        client.getConnectOptions().setCredentials(defaultCredentials);
+
+        log.info("Subscribe first receiver");
+        Future<List<Message>> recvResults = client.recvMessages(AddressUtils.getQualifiedSubscriptionAddress(subscription), 30);
+        sendDurableMessages(getSharedAddressSpace(), topic, defaultCredentials, 30);
+        assertThat("Wrong messages received: ", recvResults.get(1, TimeUnit.MINUTES).size(), is(30));
+        sendDurableMessages(getSharedAddressSpace(), topic, defaultCredentials, 30);
+
+        restartBrokers(podCount);
+
+        // Seems that the service/route can sometimes not be immediately available despite the pod being Ready.
+        assertConnectable(getSharedAddressSpace(), defaultCredentials);
+
+        log.info("Subscribe receiver again");
+        recvResults = client.recvMessages(AddressUtils.getQualifiedSubscriptionAddress(subscription), 30);
+        assertThat("Wrong messages received: batch2", recvResults.get(1, TimeUnit.MINUTES).size(), is(30));
+    }
+
+    private void restartBrokers(int podCount) throws InterruptedException {
+        kubernetes.deletePod(kubernetes.getInfraNamespace(), Collections.singletonMap("role", "broker"));
+        Thread.sleep(20_000);
+        TestUtils.waitForExpectedReadyPods(kubernetes, kubernetes.getInfraNamespace(), podCount, new TimeoutBudget(10, TimeUnit.MINUTES));
+        log.info("Broker pods restarted");
+    }
+
+    private void assertConnectable(AddressSpace space, UserCredentials user) throws Exception {
+        TimeoutBudget budget = new TimeoutBudget(1, TimeUnit.MINUTES);
+        String name = space.getMetadata().getName();
+        do {
+            try {
+                connectAddressSpace(space, user);
+                log.info("Successfully connected to address space : {}", name);
+                return;
+            } catch (IOException e) {
+                log.info("Failed to connect to address space: {} - {}", name, e.getMessage());
+            }
+            Thread.sleep(1000);
+        } while (!budget.timeoutExpired());
+
+        fail(String.format("Failed to assert address space %s connectable within timeout", name));
+    }
+
+}
--- systemtests/src/test/java/io/enmasse/systemtest/common/CommonTest.java
+++ systemtests/src/test/java/io/enmasse/systemtest/common/CommonTest.java
@@ -15,12 +15,10 @@ import io.enmasse.systemtest.cmdclients.KubeCMDClient;
 import io.enmasse.systemtest.utils.AddressUtils;
 import io.enmasse.systemtest.utils.TestUtils;
 import io.fabric8.kubernetes.api.model.Pod;
-import org.apache.qpid.proton.message.Message;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 
-import java.io.IOException;
 import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Future;
@@ -30,8 +28,6 @@ import java.util.stream.Collectors;
 
 import static io.enmasse.systemtest.TestTag.isolated;
 import static io.enmasse.systemtest.TestTag.nonPR;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @Tag(isolated)
@@ -296,149 +292,6 @@ class CommonTest extends TestBase {
 
     }
 
-    @Test
-    void testMessagePersistent() throws Exception {
-        UserCredentials user = new UserCredentials("frantisek", "dobrota");
-        AddressSpace standard = new AddressSpaceBuilder()
-                .withNewMetadata()
-                .withName("space-standard-persistent")
-                .withNamespace(kubernetes.getInfraNamespace())
-                .endMetadata()
-                .withNewSpec()
-                .withType(AddressSpaceType.STANDARD.toString())
-                .withPlan(AddressSpacePlans.STANDARD_UNLIMITED)
-                .withNewAuthenticationService()
-                .withName("standard-authservice")
-                .endAuthenticationService()
-                .endSpec()
-                .build();
-        AddressSpace brokered = new AddressSpaceBuilder()
-                .withNewMetadata()
-                .withName("space-brokered-persistent")
-                .withNamespace(kubernetes.getInfraNamespace())
-                .endMetadata()
-                .withNewSpec()
-                .withType(AddressSpaceType.BROKERED.toString())
-                .withPlan(AddressSpacePlans.BROKERED)
-                .withNewAuthenticationService()
-                .withName("standard-authservice")
-                .endAuthenticationService()
-                .endSpec()
-                .build();
-
-        createAddressSpaceList(standard, brokered);
-        createOrUpdateUser(brokered, user);
-        createOrUpdateUser(standard, user);
-
-        Address brokeredQueue = new AddressBuilder()
-                .withNewMetadata()
-                .withNamespace(standard.getMetadata().getNamespace())
-                .withName(AddressUtils.generateAddressMetadataName(brokered, "test-queue-brokered"))
-                .endMetadata()
-                .withNewSpec()
-                .withType("queue")
-                .withAddress("test-queue-brokered")
-                .withPlan(DestinationPlan.BROKERED_QUEUE)
-                .endSpec()
-                .build();
-
-        Address standardQueue = new AddressBuilder()
-                .withNewMetadata()
-                .withNamespace(standard.getMetadata().getNamespace())
-                .withName(AddressUtils.generateAddressMetadataName(standard, "test-queue-standard"))
-                .endMetadata()
-                .withNewSpec()
-                .withType("queue")
-                .withAddress("test-queue-standard")
-                .withPlan(DestinationPlan.STANDARD_SMALL_QUEUE)
-                .endSpec()
-                .build();
-
-        Address standardLargeQueue = new AddressBuilder()
-                .withNewMetadata()
-                .withNamespace(standard.getMetadata().getNamespace())
-                .withName(AddressUtils.generateAddressMetadataName(standard, "test-large-queue-standard"))
-                .endMetadata()
-                .withNewSpec()
-                .withType("queue")
-                .withAddress("test-large-queue-standard")
-                .withPlan(DestinationPlan.STANDARD_LARGE_QUEUE)
-                .endSpec()
-                .build();
-
-        Address standardXLargeQueue = new AddressBuilder()
-                .withNewMetadata()
-                .withNamespace(standard.getMetadata().getNamespace())
-                .withName(AddressUtils.generateAddressMetadataName(standard, "test-xlarge-queue-standard"))
-                .endMetadata()
-                .withNewSpec()
-                .withType("queue")
-                .withAddress("test-xlarge-queue-standard")
-                .withPlan(DestinationPlan.STANDARD_XLARGE_QUEUE)
-                .endSpec()
-                .build();
-
-        Address standardTopic = new AddressBuilder()
-                .withNewMetadata()
-                .withNamespace(standard.getMetadata().getNamespace())
-                .withName(AddressUtils.generateAddressMetadataName(standard, "test-topic-standard"))
-                .endMetadata()
-                .withNewSpec()
-                .withType("topic")
-                .withAddress("test-topic-standard")
-                .withPlan(DestinationPlan.STANDARD_LARGE_TOPIC)
-                .endSpec()
-                .build();
-
-        Address standardSub = new AddressBuilder()
-                .withNewMetadata()
-                .withNamespace(standard.getMetadata().getNamespace())
-                .withName(AddressUtils.generateAddressMetadataName(standard, "test-sub-standard"))
-                .endMetadata()
-                .withNewSpec()
-                .withType("subscription")
-                .withAddress("test-sub-standard")
-                .withTopic(standardTopic.getSpec().getAddress())
-                .withPlan(DestinationPlan.STANDARD_SMALL_SUBSCRIPTION)
-                .endSpec()
-                .build();
-
-        setAddresses(brokeredQueue, standardQueue, standardLargeQueue, standardXLargeQueue, standardTopic, standardSub);
-
-        int podCount = kubernetes.listPods().size();
-
-        sendDurableMessages(brokered, brokeredQueue, user, 100);
-        sendDurableMessages(standard, standardQueue, user, 30);
-        sendDurableMessages(standard, standardLargeQueue, user, 30);
-        sendDurableMessages(standard, standardXLargeQueue, user, 30);
-
-        AmqpClient client = amqpClientFactory.createTopicClient(standard);
-        client.getConnectOptions().setCredentials(user);
-
-        log.info("Subscribe first receiver");
-        Future<List<Message>> recvResults = client.recvMessages(AddressUtils.getQualifiedSubscriptionAddress(standardSub), 30);
-        sendDurableMessages(standard, standardTopic, user, 30);
-        assertThat("Wrong messages received: ", recvResults.get(1, TimeUnit.MINUTES).size(), is(30));
-        sendDurableMessages(standard, standardTopic, user, 30);
-
-        kubernetes.deletePod(kubernetes.getInfraNamespace(), Collections.singletonMap("role", "broker"));
-        Thread.sleep(20_000);
-        TestUtils.waitForExpectedReadyPods(kubernetes, kubernetes.getInfraNamespace(), podCount, new TimeoutBudget(10, TimeUnit.MINUTES));
-        log.info("Broker pods restarted");
-
-        // Seems that the service/route can sometimes not be immediately available despite the pod being Ready.
-        assertConnectable(brokered, user);
-        assertConnectable(standard, user);
-        receiveDurableMessages(brokered, brokeredQueue, user, 100);
-        receiveDurableMessages(standard, standardQueue, user, 30);
-        receiveDurableMessages(standard, standardLargeQueue, user, 30);
-        receiveDurableMessages(standard, standardXLargeQueue, user, 30);
-
-        log.info("Subscribe receiver again");
-        recvResults = client.recvMessages(AddressUtils.getQualifiedSubscriptionAddress(standardSub), 30);
-        assertThat("Wrong messages received: batch2", recvResults.get(1, TimeUnit.MINUTES).size(), is(30));
-    }
-
     /////////////////////////////////////////////////////////////////////
     // help methods
     /////////////////////////////////////////////////////////////////////
@@ -534,21 +387,5 @@ class CommonTest extends TestBase {
         return resultPromise;
     }
 
-    private void assertConnectable(AddressSpace space, UserCredentials user) throws Exception {
-        TimeoutBudget budget = new TimeoutBudget(1, TimeUnit.MINUTES);
-        String name = space.getMetadata().getName();
-        do {
-            try {
-                connectAddressSpace(space, user);
-                log.info("Successfully connected to address space : {}", name);
-                return;
-            } catch (IOException e) {
-                log.info("Failed to connect to address space: {} - {}", name, e.getMessage());
-            }
-            Thread.sleep(1000);
-        } while (!budget.timeoutExpired());
-
-        fail(String.format("Failed to assert address space %s connectable within timeout", name));
-    }
 }
 
