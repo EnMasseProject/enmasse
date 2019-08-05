f7b76b3893d67e47127e66d428aade869680e3e1
David Kornel <kornys@outlook.com>
2019-08-05 14:58:44 +0200

    Fix olm maven test profile

--- systemtests/scripts/run_test_component.sh
+++ systemtests/scripts/run_test_component.sh
@@ -43,6 +43,7 @@ elif [[ "${TEST_PROFILE}" = "systemtests-marathon" ]] \
     || [[ "${TEST_PROFILE}" = "smoke" ]] \
     || [[ "${TEST_PROFILE}" = "systemtests-smoke" ]] \
     || [[ "${TEST_PROFILE}" = "iot-release" ]] \
+    || [[ "${TEST_PROFILE}" = "olm" ]] \
     || [[ "${TEST_PROFILE}" = "smoke-iot" ]]; then
     run_test $(echo ${TEST_PROFILE} | sed 's/systemtests-//') ${TESTCASE} || failure=$(($failure + 1))
 elif [[ "${TEST_PROFILE}" = "systemtests-release" ]]; then
