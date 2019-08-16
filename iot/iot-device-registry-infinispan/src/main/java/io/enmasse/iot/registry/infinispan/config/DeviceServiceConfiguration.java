f973c5c0ca4a0fe5ed6128126be2bb66187b8b8b
Jens Reimann <jreimann@redhat.com>
2019-08-16 16:02:58 +0200

    Add missing license headers

--- iot/iot-device-registry-infinispan/src/main/java/io/enmasse/iot/registry/infinispan/config/DeviceServiceConfiguration.java
+++ iot/iot-device-registry-infinispan/src/main/java/io/enmasse/iot/registry/infinispan/config/DeviceServiceConfiguration.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2019, EnMasse authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+
 package io.enmasse.iot.registry.infinispan.config;
 
 import org.eclipse.hono.service.credentials.CredentialsAmqpEndpoint;
