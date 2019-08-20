e94ae431411d7cc8de16b9aefc3f6ac4f71a9d29
Ulf Lilleengen <lulf@redhat.com>
2019-08-20 11:15:21 +0200

    Ensure that addresses are not synced until addresses have been defined
    As proposed in #3101

--- agent/bin/agent.js
+++ agent/bin/agent.js
@@ -46,6 +46,7 @@ function start(env) {
             }
 
             server_promise.then(() => {
+                address_source.start();
                 if (env.ADDRESS_SPACE_TYPE === 'brokered') {
                     bind_event(address_source, 'addresses_defined', console_server.metrics);
                     console_server.listen_health(env);
