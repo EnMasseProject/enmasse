f9e003162bd32e5980380312ea70267b597d6cbb
Fabian Martinez <46371672+famartinrh@users.noreply.github.com>
2019-09-17 08:30:32 +0200

    basic tests of forwarders (#3194)

--- systemtests/src/test/java/io/enmasse/systemtest/isolated/bridging/ConnectorsTest.java
+++ systemtests/src/test/java/io/enmasse/systemtest/isolated/bridging/ConnectorsTest.java
@@ -11,13 +11,8 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtensionContext;
-import org.slf4j.Logger;
-
 import io.enmasse.address.model.Address;
 import io.enmasse.address.model.AddressBuilder;
 import io.enmasse.address.model.AddressSpace;
@@ -26,32 +21,30 @@ import io.enmasse.address.model.AddressSpaceSpecConnectorAddressRuleBuilder;
 import io.enmasse.address.model.AddressSpaceSpecConnectorBuilder;
 import io.enmasse.address.model.AddressSpaceSpecConnectorCredentialsBuilder;
 import io.enmasse.address.model.AddressSpaceSpecConnectorEndpointBuilder;
-import io.enmasse.systemtest.Endpoint;
 import io.enmasse.systemtest.UserCredentials;
 import io.enmasse.systemtest.amqp.AmqpClient;
-import io.enmasse.systemtest.amqp.AmqpConnectOptions;
-import io.enmasse.systemtest.amqp.QueueTerminusFactory;
-import io.enmasse.systemtest.bases.TestBase;
-import io.enmasse.systemtest.bases.isolated.ITestIsolatedStandard;
-import io.enmasse.systemtest.logs.CustomLogger;
+import io.enmasse.systemtest.bases.bridging.BridgingBase;
 import io.enmasse.systemtest.model.addressspace.AddressSpacePlans;
 import io.enmasse.systemtest.model.addressspace.AddressSpaceType;
-import io.enmasse.systemtest.platform.apps.SystemtestsKubernetesApps;
 import io.enmasse.systemtest.shared.standard.QueueTest;
 import io.enmasse.systemtest.time.TimeoutBudget;
 import io.enmasse.systemtest.utils.AddressSpaceUtils;
 import io.enmasse.systemtest.utils.TestUtils;
-import io.vertx.proton.ProtonClientOptions;
-import io.vertx.proton.ProtonQoS;
 
-class ConnectorsTest extends TestBase implements ITestIsolatedStandard{
+class ConnectorsTest extends BridgingBase {
 
     //tested usecases
     //Sending messages to a remote AMQP endpoint via a local address space - by creating a connector and using prefixing
     //Receiving messages from a remote AMQP endpoint via a local address space - by creating a connector and using prefixing
     //If I config a connector to refer to a host that does not exist, I'd expect the addressspace overall to report ready true, whereas the connector's status should report the failure.
 
-    private static final String REMOTE_NAME = "remote1";
+    //usecases to test
+    //Invalid connector names
+    //Invalid patterns
+    //Restart broker to ensure router reattached to broker and you can send/recv messages
+    //Using TLS
+    //Using mutual TLS (SASL EXTERNAL) instead of credentials
+
     private static final String BASIC_QUEUE1 = "basic1";
     private static final String BASIC_QUEUE2 = "basic2";
     private static final String SLASHED_QUEUE1 = "dummy/foo";
@@ -59,27 +52,9 @@ class ConnectorsTest extends TestBase implements ITestIsolatedStandard{
     private static final String BASIC_QUEUES_PATTERN = "*";
     private static final String SLASHED_QUEUES_PATTERN = "dummy/*";
 
-    private static Logger log = CustomLogger.getLogger();
-
-    private final String remoteBrokerNamespace = "systemtests-external-broker";
-    private final String remoteBrokerUsername = "test-user";
-    private final String remoteBrokerPassword = "test-password";
-    private Endpoint remoteBrokerEndpoint;
-
-    @BeforeEach
-    void deployBroker() throws Exception {
-        SystemtestsKubernetesApps.deployAMQBroker(remoteBrokerNamespace, remoteBrokerUsername, remoteBrokerPassword, SLASHED_QUEUE1, SLASHED_QUEUE2, BASIC_QUEUE1, BASIC_QUEUE2);
-        remoteBrokerEndpoint = SystemtestsKubernetesApps.getAMQBrokerEndpoint(remoteBrokerNamespace);
-        log.info("Broker endpoint: {}", remoteBrokerEndpoint);
-    }
-
-    @AfterEach
-    void undeployBroker(ExtensionContext context) throws Exception {
-        if (context.getExecutionException().isPresent()) { //test failed
-            logCollector.collectLogsOfPodsInNamespace(remoteBrokerNamespace);
-            logCollector.collectEvents(remoteBrokerNamespace);
-        }
-        SystemtestsKubernetesApps.deleteAMQBroker(remoteBrokerNamespace);
+    @Override
+    protected String[] remoteBrokerQueues() {
+        return new String[] {SLASHED_QUEUE1, SLASHED_QUEUE2, BASIC_QUEUE1, BASIC_QUEUE2};
     }
 
     @Test
@@ -169,7 +144,7 @@ class ConnectorsTest extends TestBase implements ITestIsolatedStandard{
         localClient.getConnectOptions().setCredentials(localUser);
 
         for(String remoteQueue : remoteQueues) {
-            String connectorQueue = REMOTE_NAME + "/" + remoteQueue;
+            String connectorQueue = getRemoteName(remoteQueue);
             localClient.sendMessages(connectorQueue, TestUtils.generateMessages(messagesBatch));
         }
 
@@ -206,62 +181,15 @@ class ConnectorsTest extends TestBase implements ITestIsolatedStandard{
         localClient.getConnectOptions().setCredentials(localUser);
 
         for(String remoteQueue : remoteQueues) {
-            String connectorQueue = REMOTE_NAME + "/" + remoteQueue;
+            String connectorQueue = getRemoteName(remoteQueue);
             var receivedFromQueue = localClient.recvMessages(connectorQueue, messagesBatch);
             assertThat("Wrong count of messages received from connector queue: "+connectorQueue, receivedFromQueue.get(1, TimeUnit.MINUTES).size(), is(messagesBatch));
         }
 
     }
 
-    private AddressSpace createAddressSpace(String name, String addressRule) throws Exception {
-        AddressSpace space = new AddressSpaceBuilder()
-                .withNewMetadata()
-                .withNamespace(kubernetes.getInfraNamespace())
-                .withName(name)
-                .endMetadata()
-                .withNewSpec()
-                .withType(AddressSpaceType.STANDARD.toString())
-                .withPlan(AddressSpacePlans.STANDARD_SMALL)
-                .withConnectors(new AddressSpaceSpecConnectorBuilder()
-                        .withName(REMOTE_NAME)
-                        .addToEndpointHosts(new AddressSpaceSpecConnectorEndpointBuilder()
-                                .withHost(remoteBrokerEndpoint.getHost())
-                                .withPort(remoteBrokerEndpoint.getPort())
-                                .build())
-                        .withCredentials(new AddressSpaceSpecConnectorCredentialsBuilder()
-                                .withNewUsername()
-                                    .withValue(remoteBrokerUsername)
-                                    .endUsername()
-                                .withNewPassword()
-                                    .withValue(remoteBrokerPassword)
-                                    .endPassword()
-                                .build())
-                        .addToAddresses(new AddressSpaceSpecConnectorAddressRuleBuilder()
-                                .withName("queuesrule")
-                                .withPattern(addressRule)
-                                .build())
-                        .build())
-                .endSpec()
-                .build();
-        resourcesManager.createAddressSpace(space);
-        AddressSpaceUtils.waitForAddressSpaceConnectorsReady(space);
-        return space;
-    }
-
-    private AmqpClient createClientToRemoteBroker() {
-
-        ProtonClientOptions clientOptions = new ProtonClientOptions();
-        clientOptions.setSsl(false);
-
-        AmqpConnectOptions connectOptions = new AmqpConnectOptions()
-                .setTerminusFactory(new QueueTerminusFactory())
-                .setEndpoint(remoteBrokerEndpoint)
-                .setProtonClientOptions(clientOptions)
-                .setQos(ProtonQoS.AT_LEAST_ONCE)
-                .setUsername(remoteBrokerUsername)
-                .setPassword(remoteBrokerPassword);
-
-        return getAmqpClientFactory().createClient(connectOptions);
+    private String getRemoteName(String remoteQueue) {
+        return REMOTE_NAME + "/" + remoteQueue;
     }
 
 }
---
+++ systemtests/src/test/java/io/enmasse/systemtest/isolated/bridging/ForwardersTest.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright 2019, EnMasse authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+package io.enmasse.systemtest.isolated.bridging;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.Test;
+import io.enmasse.address.model.Address;
+import io.enmasse.address.model.AddressBuilder;
+import io.enmasse.address.model.AddressSpace;
+import io.enmasse.address.model.AddressSpecForwarderBuilder;
+import io.enmasse.address.model.AddressSpecForwarderDirection;
+import io.enmasse.systemtest.UserCredentials;
+import io.enmasse.systemtest.amqp.AmqpClient;
+import io.enmasse.systemtest.bases.bridging.BridgingBase;
+import io.enmasse.systemtest.model.address.AddressType;
+import io.enmasse.systemtest.model.addressplan.DestinationPlan;
+import io.enmasse.systemtest.time.TimeoutBudget;
+import io.enmasse.systemtest.utils.AddressUtils;
+import io.enmasse.systemtest.utils.TestUtils;
+
+public class ForwardersTest extends BridgingBase {
+
+    //tested usecases
+    //Forwarding messages from a local queue in a local address space to a destination on a remote AMQP endpoint
+    //Forwarding messages to a local queue in a local address space from a destination on a remote AMQP endpoint
+
+    //forward to FULL remote queue
+    //forward from FULL remote queue
+
+    private static final String REMOTE_QUEUE1 = "queue1";
+    private static final String REMOTE_QUEUE2 = "queue2";
+
+    @Override
+    protected String[] remoteBrokerQueues() {
+        return new String[] {REMOTE_QUEUE1, REMOTE_QUEUE2};
+    }
+
+    @Test
+    public void testForwardToRemoteQueue() throws Exception {
+        AddressSpace space = createAddressSpace("forward-to-remote", "*");
+        Address forwarder = new AddressBuilder()
+                .withNewMetadata()
+                .withName(AddressUtils.generateAddressMetadataName(space, "forwarder-queue1"))
+                .withNamespace(kubernetes.getInfraNamespace())
+                .endMetadata()
+                .withNewSpec()
+                .withAddress("forwarder-queue1")
+                .withType(AddressType.QUEUE.toString())
+                .withPlan(DestinationPlan.STANDARD_SMALL_QUEUE)
+                .addToForwarders(new AddressSpecForwarderBuilder()
+                        .withName("forwarder1")
+                        .withRemoteAddress(REMOTE_NAME + "/" + REMOTE_QUEUE1)
+                        .withDirection(AddressSpecForwarderDirection.out)
+                        .build())
+                .endSpec()
+                .build();
+        resourcesManager.setAddresses(forwarder);
+        AddressUtils.waitForForwardersReady(new TimeoutBudget(1, TimeUnit.MINUTES), forwarder);
+
+        UserCredentials localUser = new UserCredentials("test", "test");
+        resourcesManager.createOrUpdateUser(space, localUser);
+
+        int messagesBatch = 50;
+
+        //send to address with forwarder
+
+        AmqpClient localClient = getAmqpClientFactory().createQueueClient(space);
+        localClient.getConnectOptions().setCredentials(localUser);
+
+        localClient.sendMessages(forwarder.getSpec().getAddress(), TestUtils.generateMessages(messagesBatch));
+
+        //receive in remote broker
+
+        AmqpClient clientToRemote = createClientToRemoteBroker();
+
+        var receivedInRemote = clientToRemote.recvMessages(REMOTE_QUEUE1, messagesBatch);
+
+        assertThat("Wrong count of messages received from remote queue: "+REMOTE_QUEUE1, receivedInRemote.get(1, TimeUnit.MINUTES).size(), is(messagesBatch));
+
+    }
+
+    @Test
+    public void testForwardFromRemoteQueue() throws Exception {
+        AddressSpace space = createAddressSpace("forward-from-remote", "*");
+        Address forwarder = new AddressBuilder()
+                .withNewMetadata()
+                .withName(AddressUtils.generateAddressMetadataName(space, "forwarder-queue1"))
+                .withNamespace(kubernetes.getInfraNamespace())
+                .endMetadata()
+                .withNewSpec()
+                .withAddress("forwarder-queue1")
+                .withType(AddressType.QUEUE.toString())
+                .withPlan(DestinationPlan.STANDARD_SMALL_QUEUE)
+                .addToForwarders(new AddressSpecForwarderBuilder()
+                        .withName("forwarder1")
+                        .withRemoteAddress(REMOTE_NAME + "/" + REMOTE_QUEUE1)
+                        .withDirection(AddressSpecForwarderDirection.in)
+                        .build())
+                .endSpec()
+                .build();
+        resourcesManager.setAddresses(forwarder);
+        AddressUtils.waitForForwardersReady(new TimeoutBudget(1, TimeUnit.MINUTES), forwarder);
+
+        UserCredentials localUser = new UserCredentials("test", "test");
+        resourcesManager.createOrUpdateUser(space, localUser);
+
+        int messagesBatch = 50;
+
+        //send to remote broker
+
+        AmqpClient clientToRemote = createClientToRemoteBroker();
+
+        clientToRemote.sendMessages(REMOTE_QUEUE1, TestUtils.generateMessages(messagesBatch));
+
+        //receive in address with forwarder
+
+        AmqpClient localClient = getAmqpClientFactory().createQueueClient(space);
+        localClient.getConnectOptions().setCredentials(localUser);
+
+        var receivedInRemote = localClient.recvMessages(forwarder.getSpec().getAddress(), messagesBatch);
+
+        assertThat("Wrong count of messages received in local address: "+forwarder.getSpec().getAddress(), receivedInRemote.get(1, TimeUnit.MINUTES).size(), is(messagesBatch));
+
+    }
+
+}
