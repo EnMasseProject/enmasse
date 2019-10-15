1d23d1c8e73d846b64bcfebcf4a9d13d1cea7ff0
Ulf Lilleengen <lulf@redhat.com>
2019-10-15 05:17:11 +0200

    Update broker 2.10.1 (#3306)
    * Update to Artemis 2.10.1

--- Makefile.env.mk
+++ Makefile.env.mk
@@ -39,7 +39,7 @@ ALERTMANAGER_IMAGE ?= prom/alertmanager:v0.15.2
 GRAFANA_IMAGE ?= grafana/grafana:5.3.1
 APPLICATION_MONITORING_OPERATOR_IMAGE ?= quay.io/integreatly/application-monitoring-operator:0.0.25
 KUBE_STATE_METRICS_IMAGE ?= quay.io/coreos/kube-state-metrics:v1.4.0
-BROKER_IMAGE ?= quay.io/enmasse/artemis-base:2.10.0
+BROKER_IMAGE ?= quay.io/enmasse/artemis-base:2.10.1
 
 CONTROLLER_MANAGER_IMAGE   ?= $(DOCKER_REGISTRY_PREFIX)$(DOCKER_ORG)/controller-manager:$(IMAGE_VERSION)
 
