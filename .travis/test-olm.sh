dbcab65955f3d78de977e9e6680b92b414de9369
Ulf Lilleengen <lulf@redhat.com>
2019-11-13 15:08:45 +0100

    Load catalog source from registry

--- .travis/test-olm.sh
+++ .travis/test-olm.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 set -e
+set -x
 CURDIR=`readlink -f \`dirname $0\``
 source ${CURDIR}/common.sh
 
+DOCKER_ORG=${DOCKER_ORG:-$USER}
 TAG=${TAG:-latest}
-SYSTEMTEST_ARGS=${SYSTEMTEST_ARGS:-"io.enmasse.**.SmokeTest"}
-SYSTEMTEST_PROFILE=${SYSTEMTEST_PROFILE:-"smoke"}
+if use_external_registry
+then
+    export IMAGE_VERSION=${TAG}
+else
+    export DOCKER_REGISTRY="localhost:5000"
+fi
 
-echo "NOT YET IMPLEMENTED"
+cat<<EOF | kubectl create -f -
+apiVersion: operators.coreos.com/v1
+kind: CatalogSource
+metadata:
+  name: enmasse-ocs
+  namespace: 
+  labels:
+    operator: test
+spec:
+  displayName: EnMasse Community Operator
+  image: ${DOCKER_REGISTRY}/${DOCKER_ORG}/olm-manifest:${TAG}
+  publisher: travis
+  sourceType: grpc
+EOF
