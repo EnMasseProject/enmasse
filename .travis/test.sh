929dd63f028dad998c1f9e87441c0ef2a145a75e
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2019-12-04 08:39:53 +0100

    Use env var in assembly

--- .travis/test.sh
+++ .travis/test.sh
@@ -4,7 +4,8 @@ CURDIR=`readlink -f \`dirname $0\``
 source ${CURDIR}/common.sh
 
 VERSION=$(grep "release.version" pom.properties| cut -d'=' -f2)
-export TEMPLATES=${PWD}/templates/build/enmasse-${VERSION}
+TAG=${TAG:-latest}
+export TEMPLATES=${PWD}/templates/build/enmasse-${TAG}
 
 echo "Running smoke tests"
 time make SYSTEMTEST_PROFILE=smoke systemtests
--- Makefile
+++ Makefile
@@ -48,7 +48,7 @@ templates: imageenv
 deploy: build_go
 	$(IMAGE_ENV) mvn -Prelease deploy $(MAVEN_ARGS)
 
-build_java: build_go templates
+build_java: build_go
 	$(IMAGE_ENV) mvn package -q -B $(MAVEN_ARGS)
 
 build_go: $(GO_DIRS) test_go
