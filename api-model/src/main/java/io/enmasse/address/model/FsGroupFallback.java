e325ae63922b821d6a1162798715ffa4b395d4e0
Keith Wall <kwall@apache.org>
2019-12-05 11:12:23 +0000

    Guard empty env var

--- api-model/src/main/java/io/enmasse/address/model/FsGroupFallback.java
+++ api-model/src/main/java/io/enmasse/address/model/FsGroupFallback.java
@@ -8,7 +8,9 @@ package io.enmasse.address.model;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Strings;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class FsGroupFallback {
@@ -19,14 +21,16 @@ public class FsGroupFallback {
     private static final Map<String, Long> FS_GROUP_FALLBACK;
 
     static {
-
-        final String json = System.getenv().getOrDefault("FS_GROUP_FALLBACK_MAP", "{}");
-        ObjectMapper objectMapper = new ObjectMapper();
-        try {
-
-            FS_GROUP_FALLBACK = objectMapper.readValue(json, MAP_STRING_LONG);
-        } catch (JsonProcessingException e) {
-            throw new ExceptionInInitializerError(e);
+        final String json = System.getenv().get("FS_GROUP_FALLBACK_MAP");
+        if (Strings.isNullOrEmpty(json)) {
+            FS_GROUP_FALLBACK = new HashMap<>();
+        } else {
+            ObjectMapper objectMapper = new ObjectMapper();
+            try {
+                FS_GROUP_FALLBACK = objectMapper.readValue(json, MAP_STRING_LONG);
+            } catch (JsonProcessingException e) {
+                throw new ExceptionInInitializerError(e);
+            }
         }
     }
 
