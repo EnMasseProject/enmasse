786afa69cf040197f139ff5c3d5408b9c96baa5d
Jens Reimann <jreimann@redhat.com>
2019-12-06 09:03:22 +0100

    Fix credentials when using Infinispan 10

--- systemtests/src/main/java/io/enmasse/systemtest/iot/DefaultDeviceRegistry.java
+++ systemtests/src/main/java/io/enmasse/systemtest/iot/DefaultDeviceRegistry.java
@@ -5,13 +5,43 @@
 
 package io.enmasse.systemtest.iot;
 
+import java.util.Optional;
+
 import io.enmasse.iot.model.v1.DeviceRegistryServiceConfig;
 import io.enmasse.iot.model.v1.DeviceRegistryServiceConfigBuilder;
+import io.enmasse.iot.model.v1.ExternalInfinispanServer;
+import io.enmasse.iot.model.v1.ExternalInfinispanServerBuilder;
+import io.enmasse.systemtest.Endpoint;
 import io.enmasse.systemtest.platform.apps.SystemtestsKubernetesApps;
 
 public final class DefaultDeviceRegistry {
 
-    private DefaultDeviceRegistry() {
+    public static enum InfinispanVersion {
+        V9, V10,
+    }
+
+    private DefaultDeviceRegistry() {}
+
+    public static ExternalInfinispanServer externalServer(final InfinispanVersion version, final Endpoint infinispanEndpoint) {
+        var builder = new ExternalInfinispanServerBuilder()
+                .withHost(infinispanEndpoint.getHost())
+                .withPort(infinispanEndpoint.getPort());
+
+        // credentials aligned with 'templates/iot/examples/infinispan/manual'
+        builder = builder.withUsername("app")
+                .withPassword("test12");
+
+        switch (version) {
+            case V10:
+                builder = builder.withSaslRealm("default")
+                        .withSaslServerName("infinispan");
+            default:
+                builder = builder.withSaslRealm("ApplicationRealm")
+                        .withSaslServerName("hotrod");
+                break;
+        }
+
+        return builder.build();
     }
 
     public static DeviceRegistryServiceConfig newInfinispanBased() throws Exception {
@@ -20,19 +50,23 @@ public final class DefaultDeviceRegistry {
                 .withNewInfinispan()
                 .withNewServer()
 
-                .withNewExternal()
-                .withHost(infinispanEndpoint.getHost())
-                .withPort(infinispanEndpoint.getPort())
-                // credentials aligned with 'templates/iot/examples/infinispan/manual'
-                .withUsername("app")
-                .withPassword("test12")
-                .withSaslRealm("ApplicationRealm")
-                .withSaslServerName("hotrod")
-                .endExternal()
+                .withExternal(externalServer(version(), infinispanEndpoint))
 
                 .endServer()
                 .endInfinispan()
                 .build();
+    }
+
+    /**
+     * Get the version to use for infinispan.
+     * @return The version to use, never returns {@code null}.
+     * @throws IllegalArgumentException If a value is set, but cannot be mapped to an enum literal.
+     */
+    private static InfinispanVersion version() {
+        return Optional
+                .ofNullable(System.getenv("INFINISPAN_VERSION"))
+                .map(InfinispanVersion::valueOf)
+                .orElse(InfinispanVersion.V10);
     }
 
     public static DeviceRegistryServiceConfig newFileBased() {
