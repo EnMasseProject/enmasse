bebbb66da39538a0b3b5027cfa8da67591a15400
Jens Reimann <jreimann@redhat.com>
2019-12-11 13:17:22 +0100

    Have Infinispan 9 and 10 in parallel

---
+++ iot/iot-infinispan-base-9/src/main/java/io/enmasse/iot/infinispan/cache/DeviceConnectionCacheProvider.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2019, EnMasse authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+
+package io.enmasse.iot.infinispan.cache;
+
+import java.util.Optional;
+
+import org.infinispan.client.hotrod.RemoteCache;
+import org.infinispan.client.hotrod.RemoteCacheManager;
+import org.infinispan.client.hotrod.configuration.ServerConfigurationBuilder;
+import org.infinispan.client.hotrod.marshall.ProtoStreamMarshaller;
+import org.infinispan.configuration.cache.CacheMode;
+import org.infinispan.configuration.cache.Index;
+import org.infinispan.protostream.SerializationContext;
+import org.infinispan.protostream.annotations.ProtoSchemaBuilder;
+import org.infinispan.query.remote.client.ProtobufMetadataManagerConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import io.enmasse.iot.infinispan.config.InfinispanProperties;
+import io.enmasse.iot.infinispan.devcon.DeviceConnectionKey;
+
+@Component
+public class DeviceConnectionCacheProvider extends AbstractCacheProvider {
+
+    private static final Logger log = LoggerFactory.getLogger(DeviceConnectionCacheProvider.class);
+
+    private static final String GENERATED_PROTOBUF_FILE_NAME = "deviceConnection.proto";
+
+    @Autowired
+    public DeviceConnectionCacheProvider(final InfinispanProperties properties) throws Exception {
+        super(properties);
+    }
+
+    @Override
+    protected void customizeServerConfiguration(ServerConfigurationBuilder configuration) {
+        configuration.marshaller(ProtoStreamMarshaller.class);
+    }
+
+    @Override
+    public void start() throws Exception {
+       super.start();
+       configureSerializer(this.remoteCacheManager);
+    }
+
+    private static void configureSerializer(RemoteCacheManager remoteCacheManager) throws Exception {
+        final SerializationContext serCtx = ProtoStreamMarshaller.getSerializationContext(remoteCacheManager);
+
+        final String generatedSchema = new ProtoSchemaBuilder()
+
+                .addClass(DeviceConnectionKey.class)
+                .packageName(DeviceConnectionKey.class.getPackageName())
+                .fileName(GENERATED_PROTOBUF_FILE_NAME)
+                .build(serCtx);
+
+        log.debug("Generated protobuf schema - {}", generatedSchema);
+
+        remoteCacheManager
+                .getCache(ProtobufMetadataManagerConstants.PROTOBUF_METADATA_CACHE_NAME)
+                .put(GENERATED_PROTOBUF_FILE_NAME, generatedSchema);
+
+    }
+
+    public org.infinispan.configuration.cache.Configuration buildConfiguration() {
+        return new org.infinispan.configuration.cache.ConfigurationBuilder()
+
+                .indexing()
+                .index(Index.NONE)
+
+                .clustering()
+                .cacheMode(CacheMode.DIST_SYNC)
+                .hash()
+                .numOwners(1)
+
+                .build();
+    }
+
+    public RemoteCache<DeviceConnectionKey, String> getOrCreateDeviceStateCache() {
+        return getOrCreateCache(properties.getDeviceStatesCacheName(), this::buildConfiguration);
+    }
+
+    public Optional<RemoteCache<DeviceConnectionKey, String>> getDeviceStateCache() {
+        return getCache(properties.getDeviceStatesCacheName());
+    }
+
+    public RemoteCache<DeviceConnectionKey, String> getDeviceStateTestCache() {
+        return getOrCreateTestCache(properties.getDeviceStatesCacheName(), buildConfiguration());
+    }
+}
---
+++ iot/iot-infinispan-base-9/src/main/java/io/enmasse/iot/infinispan/cache/DeviceManagementCacheProvider.java
@@ -0,0 +1,140 @@
+/*
+ * Copyright 2019, EnMasse authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+
+package io.enmasse.iot.infinispan.cache;
+
+import java.util.Optional;
+
+import org.infinispan.client.hotrod.RemoteCache;
+import org.infinispan.client.hotrod.RemoteCacheManager;
+import org.infinispan.client.hotrod.configuration.ServerConfigurationBuilder;
+import org.infinispan.client.hotrod.marshall.ProtoStreamMarshaller;
+import org.infinispan.configuration.cache.CacheMode;
+import org.infinispan.configuration.cache.Index;
+import org.infinispan.protostream.SerializationContext;
+import org.infinispan.protostream.annotations.ProtoSchemaBuilder;
+import org.infinispan.query.remote.client.ProtobufMetadataManagerConstants;
+import org.infinispan.transaction.TransactionMode;
+import org.infinispan.util.concurrent.IsolationLevel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import io.enmasse.iot.infinispan.config.InfinispanProperties;
+import io.enmasse.iot.infinispan.device.CredentialKey;
+import io.enmasse.iot.infinispan.device.DeviceCredential;
+import io.enmasse.iot.infinispan.device.DeviceInformation;
+import io.enmasse.iot.infinispan.device.DeviceKey;
+
+@Component
+public class DeviceManagementCacheProvider extends AbstractCacheProvider {
+
+    private static final Logger log = LoggerFactory.getLogger(DeviceManagementCacheProvider.class);
+    private static final String GENERATED_PROTOBUF_FILE_NAME = "deviceRegistry.proto";
+
+    @Autowired
+    public DeviceManagementCacheProvider(final InfinispanProperties properties) {
+        super(properties);
+    }
+
+    @Override
+    protected void customizeServerConfiguration(ServerConfigurationBuilder configuration) {
+        configuration.marshaller(ProtoStreamMarshaller.class);
+    }
+
+    @Override
+    public void start() throws Exception {
+       super.start();
+       configureSerializer(this.remoteCacheManager);
+    }
+
+    private static void configureSerializer(RemoteCacheManager remoteCacheManager) throws Exception {
+        final SerializationContext serCtx = ProtoStreamMarshaller.getSerializationContext(remoteCacheManager);
+
+        final String generatedSchema = new ProtoSchemaBuilder()
+
+                .addClass(DeviceKey.class)
+                .addClass(DeviceInformation.class)
+                .addClass(DeviceCredential.class)
+
+                .packageName("io.enmasse.iot.registry.infinispan.data")
+                .fileName(GENERATED_PROTOBUF_FILE_NAME)
+                .build(serCtx);
+
+        log.debug("Generated protobuf schema - {}", generatedSchema);
+
+        remoteCacheManager
+                .getCache(ProtobufMetadataManagerConstants.PROTOBUF_METADATA_CACHE_NAME)
+                .put(GENERATED_PROTOBUF_FILE_NAME, generatedSchema);
+
+    }
+
+    public org.infinispan.configuration.cache.Configuration buildDeviceManagementConfiguration() {
+        return new org.infinispan.configuration.cache.ConfigurationBuilder()
+
+                .indexing()
+                .index(Index.PRIMARY_OWNER)
+                .addProperty("default.indexmanager", "org.infinispan.query.indexmanager.InfinispanIndexManager")
+                .addProperty("default.worker.execution", "async")
+                .addProperty("default.index_flush_interval", "500")
+                .addIndexedEntity(DeviceInformation.class)
+                .addIndexedEntity(DeviceCredential.class)
+
+//                .persistence()
+//                .addSingleFileStore()
+//                .fetchPersistentState(true)
+
+                .clustering()
+                .cacheMode(CacheMode.DIST_SYNC)
+                .hash()
+                .numOwners(1)
+
+                .invocationBatching()
+                .enable()
+
+                .transaction()
+                .autoCommit(true)
+                .transactionMode(TransactionMode.TRANSACTIONAL)
+                .useSynchronization(true)
+                .recovery().disable()
+
+                .locking()
+                .isolationLevel(IsolationLevel.READ_COMMITTED)
+
+                .build();
+    }
+
+    public RemoteCache<DeviceKey, DeviceInformation> getOrCreateDeviceManagementCache() {
+        return getOrCreateCache(properties.getDevicesCacheName(), this::buildDeviceManagementConfiguration);
+    }
+
+    public Optional<RemoteCache<DeviceKey, DeviceInformation>> getDeviceManagementCache() {
+        return getCache(properties.getDevicesCacheName());
+    }
+
+    public org.infinispan.configuration.cache.Configuration buildAdapterCredentialsConfiguration() {
+        return new org.infinispan.configuration.cache.ConfigurationBuilder()
+
+                .indexing()
+                .index(Index.NONE)
+
+                .clustering()
+                .cacheMode(CacheMode.DIST_SYNC)
+                .hash()
+                .numOwners(1)
+
+                .build();
+    }
+
+    public RemoteCache<CredentialKey, String> getOrCreateAdapterCredentialsCache() {
+        return getOrCreateCache(properties.getAdapterCredentialsCacheName(), this::buildAdapterCredentialsConfiguration);
+    }
+
+    public Optional<RemoteCache<CredentialKey, String>> getAdapterCredentialsCache() {
+        return getCache(properties.getAdapterCredentialsCacheName());
+    }
+
+}
