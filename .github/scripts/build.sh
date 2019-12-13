ec378e4efdaed435a5e3d0564b1a648b15f91af9
obabec <ond.babec@gmail.com>
2019-12-13 14:22:32 +0100

    Update build script

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -13,11 +13,11 @@ if [[ -v RELEASE ]]
 then
     echo "Logging in to Docker Hub"
     docker login -u ${REGISTRY_USER} -p ${REGISTRY_PASS} ${DOCKER_REGISTRY}
+    make TAG=${VERSION} docker_tag docker_push
 fi
 
 echo "Push to registry"
 make -j 4 docker_tag docker_push
-make TAG=${VERSION} docker_push
 
 echo "Generate templates"
 make templates
