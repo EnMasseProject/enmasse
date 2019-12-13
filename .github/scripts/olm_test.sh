07b6b513eb21804cbc8174df6738c5759dd2ed32
David Kornel <kornys@outlook.com>
2019-12-13 20:50:39 +0100

    Split github tests to stages

--- .github/scripts/olm_test.sh
+++ .github/scripts/olm_test.sh
@@ -4,8 +4,5 @@ set -e
 TAG=${TAG:-latest}
 export TEMPLATES=${PWD}/templates/build/enmasse-${TAG}
 
-echo "Running smoke tests"
-time make SYSTEMTEST_PROFILE=smoke systemtests
-
 echo "Running OLM tests"
 time make SYSTEMTEST_ARGS=olm.** systemtests
