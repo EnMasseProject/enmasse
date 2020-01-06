a93854d99cda3799feb2e2145e33fe7f205efb8e
Ondrej Babec <ond.babec@gmail.com>
2020-01-06 12:22:03 +0100

    Deploy certs

---
+++ .github/scripts/deploy_iot_certs.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+
+./templates/build/enmasse-latest/install/components/iot/examples/k8s-tls/create
+./templates/build/enmasse-latest/install/components/iot/examples/k8s-tls/deploy
