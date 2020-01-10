6e57f26330609032ad9a3a7f8abd6029b1d703d9
Keith Wall <kwall@apache.org>
2020-01-10 12:32:42 +0000

    Implement the improved address space type/address type API

--- pkg/consolegraphql/resolvers/resolver_plans_and_types.go
+++ pkg/consolegraphql/resolvers/resolver_plans_and_types.go
@@ -7,9 +7,12 @@ package resolvers
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/enmasseproject/enmasse/pkg/apis/admin/v1beta2"
 	"github.com/enmasseproject/enmasse/pkg/consolegraphql/cache"
+	"io/ioutil"
+	"os"
 	"sort"
 )
 
@@ -164,13 +167,115 @@ func (r *addressPlanSpecK8sResolver) AddressType(ctx context.Context, obj *v1bet
 }
 
 func (r *queryResolver) AddressSpaceTypes(ctx context.Context) ([]AddressSpaceType, error) {
+	err := lazyLoadAddressSpaceTypes()
+	if err != nil {
+		return nil, err
+	}
+
 	types := make([]AddressSpaceType, 0)
-	types = append(types, AddressSpaceTypeStandard, AddressSpaceTypeBrokered)
+	for _, t := range addressSpaceTypes {
+		types = append(types, t.Spec.AddressSpaceType)
+	}
 	return types, nil
 }
 
 func (r *queryResolver) AddressTypes(ctx context.Context) ([]AddressType, error) {
+	contains := func(s []AddressType, e AddressType) bool {
+		for _, a := range s {
+			if a == e {
+				return true
+			}
+		}
+		return false
+	}
+
+	err := lazyLoadAddressTypes()
+	if err != nil {
+		return nil, err
+	}
 	types := make([]AddressType, 0)
-	types = append(types, AddressTypeTopic, AddressTypeQueue, AddressTypeMulticast, AddressTypeAnycast)
+	for _, t := range addressTypes {
+		if !contains(types, t.Spec.AddressType) {
+			types = append(types, t.Spec.AddressType)
+		}
+	}
 	return types, nil
 }
+
+func (r *queryResolver) AddressSpaceTypesV2(ctx context.Context) ([]*AddressSpaceTypeConsoleapiEnmasseIoV1beta1, error) {
+	err := lazyLoadAddressSpaceTypes()
+	return addressSpaceTypes, err
+}
+
+func (r *queryResolver) AddressTypesV2(ctx context.Context, addressSpaceType *AddressSpaceType) ([]*AddressTypeConsoleapiEnmasseIoV1beta1, error) {
+	err := lazyLoadAddressTypes()
+	if err != nil {
+		return nil, err
+	}
+	if addressSpaceType != nil {
+		filtered := make([]*AddressTypeConsoleapiEnmasseIoV1beta1, 0)
+		for _, t := range addressTypes {
+			if t.Spec.AddressSpaceType == *addressSpaceType {
+				filtered = append(filtered, t)
+			}
+		}
+		return filtered, nil
+
+	} else {
+		return addressTypes, err
+	}
+}
+
+const defaultAddressSpaceTypes = "addressSpaceTypes.json"
+const defaultAddressTypes = "addressTypes.json"
+
+var addressSpaceTypes []*AddressSpaceTypeConsoleapiEnmasseIoV1beta1
+var addressTypes []*AddressTypeConsoleapiEnmasseIoV1beta1
+
+func lazyLoadAddressSpaceTypes() error {
+	if addressSpaceTypes != nil {
+		return nil
+	}
+
+	fileName := os.Getenv("ENMASSE_ADDRESS_SPACE_TYPE_FILE")
+	if fileName == "" {
+		fileName = defaultAddressSpaceTypes
+	}
+
+	addressSpaceTypes = make([]*AddressSpaceTypeConsoleapiEnmasseIoV1beta1, 0)
+	err := loadTypesFrom(fileName, &addressSpaceTypes)
+	return err
+}
+
+func lazyLoadAddressTypes() error {
+	if addressTypes != nil {
+		return nil
+	}
+
+	fileName := os.Getenv("ENMASSE_ADDRESS_TYPE_FILE")
+	if fileName == "" {
+		fileName = defaultAddressTypes
+	}
+
+	addressTypes = make([]*AddressTypeConsoleapiEnmasseIoV1beta1, 0)
+	err := loadTypesFrom(fileName, &addressTypes)
+	return err
+}
+
+func loadTypesFrom(fileName string, data interface{}) error {
+	file, err := os.Open(fileName)
+	if err != nil {
+		return err
+	}
+
+	defer func() { _ = file.Close() }()
+
+	byteValue, err := ioutil.ReadAll(file)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(byteValue, data)
+	return err
+}
+
