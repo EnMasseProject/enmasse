bababd81eb93e4a21e380ebb7edb374a28df8544
Sanjeev Kumar <sanjeevkumar201418@gmail.com>
2020-01-28 17:55:16 +0530

    fix address space list error when message in status is null

--- console/console-init/ui/src/Pages/AddressSpaceDetail/AddressList/AddressListPage.tsx
+++ console/console-init/ui/src/Pages/AddressSpaceDetail/AddressList/AddressListPage.tsx
@@ -131,18 +131,16 @@ export const AddressListPage: React.FunctionComponent<IAddressListPageProps> = (
     receivers: getFilteredValue(address.Metrics, "enmasse_receivers"),
     partitions:
       address.Status &&
-      address.Status.PlanStatus &&
-      address.Status.PlanStatus !== null
+      address.Status.PlanStatus 
         ? address.Status.PlanStatus.Partitions
         : null,
     isReady:
-      address.Status && address.Status !== null && address.Status.IsReady,
+      address.Status && address.Status.IsReady,
     status:
-      address.Status && address.Status !== null ? address.Status.Phase : "",
+      address.Status && address.Status.Phase ? address.Status.Phase : "",
     errorMessages:
       address.Status &&
-      address.Status !== null &&
-      address.Status.Messages != null
+      address.Status.Messages 
         ? address.Status.Messages
         : [],
     selected:
