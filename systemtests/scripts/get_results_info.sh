e1d84b204e9111b453198f80b07e4d7b8421f2d5
David Kornel <kornys@outlook.com>
2020-01-28 16:19:54 +0100

    Add build number to PR response

--- systemtests/scripts/get_results_info.sh
+++ systemtests/scripts/get_results_info.sh
@@ -3,6 +3,7 @@
 RESULTS_PATH=${1}
 TEST_CASE=${2}
 TEST_PROFILE=${3}
+BUILD_ID=${4:-0}
 
 JSON_FILE_RESULTS=results.json
 
@@ -27,7 +28,7 @@ TEST_FAILURES_COUNT=$(get_test_count "failures")
 
 TEST_ALL_FAILED_COUNT=$((TEST_ERRORS_COUNT + TEST_FAILURES_COUNT))
 
-SUMMARY="**TEST_PROFILE**: ${TEST_PROFILE}\n**TEST_CASE:** ${TEST_CASE}\n**TOTAL:** ${TEST_COUNT}\n**PASS:** $((TEST_COUNT - TEST_ALL_FAILED_COUNT - TEST_SKIPPED_COUNT))\n**FAIL:** ${TEST_ALL_FAILED_COUNT}\n**SKIP:** ${TEST_SKIPPED_COUNT}\n"
+SUMMARY="**TEST_PROFILE**: ${TEST_PROFILE}\n**TEST_CASE:** ${TEST_CASE}\n**TOTAL:** ${TEST_COUNT}\n**PASS:** $((TEST_COUNT - TEST_ALL_FAILED_COUNT - TEST_SKIPPED_COUNT))\n**FAIL:** ${TEST_ALL_FAILED_COUNT}\n**SKIP:** ${TEST_SKIPPED_COUNT}\n**BUILD_NUMBER:** ${BUILD_ID}"
 
 FAILED_TESTS=$(find "${RESULTS_PATH}" -name 'TEST*.xml' -type f -print0 | xargs -0 sed -n "s#\(<testcase.*time=\"[0-9]*,\{0,1\}[0-9]\{1,3\}\..*[^\/]>\)#\1#p" | awk -F '"' '{print "\\n- " $2 " in "  $4}')
 echo ${FAILED_TESTS}
