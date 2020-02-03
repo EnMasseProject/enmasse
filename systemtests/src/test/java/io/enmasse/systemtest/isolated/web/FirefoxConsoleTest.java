5a8fc158e7bfb3a4e484edb830ef77df109984c9
David Kornel <kornys@outlook.com>
2020-02-03 15:07:54 +0100

    Rename isolated package for console tests

--- systemtests/src/test/java/io/enmasse/systemtest/isolated/web/FirefoxConsoleTest.java
+++ systemtests/src/test/java/io/enmasse/systemtest/isolated/web/FirefoxConsoleTest.java
@@ -2,7 +2,7 @@
  * Copyright 2018, EnMasse authors.
  * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
  */
-package io.enmasse.systemtest.isolated.console;
+package io.enmasse.systemtest.isolated.web;
 
 import io.enmasse.address.model.AddressSpaceBuilder;
 import io.enmasse.systemtest.bases.isolated.ITestIsolatedStandard;
