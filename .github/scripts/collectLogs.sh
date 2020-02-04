55bdddaa0897d0b9f5d03f29bdcb4a9d7d6a5429
David Kornel <kornys@outlook.com>
2020-02-04 16:17:48 +0100

    Remove SYSTEMTEST prefix from make env vars

--- .github/scripts/collectLogs.sh
+++ .github/scripts/collectLogs.sh
@@ -3,8 +3,8 @@ set -e
 echo "Collecting test reports"
 mkdir -p artifacts/test-reports
 cp -r /tmp/testlogs artifacts
-for i in `find . -name "TEST-*.xml"`
+for i in $(find . -name "TEST-*.xml")
 do
-    cp ${i} artifacts/test-reports
+    cp "${i}" artifacts/test-reports
 done
 zip -r test-logs.zip artifacts
--- systemtests/scripts/run_tests.sh
+++ systemtests/scripts/run_tests.sh
@@ -3,11 +3,11 @@
 set -e
 
 PROFILE=${1:-"systemtests"}
-TESTCASE=${2}
+TEST=${2}
 
 if [[ -n "$TESTCASE" ]]; then
-    EXTRA_ARGS="-Dtest=$TESTCASE"
+    EXTRA_ARGS="-Dtest=${TEST}"
 fi
 
-cd .. && mvn test -pl systemtests -P${PROFILE} -am -Djava.net.preferIPv4Stack=true -Djansi.force=true -DfailIfNoTests=false -Dstyle.color=always -DskipTests ${EXTRA_ARGS} -DtrimStackTrace=false
+cd .. && mvn test -pl systemtests -P"${PROFILE}" -am -Djava.net.preferIPv4Stack=true -Djansi.force=true -DfailIfNoTests=false -Dstyle.color=always -DskipTests "${EXTRA_ARGS}" -DtrimStackTrace=false
 cd systemtests
