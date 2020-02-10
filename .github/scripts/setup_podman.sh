d2b06bd2ac08cea9508cb8ebd3040f794888231d
Ondrej Babec <ond.babec@gmail.com>
2020-02-10 09:42:15 +0100

    Change podman installation

--- .github/scripts/setup_podman.sh
+++ .github/scripts/setup_podman.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-sudo apt update
-
-sudo apt -y  install software-properties-common
-sudo add-apt-repository -y ppa:projectatomic/ppa
-
-sudo apt update
-sudo apt -y install podman
+. /etc/os-release
+sudo sh -c "echo 'deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list"
+wget -nv https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/xUbuntu_${VERSION_ID}/Release.key -O- | sudo apt-key add -
+sudo apt-get update -qq
+sudo apt-get -qq -y install podman
