a5ccf9d015ff8a0b57b1a4b101c276a551afd19f
Ondrej Babec <ond.babec@gmail.com>
2020-02-11 11:10:44 +0100

    Change github folder

--- .github/scripts/setup_docker.sh
+++ .github/scripts/setup_docker.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+set -e
+
 sudo apt install docker.io
 sudo systemctl unmask docker
+
+sudo mkdir /mnt/docker
+
+sudo cat /lib/systemd/system/docker.service
+sudo sh -c "sed -i 's#ExecStart=/usr/bin/dockerd -H fd://#ExecStart=/usr/bin/dockerd -g /mnt/docker -H fd://#' /lib/systemd/system/docker.service"
+sudo cat /lib/systemd/system/docker.service
+
+
+sudo systemctl daemon-reload
+sudo rsync -aqxP /var/lib/docker/ /mnt/docker
+
 sudo systemctl start docker
+
+ps aux | grep -i docker | grep -v grep
