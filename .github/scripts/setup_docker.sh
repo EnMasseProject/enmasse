807c0412120d6c21343af3e8fddc5777872d4a40
Ondrej Babec <ond.babec@gmail.com>
2020-02-13 10:00:53 +0100

    Change test class

--- .github/scripts/setup_docker.sh
+++ .github/scripts/setup_docker.sh
@@ -6,10 +6,7 @@ sudo systemctl unmask docker
 
 sudo mkdir /mnt/docker
 
-sudo cat /lib/systemd/system/docker.service
 sudo sh -c "sed -i 's#ExecStart=/usr/bin/dockerd -H fd://#ExecStart=/usr/bin/dockerd -g /mnt/docker -H fd://#' /lib/systemd/system/docker.service"
-sudo cat /lib/systemd/system/docker.service
-
 
 sudo systemctl daemon-reload
 sudo rsync -aqxP /var/lib/docker/ /mnt/docker
