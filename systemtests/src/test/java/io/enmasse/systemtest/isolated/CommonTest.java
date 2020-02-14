d342f4eae7c2cde3374f91e7e73a375462a2f6f8
Fabian Martinez <famartin@redhat.com>
2020-02-14 15:03:43 +0100

    add tests verify broker remains operable after full queue

--- systemtests/src/test/java/io/enmasse/systemtest/isolated/CommonTest.java
+++ systemtests/src/test/java/io/enmasse/systemtest/isolated/CommonTest.java
@@ -10,6 +10,20 @@ import io.enmasse.address.model.Address;
 import io.enmasse.address.model.AddressBuilder;
 import io.enmasse.address.model.AddressSpace;
 import io.enmasse.address.model.AddressSpaceBuilder;
+import io.enmasse.admin.model.v1.AddressPlan;
+import io.enmasse.admin.model.v1.AddressSpacePlan;
+import io.enmasse.admin.model.v1.AddressSpacePlanBuilder;
+import io.enmasse.admin.model.v1.BrokeredInfraConfig;
+import io.enmasse.admin.model.v1.BrokeredInfraConfigBuilder;
+import io.enmasse.admin.model.v1.BrokeredInfraConfigSpecAdminBuilder;
+import io.enmasse.admin.model.v1.BrokeredInfraConfigSpecBrokerBuilder;
+import io.enmasse.admin.model.v1.ResourceAllowance;
+import io.enmasse.admin.model.v1.ResourceRequest;
+import io.enmasse.admin.model.v1.StandardInfraConfig;
+import io.enmasse.admin.model.v1.StandardInfraConfigBuilder;
+import io.enmasse.admin.model.v1.StandardInfraConfigSpecAdminBuilder;
+import io.enmasse.admin.model.v1.StandardInfraConfigSpecBrokerBuilder;
+import io.enmasse.admin.model.v1.StandardInfraConfigSpecRouterBuilder;
 import io.enmasse.systemtest.UserCredentials;
 import io.enmasse.systemtest.amqp.AmqpClient;
 import io.enmasse.systemtest.bases.TestBase;
@@ -20,19 +34,32 @@ import io.enmasse.systemtest.model.addressplan.DestinationPlan;
 import io.enmasse.systemtest.model.addressspace.AddressSpacePlans;
 import io.enmasse.systemtest.model.addressspace.AddressSpaceType;
 import io.enmasse.systemtest.platform.KubeCMDClient;
+import io.enmasse.systemtest.shared.standard.QueueTest;
 import io.enmasse.systemtest.time.TimeoutBudget;
 import io.enmasse.systemtest.utils.AddressUtils;
+import io.enmasse.systemtest.utils.PlanUtils;
 import io.enmasse.systemtest.utils.TestUtils;
 import io.fabric8.kubernetes.api.model.Pod;
+import io.github.artsok.RepeatedIfExceptionsTest;
+
+import org.apache.qpid.proton.amqp.Binary;
+import org.apache.qpid.proton.amqp.messaging.AmqpValue;
+import org.apache.qpid.proton.amqp.messaging.Data;
+import org.apache.qpid.proton.amqp.transport.DeliveryState.DeliveryStateType;
+import org.apache.qpid.proton.message.Message;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
@@ -421,6 +448,237 @@ class CommonTest extends TestBase implements ITestBaseIsolated {
                 getClientUtils().assertCanConnect(standard, user, standardAddresses, resourcesManager);
             }
         }
+    }
+
+    @Test
+    @Tag(NON_PR)
+    public void testFullBrokerRemainsOperableStandardWithRetry() throws Exception {
+        AddressPlan exampleAddressPlan = getAddressPlanBrokerOperableOnFullQueue();
+        AddressSpace exampleAddressSpace = getAddressSpaceBrokerOperableOnFullQueue(exampleAddressPlan);
+        doTestBrokerRemainsOperableOnFullAddress(exampleAddressPlan, exampleAddressSpace, true);
+    }
+
+    private AddressPlan getAddressPlanBrokerOperableOnFullQueue() throws Exception {
+        AddressPlan exampleAddressPlan = PlanUtils.createAddressPlanObject("test-queue-keeps-operable", AddressType.QUEUE,
+                Arrays.asList(new ResourceRequest("broker", 0.5), new ResourceRequest("router", 0.5)));
+
+        resourcesManager.createAddressPlan(exampleAddressPlan);
+        return exampleAddressPlan;
+    }
+
+    private AddressSpace getAddressSpaceBrokerOperableOnFullQueue(AddressPlan exampleAddressPlan) throws Exception {
+        StandardInfraConfig testInfra = new StandardInfraConfigBuilder()
+                .withNewMetadata()
+                .withName("infra-keeps-operable-s")
+                .endMetadata()
+                .withNewSpec()
+                .withBroker(new StandardInfraConfigSpecBrokerBuilder()
+                        .withAddressFullPolicy("FAIL")
+                        .withNewResources()
+                        .withMemory("512Mi")
+                        .withStorage("512Mi")
+                        .endResources()
+                        .withGlobalMaxSize("1Mb")
+                        .build())
+                .withRouter(new StandardInfraConfigSpecRouterBuilder()
+                        .withNewResources()
+                        .withMemory("256Mi")
+                        .endResources()
+                        .withMinReplicas(1)
+                        .withLinkCapacity(250)
+                        .build())
+                .withAdmin(new StandardInfraConfigSpecAdminBuilder()
+                        .withNewResources()
+                        .withMemory("512Mi")
+                        .endResources()
+                        .build())
+                .endSpec()
+                .build();
+        resourcesManager.createInfraConfig(testInfra);
+
+        AddressSpacePlan exampleSpacePlan = new AddressSpacePlanBuilder()
+                .withNewMetadata()
+                .withName("test-plan-keeps-operable")
+                .withNamespace(kubernetes.getInfraNamespace())
+                .endMetadata()
+                .withNewSpec()
+                .withAddressSpaceType(AddressSpaceType.STANDARD.toString())
+                .withInfraConfigRef(testInfra.getMetadata().getName())
+                .withResourceLimits(Arrays.asList(
+                        new ResourceAllowance("broker", 1.0),
+                        new ResourceAllowance("router", 1.0),
+                        new ResourceAllowance("aggregate", 2.0))
+                        .stream().collect(Collectors.toMap(ResourceAllowance::getName, ResourceAllowance::getMax)))
+                .withAddressPlans(Arrays.asList(exampleAddressPlan.getMetadata().getName()))
+                .endSpec()
+                .build();
+        resourcesManager.createAddressSpacePlan(exampleSpacePlan);
+
+        AddressSpace exampleAddressSpace = new AddressSpaceBuilder()
+                .withNewMetadata()
+                .withName("test-address-space-keeps-operable")
+                .withNamespace(kubernetes.getInfraNamespace())
+                .endMetadata()
+                .withNewSpec()
+                .withType(AddressSpaceType.STANDARD.toString())
+                .withPlan(exampleSpacePlan.getMetadata().getName())
+                .withNewAuthenticationService()
+                .withName("standard-authservice")
+                .endAuthenticationService()
+                .endSpec()
+                .build();
+
+        resourcesManager.createAddressSpace(exampleAddressSpace);
+        return exampleAddressSpace;
+    }
+
+    @Tag(NON_PR)
+    @DisplayName("testFullBrokerRemainsOperableBrokered")
+    @RepeatedIfExceptionsTest(repeats = 2, name = "")
+    public void testFullBrokerRemainsOperableBrokered() throws Exception {
+        BrokeredInfraConfig testInfra = new BrokeredInfraConfigBuilder()
+                .withNewMetadata()
+                .withName("infra-keeps-operable-b")
+                .endMetadata()
+                .withNewSpec()
+                .withBroker(new BrokeredInfraConfigSpecBrokerBuilder()
+                        .withAddressFullPolicy("FAIL")
+                        .withNewResources()
+                        .withMemory("512Mi")
+                        .withStorage("512Mi")
+                        .endResources()
+                        .withGlobalMaxSize("1Mb")
+                        .build())
+                .withAdmin(new BrokeredInfraConfigSpecAdminBuilder()
+                        .withNewResources()
+                        .withMemory("512Mi")
+                        .endResources()
+                        .build())
+                .endSpec()
+                .build();
+        resourcesManager.createInfraConfig(testInfra);
+
+        AddressPlan exampleAddressPlan = PlanUtils.createAddressPlanObject("test-queue-keeps-operable", AddressType.QUEUE,
+                Arrays.asList(new ResourceRequest("broker", 0.5)));
+
+        resourcesManager.createAddressPlan(exampleAddressPlan);
+
+        AddressSpacePlan exampleSpacePlan = new AddressSpacePlanBuilder()
+                .withNewMetadata()
+                .withName("test-plan-keeps-operable")
+                .withNamespace(kubernetes.getInfraNamespace())
+                .endMetadata()
+                .withNewSpec()
+                .withAddressSpaceType(AddressSpaceType.BROKERED.toString())
+                .withInfraConfigRef(testInfra.getMetadata().getName())
+                .withResourceLimits(Arrays.asList(
+                        new ResourceAllowance("broker", 1.0))
+                        .stream().collect(Collectors.toMap(ResourceAllowance::getName, ResourceAllowance::getMax)))
+                .withAddressPlans(Arrays.asList(exampleAddressPlan.getMetadata().getName()))
+                .endSpec()
+                .build();
+        resourcesManager.createAddressSpacePlan(exampleSpacePlan);
+
+        AddressSpace exampleAddressSpace = new AddressSpaceBuilder()
+                .withNewMetadata()
+                .withName("test-address-space-keeps-operable")
+                .withNamespace(kubernetes.getInfraNamespace())
+                .endMetadata()
+                .withNewSpec()
+                .withType(AddressSpaceType.BROKERED.toString())
+                .withPlan(exampleSpacePlan.getMetadata().getName())
+                .withNewAuthenticationService()
+                .withName("standard-authservice")
+                .endAuthenticationService()
+                .endSpec()
+                .build();
+
+        resourcesManager.createAddressSpace(exampleAddressSpace);
+
+        doTestBrokerRemainsOperableOnFullAddress(exampleAddressPlan, exampleAddressSpace, false);
+
+    }
+
+    private void doTestBrokerRemainsOperableOnFullAddress(AddressPlan addressPlan, AddressSpace addressSpace, boolean retry) throws Exception {
+        UserCredentials user = new UserCredentials("dummy", "supersecure");
+        resourcesManager.createOrUpdateUser(addressSpace, user);
+
+        var address1 = new AddressBuilder()
+                .withNewMetadata()
+                .withNamespace(kubernetes.getInfraNamespace())
+                .withName(AddressUtils.generateAddressMetadataName(addressSpace, "example-queue-1"))
+                .endMetadata()
+                .withNewSpec()
+                .withType(AddressType.QUEUE.toString())
+                .withAddress("example-queue-1")
+                .withPlan(addressPlan.getMetadata().getName())
+                .endSpec()
+                .build();
+
+        var address2 = new AddressBuilder()
+                .withNewMetadata()
+                .withNamespace(kubernetes.getInfraNamespace())
+                .withName(AddressUtils.generateAddressMetadataName(addressSpace, "example-queue-2"))
+                .endMetadata()
+                .withNewSpec()
+                .withType(AddressType.QUEUE.toString())
+                .withAddress("example-queue-2")
+                .withPlan(addressPlan.getMetadata().getName())
+                .endSpec()
+                .build();
+
+        resourcesManager.setAddresses(address1, address2);
+        assertEquals(
+                resourcesManager.getAddress(address1.getMetadata().getNamespace(), address1).getStatus().getBrokerStatuses(),
+                resourcesManager.getAddress(address2.getMetadata().getNamespace(), address2).getStatus().getBrokerStatuses(),
+                "Addresses are not colocated on the same broker");
+
+        AmqpClient client = getAmqpClientFactory().createQueueClient(addressSpace);
+        client.getConnectOptions().setCredentials(user);
+        boolean full = false;
+        byte[] bytes = new byte[1024 * 100];
+        Random random = new Random();
+        int messagesSent = 0;
+        TimeoutBudget timeout = new TimeoutBudget(30, TimeUnit.SECONDS);
+        log.info("Start time: get queue full");
+        do {
+            Message message = Message.Factory.create();
+            random.nextBytes(bytes);
+            message.setBody(new AmqpValue(new Data(new Binary(bytes))));
+            message.setAddress(address1.getSpec().getAddress());
+            message.setDurable(true);
+            try {
+                var deliveries = client.sendMessage(address1.getSpec().getAddress(), message).get(5, TimeUnit.SECONDS);
+
+                assertEquals(deliveries.size(), 1);
+                var state = deliveries.get(0).getRemoteState();
+                if (state.getType() == DeliveryStateType.Modified || state.getType() == DeliveryStateType.Rejected) {
+                    full = true;
+                    log.info("broker is full with state {} after sending {} messages", state, messagesSent);
+                }
+                messagesSent++;
+
+                if (timeout.timeoutExpired()) {
+                    log.info("Delivery state is {}", state.getType());
+                    Assertions.fail("Timeout waiting for broker to become full, probably error in test env configuration");
+                }
+            } catch (Exception e) {
+                full = true;
+                log.info("broker is full after sending {} messages, exception", messagesSent, e);
+            }
+        } while(!full);
+
+        assertTrue(messagesSent > 0, "Incorrect test set up, 0 messages sent");
+
+        resourcesManager.deleteAddresses(address1);
+        AddressUtils.waitForAddressDeleted(address1, new TimeoutBudget(1, TimeUnit.MINUTES));
+
+        if (retry) {
+            QueueTest.runQueueTestRetrying(client, address2, messagesSent/2, Duration.ofSeconds(30));
+        } else {
+            QueueTest.runQueueTest(client, address2, messagesSent/2);
+        }
+
     }
 
     /////////////////////////////////////////////////////////////////////
