9a54700ea57752a5bc9d1267a561fb724606e5f4
Sanjeev Kumar <sanjeevkumar201418@gmail.com>
2020-02-20 11:57:44 +0530

    display empty state if server returns empty array for any query

---
+++ console/console-init/ui/src/components/common/NoDataFound.tsx
@@ -0,0 +1,40 @@
+import * as React from "react";
+import {
+  EmptyState,
+  EmptyStateVariant,
+  EmptyStateIcon,
+  Title,
+  EmptyStateBody,
+  PageSection
+} from "@patternfly/react-core";
+import { Link } from "react-router-dom";
+import { CubesIcon, GlobeRouteIcon } from "@patternfly/react-icons";
+
+interface INoDataFoundProps {
+  type: string;
+  name: string;
+  routeLink: string;
+}
+export const NoDataFound: React.FunctionComponent<INoDataFoundProps> = ({
+  type,
+  name,
+  routeLink
+}) => {
+  return (
+    <PageSection>
+      <PageSection variant="light">
+        <EmptyState variant={EmptyStateVariant.full}>
+          <EmptyStateIcon icon={GlobeRouteIcon} />
+          <Title headingLevel="h5" size="lg">
+            {type} Not Found
+          </Title>
+          <EmptyStateBody>
+            The {type} <b>{name}</b> you are looking for, does not exist
+            anymore.
+          </EmptyStateBody>
+          <Link to={routeLink}>Go to {type} list</Link>
+        </EmptyState>
+      </PageSection>
+    </PageSection>
+  );
+};
