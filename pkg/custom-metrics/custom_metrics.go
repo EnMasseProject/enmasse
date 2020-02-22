5484dc5cb693d72ffa41e2fcaffa670b5d3c1637
Rob Shelly <rshelly@redhat.com>
2020-02-22 17:39:19 +0000

    Removed extra 'enmasse' from version metrics name

--- pkg/custom-metrics/custom_metrics.go
+++ pkg/custom-metrics/custom_metrics.go
@@ -11,7 +11,7 @@ import (
 var (
 	versionInfo = prometheus.NewGauge(
 		prometheus.GaugeOpts{
-			Name:        "enmasse_version_info",
+			Name:        "version_info",
 			Help:        "Enmasse Operator version and product version",
 			ConstLabels: prometheus.Labels{"operator_version": version.Version, "version": os.Getenv("VERSION")},
 		},
