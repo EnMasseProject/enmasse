a5d16cae592be1893def553a549b585449435781
David Kornel <kornys@outlook.com>
2020-03-08 21:28:19 +0100

    Prevent remove addresspace race condition in test (#4063)

--- systemtests/src/test/java/io/enmasse/systemtest/bases/web/ConsoleTest.java
+++ systemtests/src/test/java/io/enmasse/systemtest/bases/web/ConsoleTest.java
@@ -145,7 +145,7 @@ public abstract class ConsoleTest extends TestBase {
         getAndExecAddressSpaceDeploymentSnippet(addressSpace);
         assertTrue(AddressSpaceUtils.addressSpaceExists(Kubernetes.getInstance().getInfraNamespace(),
                 addressSpace.getMetadata().getName()));
-        resourcesManager.deleteAddressSpace(addressSpace);
+        resourcesManager.waitForAddressSpaceReady(addressSpace);
     }
 
 
