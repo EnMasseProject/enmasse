8fd600cf9473addd6449a02c75f7bc3f8d2480ba
Keith Wall <kwall@apache.org>
2020-03-23 15:46:01 +0000

    Unknown address plan test
    Test that an unknown address plan is reported properly and the existences
    of an address with an unknown plan does not prevent other addresses
    becoming ready.

--- systemtests/src/test/java/io/enmasse/systemtest/isolated/plans/standard/PlansTestStandard.java
+++ systemtests/src/test/java/io/enmasse/systemtest/isolated/plans/standard/PlansTestStandard.java
@@ -27,7 +27,10 @@ import io.enmasse.systemtest.bases.TestBase;
 import io.enmasse.systemtest.bases.isolated.ITestIsolatedStandard;
 import io.enmasse.systemtest.logs.CustomLogger;
 import io.enmasse.systemtest.model.address.AddressType;
+import io.enmasse.systemtest.model.addressplan.DestinationPlan;
+import io.enmasse.systemtest.model.addressspace.AddressSpacePlans;
 import io.enmasse.systemtest.model.addressspace.AddressSpaceType;
+import io.enmasse.systemtest.platform.Kubernetes;
 import io.enmasse.systemtest.selenium.SeleniumFirefox;
 import io.enmasse.systemtest.selenium.SeleniumProvider;
 import io.enmasse.systemtest.shared.standard.QueueTest;
@@ -37,15 +40,22 @@ import io.enmasse.systemtest.utils.AddressSpaceUtils;
 import io.enmasse.systemtest.utils.AddressUtils;
 import io.enmasse.systemtest.utils.PlanUtils;
 import io.enmasse.systemtest.utils.TestUtils;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.StringDescription;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -1134,6 +1144,122 @@ class PlansTestStandard extends TestBase implements ITestIsolatedStandard {
         }
     }
 
+    @Test
+    void testUnknownAddressPlan() throws Exception {
+        AddressSpace addressSpace = new AddressSpaceBuilder()
+                .withNewMetadata()
+                .withName("test-pooled-space")
+                .withNamespace(kubernetes.getInfraNamespace())
+                .endMetadata()
+                .withNewSpec()
+                .withType(AddressSpaceType.STANDARD.toString())
+                .withPlan(AddressSpacePlans.STANDARD_SMALL)
+                .withNewAuthenticationService()
+                .withName("standard-authservice")
+                .endAuthenticationService()
+                .endSpec()
+                .build();
+
+        resourcesManager.createAddressSpace(addressSpace);
+
+        class StageHolder {
+            class Stage {
+                private final String plan;
+                private final Matcher<Address> matcher;
+
+                Stage(Matcher<Address> matcher, String plan) {
+                    this.plan = plan;
+                    this.matcher = matcher;
+                }
+
+                public Address getAddress() {
+                    return new AddressBuilder()
+                            .withNewMetadata()
+                            .withNamespace(addressSpace.getMetadata().getNamespace())
+                            .withName(AddressUtils.generateAddressMetadataName(addressSpace, addressName))
+                            .endMetadata()
+                            .withNewSpec()
+                            .withType("queue")
+                            .withAddress(StageHolder.this.addressName)
+                            .withPlan(plan)
+                            .endSpec()
+                            .build();
+                }
+
+                public Matcher<Address> getMatcher() {
+                    return matcher;
+                }
+            }
+            private String addressName;
+            private final List<Stage> stages = new ArrayList<>();
+
+            public StageHolder(String addressName) {
+                this.addressName = addressName;
+            }
+
+            public StageHolder addStage(String plan, Matcher<Address> addressMatcher) {
+                stages.add(new Stage(addressMatcher, plan));
+                return this;
+            }
+
+            public boolean hasStage() {
+                return !stages.isEmpty();
+            }
+
+            public Stage popStage() {
+                return stages.remove(0);
+            }
+        }
+
+        String unknownPlan = "unknown-plan";
+        String unknownPlanMessages = String.format("Unknown address plan '%s'", unknownPlan);
+        List<StageHolder> stageHolders = new ArrayList<>();
+        stageHolders.add(new StageHolder("initial-unknown-plan")
+                .addStage(unknownPlan, assertAddressStatusNotReady(unknownPlanMessages)));
+        stageHolders.add(new StageHolder("initial-unknown-plan-for-type")
+                .addStage(DestinationPlan.STANDARD_SMALL_ANYCAST, assertAddressStatusNotReady(String.format("Unknown address plan '%s'", DestinationPlan.STANDARD_SMALL_ANYCAST)))
+                .addStage(DestinationPlan.STANDARD_SMALL_QUEUE, assertAddressStatusReady(DestinationPlan.STANDARD_SMALL_QUEUE)));
+        stageHolders.add(new StageHolder("becomes-unknown-plan")
+                .addStage( DestinationPlan.STANDARD_SMALL_QUEUE, assertAddressStatusReady(DestinationPlan.STANDARD_SMALL_QUEUE))
+                .addStage(unknownPlan, assertAddressStatusNotReady(unknownPlanMessages)));
+        stageHolders.add(new StageHolder("good").addStage(DestinationPlan.STANDARD_SMALL_QUEUE,
+                assertAddressStatusReady(DestinationPlan.STANDARD_SMALL_QUEUE)));
+
+        do {
+            log.info("Starting stage");
+
+            List<StageHolder.Stage> stages = stageHolders.stream().filter(StageHolder::hasStage).map(StageHolder::popStage).collect(Collectors.toList());
+
+            if (stages.isEmpty()) {
+                break;
+            }
+
+            stages.stream().map(StageHolder.Stage::getAddress).forEach(address -> {
+                Kubernetes.getInstance().getAddressClient(address.getMetadata().getNamespace()).createOrReplace(address);
+            });
+
+            stages.forEach(s -> {
+                AtomicReference<String> lastMatch = new AtomicReference<>();
+
+                boolean rv = TestUtils.waitUntilCondition(() -> {
+                    Address current = resourcesManager.getAddress(s.getAddress().getMetadata().getNamespace(), s.getAddress());
+                    Matcher<Address> matcher = s.getMatcher();
+                    boolean matches = matcher.matches(current);
+                    StringDescription desc = new StringDescription();
+                    matcher.describeMismatch(current, desc);
+                    lastMatch.set(desc.toString());
+                    if (matches) {
+                        log.info("Address {} is now in expected state: {}", current.getMetadata().getName(), current.getStatus());
+                    } else {
+                        log.info("Address {} is not in expected state: {} {}", current.getMetadata().getName(), lastMatch, current.getStatus());
+                    }
+                    return matches;
+                }, Duration.ofMinutes(2), Duration.ofSeconds(10));
+                assertTrue(rv, String.format("address %s did not reach desired state : %s", s.getAddress().getMetadata().getName(), lastMatch));
+            });
+        } while(true);
+    }
+
     //------------------------------------------------------------------------------------------------
     // Help methods
     //------------------------------------------------------------------------------------------------
@@ -1184,4 +1310,64 @@ class PlansTestStandard extends TestBase implements ITestIsolatedStandard {
 
         resourcesManager.deleteAddresses(addressSpace);
     }
+
+    private static Matcher<Address> assertAddressStatusNotReady(final String messageContains) {
+        return assertAddressStatus(false, Optional.empty(), Optional.of(messageContains));
+    }
+
+    private static Matcher<Address> assertAddressStatusReady(String actualPlan) {
+        return assertAddressStatus(true, Optional.of(actualPlan), Optional.empty());
+    }
+
+    private static Matcher<Address> assertAddressStatus(final boolean ready, final Optional<String> actualPlan, final Optional<String> messageContains) {
+        return new TypeSafeMatcher<>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("should match ready ").appendValue(ready);
+                actualPlan.ifPresent(s -> description.appendText("should match plan ").appendValue(s));
+                messageContains.ifPresent(s -> description.appendText("should status should contain ").appendValue(s));
+            }
+
+            @Override
+            protected void describeMismatchSafely(Address a, Description description) {
+                if (a.getStatus() == null) {
+                    description.appendText("address.status is absent");
+                }
+                if (ready != a.getStatus().isReady()) {
+                    description.appendText("ready was ").appendValue(a.getStatus().isReady());
+                }
+                if (actualPlan.isPresent())
+                    if (a.getStatus().getPlanStatus() == null) {
+                        description.appendText("address.status.planStatus is absent");
+                    } else if (!actualPlan.get().equals(a.getStatus().getPlanStatus().getName())) {
+                        description.appendText("actual plan was ").appendValue(a.getStatus().getPlanStatus().getName());
+                    }
+                if (messageContains.isPresent()) {
+                    String cc = String.join(":", a.getStatus().getMessages());
+                    description.appendText("messages were: ").appendValue(cc);
+                }
+
+            }
+
+            @Override
+            public boolean matchesSafely(Address a) {
+                if (a.getStatus() == null) {
+                    return false;
+                }
+                if (ready != a.getStatus().isReady()) {
+                    return false;
+                }
+                if (actualPlan.isPresent() && !actualPlan.get().equals(a.getStatus().getPlanStatus().getName())) {
+                    return false;
+                }
+                if (messageContains.isPresent()) {
+                    Optional<String> match = a.getStatus().getMessages().stream().filter(m -> m.contains(messageContains.get())).findFirst();
+                    return match.isPresent();
+                } else {
+                    return true;
+                }
+            }
+        };
+    }
+
 }
