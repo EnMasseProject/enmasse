0b7276ea7f1c5c99db563e51f790db21e8aa008b
Sanjeev Kumar <sanjeevkumar201418@gmail.com>
2020-03-23 19:12:49 +0530

    adding a dummy component for ASToolbar

--- console/console-init/ui/src/modules/address/containers/AddressListFilter/AddressListFilter.tsx
+++ console/console-init/ui/src/modules/address/containers/AddressListFilter/AddressListFilter.tsx
@@ -33,13 +33,13 @@ import {
 } from "@patternfly/react-core";
 import { FilterIcon, SearchIcon } from "@patternfly/react-icons";
 import { useApolloClient } from "@apollo/react-hooks";
-import { IAddressListNameSearchResponse } from "types/ResponseTypes";
+import { IAddressListNameSearchResponse } from "schema/ResponseTypes";
 import { RETURN_ALL_ADDRESS_NAMES_OF_ADDRESS_SPACES_FOR_TYPEAHEAD_SEARCH } from "graphql-module/queries";
 import {
   TypeAheadMessage,
   TYPEAHEAD_REQUIRED_LENGTH,
   FetchPolicy
-} from "constants/constants";
+} from "constant";
 import { getSelectOptionList, ISelectOption } from "utils";
 
 interface IAddressListFilterProps {
