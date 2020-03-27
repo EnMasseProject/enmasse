f0aacf68ba797e12c6b0d4b231042914e22ed34d
Jean-Baptiste Trystram <jbtrystram@redhat.com>
2020-03-27 10:07:42 +0100

    fix sql scripts in iot examples

--- templates/iot/examples/postgresql/drop.sql
+++ templates/iot/examples/postgresql/drop.sql
@@ -1,4 +1,7 @@
 DROP TABLE devices;
-DROP TABLE device_information;
+DROP TABLE device_states;
 
 DROP INDEX idx_devices_tenant;
+DROP INDEX idx_device_states_tenant;
+DROP INDEX idx_devices_data;
+DROP INDEX idx_credentials;
---
+++ templates/iot/examples/postgresql/drop.table.sql
@@ -0,0 +1,7 @@
+DROP TABLE device_registrations;
+DROP TABLE device_credentials;
+DROP TABLE device_states ;
+
+DROP INDEX idx_device_registrations_tenant;
+DROP INDEX idx_device_credentials_tenant;
+DROP INDEX idx_device_states_tenant;
