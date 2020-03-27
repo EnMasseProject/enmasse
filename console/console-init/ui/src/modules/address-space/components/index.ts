54b9f68e6dbdb7a5a7d529791ec3375e614d90b9
sanjeev kumar <sanjeevkumar201418@gmail.com>
2020-03-27 15:16:36 +0530

    refactor address space filter (#4192)
    * refactor address space filter

--- console/console-init/ui/src/modules/address-space/components/index.ts
+++ console/console-init/ui/src/modules/address-space/components/index.ts
@@ -2,13 +2,13 @@
  * Copyright 2020, EnMasse authors.
  * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
  */
-export * from "./AddressSpaceToolbar";
 export * from "./EmptyAddressSpace";
-export * from "./AddressSpaceFilter";
 export * from "./AddressSpaceList";
 export * from "./AddressSpaceConfiguration";
 export * from "./EditAddressSpace";
 export * from "./AddressSpaceReview";
 export * from "./AddressSpaceListKebab";
+export * from "./MessagingToolbar";
 export * from "./AddressSpaceHeader";
 export * from "./AddressSpaceNavigation";
+export * from "./MessagingDatatoolbarToggleGroup";
