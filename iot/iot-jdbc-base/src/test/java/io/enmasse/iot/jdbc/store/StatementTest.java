9b4f90c00ec90b5e8f8e6e6183bbcaafe04f7795
Jens Reimann <jreimann@redhat.com>
2020-03-30 11:52:26 +0200

    Restrict YAML parser to not create Java objects

--- iot/iot-jdbc-base/src/test/java/io/enmasse/iot/jdbc/store/StatementTest.java
+++ iot/iot-jdbc-base/src/test/java/io/enmasse/iot/jdbc/store/StatementTest.java
@@ -5,9 +5,18 @@
 
 package io.enmasse.iot.jdbc.store;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.ConstructorException;
 
 public class StatementTest {
 
@@ -42,6 +51,47 @@ public class StatementTest {
                 "AND\n" +
                 "   device_id=?");
         statement.validateParameters("data", "device_id", "next_version", "tenant_id");
+    }
+
+    /**
+     * Test that we didn't break basic YAML loading.
+     */
+    @Test
+    public void testPlainYamlStillWorks() {
+        final Yaml parser = StatementConfiguration.createYamlParser();
+        parser.load("foo: bar");
+    }
+
+    /**
+     * Test that we create a YAML parser which reject class instantiation.
+     */
+    @Test
+    public void testObjectCreationRejected(@TempDir Path tempDir) {
+        final Path markerFile = tempDir.resolve("testObjectCreationRejected.marker");
+        final String yaml = "!!java.io.FileOutputStream [" + markerFile.toAbsolutePath().toString() + "]";
+
+        assertThrows(ConstructorException.class, () -> {
+            final Yaml parser = StatementConfiguration.createYamlParser();
+            parser.load(yaml);
+        });
+
+        assertFalse(Files.isRegularFile(markerFile), "Marker file must not exist");
+    }
+
+    /**
+     * Test that we actually make use of the parser, preventing class instantiation.
+     */
+    @Test
+    public void testObjectCreationRejectedFull(@TempDir Path tempDir) {
+        final Path markerFile = tempDir.resolve("testObjectCreationRejectedFull.marker");
+        final String yaml = "!!java.io.FileOutputStream [" + markerFile.toAbsolutePath().toString() + "]";
+
+        assertThrows(ConstructorException.class, () -> {
+            var cfg = StatementConfiguration.empty();
+            cfg.overrideWith(new ByteArrayInputStream(yaml.getBytes(StandardCharsets.UTF_8)), false);
+        });
+
+        assertFalse(Files.isRegularFile(markerFile), "Marker file must not exist");
     }
 
 }
