7ee43168bcca63f0184bd126d69dac31be576633
Fabian Martinez <famartin@redhat.com>
2020-04-14 10:32:01 +0200

    allow override docker

--- systemtests/custom-operator-registry/build_push_registry.sh
+++ systemtests/custom-operator-registry/build_push_registry.sh
@@ -2,21 +2,23 @@
 FROM=${1}
 CUSTOM_IMAGE=${2}
 
+DOCKER=${DOCKER:-docker}
+
 rm -rf manifests
-id=$(docker create $FROM)
-docker cp $id:/manifests $PWD
+id=$($DOCKER create $FROM)
+$DOCKER cp $id:/manifests $PWD
 echo "manifests copied"
 MANIFESTS_CONTENT_REPLACER=./manifests_replacer.sh
 if [ -f "$MANIFESTS_CONTENT_REPLACER" ]; then
     echo "executing $MANIFESTS_CONTENT_REPLACER"
     bash $MANIFESTS_CONTENT_REPLACER
 fi
-docker rm -v $id
+$DOCKER rm -v $id
 
-docker build -t $CUSTOM_IMAGE .
+$DOCKER build -t $CUSTOM_IMAGE .
 
 rm -rf manifests
 
-docker login $OCP4_EXTERNAL_IMAGE_REGISTRY -u $(oc whoami) -p $(oc whoami -t)
+$DOCKER login $OCP4_EXTERNAL_IMAGE_REGISTRY -u $(oc whoami) -p $(oc whoami -t)
 
-docker push $CUSTOM_IMAGE
+$DOCKER push $CUSTOM_IMAGE
