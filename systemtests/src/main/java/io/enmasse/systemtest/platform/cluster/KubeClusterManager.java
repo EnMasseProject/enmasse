b0447b259f87b71ae9c20a4918169b803b778c19
David Kornel <kornys@outlook.com>
2020-04-15 21:26:27 +0200

    Add ability to restore configurations

--- systemtests/src/main/java/io/enmasse/systemtest/platform/cluster/KubeClusterManager.java
+++ systemtests/src/main/java/io/enmasse/systemtest/platform/cluster/KubeClusterManager.java
@@ -4,11 +4,9 @@
  */
 package io.enmasse.systemtest.platform.cluster;
 
-import io.enmasse.systemtest.UserCredentials;
 import io.enmasse.systemtest.bases.ThrowableRunner;
 import io.enmasse.systemtest.executor.Exec;
 import io.enmasse.systemtest.logs.CustomLogger;
-import io.enmasse.systemtest.platform.KubeCMDClient;
 import io.enmasse.systemtest.platform.Kubernetes;
 import io.enmasse.systemtest.time.TimeoutBudget;
 import io.enmasse.systemtest.utils.TestUtils;
@@ -30,6 +28,9 @@ public class KubeClusterManager {
     private Kubernetes kube = Kubernetes.getInstance();
     private static KubeClusterManager instance;
     private final String CMD = Kubernetes.getInstance().getCluster().getKubeCmd();
+    private Stack<ThrowableRunner> classConfigurations = new Stack<>();
+    private Stack<ThrowableRunner> methodConfigurations = new Stack<>();
+    private Stack<ThrowableRunner> pointerConfigurations = classConfigurations;
 
     private KubeClusterManager() {
     }
@@ -41,20 +42,45 @@ public class KubeClusterManager {
         return instance;
     }
 
+    public void setMethodConfigurations() {
+        LOGGER.info("Setting pointer to method configurations");
+        pointerConfigurations = methodConfigurations;
+    }
+
+    public void setClassConfigurations() {
+        LOGGER.info("Setting pointer to class configurations");
+        pointerConfigurations = classConfigurations;
+    }
+
+    public void restoreClassConfigurations() throws Exception {
+        LOGGER.info("Going to restore all class configurations");
+        LOGGER.info("------------------------------------");
+        while (!classConfigurations.empty()) {
+            classConfigurations.pop().run();
+        }
+        LOGGER.info("------------------------------------");
+        classConfigurations.clear();
+    }
+
+    public void restoreMethodConfigurations() throws Exception {
+        LOGGER.info("Going to restore all method configurations");
+        LOGGER.info("------------------------------------");
+        while (!methodConfigurations.empty()) {
+            methodConfigurations.pop().run();
+        }
+        LOGGER.info("------------------------------------");
+        methodConfigurations.clear();
+        pointerConfigurations = methodConfigurations;
+    }
+
+    public void scheduleConfigurationRestore(String what, ThrowableRunner runner) {
+        LOGGER.info("Schedule restore of {}", what);
+        pointerConfigurations.push(runner);
+    }
+
     //////////////////////////////////////////////////////////////
     // User operations
     //////////////////////////////////////////////////////////////
-    public String loginUser(UserCredentials credentials) {
-        return loginUser(credentials.getUsername(), credentials.getPassword());
-    }
-
-    public String loginUser(String username, String password) {
-        return KubeCMDClient.loginUser(username, password);
-    }
-
-    public String getToken() {
-        return Exec.execute(Arrays.asList(CMD, "whoami", "-t")).getStdOut();
-    }
 
     public List<Node> getWorkerNodes() {
         return kube.getClient().nodes().list().getItems().stream().filter(node ->
@@ -72,14 +98,27 @@ public class KubeClusterManager {
     public void scaleOpenshiftWorkers(int workerCount) {
         LOGGER.info("Cluster is going to be scaled to {} worker nodes", workerCount);
         String machineSetName = Exec.execute(Arrays.asList(CMD, "get", "machinesets", "-n", "openshift-machine-api", "-o", "jsonpath='{.items[0].metadata.name}'")).getTrimmedStdOut();
-        Exec.executeAndCheck(CMD, "scale", "--replicas", Integer.toString(workerCount), "machineset", machineSetName, "-n", "openshift-machine-api");
+        int originalCount = getReadyWorkerCount();
 
+        scheduleConfigurationRestore("Scale worker nodes", () -> {
+            Exec.executeAndCheck(CMD, "scale", "--replicas", Integer.toString(originalCount), "machineset", machineSetName, "-n", "openshift-machine-api");
+            waitUtilWorkerScaled(originalCount);
+        });
+
+        Exec.executeAndCheck(CMD, "scale", "--replicas", Integer.toString(workerCount), "machineset", machineSetName, "-n", "openshift-machine-api");
+        waitUtilWorkerScaled(workerCount);
+    }
+
+    private void waitUtilWorkerScaled(int workerCount) {
         TestUtils.waitUntilCondition(String.format("Cluster worker scale to %d", workerCount), waitPhase -> {
-            String count = Exec.execute(Arrays.asList(CMD, "get", "machinesets", "-n", "openshift-machine-api", "-o", "jsonpath='{.items[0].status.readyReplicas}'"), false)
-                    .getTrimmedStdOut();
+            int count = getReadyWorkerCount();
             LOGGER.info("Ready worker replicas {}/{}", count, workerCount);
-            return count.equals(Integer.toString(workerCount));
+            return count == workerCount;
         }, new TimeoutBudget(20, TimeUnit.MINUTES));
+    }
+
+    private int getReadyWorkerCount() {
+        return Integer.parseInt(Exec.execute(Arrays.asList(CMD, "get", "machinesets", "-n", "openshift-machine-api", "-o", "jsonpath='{.items[0].status.readyReplicas}'"), false).getTrimmedStdOut());
     }
 
     //////////////////////////////////////////////////////////////
@@ -93,6 +132,9 @@ public class KubeClusterManager {
 
     public void setNodeSchedule(String node, boolean schedule) {
         LOGGER.info("Set {} schedule {}", node, schedule);
+
+        scheduleConfigurationRestore("Set node schedule", () -> Exec.executeAndCheck(CMD, "adm", !schedule ? "uncordon" : "cordon", node));
+
         Exec.executeAndCheck(CMD, "adm", schedule ? "uncordon" : "cordon", node);
     }
 
