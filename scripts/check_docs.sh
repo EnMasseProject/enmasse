8b6a6c3a70f3991531a110a7ed329884f55e7d50
Keith Wall <kwall@apache.org>
2020-04-16 12:41:04 +0100

    Documentation for the Broker support access

--- scripts/check_docs.sh
+++ scripts/check_docs.sh
@@ -6,7 +6,7 @@ function grep_check {
   local pattern=$1
   local description=$2
   local fatalness=${3:-1}
-  x=$(grep -i -E -r -Hn "$pattern" --exclude="*.gif" --exclude="*.png" --exclude="*.svg" --exclude-dir="design" --exclude-dir="contributing" --exclude-dir="build" --exclude-dir="html" documentation)
+  x=$(grep -i -E -r -Hn "$pattern" --exclude="*.gif" --exclude="*.jar" --exclude="*.png" --exclude="*.svg" --exclude-dir="design" --exclude-dir="contributing" --exclude-dir="build" --exclude-dir="html" documentation)
   if [ -n "$x" ]; then
     echo "$description:"
     echo "$x"
