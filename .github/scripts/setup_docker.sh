6b210f91146e97f14cadcca4fc9b9df1f1950a13
Ondrej Babec <ond.babec@gmail.com>
2020-04-20 21:12:21 +0200

    free space

--- .github/scripts/setup_docker.sh
+++ .github/scripts/setup_docker.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
 set -e
 
+#Remove github thrash
+
+echo "=============================================================================="
+echo "Freeing up disk space on CI system"
+echo "=============================================================================="
+
+echo "Listing 100 largest packages"
+dpkg-query -Wf '${Installed-Size}\t${Package}\n' | sort -n | tail -n 100
+df -h
+echo "Removing large packages"
+sudo apt-get remove -y '^ghc-8.*'
+sudo apt-get remove -y '^dotnet-.*'
+sudo apt-get remove -y '^llvm-.*'
+sudo apt-get remove -y 'php.*'
+sudo apt-get remove -y azure-cli google-cloud-sdk hhvm google-chrome-stable firefox powershell mono-devel
+sudo apt-get autoremove -y
+sudo apt-get clean
+df -h
+echo "Removing large directories"
+# deleting 15GB
+rm -rf /usr/share/dotnet/
+df -h
+
+sudo apt-get -y clean
+
 #Docker installation from google default repositories
 sudo apt-get -y update
 
@@ -19,7 +44,7 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io
 #Configuring docker to github actions disk
 
 sudo systemctl stop docker
-sudo sh -c "sed -i 's#ExecStart=/usr/bin/dockerd -H fd://#ExecStart=/usr/bin/dockerd -g /mnt/docker -H fd://#' /lib/systemd/system/docker.service"
+sudo sh -c "sed -i 's#ExecStart=/usr/bin/dockerd -H fd://#ExecStart=/usr/bin/dockerd -H fd://#' /lib/systemd/system/docker.service"
 sudo systemctl daemon-reload
 sudo rsync -aqxP /var/lib/docker/ /mnt/docker
 
