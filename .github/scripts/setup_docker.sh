90b7da8f192f86b73f4d5a0ed702bb2dacd92539
Ondrej Babec <ond.babec@gmail.com>
2020-04-21 09:00:11 +0200

    Finalize disk clearance

--- .github/scripts/setup_docker.sh
+++ .github/scripts/setup_docker.sh
@@ -1,30 +1,6 @@
 #!/bin/bash
 set -e
 
-#Remove github thrash
-
-echo "=============================================================================="
-echo "Freeing up disk space on CI system"
-echo "=============================================================================="
-sudo rm -rf "/usr/local/share/boost"
-sudo rm -rf "$AGENT_TOOLSDIRECTORY"
-echo "Listing 100 largest packages"
-dpkg-query -Wf '${Installed-Size}\t${Package}\n' | sort -n | tail -n 100
-df -h
-echo "Removing large packages"
-sudo apt-get remove -y '^ghc-8.*'
-sudo apt-get remove -y '^dotnet-.*'
-sudo apt-get remove -y '^llvm-.*'
-sudo apt-get remove -y 'php.*'
-sudo apt-get remove -y azure-cli google-cloud-sdk hhvm google-chrome-stable firefox powershell mono-devel
-sudo apt-get autoremove -y
-sudo apt-get clean -y
-df -h
-echo "Removing large directories"
-# deleting 15GB
-rm -rf /usr/share/dotnet/
-df -h
-
 #Docker installation from google default repositories
 sudo apt-get -y update
 
@@ -50,5 +26,3 @@ sudo rsync -aqxP /var/lib/docker/ /mnt/docker
 
 #Starting docker
 sudo systemctl start docker
-
-df -h
