085206e353eed73406cec8245852baf1f4ba044e
rkpattnaik780 <rkpattnaik780@gmail.com>
2020-04-23 17:02:37 +0530

    Style: add copyright/license in UI source files
    Copyright and license have been prepended to the UI source files where they were missing.

--- console/console-init/ui/src/modules/address/utils/utils.ts
+++ console/console-init/ui/src/modules/address/utils/utils.ts
@@ -1,10 +1,10 @@
-import { statusToDisplay } from "modules/address-space";
-import { AddressTypes } from "constant";
-
 /*
  * Copyright 2020, EnMasse authors.
  * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
  */
+
+import { statusToDisplay } from "modules/address-space";
+import { AddressTypes } from "constant";
 
 interface IAddressStatusProps {
   phase: string;
