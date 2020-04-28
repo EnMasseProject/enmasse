4e0b770277a82d32e2481a3bb3ec8597173aada8
David Kornel <kornys@outlook.com>
2020-04-28 14:14:43 +0200

    Add build env info into PR comment response (#4400)
    * Add build env info into PR comment response

--- systemtests/scripts/build_pr_response.sh
+++ systemtests/scripts/build_pr_response.sh
@@ -4,6 +4,7 @@ RESULTS_PATH=${1}
 TEST_CASE=${2}
 TEST_PROFILE=${3}
 BUILD_ID=${4:-0}
+OCP_VERSION=${5:-3}
 
 JSON_FILE_RESULTS=results.json
 
@@ -28,7 +29,14 @@ TEST_FAILURES_COUNT=$(get_test_count "failures")
 
 TEST_ALL_FAILED_COUNT=$((TEST_ERRORS_COUNT + TEST_FAILURES_COUNT))
 
-SUMMARY="**TEST_PROFILE**: ${TEST_PROFILE}\n**TEST_CASE:** ${TEST_CASE}\n**TOTAL:** ${TEST_COUNT}\n**PASS:** $((TEST_COUNT - TEST_ALL_FAILED_COUNT - TEST_SKIPPED_COUNT))\n**FAIL:** ${TEST_ALL_FAILED_COUNT}\n**SKIP:** ${TEST_SKIPPED_COUNT}\n**BUILD_NUMBER:** ${BUILD_ID}\n"
+
+if [[ "${OCP_VERSION}" == "4" ]]; then
+  BUILD_ENV="crc"
+else
+  BUILD_ENV="oc cluster up"
+fi
+
+SUMMARY="**TEST_PROFILE**: ${TEST_PROFILE}\n**TEST_CASE:** ${TEST_CASE}\n**TOTAL:** ${TEST_COUNT}\n**PASS:** $((TEST_COUNT - TEST_ALL_FAILED_COUNT - TEST_SKIPPED_COUNT))\n**FAIL:** ${TEST_ALL_FAILED_COUNT}\n**SKIP:** ${TEST_SKIPPED_COUNT}\n**BUILD_NUMBER:** ${BUILD_ID}\n**BUILD_ENV:** ${BUILD_ENV}\n"
 
 FAILED_TESTS=$(find "${RESULTS_PATH}" -name 'TEST*.xml' -type f -print0 | xargs -0 sed -n "s#\(<testcase.*time=\"[0-9]*,\{0,1\}[0-9]\{1,3\}\..*[^\/]>\)#\1#p" | awk -F '"' '{print "\\n- " $2 " in "  $4}')
 echo ${FAILED_TESTS}
