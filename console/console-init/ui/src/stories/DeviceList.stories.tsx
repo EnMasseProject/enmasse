c0eafecbde065692477ec21d3676b17a7f3844cd
rkpattnaik780 <rkpattnaik780@gmail.com>
2020-05-04 15:51:27 +0530

    Feat: table component for DeviceList page
    Implemented the visual component for the DeviceList table along with its storybook and a few test cases.

--- console/console-init/ui/src/stories/DeviceList.stories.tsx
+++ console/console-init/ui/src/stories/DeviceList.stories.tsx
@@ -6,12 +6,46 @@
 import React from "react";
 import { MemoryRouter } from "react-router";
 
-import { DeviceListAlert } from "modules/device";
+import {
+  DeviceListAlert,
+  DeviceList,
+  IDevice,
+  getTableCells
+} from "modules/device";
 import { text } from "@storybook/addon-knobs";
+import { IRowData } from "@patternfly/react-table";
 
 export default {
   title: "Device"
 };
+
+const rows: IDevice[] = [
+  {
+    id: "littlesensor1",
+    type: "Using gateways",
+    status: true,
+    selected: true,
+    lastSeen: "2020-01-20T11:44:28.607Z",
+    lastUpdated: "2020-01-20T11:44:28.607Z",
+    creationTimeStamp: "2020-01-20T11:44:28.607Z"
+  },
+  {
+    id: "jboss20",
+    type: "Using gateways",
+    status: false,
+    selected: false,
+    lastSeen: "2020-04-20T11:44:28.607Z",
+    lastUpdated: "2020-04-29T11:44:28.607Z",
+    creationTimeStamp: "2020-04-30T11:44:28.607Z"
+  }
+];
+
+const actionResolver = (rowData: IRowData) => [
+  {
+    title: "Edit Device",
+    onClick: () => {}
+  }
+];
 
 export const deviceAlert = () => (
   <MemoryRouter>
@@ -23,6 +57,16 @@ export const deviceAlert = () => (
         "Alert description",
         "You have a total of 36,300 devices"
       )}
+    />
+  </MemoryRouter>
+);
+
+export const deviceTable = () => (
+  <MemoryRouter>
+    <DeviceList
+      rows={rows.map(getTableCells)}
+      onSelect={async () => {}}
+      actionResolver={actionResolver}
     />
   </MemoryRouter>
 );
