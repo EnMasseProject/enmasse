889075532c75cd246dd2862e51a708a1c672d919
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2020-05-06 10:03:13 +0200

    Update comment

--- pkg/state/router.go
+++ pkg/state/router.go
@@ -167,10 +167,8 @@ func (r *RouterState) doRequest(request *amqp.Message) (*amqp.Message, error) {
 }
 
 func isConnectionError(err error) bool {
+	// TODO: Handle errors that are not strictly connection-related potentially with retries
 	return err != nil
-	/*
-		var amqpErr *amqp.Error
-		return errors.Is(err, amqp.ErrConnClosed) || errors.Is(err, amqpcommand.NotConnectedError) || errors.As(err, &amqpErr)*/
 }
 
 func (r *RouterState) createEntity(entity RouterEntityType, name string, data map[string]interface{}) error {
