8bf25b48d53d0ee7b3f515938552e49ac25bd942
ajaypratap003 <ajay.pratap233@gmail.com>
2020-05-11 23:01:43 +0530

    create MetdataListTable and DeviceInfo

--- console/console-init/ui/src/modules/device-detail/components/DeviceInfoGateways/index.ts
+++ console/console-init/ui/src/modules/device-detail/components/DeviceInfoGateways/index.ts
@@ -3,8 +3,4 @@
  * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
  */
 
-export * from "./DeviceDetailNavigation";
-export * from "./DeviceInfo";
-export * from "./DeviceInfoCredentials";
-export * from "./DeviceInfoMetadata";
 export * from "./DeviceInfoGateways";
