6f1eefd66563a8e580c612e6c8745027793756b8
sanjeev kumar <sanjeevkumar201418@gmail.com>
2020-05-13 19:32:55 +0530

    add device detail header visual component

---
+++ console/console-init/ui/src/stories/DeviceDetail.stories.tsx
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2020, EnMasse authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+
+import React from "react";
+import { MemoryRouter } from "react-router";
+import { text, boolean } from "@storybook/addon-knobs";
+import { action } from "@storybook/addon-actions";
+import { DeviceDetailHeader } from "modules/device-detail";
+
+export default {
+  title: "Device"
+};
+export const deviceDetailHeader = () => (
+  <MemoryRouter>
+    <DeviceDetailHeader
+      deviceName={text("Device Name", "Device foo")}
+      addedDate="2019-11-25T05:24:05.755Z"
+      lastTimeSeen="2019-11-25T05:24:05.755Z"
+      onChange={action("onEnableChange Clicked")}
+      onDelete={action("onDelete Clicked")}
+      onEdit={action("onEdit Clicked")}
+      onClone={action("onClone Clicked")}
+    />
+  </MemoryRouter>
+);
