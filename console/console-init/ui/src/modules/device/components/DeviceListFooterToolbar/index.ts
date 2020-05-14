24ce6baaed8bdd01851f0f9c0012eb75b9c493d8
Ramakrishna Pattnaik <rkpattnaik780@gmail.com>
2020-05-14 18:14:06 +0530

    Feat: story book for device list page (#4523)
    * A storybook has been created using all visual components made for device list page
    * Fixed a typo in DeviceList

---
+++ console/console-init/ui/src/modules/device/components/DeviceListFooterToolbar/index.ts
@@ -0,0 +1,6 @@
+/*
+ * Copyright 2020, EnMasse authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+
+export * from "./DeviceListFooterToolbar";
