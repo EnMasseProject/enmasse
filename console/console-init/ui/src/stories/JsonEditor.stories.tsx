516717a020306f21d3f33a66a3f0e15efb801486
sanjeev kumar <sanjeevkumar201418@gmail.com>
2020-05-18 20:58:43 +0530

    Create a device using json

--- console/console-init/ui/src/stories/JsonEditor.stories.tsx
+++ console/console-init/ui/src/stories/JsonEditor.stories.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { MemoryRouter } from "react-router";
-import { JsonViewEditor } from "components";
+import { JsonEditor } from "components";
 import { boolean } from "@storybook/addon-knobs";
 import { Page } from "@patternfly/react-core";
 
@@ -13,17 +13,13 @@ export const JsonEditorView = () => {
       test1: "value1"
     }
   };
-  const style = {
-    maxWidth: 1600,
-    minWidth: 800
-  };
   return (
     <MemoryRouter>
       <Page>
-        <JsonViewEditor
+        <JsonEditor
           readOnly={boolean("readOnly", true)}
-          detailInJson={jsonData}
-          style={style}
+          value={JSON.stringify(jsonData)}
+          width="auto"
         />
       </Page>
     </MemoryRouter>
