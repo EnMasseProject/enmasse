3ca07598e1e9f61f4f5aaadb05ee4e07553286a7
sanjeev kumar <sanjeevkumar201418@gmail.com>
2020-05-18 20:58:43 +0530

    Create a device using json

---
+++ console/console-init/ui/src/modules/device/components/AddDeviceWithJson/AddDeviceWithJson.tsx
@@ -0,0 +1,171 @@
+/*
+ * Copyright 2020, EnMasse authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+
+import React, { useState } from "react";
+import {
+  PageSection,
+  PageSectionVariants,
+  Button,
+  Grid,
+  GridItem
+} from "@patternfly/react-core";
+import { StyleSheet, css } from "@patternfly/react-styles";
+import { JsonEditor } from "components";
+import {
+  AddJsonUsingTemplate,
+  connectedViaGatewayDeviceTemplate,
+  directlyConnectedDeviceTemplate
+} from "./AddJsonUsingTemplate";
+import { compareJsonObject, getFormattedJsonString } from "utils";
+import { types, MODAL_TYPES, useStoreContext } from "context-state-reducer";
+import { DeviceListAlert } from "modules/device";
+import { TemplateType } from "constant";
+
+const styles = StyleSheet.create({
+  box_align_style: {
+    minHeight: "40em",
+    maxHeight: "40em",
+    border: "1px solid",
+    borderColor: "grey",
+    padding: 20,
+    marginRight: 20
+  }
+});
+interface IAddDeviceWithJsonProps {}
+
+const AddDeviceWithJson: React.FunctionComponent<IAddDeviceWithJsonProps> = () => {
+  const { dispatch } = useStoreContext();
+  const [deviceDetail, setDeviceDetail] = useState<string>();
+  const [selectedTemplate, setSelectedTemplate] = useState<string>(
+    TemplateType.DIRECTLY_CONNECTED
+  );
+  const [showJsonValidationError, setShowJsonValidationError] = useState<
+    boolean
+  >(false);
+
+  const setDeviceInfoInDetail = (value?: string) => {
+    if (showJsonValidationError) {
+      setShowJsonValidationError(false);
+    }
+    setDeviceDetail(value);
+  };
+
+  const onLeaveModal = () => {
+    //TODO: handle onConfirm event for modal
+  };
+
+  const onCancel = () => {
+    let isJsonPresent: boolean = false;
+    try {
+      const detail = deviceDetail && JSON.parse(deviceDetail);
+      if (detail && selectedTemplate === TemplateType.DIRECTLY_CONNECTED) {
+        if (!compareJsonObject(detail, directlyConnectedDeviceTemplate)) {
+          isJsonPresent = true;
+        }
+      } else if (selectedTemplate === TemplateType.VIA_GATEWAY) {
+        if (!compareJsonObject(detail, connectedViaGatewayDeviceTemplate)) {
+          isJsonPresent = true;
+        }
+      }
+    } catch {
+      if (deviceDetail) isJsonPresent = true;
+    }
+    if (isJsonPresent) {
+      dispatch({
+        type: types.SHOW_MODAL,
+        modalType: MODAL_TYPES.LEAVE_CREATE_DEVICE,
+        modalProps: {
+          onConfirm: onLeaveModal,
+          option: "Leave",
+          detail: `Do you want to leave this creation page without saving? All information will be lost.`,
+          header: "Leave without saving ?",
+          confirmButtonLabel: "Leave"
+        }
+      });
+    }
+  };
+
+  const isJsonValid = () => {
+    try {
+      if (!deviceDetail || deviceDetail.trim() === "") {
+        if (deviceDetail !== "") {
+          setDeviceInfoInDetail(undefined);
+        }
+        return true;
+      }
+      const detail = JSON.parse(deviceDetail.trim());
+      if (detail) {
+        setDeviceInfoInDetail(getFormattedJsonString(detail));
+        return true;
+      }
+    } catch {
+      return false;
+    }
+  };
+  const onFinish = () => {
+    if (!isJsonValid()) {
+      setShowJsonValidationError(true);
+    } else {
+    }
+  };
+  const onPreview = () => {
+    if (!isJsonValid()) {
+      setShowJsonValidationError(true);
+    } else {
+    }
+  };
+
+  return (
+    <PageSection variant={PageSectionVariants.light}>
+      <DeviceListAlert
+        id="alert-error-invalid-json"
+        variant="danger"
+        isInline
+        visible={showJsonValidationError}
+        title="Error"
+        description="Invalid JSON syntax, unable to parse JSON"
+      />
+
+      <br />
+      <Grid>
+        <GridItem span={9} className={css(styles.box_align_style)}>
+          <JsonEditor
+            value={deviceDetail}
+            readOnly={false}
+            name={"editor-add-device"}
+            setDetail={setDeviceInfoInDetail}
+            style={{
+              minHeight: "39em"
+            }}
+          />
+        </GridItem>
+        <GridItem span={3} className={css(styles.box_align_style)}>
+          <PageSection variant={PageSectionVariants.light}>
+            <AddJsonUsingTemplate
+              setDetail={setDeviceInfoInDetail}
+              selectedTemplate={selectedTemplate}
+              setSelectedTemplate={setSelectedTemplate}
+            />
+          </PageSection>
+        </GridItem>
+      </Grid>
+      <br />
+
+      <Button variant="primary" onClick={onFinish}>
+        Finish
+      </Button>
+      {"  "}
+      <Button variant="secondary" onClick={onPreview}>
+        Preview
+      </Button>
+      {"  "}
+      <Button variant="link" onClick={onCancel}>
+        Cancel
+      </Button>
+    </PageSection>
+  );
+};
+
+export { AddDeviceWithJson };
---
+++ console/console-init/ui/src/modules/device/components/AddDeviceWithJson/AddJsonUsingTemplate.tsx
@@ -0,0 +1,140 @@
+/*
+ * Copyright 2020, EnMasse authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+
+import React, { useState } from "react";
+import {
+  Title,
+  Radio,
+  Button,
+  Modal,
+  FileUpload
+} from "@patternfly/react-core";
+import { UploadIcon } from "@patternfly/react-icons";
+import { TemplateType } from "constant";
+import { getFormattedJsonString } from "utils";
+
+export const directlyConnectedDeviceTemplate = {
+  id: "<optional-id>",
+  registration: {
+    enabled: true,
+    defaults: {},
+    ext: {},
+    credentials: []
+  }
+};
+export const connectedViaGatewayDeviceTemplate = {
+  id: "<optional-id>",
+  registration: {
+    enabled: true,
+    defaults: {
+      "content-type": "text/plain"
+    },
+    via: [],
+    ext: {}
+  }
+};
+interface IAddJsonUsingTemplate {
+  setDetail: (value: string) => void;
+  selectedTemplate: string;
+  setSelectedTemplate: (value: string) => void;
+}
+const AddJsonUsingTemplate: React.FunctionComponent<IAddJsonUsingTemplate> = ({
+  setDetail,
+  selectedTemplate,
+  setSelectedTemplate
+}) => {
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const onChange = (event: any) => {
+    const name: string = event.target.name;
+    if (name === "device-connected-directly") {
+      setSelectedTemplate(TemplateType.DIRECTLY_CONNECTED);
+    } else if (name === "device-via-gateway") {
+      setSelectedTemplate(TemplateType.VIA_GATEWAY);
+    }
+  };
+  const onClickTry = () => {
+    if (selectedTemplate === TemplateType.DIRECTLY_CONNECTED) {
+      setDetail(getFormattedJsonString(directlyConnectedDeviceTemplate));
+    } else if (selectedTemplate === TemplateType.VIA_GATEWAY) {
+      setDetail(getFormattedJsonString(connectedViaGatewayDeviceTemplate));
+    }
+  };
+  const onReaderLoad = (event: any) => {
+    try {
+      const detail = JSON.parse(event.target.result);
+      setDetail(getFormattedJsonString(detail));
+    } catch {
+      //TODO: Error handling for invalid json
+      console.log("Invalid Json");
+    }
+  };
+
+  const onClickUpload = () => {
+    setShowModal(true);
+  };
+
+  const onUploadFile = (value: string | File, filename: string, event: any) => {
+    var reader = new FileReader();
+    reader.onload = onReaderLoad;
+    reader.readAsText(event.target.files[0]);
+    setShowModal(false);
+  };
+
+  return (
+    <>
+      <Title headingLevel="h1" size="lg" id="title-select-json">
+        Select a JSON template for quick start
+      </Title>
+      <br />
+      <Radio
+        style={{ marginLeft: 10 }}
+        isLabelWrapped
+        isChecked={selectedTemplate === TemplateType.DIRECTLY_CONNECTED}
+        onClick={onChange}
+        label="Device connected directly"
+        id="radio-device-connected-directly"
+        name="device-connected-directly"
+      />
+      <br />
+      <Radio
+        style={{ marginLeft: 10 }}
+        isLabelWrapped
+        isChecked={selectedTemplate === TemplateType.VIA_GATEWAY}
+        onClick={onChange}
+        label="Device connected via gateway"
+        id="radio-device-via-gateway"
+        name="device-via-gateway"
+      />
+      <br />
+      <Button variant="secondary" onClick={onClickTry} id="btn-try-it">
+        Try it
+      </Button>
+      <br />
+      <br />
+      <Title headingLevel="h3" size="md" id="title-upload-json">
+        or Upload a JSON file
+      </Title>
+      <br />
+      <Button variant="secondary" onClick={onClickUpload} id="btn-upload-json">
+        <UploadIcon /> Upload a JSON file
+      </Button>
+      {showModal && (
+        <Modal title="" isOpen={showModal} onClose={() => setShowModal(false)}>
+          <FileUpload
+            id="file-upload-json"
+            value={""}
+            filename={""}
+            onChange={onUploadFile}
+            dropzoneProps={{
+              accept: ".json"
+            }}
+          />
+        </Modal>
+      )}
+    </>
+  );
+};
+
+export { AddJsonUsingTemplate };
