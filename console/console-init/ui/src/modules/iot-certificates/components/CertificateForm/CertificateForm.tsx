699645f6fd9ffb1369dba5ba507b31ff076d9d8f
Ramakrishna Pattnaik <rkpattnaik780@gmail.com>
2020-05-29 14:22:59 +0530

    Feat: Add visual components for IoT Certificates (#4616)
    Creates visual components including certificate card, form, list and toolbar.

---
+++ console/console-init/ui/src/modules/iot-certificates/components/CertificateForm/CertificateForm.tsx
@@ -0,0 +1,211 @@
+/*
+ * Copyright 2020, EnMasse authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+
+import React, { useState, useEffect } from "react";
+import {
+  Card,
+  CardBody,
+  Form,
+  FormGroup,
+  TextInput,
+  Grid,
+  GridItem,
+  Split,
+  SplitItem,
+  ActionGroup,
+  Button,
+  DropdownPosition
+} from "@patternfly/react-core";
+import { DropdownWithToggle, SwitchWithToggle } from "components";
+import { css, StyleSheet } from "@patternfly/react-styles";
+import { getLabelByKey } from "utils";
+import {
+  algorithmTypeOptions,
+  IProjectCertificate
+} from "modules/iot-certificates";
+
+const styles = StyleSheet.create({
+  dropdown_align: {
+    display: "flex"
+  },
+  dropdown_toggle_align: {
+    flex: "1"
+  },
+  capitalize: {
+    textTransform: "capitalize"
+  }
+});
+
+export interface ICertificateFormProps {
+  certificate?: IProjectCertificate;
+  setOnEditMode: React.Dispatch<React.SetStateAction<boolean>>;
+  id: string;
+}
+
+export const CertificateForm: React.FunctionComponent<ICertificateFormProps> = ({
+  setOnEditMode,
+  certificate,
+  id
+}) => {
+  const initialFormState: IProjectCertificate = {
+    "subject-dn": "",
+    "public-key": "",
+    "auto-provisioning-enabled": false,
+    algorithm: "RSA",
+    "not-before": "",
+    "not-after": ""
+  };
+
+  const [certificateFormData, setCertificateFormData] = useState<
+    IProjectCertificate
+  >(initialFormState);
+
+  useEffect(() => {
+    // code to initialize the form if it is being used for edit
+    certificate && setCertificateFormData(certificate);
+  }, []);
+
+  const onCertificateFormDataChange = (
+    value: string | boolean,
+    event: React.FormEvent<HTMLInputElement>
+  ) => {
+    const cerftificateField = event?.target?.name;
+    const newCertificateData = { ...certificateFormData };
+    (newCertificateData as any)[cerftificateField] = value;
+    setCertificateFormData(newCertificateData);
+  };
+
+  const cancelEdit = () => {
+    setOnEditMode(false);
+  };
+
+  const saveCertificate = () => {
+    // TODO: A mutation to save the certificate and hide form on success
+  };
+
+  const {
+    ["subject-dn"]: subjectDn,
+    ["public-key"]: publicKey,
+    ["auto-provisioning-enabled"]: autoProvision,
+    algorithm,
+    ["not-before"]: notBefore,
+    ["not-after"]: notAfter
+  } = certificateFormData;
+
+  return (
+    <Card id={`cf-card-${id}`}>
+      <CardBody>
+        <Form>
+          <FormGroup
+            fieldId={`cf-subjectdn-${id}`}
+            isRequired
+            label={getLabelByKey("subject-dn")}
+          >
+            <TextInput
+              id={`cf-subjectdn-${id}`}
+              type="text"
+              name="subject-dn"
+              isRequired
+              value={subjectDn}
+              onChange={onCertificateFormDataChange}
+            />
+          </FormGroup>
+          <FormGroup
+            fieldId={`cf-public-key-${id}`}
+            isRequired
+            label={getLabelByKey("public-key")}
+          >
+            <TextInput
+              id={`cf-public-key-${id}`}
+              type="text"
+              name="public-key"
+              isRequired
+              value={publicKey}
+              onChange={onCertificateFormDataChange}
+            />
+          </FormGroup>
+          <FormGroup
+            fieldId={`cf-algo-${id}`}
+            isRequired
+            label={getLabelByKey("algorithm")}
+            className={css(styles.capitalize)}
+          >
+            <DropdownWithToggle
+              id={`cf-algo-${id}`}
+              name="algorithm"
+              className={css(styles.dropdown_align)}
+              toggleClass={css(styles.dropdown_toggle_align)}
+              position={DropdownPosition.left}
+              onSelectItem={onCertificateFormDataChange}
+              dropdownItems={algorithmTypeOptions}
+              value={algorithm}
+            />
+          </FormGroup>
+          <Grid>
+            <GridItem span={6}>
+              <FormGroup
+                fieldId={`cf-not-before-${id}`}
+                isRequired
+                label={getLabelByKey("not-before")}
+              >
+                <TextInput
+                  id={`cf-not-before-${id}`}
+                  name="not-before"
+                  isRequired
+                  value={notBefore}
+                  onChange={onCertificateFormDataChange}
+                />
+              </FormGroup>
+            </GridItem>
+            <GridItem span={6}>
+              <FormGroup
+                fieldId={`cf-not-after-${id}`}
+                isRequired
+                label={getLabelByKey("not-after")}
+              >
+                <TextInput
+                  id={`cf-not-after-${id}`}
+                  name="not-after"
+                  isRequired
+                  value={notAfter}
+                  onChange={onCertificateFormDataChange}
+                />
+              </FormGroup>
+            </GridItem>
+          </Grid>
+          <Split>
+            <SplitItem>
+              <h1>
+                <b>{getLabelByKey("auto-provisioning-enabled")}</b>
+              </h1>
+            </SplitItem>
+            <SplitItem isFilled />
+            <SplitItem>
+              <SwitchWithToggle
+                id="cf-auto-provision-switch"
+                label="Enabled"
+                labelOff="Disabled"
+                isChecked={autoProvision}
+                onChange={onCertificateFormDataChange}
+              />
+            </SplitItem>
+          </Split>
+          <ActionGroup>
+            <Button
+              variant="primary"
+              onClick={saveCertificate}
+              id="cf-save-button"
+            >
+              Save
+            </Button>
+            <Button variant="link" onClick={cancelEdit} id="cf-cancel-button">
+              Cancel
+            </Button>
+          </ActionGroup>
+        </Form>
+      </CardBody>
+    </Card>
+  );
+};
---
+++ console/console-init/ui/src/modules/iot-certificates/components/IoTCertificateToolbar/IoTCertificateToolbar.tsx
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2020, EnMasse authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+
+import React from "react";
+import {
+  DataToolbar,
+  DataToolbarContent,
+  DataToolbarItem,
+  Button
+} from "@patternfly/react-core";
+import { SwitchWithToggle } from "components";
+
+export interface IProjectCertificateToolbarProps {
+  setShowCertificateForm: React.Dispatch<React.SetStateAction<boolean>>;
+  isJsonView: boolean;
+  handleJsonViewChange: (val: boolean) => void;
+}
+
+export const ProjectCertificateToolbar: React.FunctionComponent<IProjectCertificateToolbarProps> = ({
+  setShowCertificateForm,
+  isJsonView,
+  handleJsonViewChange
+}) => {
+  const handleAddCertificateClick = () => {
+    setShowCertificateForm(true);
+  };
+
+  const handleUploadCertificateClick = () => {
+    // TODO: Mechanism to upload a certificate
+  };
+
+  return (
+    <DataToolbar id="pct-data-toolbar">
+      <DataToolbarContent>
+        <DataToolbarItem>
+          <Button
+            id="pct-add-certificate-button"
+            onClick={handleAddCertificateClick}
+          >
+            Add certificate
+          </Button>
+        </DataToolbarItem>
+        <DataToolbarItem>
+          <Button
+            id="pct-upload-certificate-button"
+            variant="link"
+            onClick={handleUploadCertificateClick}
+          >
+            Upload certificate
+          </Button>
+        </DataToolbarItem>
+        <DataToolbarItem
+          breakpointMods={[{ modifier: "align-right", breakpoint: "md" }]}
+        >
+          <SwitchWithToggle
+            id="pct-edit-json-switch"
+            label="Edit in Json"
+            isChecked={isJsonView}
+            onChange={handleJsonViewChange}
+          />
+        </DataToolbarItem>
+      </DataToolbarContent>
+    </DataToolbar>
+  );
+};
