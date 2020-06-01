9fb400f54f58731640b341bda14f27a75cf3baf1
Ajay Pratap <ajaypratap003@users.noreply.github.com>
2020-06-01 19:31:45 +0530

    Create ConfigurationInfo Page (#4647)

---
+++ console/console-init/ui/src/components/AdapterList/AdapterList.tsx
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2020, EnMasse authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+
+import React from "react";
+import { Title, Grid, GridItem } from "@patternfly/react-core";
+import { InputText, StatusLabelWithIcon } from "components";
+import { Protocols } from "constant";
+
+export interface IAdapterConfig {
+  url?: string;
+  host?: string;
+  port?: number;
+  tlsEnabled?: boolean;
+}
+
+export interface IAdapter {
+  type:
+    | Protocols.HTTP
+    | Protocols.HTTPS
+    | Protocols.MQTT
+    | Protocols.AMQP
+    | Protocols.AMQPS
+    | Protocols.COAP;
+  value: IAdapterConfig;
+}
+
+export interface IAdapterListProps {
+  id: string;
+  adapters: IAdapter[];
+}
+
+export const AdapterList: React.FC<IAdapterListProps> = ({ id, adapters }) => {
+  return (
+    <div id={id}>
+      {adapters &&
+        adapters.map((adapter: IAdapter, index: number) => (
+          <Adapter adapter={adapter} key={`adapter-${adapter.type}`} />
+        ))}
+    </div>
+  );
+};
+
+const Adapter: React.FC<{ adapter: IAdapter }> = ({ adapter }) => {
+  const { type, value } = adapter || {};
+  const { tlsEnabled, host, port, url } = value || {};
+
+  return (
+    <>
+      <Title size="xl" headingLevel="h2">
+        {type.toUpperCase() + " Adapter"}
+      </Title>
+      {tlsEnabled && (
+        <>
+          <br />
+          <Grid>
+            <GridItem span={4}>
+              <b>TLS</b>
+            </GridItem>
+            <GridItem span={8}>
+              <StatusLabelWithIcon
+                id="adapter-list-tls-status-label"
+                isEnabled={tlsEnabled}
+              />
+            </GridItem>
+          </Grid>
+        </>
+      )}
+      {url && (
+        <>
+          <br />
+          <InputText
+            label={"URL"}
+            type={"text"}
+            value={url}
+            isReadOnly={true}
+            enableCopy={true}
+            id={`adapter-${type}-api-url-input`}
+            key={`adapter-${type}-api-url-input`}
+            ariaLabel={`adapter-${type}-api-url`}
+            isExpandable={false}
+          />
+        </>
+      )}
+      {host && (
+        <>
+          <br />
+          <InputText
+            label={"Host"}
+            type={"number"}
+            value={host}
+            isReadOnly={true}
+            enableCopy={true}
+            id={`adapter-${type}-api-host-input`}
+            ariaLabel={`adapter ${type} api host`}
+            isExpandable={false}
+          />
+        </>
+      )}
+      {port && (
+        <>
+          <br />
+          <InputText
+            label={"Port"}
+            type={"number"}
+            value={port}
+            isReadOnly={true}
+            enableCopy={true}
+            id={`adapter-${type}-api-port-input`}
+            ariaLabel={`adapter ${type} api port`}
+            isExpandable={false}
+          />
+        </>
+      )}
+    </>
+  );
+};
--- console/console-init/ui/src/modules/iot-device-detail/components/ConfigurationInfo/index.ts
+++ console/console-init/ui/src/modules/iot-device-detail/components/ConfigurationInfo/index.ts
@@ -3,8 +3,4 @@
  * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
  */
 
-export * from "./DeviceDetailNavigation";
-export * from "./DeviceDetailHeader";
-export * from "./DeviceInfo";
-export * from "./CredentialsView";
 export * from "./ConfigurationInfo";
