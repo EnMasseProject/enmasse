ea02121810d74f05b67feed9f8421b2b9b4f2829
Anukriti Jha <--global>
2020-06-01 15:15:38 +0530

    Remove UI warnings and resolve conflicts

---
+++ console/console-init/ui/src/modules/iot-project-detail/ProjectDetailPage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { PageSection } from "@patternfly/react-core";
+
+export default function ProjectDetailPage() {
+  return (<PageSection>
+      
+  </PageSection>);
+}
