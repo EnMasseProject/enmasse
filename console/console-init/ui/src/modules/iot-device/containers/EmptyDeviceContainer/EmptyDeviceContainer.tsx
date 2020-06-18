ce6e61838be178ce9dbbbd9b46bb82827bd9e097
Ramakrishna Pattnaik <rkpattnaik780@gmail.com>
2020-06-18 13:10:52 +0530

    Feat: EmptyDeviceContainer and alert messages (#4764)
    * Feat: EmptyDeviceContainer and alert messages
    
    The empty state and alert are now being adjusted according to the number of devices available and filter applied.
    
    * Style: remove logs and redundant imports

---
+++ console/console-init/ui/src/modules/iot-device/containers/EmptyDeviceContainer/EmptyDeviceContainer.tsx
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2020, EnMasse authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+
+import React from "react";
+import {
+  IEmptyDeviceListProps,
+  EmptyDeviceList
+} from "modules/iot-device/components";
+import { useQuery } from "@apollo/react-hooks";
+import { POLL_INTERVAL, FetchPolicy } from "constant";
+import { useParams } from "react-router";
+import { IIoTDevicesCountResponse } from "schema/iot_device";
+import { RETURN_ALL_DEVICES_COUNT_FOR_IOT_PROJECT } from "graphql-module/queries";
+
+export interface IEmptyDeviceContainerProps extends IEmptyDeviceListProps {
+  setTotalDevices: (val: number) => void;
+}
+
+export const EmptyDeviceContainer: React.FC<IEmptyDeviceContainerProps> = ({
+  handleInputDeviceInfo,
+  handleJSONUpload,
+  setTotalDevices
+}) => {
+  const { projectname } = useParams();
+
+  const { data } = useQuery<IIoTDevicesCountResponse>(
+    RETURN_ALL_DEVICES_COUNT_FOR_IOT_PROJECT(projectname),
+    { pollInterval: POLL_INTERVAL, fetchPolicy: FetchPolicy.NETWORK_ONLY }
+  );
+
+  const total = data?.devices?.total || 0;
+
+  total > 0 && setTotalDevices(total);
+
+  return (
+    <EmptyDeviceList
+      handleInputDeviceInfo={handleInputDeviceInfo}
+      handleJSONUpload={handleJSONUpload}
+    />
+  );
+};
