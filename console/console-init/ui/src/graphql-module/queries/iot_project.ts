472bc28aacbb3f530952cab5de73f05aef4985fb
Ramakrishna Pattnaik <rkpattnaik780@gmail.com>
2020-06-23 12:10:27 +0530

    Feat: Integrate Project detail header component (#4784)
    * Integrate Project Detail Page
    
    * Feat: Integrate IoTProjectDetailHeader component
    
    * Integrate Project Detail Header component.
    * Undefined checks for IoTProjectDetailHeader
    
    * Make changes according to review comments
    
    Co-authored-by: Anukriti Jha <--global>

---
+++ console/console-init/ui/src/graphql-module/queries/iot_project.ts
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2020, EnMasse authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+
+import gql from "graphql-tag";
+
+const FILTER_RETURN_IOT_PROJECTS = (filterObject: any) => {
+  const { projectName } = filterObject;
+  let filter: string = "";
+
+  if (projectName && projectName.trim() !== "") {
+    filter += "`$.metadata.name` = '" + projectName + "'";
+  }
+
+  // TODO: Filters to be incrementally added
+
+  return filter;
+};
+
+export const RETURN_IOT_PROJECTS = (
+  filterObj?: any,
+  queryResolver?: string
+) => {
+  // TODO: Default resolver is subjected to change, with respect to most used query
+  const defaultQueryResolver = `
+    total
+    iotProjects {
+      metadata {
+        name
+      }
+      enabled
+      spec {
+        downstreamStrategyType
+      }
+      status {
+        phase
+      }
+    }
+  `;
+
+  if (!queryResolver) {
+    queryResolver = defaultQueryResolver;
+  }
+
+  let filter = FILTER_RETURN_IOT_PROJECTS(filterObj);
+
+  const IOT_PROJECT_DETAIL = gql(
+    `query allProjects {
+      allProjects(
+        filter: "${filter}",
+        projectType: iotProject
+      ) {
+         ${queryResolver}
+      }
+    }`
+  );
+
+  return IOT_PROJECT_DETAIL;
+};
