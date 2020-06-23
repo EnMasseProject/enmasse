cc8054d520330a57f06d5bb94d9e79aafb902cbe
Ramakrishna Pattnaik <rkpattnaik780@gmail.com>
2020-06-23 12:10:27 +0530

    Feat: Integrate Project detail header component (#4784)
    * Integrate Project Detail Page
    
    * Feat: Integrate IoTProjectDetailHeader component
    
    * Integrate Project Detail Header component.
    * Undefined checks for IoTProjectDetailHeader
    
    * Make changes according to review comments
    
    Co-authored-by: Anukriti Jha <--global>

--- console/console-init/ui/src/modules/iot-project-detail/index.ts
+++ console/console-init/ui/src/modules/iot-project-detail/index.ts
@@ -3,4 +3,6 @@
  * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
  */
 
-export * from "./IoTProjectDetailHeaderContainer";
+export * from "./components";
+export * from "./containers";
+export * from "./IoTProjectDetailPage";
