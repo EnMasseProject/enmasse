a084faca58a944965ba8eb91bee54933c340dd68
Ajay Pratap <ajaypratap003@users.noreply.github.com>
2020-06-25 17:07:01 +0530

    Fixed patternfly issues (#4822)
    * Fixed css issues  and fix AppLayout issue
    * Create use-patternfly module and fix issues and remove depenedency from external package
    * Fixed  patternfly 4 issues
    * Fix compile issue  and remove unused imports
    * Fixed alignment issue on address space review page
    * created hook to reset page and implemented review comments

--- console/console-init/ui/src/modules/address/AddressPage.tsx
+++ console/console-init/ui/src/modules/address/AddressPage.tsx
@@ -38,7 +38,7 @@ import { compareObject } from "utils";
 import { TablePagination } from "components/TablePagination";
 import { AddressTypes } from "constant";
 import { AddressToolbarContainer } from "modules/address/containers";
-import { useMutationQuery } from "hooks";
+import { useMutationQuery, useSearchParamsPageChange } from "hooks";
 
 export const GridStylesForTableHeader = StyleSheet.create({
   filter_left_margin: {
@@ -78,6 +78,8 @@ export default function AddressPage() {
   const searchParams = new URLSearchParams(location.search);
   const page = parseInt(searchParams.get("page") || "", 10) || 1;
   const perPage = parseInt(searchParams.get("perPage") || "", 10) || 10;
+
+  useSearchParamsPageChange([typeValue, statusValue, filterNames]);
 
   const [sortDropDownValue, setSortDropdownValue] = useState<ISortBy>();
 
