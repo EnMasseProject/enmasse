4015e3b1b694a3c42cdcca8895a7a3a78c0f5591
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2020-06-29 10:12:47 +0200

    Run CI using kind

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -3,21 +3,11 @@ set -e
 
 echo "${VERSION}"
 
-echo "Make"
-make
-
-echo "Build"
-make docker_build
-
 if [[ -v PUSH_REGISTRY ]]
 then
     echo "Logging in to registry"
     docker login -u "${REGISTRY_USER}" -p "${REGISTRY_PASS}" "${DOCKER_REGISTRY}"
-    make TAG="${VERSION}" docker_tag docker_push
+    make TAG="${VERSION}" buildpush
+else
+    make IMAGE_PULL_POLICY=IfNotPresent buildpushkind
 fi
-
-echo "Push to registry"
-make -j 4 docker_tag docker_push
-
-echo "Generate templates"
-make templates
