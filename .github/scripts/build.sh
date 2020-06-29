2342aa424743b845e7845491d537ca935917b55f
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2020-06-29 22:41:22 +0200

    Remove metrics api and remove nonexisting project deps

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -7,7 +7,7 @@ if [[ -v PUSH_REGISTRY ]]
 then
     echo "Logging in to registry"
     docker login -u "${REGISTRY_USER}" -p "${REGISTRY_PASS}" "${DOCKER_REGISTRY}"
-    make TAG="${VERSION}" buildpush
+    make buildpush
 else
     make IMAGE_PULL_POLICY=IfNotPresent buildpushkind
 fi
