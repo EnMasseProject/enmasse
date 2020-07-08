489f9cf53b33d6fda7877caed74fd1d9e240e131
David Kornel <kornys@outlook.com>
2020-07-08 13:41:29 +0200

    Enable framework unit tests without kube connection (#4923)

--- systemtests/src/main/java/io/enmasse/systemtest/framework/TestPlanExecutionListener.java
+++ systemtests/src/main/java/io/enmasse/systemtest/framework/TestPlanExecutionListener.java
@@ -17,7 +17,6 @@ import org.slf4j.Logger;
  */
 public class TestPlanExecutionListener implements TestExecutionListener {
     private static final Logger LOGGER = LoggerUtils.getLogger();
-    EnmasseOperatorManager operator = EnmasseOperatorManager.getInstance();
     Environment env = Environment.getInstance();
 
     @Override
@@ -28,9 +27,15 @@ public class TestPlanExecutionListener implements TestExecutionListener {
 
     @Override
     public void testPlanExecutionFinished(TestPlan testPlan) {
+        var tags = TestPlanInfo.getInstance().getTestRunTags();
+        if (tags != null && tags.size() == 1 && tags.get(0).equals(TestTag.FRAMEWORK)) {
+            LOGGER.info("Running framework tests, no cleanup performed");
+            return;
+        }
+
         try {
             if (!env.skipUninstall()) {
-                operator.deleteEnmasseBundle();
+                EnmasseOperatorManager.getInstance().deleteEnmasseBundle();
             }
         } catch (Exception | AssertionError ex) {
             LOGGER.error(ex.getMessage());
