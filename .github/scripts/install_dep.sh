27cba110d6b46bec3fc5a5774161d2736725d04b
Ondrej Babec <ond.babec@gmail.com>
2020-07-14 19:55:09 +0200

    Delete nodejs installation

--- .github/scripts/install_dep.sh
+++ .github/scripts/install_dep.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
 echo "Update archives"
-
-sudo add-apt-repository ppa:longsleep/golang-backports
 sudo apt-get update
 
 echo "Install maven"
 
 sudo apt install maven
-
-echo "Install nodeJS"
-
-sudo apt-get install curl
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo apt-get install -y nodejs yarn
 
 echo "Clean cache"
 sudo apt-get clean
