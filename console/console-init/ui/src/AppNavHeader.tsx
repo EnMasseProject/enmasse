5eda3a3b66f58a45ff8680685f50bbaf717b5af5
Vanessa Busch <vbusch@redhat.com>
2020-07-14 22:58:02 -0400

    Refactored header to make it easier to swap the nav
    Signed-off-by: Vanessa Busch <vbusch@redhat.com>

---
+++ console/console-init/ui/src/AppNavHeader.tsx
@@ -0,0 +1,54 @@
+import * as React from "react";
+import styles from "@patternfly/react-styles/css/components/Page/page";
+import { css } from "@patternfly/react-styles";
+import { PageHeaderProps, PageContextConsumer } from "@patternfly/react-core";
+
+export class AppNavHeader extends React.Component<PageHeaderProps> {
+  render() {
+    const {
+      logo = null as React.ReactNode,
+      logoProps = (null as unknown) as object,
+      logoComponent = "a",
+      toolbar = null as React.ReactNode,
+      avatar = null as React.ReactNode,
+      topNav = null as React.ReactNode,
+      ...props
+    } = this.props;
+
+    const LogoComponent = logoComponent as any;
+
+    return (
+      <PageContextConsumer>
+        {PageHeaderProps => {
+          return (
+            <header
+              role="banner"
+              className={`${css(styles.pageHeader)} `}
+              {...props}
+            >
+              {logo && (
+                <div className={css(styles.pageHeaderBrand)}>
+                  <LogoComponent
+                    className={css(styles.pageHeaderBrandLink)}
+                    {...logoProps}
+                  >
+                    {logo}
+                  </LogoComponent>
+                </div>
+              )}
+              {topNav && (
+                <div className={css(styles.pageHeaderNav)}>{topNav}</div>
+              )}
+              {(toolbar || avatar) && (
+                <div className={css(styles.pageHeaderTools)}>
+                  {toolbar}
+                  {avatar}
+                </div>
+              )}
+            </header>
+          );
+        }}
+      </PageContextConsumer>
+    );
+  }
+}
