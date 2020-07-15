1fc2b4b9f1c2c3b65fc1c79a1f66d65b2bd34ebf
obabec <ond.babec@gmail.com>
2020-07-15 19:58:11 +0200

    Remove nodejs installation on actions (#4977)
    * Delete nodejs installation

--- .github/scripts/install_dep.sh
+++ .github/scripts/install_dep.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 
 echo "Update archives"
-
-sudo add-apt-repository ppa:longsleep/golang-backports
 sudo apt-get update
 
 echo "Install maven"
 
 sudo apt install maven
-
-echo "Install nodeJS"
-
-sudo apt-get install curl
-curl -sL https://deb.nodesource.com/setup_13.x
-sudo -E bash -
-sudo apt-get install nodejs
 
 echo "Clean cache"
 sudo apt-get clean
