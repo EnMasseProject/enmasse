480e2192fa54cb851673f4d11a3c103f1029eb37
David Kornel <kornys@outlook.com>
2020-07-17 14:36:33 +0200

    Clean test base unused methods (#4976)

--- systemtests/src/test/java/io/enmasse/systemtest/TestBase.java
+++ systemtests/src/test/java/io/enmasse/systemtest/TestBase.java
@@ -35,51 +35,8 @@ public abstract class TestBase implements ITestSeparator {
     protected final ResourceManager resourceManager = ResourceManager.getInstance();
     protected final MessagingClientRunner clientRunner = new MessagingClientRunner();
 
-
-    protected List<ThrowingCallable> cleanup = new LinkedList<>();
-
-    /**
-     * Close any provided resource at the end of test, by calling the cleaner.
-     *
-     * @param <T>      The type of the closable.
-     * @param resource The closable to close.
-     * @param cleaner  The cleaner, which will be called.
-     * @return The input value, for chained calls.
-     */
-    protected <T> T cleanup(final T resource, final ThrowingConsumer<T> cleaner) {
-        this.cleanup.add(() -> cleaner.accept(resource));
-        return resource;
-    }
-
-    /**
-     * Close an {@link AutoCloseable} at the end of the test.
-     *
-     * @param <T>       The type of the closable.
-     * @param closeable The closable to close.
-     * @return The input value, for chained calls.
-     */
-    protected <T extends AutoCloseable> T cleanup(final T closeable) {
-        return cleanup(closeable, AutoCloseable::close);
-    }
-
     @AfterEach
     void cleanup() throws Exception {
         clientRunner.cleanClients();
-        Exception exception = null;
-        for (ThrowingCallable cleanup : this.cleanup) {
-            try {
-                cleanup.call();
-            } catch (Exception e) {
-                if (exception == null) {
-                    exception = e;
-                } else {
-                    exception.addSuppressed(e);
-                }
-            }
-        }
-        this.cleanup.clear();
-        if (exception != null) {
-            throw exception;
-        }
     }
 }
