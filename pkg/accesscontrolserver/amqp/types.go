2114464053d415b318f232f96c8fe114244cf7f7
k-wall <kwall@apache.org>
2020-07-17 21:14:16 +0100

    [Access Control] Remove Go-shim func now from AMQP codec we are on Go 1.14 (#5011)

--- pkg/accesscontrolserver/amqp/types.go
+++ pkg/accesscontrolserver/amqp/types.go
@@ -265,7 +265,7 @@ func (nf namedField) print(w io.Writer, comma bool) bool {
 		}
 	case reflect.Bool:
 		// Skip false values for plain bool, but not for bool types like role.
-		if rv.Type() == reflect.TypeOf(true) && IsZero(rv) {
+		if rv.Type() == reflect.TypeOf(true) && rv.IsZero() {
 			return comma
 		}
 	}
@@ -274,47 +274,6 @@ func (nf namedField) print(w io.Writer, comma bool) bool {
 	}
 	fmt.Fprintf(w, "%s: %v", nf.name, value)
 	return true
-}
-
-// IsZero reports whether v is the zero value for its type.
-// It panics if the argument is invalid.
-// TODO: Remove once we have Go 1.14
-func IsZero(v reflect.Value) bool {
-	switch v.Kind() {
-	case reflect.Bool:
-		return !v.Bool()
-	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		return v.Int() == 0
-	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
-		return v.Uint() == 0
-	case reflect.Float32, reflect.Float64:
-		return math.Float64bits(v.Float()) == 0
-	case reflect.Complex64, reflect.Complex128:
-		c := v.Complex()
-		return math.Float64bits(real(c)) == 0 && math.Float64bits(imag(c)) == 0
-	case reflect.Array:
-		for i := 0; i < v.Len(); i++ {
-			if !IsZero(v.Index(i)) {
-				return false
-			}
-		}
-		return true
-	case reflect.Chan, reflect.Func, reflect.Interface, reflect.Map, reflect.Ptr, reflect.Slice, reflect.UnsafePointer:
-		return v.IsNil()
-	case reflect.String:
-		return v.Len() == 0
-	case reflect.Struct:
-		for i := 0; i < v.NumField(); i++ {
-			if !IsZero(v.Field(i)) {
-				return false
-			}
-		}
-		return true
-	default:
-		// This should never happens, but will act as a safeguard for
-		// later, as a default value doesn't makes sense here.
-		panic(errorErrorf("reflect.Value.IsZero : %v", v.Kind()))
-	}
 }
 
 // compositeString generates a readable string for a composite value.
