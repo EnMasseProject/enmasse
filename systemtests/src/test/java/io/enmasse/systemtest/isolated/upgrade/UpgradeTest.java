6644b47741606c07789fab29a5489366143d3168
Ondrej Babec <ond.babec@gmail.com>
2020-07-20 17:47:33 +0200

    Enable upgrade tests for 1.4

--- systemtests/src/test/java/io/enmasse/systemtest/isolated/upgrade/UpgradeTest.java
+++ systemtests/src/test/java/io/enmasse/systemtest/isolated/upgrade/UpgradeTest.java
@@ -199,11 +199,6 @@ class UpgradeTest extends TestBase implements ITestIsolatedStandard {
                 log.warn("Skipping OLM test from version {} , because replaces version is {}", version, environment.enmasseOlmReplaces());
                 return;
             }
-            if (olmType == OLMInstallationType.DEFAULT && (version.equals("0.31.0") || version.equals("1.4.0"))) {
-                //versions with known fsgroup issue
-                log.warn("Skipping OLM test for version {}, because of known issue without fix", version);
-                return;
-            }
             installEnmasseOLM(Paths.get(templates), version);
         } else {
             installEnmasseBundle(Paths.get(templates), version);
