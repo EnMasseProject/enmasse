eebb60b29dac0f31ac812ea20f5c503e21dd65bf
David Kornel <kornys@outlook.com>
2020-07-23 13:22:58 +0200

    Remove fabric8 deprecated methods (#5050)

--- systemtests/src/main/java/io/enmasse/systemtest/messaginginfra/resources/MessagingEndpointResourceType.java
+++ systemtests/src/main/java/io/enmasse/systemtest/messaginginfra/resources/MessagingEndpointResourceType.java
@@ -15,13 +15,15 @@ import io.enmasse.api.model.MessagingInfrastructureCondition;
 import io.enmasse.systemtest.platform.Kubernetes;
 import io.fabric8.kubernetes.client.dsl.MixedOperation;
 import io.fabric8.kubernetes.client.dsl.Resource;
+import io.fabric8.kubernetes.client.dsl.base.CustomResourceDefinitionContext;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class MessagingEndpointResourceType implements ResourceType<MessagingEndpoint> {
-    private static final MixedOperation<MessagingEndpoint, MessagingEndpointList, DoneableMessagingEndpoint, Resource<MessagingEndpoint, DoneableMessagingEndpoint>> operation = Kubernetes.getClient().customResources(CoreCrd.messagingEndpoints(), MessagingEndpoint.class, MessagingEndpointList.class, DoneableMessagingEndpoint.class);
+    private static final MixedOperation<MessagingEndpoint, MessagingEndpointList, DoneableMessagingEndpoint, Resource<MessagingEndpoint, DoneableMessagingEndpoint>> operation =
+            Kubernetes.getClient().customResources(CustomResourceDefinitionContext.fromCrd(CoreCrd.messagingEndpoints()), MessagingEndpoint.class, MessagingEndpointList.class, DoneableMessagingEndpoint.class);
 
     @Override
     public String getKind() {
