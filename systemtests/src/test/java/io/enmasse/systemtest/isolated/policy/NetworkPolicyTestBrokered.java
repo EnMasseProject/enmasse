fa24678d850eac48422ba9259177603099bb5a5b
Keith Wall <kwall@apache.org>
2020-07-23 11:09:20 +0100

    fix brokered test too

--- systemtests/src/test/java/io/enmasse/systemtest/isolated/policy/NetworkPolicyTestBrokered.java
+++ systemtests/src/test/java/io/enmasse/systemtest/isolated/policy/NetworkPolicyTestBrokered.java
@@ -45,7 +45,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -70,14 +70,16 @@ class NetworkPolicyTestBrokered extends TestBase implements ITestIsolatedBrokere
     @Test
     void testNetworkPolicyWithPodSelector() throws Exception {
         int expectedMsgCount = 5;
-        HashMap<String, String> map = new HashMap<>();
-        map.put("app", allowedSpace);
 
+        LabelSelector namespace = new LabelSelectorBuilder()
+                .withMatchLabels(Collections.singletonMap("allowed", "true"))
+                .build();
         LabelSelector pod = new LabelSelectorBuilder()
-                .withMatchLabels(map)
+                .withMatchLabels(Map.of("app", allowedSpace))
                 .build();
         NetworkPolicyPeer networkPolicyPeer = new NetworkPolicyPeerBuilder()
                 .withPodSelector(pod)
+                .withNamespaceSelector(namespace)
                 .build();
 
         BrokeredInfraConfig standardInfraConfig = prepareConfig(networkPolicyPeer);
