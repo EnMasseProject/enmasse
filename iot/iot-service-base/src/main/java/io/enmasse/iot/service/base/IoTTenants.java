cba000c9cca1a72c14fafa5321424ce5feb040e6
Jens Reimann <jreimann@redhat.com>
2020-07-24 12:10:00 +0200

    fix renaming error

--- iot/iot-service-base/src/main/java/io/enmasse/iot/service/base/IoTTenants.java
+++ iot/iot-service-base/src/main/java/io/enmasse/iot/service/base/IoTTenants.java
@@ -10,7 +10,7 @@ import static java.util.Optional.empty;
 
 import java.util.Optional;
 
-import io.enmasse.iot.model.v1.DoneableIoTProject;
+import io.enmasse.iot.model.v1.DoneableIoTTenant;
 import io.enmasse.iot.model.v1.IoTTenant;
 import io.enmasse.iot.model.v1.IoTTenantList;
 import io.enmasse.model.CustomResourceDefinitions;
@@ -18,26 +18,26 @@ import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.dsl.MixedOperation;
 import io.fabric8.kubernetes.client.dsl.Resource;
 
-public final class IoTProjects {
+public final class IoTTenants {
 
     static {
         CustomResourceDefinitions.registerAll();
     }
 
-    private IoTProjects() {}
+    private IoTTenants() {}
 
-    public static MixedOperation<IoTTenant, IoTTenantList, DoneableIoTProject, Resource<IoTTenant, DoneableIoTProject>> clientForProject(final KubernetesClient client) {
+    public static MixedOperation<IoTTenant, IoTTenantList, DoneableIoTTenant, Resource<IoTTenant, DoneableIoTTenant>> clientForTenant(final KubernetesClient client) {
         return client
                 .customResources(tenant(),
-                        IoTTenant.class, IoTTenantList.class, DoneableIoTProject.class);
+                        IoTTenant.class, IoTTenantList.class, DoneableIoTTenant.class);
     }
 
-    public static Optional<IoTTenant> getProject(final KubernetesClient client, final String tenantName) {
-        return getProject(clientForProject(client), tenantName);
+    public static Optional<IoTTenant> getTenant(final KubernetesClient client, final String tenantName) {
+        return getTenant(clientForTenant(client), tenantName);
    }
 
-    public static Optional<IoTTenant> getProject(final MixedOperation<IoTTenant, IoTTenantList, DoneableIoTProject, Resource<IoTTenant, DoneableIoTProject>> projects,
-            final String tenantName) {
+    public static Optional<IoTTenant> getTenant(final MixedOperation<IoTTenant, IoTTenantList, DoneableIoTTenant, Resource<IoTTenant, DoneableIoTTenant>> projects,
+                                                final String tenantName) {
 
         final String[] toks = tenantName.split("\\.", 2);
 
@@ -48,12 +48,12 @@ public final class IoTProjects {
         final String namespace = toks[0];
         final String name = toks[1];
 
-        final IoTTenant project = projects
+        final IoTTenant tenant = projects
                 .inNamespace(namespace)
                 .withName(name)
                 .get();
 
-        return Optional.ofNullable(project);
+        return Optional.ofNullable(tenant);
 
     }
 
