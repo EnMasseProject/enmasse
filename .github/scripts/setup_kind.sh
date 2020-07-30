1ff1ad2ceb8d7fef6b9b11d7d6afc29f0ac9f26b
David Kornel <kornys@outlook.com>
2020-07-30 12:53:09 +0200

    Downgrade kubeapi for kind

--- .github/scripts/setup_kind.sh
+++ .github/scripts/setup_kind.sh
@@ -2,7 +2,7 @@
 curl -Lo ./kind "https://kind.sigs.k8s.io/dl/v0.8.1/kind-$(uname)-amd64"
 chmod +x ./kind
 
-cat <<EOF | ./kind create cluster --config=-
+cat <<EOF | ./kind create cluster --image=kindest/node:v1.17.5 --config=-
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
 nodes:
