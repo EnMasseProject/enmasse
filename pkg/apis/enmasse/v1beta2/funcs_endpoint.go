0645eaf59fb7cfd70b98b70928b6f1bd15108630
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2020-08-05 15:03:42 +0200

    Keep endpoint types

---
+++ pkg/apis/enmasse/v1beta2/funcs_endpoint.go
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2020, EnMasse authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+package v1beta2
+
+import (
+	routev1 "github.com/openshift/api/route/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func (m *MessagingEndpointStatus) GetMessagingEndpointCondition(t MessagingEndpointConditionType) *MessagingEndpointCondition {
+	for i, c := range m.Conditions {
+		if c.Type == t {
+			return &m.Conditions[i]
+		}
+	}
+
+	mc := MessagingEndpointCondition{
+		Type:               t,
+		Status:             corev1.ConditionUnknown,
+		LastTransitionTime: metav1.Now(),
+	}
+
+	m.Conditions = append(m.Conditions, mc)
+
+	return &m.Conditions[len(m.Conditions)-1]
+}
+
+func (c *MessagingEndpointCondition) SetStatus(status corev1.ConditionStatus, reason string, message string) {
+
+	if c.Status != status {
+		c.Status = status
+		c.LastTransitionTime = metav1.Now()
+	}
+
+	c.Reason = reason
+	c.Message = message
+}
+
+func (e *MessagingEndpoint) IsEdgeTerminated() bool {
+	return e.Spec.Tls == nil &&
+		(e.Spec.Ingress != nil || (e.Spec.Route != nil && (e.Spec.Route.TlsTermination == nil || *e.Spec.Route.TlsTermination == routev1.TLSTerminationEdge)))
+}
