a8e9b4e5473dab2578fff41169afe5aefd4b9f96
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2020-08-18 10:25:03 +0200

    Resources no longer required

--- systemtests/src/test/java/io/enmasse/systemtest/isolated/dlq/DeadLetterTest.java
+++ systemtests/src/test/java/io/enmasse/systemtest/isolated/dlq/DeadLetterTest.java
@@ -71,7 +71,7 @@ class DeadLetterTest extends TestBase implements ITestBaseIsolated {
     }
 
     @ParameterizedTest(name = "testAddressSpecified-{0}-space")
-    @ValueSource(strings = {"standard", "brokered"})
+    @ValueSource(strings = {"brokered"})
     void testAddressSpecified(String type) throws Exception {
         doTestMessageRedelivery(AddressSpaceType.getEnum(type), AddressType.QUEUE, null, new MessageRedeliveryBuilder().withMaximumDeliveryAttempts(2).build(),
                 new MessageRedeliveryBuilder().withMaximumDeliveryAttempts(2).build());
