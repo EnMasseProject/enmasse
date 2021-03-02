92334af27d1d45e74004e6195c928d782af88efb
k-wall <kwall@apache.org>
2021-03-02 22:03:46 +0000

    Update clean_disk.sh

--- .github/scripts/clean_disk.sh
+++ .github/scripts/clean_disk.sh
@@ -16,7 +16,7 @@ sudo apt-get remove -y '^ghc-8.*'
 sudo apt-get remove -y '^dotnet-.*'
 sudo apt-get remove -y '^llvm-.*'
 sudo apt-get remove -y 'php.*'
-sudo apt-get remove -y azure-cli google-cloud-sdk hhvm google-chrome-stable firefox powershell mono-devel
+sudo apt-get remove -y azure-cli google-cloud-sdk hhvm google-chrome-stable firefox powershell mono-devel monodoc-http
 sudo apt-get autoremove -y
 sudo apt-get clean -y
 df -h
