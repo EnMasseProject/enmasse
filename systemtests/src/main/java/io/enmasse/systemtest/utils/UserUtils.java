EnMasseProject/enmasse

commit 27e9dfbc32882465bf089bc761254ad700b804ca

Update fabric8 kubernetes-client from 4.6.4 to 5.0.2 (#5265)
* Update fabric8 kubernetes-client from 4.6.4 to 5.0.2

https://github.com/fabric8io/kubernetes-client/blob/master/doc/MIGRATION-v5.md

systemtests/src/main/java/io/enmasse/systemtest/utils/UserUtils.java
@@ -10,10 +10,8 @@ import io.enmasse.address.model.Phase;
 import io.enmasse.systemtest.UserCredentials;
 import io.enmasse.systemtest.logs.CustomLogger;
 import io.enmasse.systemtest.manager.IsolatedResourcesManager;
-import io.enmasse.systemtest.manager.ResourceManager;
 import io.enmasse.systemtest.platform.Kubernetes;
 import io.enmasse.systemtest.time.TimeoutBudget;
-import io.enmasse.user.model.v1.DoneableUser;
 import io.enmasse.user.model.v1.User;
 import io.enmasse.user.model.v1.UserAuthenticationType;
 import io.enmasse.user.model.v1.UserBuilder;
@@ -26,32 +24,31 @@ import java.util.regex.Pattern;
 
 public class UserUtils {
     private static Logger log = CustomLogger.getLogger();
-    public static DoneableUser createUserResource(UserCredentials cred) {
-        return new DoneableUser(new UserBuilder()
+    public static UserBuilder createUserResource(UserCredentials cred) {
+        return new UserBuilder()
                 .withNewSpec()
                 .withUsername(cred.getUsername())
                 .withNewAuthentication()
                 .withPassword(passwordToBase64(cred.getPassword()))
                 .withType(UserAuthenticationType.password)
                 .endAuthentication()
-                .endSpec()
-                .build());
+                .endSpec();
     }
 
     public static JsonObject userToJson(String addressspace, User user) throws Exception {
-        return new JsonObject(new ObjectMapper().writeValueAsString(new DoneableUser(user)
+        return new JsonObject(new ObjectMapper().writeValueAsString(new UserBuilder(user)
                 .editMetadata()
                 .withName(String.format("%s.%s", addressspace, Pattern.compile(".*:").matcher(user.getSpec().getUsername()).replaceAll("")))
                 .endMetadata()
-                .done()));
+                .build()));
     }
 
     public static JsonObject userToJson(String addressspace, String metaUserName, User user) throws Exception {
-        return new JsonObject(new ObjectMapper().writeValueAsString(new DoneableUser(user)
+        return new JsonObject(new ObjectMapper().writeValueAsString(new UserBuilder(user)
                 .editMetadata()
                 .withName(String.format("%s.%s", addressspace, Pattern.compile(".*:").matcher(metaUserName).replaceAll("")))
                 .endMetadata()
-                .done()));
+                .build()));
     }
 
     public static User waitForUserActive(User user, TimeoutBudget budget) throws Exception {

commit 887b5b28cdeddd89ba4abe1ee604cb5eebb3ccbd

Fix test regression introduced by #5265

systemtests/src/main/java/io/enmasse/systemtest/utils/UserUtils.java
@@ -37,16 +37,8 @@ public class UserUtils {
 
     public static JsonObject userToJson(String addressspace, User user) throws Exception {
         return new JsonObject(new ObjectMapper().writeValueAsString(new UserBuilder(user)
-                .editMetadata()
+                .editOrNewMetadata()
                 .withName(String.format("%s.%s", addressspace, Pattern.compile(".*:").matcher(user.getSpec().getUsername()).replaceAll("")))
-                .endMetadata()
-                .build()));
-    }
-
-    public static JsonObject userToJson(String addressspace, String metaUserName, User user) throws Exception {
-        return new JsonObject(new ObjectMapper().writeValueAsString(new UserBuilder(user)
-                .editMetadata()
-                .withName(String.format("%s.%s", addressspace, Pattern.compile(".*:").matcher(metaUserName).replaceAll("")))
                 .endMetadata()
                 .build()));
     }