EnMasseProject/enmasse

commit 27e9dfbc32882465bf089bc761254ad700b804ca

Update fabric8 kubernetes-client from 4.6.4 to 5.0.2 (#5265)
* Update fabric8 kubernetes-client from 4.6.4 to 5.0.2

https://github.com/fabric8io/kubernetes-client/blob/master/doc/MIGRATION-v5.md

systemtests/src/main/java/io/enmasse/systemtest/utils/UserUtils.java
@@ -10,10 +10,8 @@ import io.enmasse.address.model.Phase;
 import io.enmasse.systemtest.UserCredentials;
 import io.enmasse.systemtest.logs.CustomLogger;
 import io.enmasse.systemtest.manager.IsolatedResourcesManager;
-import io.enmasse.systemtest.manager.ResourceManager;
 import io.enmasse.systemtest.platform.Kubernetes;
 import io.enmasse.systemtest.time.TimeoutBudget;
-import io.enmasse.user.model.v1.DoneableUser;
 import io.enmasse.user.model.v1.User;
 import io.enmasse.user.model.v1.UserAuthenticationType;
 import io.enmasse.user.model.v1.UserBuilder;
@@ -26,32 +24,31 @@ import java.util.regex.Pattern;
 
 public class UserUtils {
     private static Logger log = CustomLogger.getLogger();
-    public static DoneableUser createUserResource(UserCredentials cred) {
-        return new DoneableUser(new UserBuilder()
+    public static UserBuilder createUserResource(UserCredentials cred) {
+        return new UserBuilder()
                 .withNewSpec()
                 .withUsername(cred.getUsername())
                 .withNewAuthentication()
                 .withPassword(passwordToBase64(cred.getPassword()))
                 .withType(UserAuthenticationType.password)
                 .endAuthentication()
-                .endSpec()
-                .build());
+                .endSpec();
     }
 
     public static JsonObject userToJson(String addressspace, User user) throws Exception {
-        return new JsonObject(new ObjectMapper().writeValueAsString(new DoneableUser(user)
+        return new JsonObject(new ObjectMapper().writeValueAsString(new UserBuilder(user)
                 .editMetadata()
                 .withName(String.format("%s.%s", addressspace, Pattern.compile(".*:").matcher(user.getSpec().getUsername()).replaceAll("")))
                 .endMetadata()
-                .done()));
+                .build()));
     }
 
     public static JsonObject userToJson(String addressspace, String metaUserName, User user) throws Exception {
-        return new JsonObject(new ObjectMapper().writeValueAsString(new DoneableUser(user)
+        return new JsonObject(new ObjectMapper().writeValueAsString(new UserBuilder(user)
                 .editMetadata()
                 .withName(String.format("%s.%s", addressspace, Pattern.compile(".*:").matcher(metaUserName).replaceAll("")))
                 .endMetadata()
-                .done()));
+                .build()));
     }
 
     public static User waitForUserActive(User user, TimeoutBudget budget) throws Exception {

systemtests/src/test/java/io/enmasse/systemtest/bases/authz/AuthorizationTestBase.java
@@ -119,7 +119,7 @@ public abstract class AuthorizationTestBase extends TestBase implements ITestBas
                 .withAuthorization(
                         Collections.singletonList(new UserAuthorizationBuilder().withAddresses("*").withOperations(Operation.send).build()))
                 .endSpec()
-                .done());
+                .build());
         Thread.sleep(100);
         assertSend(allowedUser);
         resourcesManager.removeUser(getSharedAddressSpace(), allowedUser.getUsername());
@@ -131,12 +131,12 @@ public abstract class AuthorizationTestBase extends TestBase implements ITestBas
                                 .withAddresses(addresses.stream().map(address -> address.getSpec().getAddress()).collect(Collectors.toList()))
                                 .withOperations(Operation.send).build()))
                 .endSpec()
-                .done());
+                .build());
         Thread.sleep(100);
         assertSend(allowedUser);
         resourcesManager.removeUser(getSharedAddressSpace(), allowedUser.getUsername());
 
-        resourcesManager.createOrUpdateUser(getSharedAddressSpace(), UserUtils.createUserResource(noAllowedUser).done());
+        resourcesManager.createOrUpdateUser(getSharedAddressSpace(), UserUtils.createUserResource(noAllowedUser).build());
         assertCannotSend(noAllowedUser);
         resourcesManager.removeUser(getSharedAddressSpace(), noAllowedUser.getUsername());
 
@@ -147,7 +147,7 @@ public abstract class AuthorizationTestBase extends TestBase implements ITestBas
                                 .withAddresses("*")
                                 .withOperations(Operation.recv).build()))
                 .endSpec()
-                .done());
+                .build());
         assertCannotSend(noAllowedUser);
         resourcesManager.removeUser(getSharedAddressSpace(), noAllowedUser.getUsername());
     }
@@ -162,7 +162,7 @@ public abstract class AuthorizationTestBase extends TestBase implements ITestBas
                 .withAuthorization(
                         Collections.singletonList(new UserAuthorizationBuilder().withAddresses("*").withOperations(Operation.recv).build()))
                 .endSpec()
-                .done());
+                .build());
         assertReceive(allowedUser);
         resourcesManager.removeUser(getSharedAddressSpace(), allowedUser.getUsername());
 
@@ -173,7 +173,7 @@ public abstract class AuthorizationTestBase extends TestBase implements ITestBas
                                 .withAddresses(addresses.stream().map(address -> address.getSpec().getAddress()).collect(Collectors.toList()))
                                 .withOperations(Operation.recv).build()))
                 .endSpec()
-                .done());
+                .build());
         assertReceive(allowedUser);
         resourcesManager.removeUser(getSharedAddressSpace(), allowedUser.getUsername());
 
@@ -184,7 +184,7 @@ public abstract class AuthorizationTestBase extends TestBase implements ITestBas
                                 .withAddresses("*")
                                 .withOperations(Operation.send).build()))
                 .endSpec()
-                .done());
+                .build());
         assertCannotReceive(noAllowedUser);
         resourcesManager.removeUser(getSharedAddressSpace(), noAllowedUser.getUsername());
     }
@@ -200,7 +200,7 @@ public abstract class AuthorizationTestBase extends TestBase implements ITestBas
                                 .withOperations(Operation.recv)
                                 .withAddresses("*").build()))
                 .endSpec()
-                .done());
+                .build());
         assertReceive(user);
         resourcesManager.removeUser(getSharedAddressSpace(), user.getUsername());
         Thread.sleep(5000);
@@ -212,7 +212,7 @@ public abstract class AuthorizationTestBase extends TestBase implements ITestBas
                                 .withOperations(Operation.recv)
                                 .withAddresses("pepa_address").build()))
                 .endSpec()
-                .done());
+                .build());
         assertCannotReceive(user);
         resourcesManager.removeUser(getSharedAddressSpace(), user.getUsername());
     }
@@ -475,7 +475,7 @@ public abstract class AuthorizationTestBase extends TestBase implements ITestBas
                         .withOperations(operation)
                         .build()))
                 .endSpec()
-                .done());
+                .build());
 
         users.add(UserUtils.createUserResource(new UserCredentials("user2", "password"))
                 .editSpec()
@@ -484,7 +484,7 @@ public abstract class AuthorizationTestBase extends TestBase implements ITestBas
                         .withOperations(operation)
                         .build()))
                 .endSpec()
-                .done());
+                .build());
 
         users.add(UserUtils.createUserResource(new UserCredentials("user3", "password"))
                 .editSpec()
@@ -493,7 +493,7 @@ public abstract class AuthorizationTestBase extends TestBase implements ITestBas
                         .withOperations(operation)
                         .build()))
                 .endSpec()
-                .done());
+                .build());
 
         users.add(UserUtils.createUserResource(new UserCredentials("user4", "password"))
                 .editSpec()
@@ -502,7 +502,7 @@ public abstract class AuthorizationTestBase extends TestBase implements ITestBas
                         .withOperations(operation)
                         .build()))
                 .endSpec()
-                .done());
+                .build());
 
         users.add(UserUtils.createUserResource(new UserCredentials("user5", "password"))
                 .editSpec()
@@ -511,7 +511,7 @@ public abstract class AuthorizationTestBase extends TestBase implements ITestBas
                         .withOperations(operation)
                         .build()))
                 .endSpec()
-                .done());
+                .build());
 
         for (User user : users) {
             resourcesManager.createOrUpdateUser(addressSpace, user);

commit c5f77d7a87362d34757bf036e7cbf99bd27d1c19

Fix test regression introduced by #5265 (fabric8 kubernetes-client)

systemtests/src/main/java/io/enmasse/systemtest/manager/ResourceManager.java
@@ -14,6 +14,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
+import io.fabric8.kubernetes.api.model.ObjectMeta;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.slf4j.Logger;
 
@@ -462,6 +463,9 @@ public abstract class ResourceManager {
     }
 
     public User createOrUpdateUser(AddressSpace addressSpace, User user, boolean wait) throws Exception {
+        if (user.getMetadata() == null) {
+            user.setMetadata(new ObjectMeta());
+        }
         if (user.getMetadata().getName() == null || !user.getMetadata().getName().contains(addressSpace.getMetadata().getName())) {
             user.getMetadata().setName(addressSpace.getMetadata().getName() + "." + user.getSpec().getUsername());
         }

systemtests/src/main/java/io/enmasse/systemtest/utils/UserUtils.java
@@ -26,6 +26,8 @@ public class UserUtils {
     private static Logger log = CustomLogger.getLogger();
     public static UserBuilder createUserResource(UserCredentials cred) {
         return new UserBuilder()
+                .withNewMetadata()
+                .endMetadata()
                 .withNewSpec()
                 .withUsername(cred.getUsername())
                 .withNewAuthentication()

systemtests/src/test/java/io/enmasse/systemtest/bases/authz/AuthorizationTestBase.java
@@ -203,7 +203,8 @@ public abstract class AuthorizationTestBase extends TestBase implements ITestBas
                 .build());
         assertReceive(user);
         resourcesManager.removeUser(getSharedAddressSpace(), user.getUsername());
-        Thread.sleep(5000);
+        // Seems the delete be racy on the keycloak side?  If we don't wait the permission change can be ineffective.
+        Thread.sleep(10_000);
 
         resourcesManager.createOrUpdateUser(getSharedAddressSpace(), UserUtils.createUserResource(user)
                 .editSpec()

systemtests/src/test/java/io/enmasse/systemtest/shared/brokered/authz/AuthorizationTest.java
@@ -26,12 +26,12 @@ class AuthorizationTest extends AuthorizationTestBase implements ITestSharedBrok
     }
 
     @Test
-    void testSendAuthzWithWIldcards() throws Exception {
+    void testSendAuthzWithWildcards() throws Exception {
         doTestSendAuthzWithWIldcards();
     }
 
     @Test
-    void testReceiveAuthzWithWIldcards() throws Exception {
+    void testReceiveAuthzWithWildcards() throws Exception {
         doTestReceiveAuthzWithWIldcards();
     }
 }

systemtests/src/test/java/io/enmasse/systemtest/shared/standard/authz/AuthorizationTest.java
@@ -30,13 +30,13 @@ class AuthorizationTest extends AuthorizationTestBase implements ITestSharedStan
 
     @Test
     @Tag(NON_PR)
-    void testSendAuthzWithWIldcards() throws Exception {
+    void testSendAuthzWithWildcards() throws Exception {
         doTestSendAuthzWithWIldcards();
     }
 
     @Test
     @Tag(NON_PR)
-    void testReceiveAuthzWithWIldcards() throws Exception {
+    void testReceiveAuthzWithWildcards() throws Exception {
         doTestReceiveAuthzWithWIldcards();
     }
 }